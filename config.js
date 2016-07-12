1ee6c8db08a559363ee21e79af8cfacafe363e09
Pedro S <snaowxt@gmail.com>
2016-07-12 12:13:47 -0700

    Update config.js: Fix LAB

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 var config = {};
 
 //Path where Kibana will look for elasticsearch
-config.elasticPath = '/elasticsearch';
+//config.elasticPath = '/elasticsearch';
+// Labs
+config.elasticPath = 'http://10.0.0.50/elasticsearch';
 
 module.exports = config;
