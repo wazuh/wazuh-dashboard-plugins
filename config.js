caf9193cfd9eced4d0c88c8f4bde7e1165b9b20a
jesmg <contacto@jesusmg.org>
2016-07-13 09:03:23 +0000

    Config.js dont needed anymore

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {};
-
-//Path where Kibana will look for elasticsearch
-//config.elasticPath = '/elasticsearch';
-// Labs
-config.elasticPath = 'http://10.0.0.50/elasticsearch';
-
-module.exports = config;
