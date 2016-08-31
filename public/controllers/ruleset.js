4f8ebf37f297f657b880ce92e2a82549ac99d9c7
jesmg <contacto@jesusmg.org>
2016-08-31 14:46:59 +0000

    Ruleset decoders tab

--- public/controllers/ruleset.js
+++ public/controllers/ruleset.js
@@ -63,7 +63,7 @@ app.controller('rulesController', function ($scope, $route, $q, alertify, shared
             return _group && _group != null;
         } else if (type == 'pci') {
             return _pci && _pci != null;
-        } 
+        }
     };
 
     $scope.rulesUnset = function (type) {
@@ -86,7 +86,7 @@ app.controller('rulesController', function ($scope, $route, $q, alertify, shared
             return _group;
         } else if (type == 'pci') {
             return _pci;
-        } 
+        }
     };
 
     $scope.filtersSearch = function (search) {
@@ -96,14 +96,13 @@ app.controller('rulesController', function ($scope, $route, $q, alertify, shared
         var result = [];
 
         if (!search) {
-            defered.reject();
-            return promise;
+            search = undefined;
         }
 
         DataFactory.getAndClean('get', '/rules/files', { 'offset': 0, 'limit': 100, 'search': search })
             .then(function (data) {
                 angular.forEach(data.data.items, function (value) {
-                    result.push({'type': 'file', 'value': value.name});
+                    result.push({ 'type': 'file', 'value': value.name });
                 });
                 DataFactory.getAndClean('get', '/rules/groups', { 'offset': 0, 'limit': 100, 'search': search })
                     .then(function (data) {
@@ -137,7 +136,7 @@ app.controller('rulesController', function ($scope, $route, $q, alertify, shared
             return null;
         }
         if (0 <= parseInt($scope.minLevel) <= parseInt($scope.maxLevel) <= 15) {
-            DataFactory.filters.set(objectsArray['/rules'], 'level', $scope.minLevel+'-'+$scope.maxLevel);
+            DataFactory.filters.set(objectsArray['/rules'], 'level', $scope.minLevel + '-' + $scope.maxLevel);
         }
     };
 
@@ -210,184 +209,28 @@ app.controller('rulesController', function ($scope, $route, $q, alertify, shared
 
 });
 
-app.controller('decodersController', function ($scope, $route, $q, alertify, sharedProperties, $location, $sce, DataFactory, tabProvider) {
+app.controller('decodersController', function ($scope, $route, $q, alertify, sharedProperties, $location, $sce, DataFactory, tabProvider, $mdToast) {
+
     //Initialisation
     $scope.load = true;
-    $scope.enableFileSearch = true;
 
     $scope.decoders = [];
-    $scope.filesDecoders = [];
 
-    $scope.dfType = 'all';
-    $scope.dfFile = '';
-    $scope.dfId = '';
+    $scope.typeFilter = 'all';
 
-    $scope.menuNavItem = 'ruleset';
-    $scope.submenuNavItem = 'decoders';
-
-    $scope.decoderType = 'all';
-
-    $scope.pageId = (Math.random().toString(36).substring(3));
-    tabProvider.register($scope.pageId);
-
+    var _lastFile = undefined;
     var objectsArray = [];
 
     //Print Error
     var printError = function (error) {
-        alertify.delay(10000).closeLogOnClick(true).error(error.html);
-    }
-
-    //Tabs
-    $scope.setTab = function (tab, group) {
-        tabProvider.setTab($scope.pageId, tab, group);
-    };
-
-    $scope.isSetTab = function (tab, group) {
-        return tabProvider.isSetTab($scope.pageId, tab, group);
-    };
-
-    //Functions
-
-    //Decoders - Filters
-
-    $scope.setDecodersFilter = function (type, value) {
-        if (type === 'type') {
-            if (value !== $scope.dfType) {
-                $scope.dfId = '';
-                $scope.dfFile = '';
-                $scope.dfType = value;
-            }
+        $mdToast.show({
+            template: '<md-toast>' + error.html + '</md-toast>',
+            position: 'bottom left',
+            hideDelay: 5000,
+        });
+        if ($scope._decoders_blocked) {
+            $scope._decoders_blocked = false;
         }
-
-        if (type === 'id') {
-            $scope.dfFile = '';
-            $scope.dfType = 'all';
-            $scope.decoder_search = '';
-            if (value === $scope.dfId) {
-                $scope.dfId = '';
-            } else {
-                $scope.dfId = value;
-            }
-        }
-
-        if (type === 'file') {
-            $scope.dfId = '';
-            if (value === $scope.dfFile) {
-                $scope.dfFile = '';
-            } else {
-                $scope.dfFile = value;
-            }
-        }
-
-        _applyDecodersFilters(type);
-    };
-
-    $scope.isSetDecodersFilter = function (type, value) {
-        if (type === 'type') {
-            return $scope.dfType === value;
-        } else if (type === 'file') {
-            return $scope.dfFile === value;
-        } else if (type === 'id') {
-            return $scope.dfId === value;
-        } else {
-            return false;
-        }
-    };
-
-    $scope.hasDecodersFilter = function (type) {
-        if (type === 'type') {
-            return $scope.dfType != '';
-        } else if (type === 'file') {
-            return $scope.dfFile != '';
-        } else if (type === 'id') {
-            return $scope.dfId != '';
-        } else {
-            return false;
-        }
-    };
-
-    var _applyDecodersFilters = function (type) {
-        if (type === 'id') {
-            var call;
-            if ($scope.dfId === '') {
-                call = '/decoders';
-            } else {
-                call = '/decoders/' + $scope.dfId;
-            }
-            DataFactory.clean(objectsArray['/decoders']);
-            DataFactory.initialize('get', call, {}, 10, 0)
-                .then(function (data) {
-                    objectsArray['/decoders'] = data;
-                    DataFactory.get(data).then(function (data) {
-                        $scope.decoders = data.data.items;
-                    });
-                }, printError);
-        } else {
-            if (type === 'type') {
-                var call;
-                if ($scope.dfType === 'all') {
-                    call = '/decoders';
-                    $scope.enableFileSearch = true;
-                } else {
-                    call = '/decoders/parents';
-                    $scope.enableFileSearch = false;
-                }
-                var body = {};
-                if ($scope.decoder_search != '') {
-                    body['search'] = $scope.decoder_search;
-                }
-                DataFactory.clean(objectsArray['/decoders']);
-                DataFactory.initialize('get', call, body, 10, 0)
-                    .then(function (data) {
-                        objectsArray['/decoders'] = data;
-                        DataFactory.get(data).then(function (data) {
-                            $scope.decoders = data.data.items;
-                        });
-                    }, printError);
-            } else if (type === 'file') {
-                if ($scope.dfFile != '') {
-                    $scope.objGet('/decoders', 'decoders', {'file': $scope.dfFile});
-                } else {
-                    $scope.objGet('/decoders', 'decoders', {});
-                }
-            }
-        }
-    };
-
-    //Decoders - aux functions
-
-    $scope.formatFile = function (file) {
-        return file.split("/").slice(-1)[0];
-    };
-
-    $scope.formatTemplate = function (file) {
-        return '<span style="width: 200px; display: inline-block; text-align: left;">'+file+'</span>';
-    };
-
-    $scope.decoderTooltips = function (key) {
-        var tooltip;
-        switch (key) {
-            case 'prematch':
-            case 'regex':
-                tooltip = 'OS_Regex type (simple library for regular expressions in C).';
-                break;
-            case 'program_name':
-                tooltip = 'OS_Match type (supports simple string matching and the following special characters).';
-                break;
-            case 'accumulate':
-                tooltip = 'Allow OSSEC to track events over multiple log messages based on a decoded id.';
-                break;
-            case 'order':
-                tooltip = 'Extracted fields by decoder regex.';
-                break;
-            case 'fts':
-                tooltip = 'First time seen';
-                break;
-            default:
-                tooltip = 'Tooltip not found for this field.';
-                break;
-        }
-        return '<div style="width: 250px;">' + tooltip + '</div>';
     };
 
     $scope.colorRegex = function (regex) {
@@ -414,109 +257,103 @@ app.controller('decodersController', function ($scope, $route, $q, alertify, sha
         return $sce.trustAsHtml(coloredString);
     };
 
-        //Obj functions
-
-    $scope.objHasNext = function (objName) {
-        return DataFactory.hasNext(objectsArray[objName]);
-    };
-    $scope.objNext = function (objName, containerName) {
-        DataFactory.next(objectsArray[objName])
-            .then(function (data) {
-                _applyContainer(data, containerName);
-            }, printError);
-    };
-
-    $scope.objHasPrev = function (objName) {
-        return DataFactory.hasPrev(objectsArray[objName]);
-    };
-    $scope.objPrev = function (objName, containerName) {
-        DataFactory.prev(objectsArray[objName])
-            .then(function (data) {
-                _applyContainer(data, containerName);
-            }, printError);
-    };
-
-    $scope.objGet = function (objName, containerName, body) {
-        //Search body modification
-        var searchField = _getSearchField(containerName);
-        if (!body) {
-            var tmpBody = DataFactory.getBody(objectsArray[objName]);
-            if (searchField !== tmpBody['search']) {
-                tmpBody['search'] = searchField;
-                body = tmpBody;
-            }
-        } else if (searchField !== body['search']) {
-            body['search'] = searchField;
-        }
-        if (body['search'] === '') {
-            body['search'] = undefined;
-        }
-
-        if (!body) {
-            DataFactory.get(objectsArray[objName])
-                .then(function (data) {
-                    _applyContainer(data, containerName);
-                }, printError);
+    $scope.decoderSearchFilter = function (search) {
+        if (!search) {
+            DataFactory.filters.unset(objectsArray['/decoders'], 'search');
         } else {
-            DataFactory.get(objectsArray[objName], body)
-                .then(function (data) {
-                    _applyContainer(data, containerName);
-                }, printError);
+            DataFactory.filters.set(objectsArray['/decoders'], 'search', search);
         }
-    };
+    }
 
-    var _getSearchField = function (containerName) {
-        switch (containerName) {
-            case 'decoders':
-                return $scope.decoder_search;
-            case 'filesDecoders':
-                return $scope.searchFilesDecoders;
-            default:
-                return '';
+    $scope.decoderFileFilter = function (file) {
+        if (!file) {
+            DataFactory.filters.unset(objectsArray['/decoders'], 'file');
+        } else {
+            _lastFile = file;
+            DataFactory.filters.set(objectsArray['/decoders'], 'file', file);
         }
-    };
+    }
 
-    var _applyContainer = function (data, containerName) {
-        switch(containerName) {
-            case 'decoders':
-                $scope.decoders.length = 0;
-                $scope.decoders = data.data.items;
-                break;
-            case 'filesDecoders':
-                $scope.filesDecoders.length = 0;
-                $scope.filesDecoders = data.data.items;
-                break;
-            default:
-                break;
-        }
-    };
-
-    //Load functions
-
-    var load_decoders_files = function () {
-        DataFactory.initialize('get', '/decoders/files', {}, 15, 0)
-            .then(function (data) {
-                objectsArray['/decoders/files'] = data;
-                DataFactory.get(data).then(function (data) {
-                    $scope.filesDecoders = data.data.items;
-                    $scope.load = false;
-                });
-            }, printError);
-    };
-
-    var load_decoders = function () {
-        DataFactory.initialize('get', '/decoders', {}, 10, 0)
+    $scope.decoderTypeFilter = function (type) {
+        DataFactory.clean(objectsArray['/decoders']);
+        DataFactory.initialize('get', (type == 'parents') ? '/decoders/parents' : '/decoders', {}, 100, 0)
             .then(function (data) {
                 objectsArray['/decoders'] = data;
-                DataFactory.get(data).then(function (data) {
-                    $scope.decoders = data.data.items;
-                    load_decoders_files();
-                });
+                DataFactory.get(objectsArray['/decoders'])
+                    .then(function (data) {
+                        $scope.decoders = data.data.items;
+                        DataFactory.filters.register(objectsArray['/decoders'], 'search', 'string');
+                        (type != 'parents') ? DataFactory.filters.register(objectsArray['/decoders'], 'file', 'string') : null;
+                        $scope.decoderSearchFilter($scope.decoderSearch);
+                        (type != 'parents') ? $scope.decoderFileFilter(_lastFile) : null;
+                    }, printError);
             }, printError);
+    }
+
+    $scope.fileSearch = function (search) {
+        var defered = $q.defer();
+        var promise = defered.promise;
+
+        if (!search) {
+            search = undefined;
+        }
+
+        DataFactory.getAndClean('get', '/decoders/files', { 'offset': 0, 'limit': 100, 'search': search })
+            .then(function (data) {
+                defered.resolve(data.data.items);
+            }, function (data) {
+                printError(data);
+                defered.reject();
+            })
+
+        return promise;
+    };
+
+    $scope.decodersObj = {
+        //Obj with methods for virtual scrolling
+        getItemAtIndex: function (index) {
+            if ($scope._decoders_blocked) {
+                return null;
+            }
+            var _pos = index - DataFactory.getOffset(objectsArray['/decoders']);
+            if (DataFactory.filters.flag(objectsArray['/decoders'])) {
+                $scope._decoders_blocked = true;
+                DataFactory.scrollTo(objectsArray['/decoders'], 50)
+                    .then(function (data) {
+                        $scope.decoders.length = 0;
+                        $scope.decoders = data.data.items;
+                        DataFactory.filters.unflag(objectsArray['/decoders']);
+                        $scope._decoders_blocked = false;
+                    }, printError);
+            } else if ((_pos > 70) || (_pos < 0)) {
+                $scope._decoders_blocked = true;
+                DataFactory.scrollTo(objectsArray['/decoders'], index)
+                    .then(function (data) {
+                        $scope.decoders.length = 0;
+                        $scope.decoders = data.data.items;
+                        $scope._decoders_blocked = false;
+                    }, printError);
+            } else {
+                return $scope.decoders[_pos];
+            }
+        },
+        getLength: function () {
+            return DataFactory.getTotalItems(objectsArray['/decoders']);
+        },
     };
 
     var load = function () {
-        load_decoders();
+        DataFactory.initialize('get', '/decoders', {}, 100, 0)
+            .then(function (data) {
+                objectsArray['/decoders'] = data;
+                DataFactory.get(objectsArray['/decoders'])
+                    .then(function (data) {
+                        $scope.decoders = data.data.items;
+                        DataFactory.filters.register(objectsArray['/decoders'], 'search', 'string');
+                        DataFactory.filters.register(objectsArray['/decoders'], 'file', 'string');
+                        $scope.load = false;
+                    }, printError);
+            }, printError);
     };
 
     //Load
@@ -527,7 +364,7 @@ app.controller('decodersController', function ($scope, $route, $q, alertify, sha
         angular.forEach(objectsArray, function (value) {
             DataFactory.clean(value)
         });
-        tabProvider.clean($scope.pageId);
+        $scope.decoders.length = 0;
     });
 });
 
