df63aa3199ceedeafab4667118568f0c6903843e
jesmg <contacto@jesusmg.org>
2016-09-14 09:42:24 +0000

    Updated colors: Using kibana labels pallete

--- public/controllers/ruleset.js
+++ public/controllers/ruleset.js
@@ -260,8 +260,15 @@ app.controller('decodersController', function ($scope, $q, $sce, DataFactory, $m
 
     $scope.colorRegex = function (regex) {
         regex = regex.toString();
-        var colors = ['blue', 'cadetblue', 'chocolate', 'darkgoldenrod', 'darkmagenta', 'darkred', 'darksalmon', 'dodgerblue', 'green',
-            'indigo', 'orange', 'purple', 'sienna', 'yellowgreen'];
+        var colors = [
+            '#3F6833', '#967302', '#2F575E', '#99440A', '#58140C', '#052B51', '#511749', '#3F2B5B', //6
+            '#508642', '#CCA300', '#447EBC', '#C15C17', '#890F02', '#0A437C', '#6D1F62', '#584477', //2
+            '#629E51', '#E5AC0E', '#64B0C8', '#E0752D', '#BF1B00', '#0A50A1', '#962D82', '#614D93', //4
+            '#7EB26D', '#EAB839', '#6ED0E0', '#EF843C', '#E24D42', '#1F78C1', '#BA43A9', '#705DA0', // Normal
+            '#9AC48A', '#F2C96D', '#65C5DB', '#F9934E', '#EA6460', '#5195CE', '#D683CE', '#806EB7', //5
+            '#B7DBAB', '#F4D598', '#70DBED', '#F9BA8F', '#F29191', '#82B5D8', '#E5A8E2', '#AEA2E0', //3
+            '#E0F9D7', '#FCEACA', '#CFFAFF', '#F9E2D2', '#FCE2DE', '#BADFF4', '#F9D9F9', '#DEDAF7'  //7
+        ];
         var valuesArray = regex.match(/\(((?!<\/span>).)*?\)(?!<\/span>)/gmi);
         var coloredString = regex;
         for (var i = 0; i < valuesArray.length; i++) {
@@ -272,8 +279,15 @@ app.controller('decodersController', function ($scope, $q, $sce, DataFactory, $m
 
     $scope.colorOrder = function (order) {
         order = order.toString();
-        var colors = ['blue', 'cadetblue', 'chocolate', 'darkgoldenrod', 'darkmagenta', 'darkred', 'darksalmon', 'dodgerblue', 'green',
-            'indigo', 'orange', 'purple', 'sienna', 'yellowgreen'];
+        var colors = [
+            '#3F6833', '#967302', '#2F575E', '#99440A', '#58140C', '#052B51', '#511749', '#3F2B5B', //6
+            '#508642', '#CCA300', '#447EBC', '#C15C17', '#890F02', '#0A437C', '#6D1F62', '#584477', //2
+            '#629E51', '#E5AC0E', '#64B0C8', '#E0752D', '#BF1B00', '#0A50A1', '#962D82', '#614D93', //4
+            '#7EB26D', '#EAB839', '#6ED0E0', '#EF843C', '#E24D42', '#1F78C1', '#BA43A9', '#705DA0', // Normal
+            '#9AC48A', '#F2C96D', '#65C5DB', '#F9934E', '#EA6460', '#5195CE', '#D683CE', '#806EB7', //5
+            '#B7DBAB', '#F4D598', '#70DBED', '#F9BA8F', '#F29191', '#82B5D8', '#E5A8E2', '#AEA2E0', //3
+            '#E0F9D7', '#FCEACA', '#CFFAFF', '#F9E2D2', '#FCE2DE', '#BADFF4', '#F9D9F9', '#DEDAF7'  //7
+        ];
         var valuesArray = order.split(',');
         var coloredString = order;
         for (var i = 0; i < valuesArray.length; i++) {
