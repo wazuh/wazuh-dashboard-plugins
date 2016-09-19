8d5ad28a0d8b16c599f25c02a21d96e58492efce
jesmg <contacto@jesusmg.org>
2016-09-19 09:11:04 +0000

    Dashboard selector in shared properties

--- public/objects/sharedProperties.js
+++ public/objects/sharedProperties.js
@@ -1,8 +1,16 @@
 require('ui/modules').get('app/wazuh', [])
     .service('sharedProperties', function () {
         var _property = '';
+        var _dashboard = { name: undefined, filter: undefined};
 
         return {
+            getDashboard: function () {
+                return _dashboard;
+            },
+            setDashboard: function (name, filter) {
+                _dashboard.name = name;
+                _dashboard.filter = filter;
+            },
             getProperty: function () {
                 return _property;
             },
