bae5f6e2c2af640a58c2fa9c109ae907f33093fe
Pedro S <snaowxt@gmail.com>
2016-10-02 07:09:46 -0700

    Agents preview

--- public/controllers/agents.js
+++ public/controllers/agents.js
@@ -96,7 +96,7 @@ app.controller('agentsController', function ($scope, DataFactory, $mdToast) {
 
 });
 
-app.controller('agentsPreviewController', function ($scope, DataFactory, $mdToast, errlog) {
+app.controller('agentsPreviewController', function ($scope, DataFactory, $mdToast, errlog, genericReq) {
 
     //Initialisation
     $scope.load = true;
@@ -204,6 +204,31 @@ app.controller('agentsPreviewController', function ($scope, DataFactory, $mdToas
                         $scope.load = false;
                     }, printError);
             }, printError);
+			
+		DataFactory.getAndClean('get', '/agents', { offset: 0, limit: 1, sort: '-id' })
+            .then(function (data) {
+                DataFactory.getAndClean('get', '/agents/' + data.data.items[0].id, {})
+                    .then(function (data) {
+                        $scope.lastAgent = data.data.name;
+                    }, printError);
+            }, printError);	
+			
+		var date = new Date();
+        date.setDate(date.getDate() - 1);
+        var timeAgo = date.getTime();
+        genericReq.request('GET', '/api/wazuh-elastic/top/AgentName/' + timeAgo)
+            .then(function (data) {
+                $scope.mostActiveAgent = data.data;
+            }, printError);	
+			
+		DataFactory.getAndClean('get', '/agents/summary', {})
+            .then(function (data) {
+                $scope.agentsCountActive = data.data.Active;
+                $scope.agentsCountDisconnected = data.data.Disconnected;
+                $scope.agentsCountNeverConnected = data.data['Never connected'];
+                $scope.agentsCountTotal = data.data.Total;
+                $scope.agentsCoverity = (data.data.Active / data.data.Total) * 100;
+            }, printError);	
     };
 
     //Load
