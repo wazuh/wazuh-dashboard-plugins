cdb5700ddaa0ed7da742d907fad070858692e3d0
Pedro S <snaowxt@gmail.com>
2016-12-09 09:43:13 -0800

    TabView buttons for Overview

--- public/controllers/general.js
+++ public/controllers/general.js
@@ -109,18 +109,19 @@ app.controller('generalController', function ($scope, $q, DataFactory, $mdToast,
             .then(function (data) {
                 objectsArray['/agents'] = data;
 				// tmp
-				/*
+				
 				     DataFactory.get(objectsArray['/agents'])
 					.then(function (data) {
-						//$scope.submenuNavItem = 'fim';
-						//$scope._agent = data.data.items[0];
-						//$scope.search = data.data.items[0].name;
-						 $scope.load = false;
+						$scope.submenuNavItem = 'overview';
+						$scope._agent = data.data.items[14];
+						$scope.search = data.data.items[14].name;
+						console.log(data.data.items);
+						$scope.load = false;
 					}, function (data) {
 						printError(data);
 						//defered.reject();
 					});
-				*/
+				
 				// tmp
                 DataFactory.get(data).then(function (data) {
                     DataFactory.filters.register(objectsArray['/agents'], 'search', 'string');
