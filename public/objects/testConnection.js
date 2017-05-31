e54de3d5a351f8e49fb46aa0b4cb0c1f83da8255
Manuel <manoloalbarral@gmail.com>
2017-05-31 16:43:45 +0200

    Requests were failing when the URL contained a path

--- public/objects/testConnection.js
+++ public/objects/testConnection.js
@@ -1,12 +1,12 @@
 import chrome from 'ui/chrome';
 require('ui/modules').get('app/wazuh', [])
-    .service('testConnection', function ($q, $http) {
+    .service('testConnection', function ($q, $http, genericReq) {
         return {
             check_stored: function () {
                 var defered = $q.defer();
                 var promise = defered.promise;
 
-                $http.get(chrome.addBasePath("/api/wazuh-api/check"))
+                genericReq.request("GET", "/api/wazuh-api/check")
                     .then(function (data) {
                         if (data.error) {
                             defered.reject(data);
@@ -22,8 +22,7 @@ require('ui/modules').get('app/wazuh', [])
 			check: function (data) {
                 var defered = $q.defer();
                 var promise = defered.promise;
-
-                $http.post(chrome.addBasePath("/api/wazuh-api/check"), data)
+                genericReq.request("POST", "/api/wazuh-api/check", data)
                     .then(function (data) {
                         if (data.error) {
                             defered.reject(data);
