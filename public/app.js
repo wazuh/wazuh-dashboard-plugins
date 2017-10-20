17d1df1cd43ed5f5eddccc21dc44d0da73abba7a
havidarou <javier.castro@wazuh.com>
2017-10-20 16:46:24 -0400

    Updating frontend initialization to reflect rebranding changes

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ require('plugins/wazuh/less/main.less');
 require('plugins/wazuh/less/ui_framework.less');
 
 // Set up Wazuh app
-var app = require('ui/modules').get('app/wazuh', ['ngCookies','ngMaterial', 'infinite-scroll'])
+var app = require('ui/modules').get('app/wazuh', ['ngCookies','ngMaterial'])
   .config(['$compileProvider', function ($compileProvider) {
     $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|data|blob):/);
   }])
@@ -11,61 +11,46 @@ var app = require('ui/modules').get('app/wazuh', ['ngCookies','ngMaterial', 'inf
     $httpProvider.useApplyAsync(true);
   }]);
 
-// Require services and factories
-require('plugins/wazuh/objects/theming.js');
-require('plugins/wazuh/objects/apiReq.js');
-require('plugins/wazuh/objects/genericReq.js');
-require('plugins/wazuh/objects/dataFactory.js');
-require('plugins/wazuh/objects/sharedProperties.js');
-require('plugins/wazuh/objects/appState.js');
-require('plugins/wazuh/objects/tabProvider.js');
-require('plugins/wazuh/objects/testConnection.js');
-require('plugins/wazuh/objects/errlog.js');
-
-// Require directives
-require('plugins/wazuh/objects/autoSizeContainer.js');
+// Require services
+require('plugins/wazuh/services/theming.js');
+require('plugins/wazuh/services/apiReq.js');
+require('plugins/wazuh/services/genericReq.js');
+require('plugins/wazuh/services/dataHandler.js');
+require('plugins/wazuh/services/appState.js');
+require('plugins/wazuh/services/testAPI.js');
 
 // Set up routes and views
-require('plugins/wazuh/objects/routes.js');
+require('plugins/wazuh/services/routes.js');
 
 // Require Kibana integrations
-require('plugins/wazuh/directives/kibanaExternalImports.js');
-require('plugins/wazuh/directives/kibanaVisualizationDirective.js');
-require('plugins/wazuh/directives/kibanaDiscoverDirective.js');
-require('plugins/wazuh/directives/kibanaSearchbarDirective.js');
+require('plugins/wazuh/kibana-integrations/kibanaExternalImports.js');
+require('plugins/wazuh/kibana-integrations/kibanaVisualizationDirective.js');
+require('plugins/wazuh/kibana-integrations/kibanaDiscoverDirective.js');
+require('plugins/wazuh/kibana-integrations/kibanaSearchbarDirective.js');
 
 // Require controllers
 
 // Overview
 require('plugins/wazuh/controllers/overview.js');
 
-// Dashboard
-require('plugins/wazuh/controllers/dashboard.js');
-
 // Manager
 require('plugins/wazuh/controllers/manager.js');
 require('plugins/wazuh/controllers/ruleset.js');
 require('plugins/wazuh/controllers/osseclog.js');
+require('plugins/wazuh/controllers/groups.js');
 
 // Agents
 require('plugins/wazuh/controllers/agents.js');
-require('plugins/wazuh/controllers/agentsOverview.js');
 require('plugins/wazuh/controllers/agentsPreview.js');
-require('plugins/wazuh/controllers/agentsFim.js');
-require('plugins/wazuh/controllers/agentsPm.js');
-require('plugins/wazuh/controllers/agentsOscap.js');
-require('plugins/wazuh/controllers/agentsAudit.js');
-require('plugins/wazuh/controllers/agentsPci.js');
 
 // Settings
 require('plugins/wazuh/controllers/settings.js');
-require('plugins/wazuh/controllers/testController.js');
 
 //Bootstrap and font awesome
 require('plugins/wazuh/../node_modules/bootstrap/dist/css/bootstrap.min.css');
 require('plugins/wazuh/../node_modules/bootstrap/dist/js/bootstrap.min.js');
 require('plugins/wazuh/utils/fontawesome/css/font-awesome.min.css');
-require('plugins/wazuh/utils/infinite_scroll/infinite-scroll.js');
+require('plugins/wazuh/utils/when-scrolled/when-scrolled.js');
 require('../../../ui_framework/dist/ui_framework.css');
 
 //Material
