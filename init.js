bfdb66ef166e6d20c87678a3d9373cebead368fb
havidarou <javier.castro@wazuh.com>
2017-10-20 16:54:14 -0400

    Fixing init

--- init.js
+++ init.js
@@ -1,6 +1,7 @@
 module.exports = function (server, options) {
-  require('./server/startup/initialize.js')(server, options);
+  require('./server/initialize.js')(server, options);
   require('./server/api/wazuh-elastic.js')(server, options);
-  require('./server/wazuh-monitoring.js')(server, options);
+  require('./server/api/wazuh-api-elastic.js')(server, options);
+  require('./server/monitoring.js')(server, options);
   require('./server/api/wazuh-api.js')(server, options);
 };
