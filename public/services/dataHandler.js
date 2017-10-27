dee65b263da6c42c092f3e9883d0b45235c6b152
Jesús Ángel González Novez <jesusgonzaleznovez@gmail.com>
2017-10-27 13:29:30 +0200

    Optimized dataHandler class. Replaced not cached loops.

--- public/services/dataHandler.js
+++ public/services/dataHandler.js
@@ -1,4 +1,4 @@
-var app = require('ui/modules').get('app/wazuh', []);
+let app = require('ui/modules').get('app/wazuh', []);
 
 app.factory('DataHandler', ($q, apiReq) => {
     class DataHandler {
@@ -16,56 +16,56 @@ app.factory('DataHandler', ($q, apiReq) => {
         nextPage () {
             if (this.busy) return;
             this.busy = true;
-            var requestData;
+            let requestData;
 
             if (this.initial) {
                 requestData = {
                     offset: this.offset,
-                    limit: this.initialBatch
+                    limit:  this.initialBatch
                 };
                 this.initial = false;
-            } else requestData = {
-                offset: this.offset,
-                limit: this.regularBatch
-            };
+            } else {
+                requestData = {
+                    offset: this.offset,
+                    limit:  this.regularBatch
+                };
+            }
 
-            angular.forEach(this.filters, function (filter) {
-                if (filter.value != '') requestData[filter.name] = filter.value;
-            });
+            for(let filter of this.filters){
+                if (filter.value !== '') requestData[filter.name] = filter.value;
+            }
 
-            var deferred = $q.defer();
-            apiReq.request('GET', this.path, requestData).then(function (data) {
-                if (data.data.data == 0)
+            let deferred = $q.defer();
+            apiReq.request('GET', this.path, requestData)
+            .then(function (data) {
+                if (data.data.data == 0){
                     deferred.resolve(false);
-
-                var totalItems = data.data.data.totalItems;
-                var items = data.data.data.items;
-                for (var i = 0; i < items.length; i++) {
+                }
+                let totalItems = data.data.data.totalItems;
+                let items      = data.data.data.items;
+                for (let i = 0,len = items.length; i < len; i++) {
                     this.items.push(items[i]);
                     this.items[i].selected = false;
                 }
                 this.offset += items.length;
-                (this.offset >= totalItems) ? this.end = true: this.busy = false
-                if (data.data.data != 0)
+                (this.offset >= totalItems) ? this.end = true: this.busy = false;
+                if (data.data.data !== 0){
                     deferred.resolve(true);
-
+                }
             }.bind(this));
 
             return deferred.promise;
         }
 
         hasFilter (filterName) {
-            var filterOrNot = false;
-            angular.forEach(this.filters, function (filter) {
-                if (filter.name == filterName) filterOrNot = true;
-            });
-            return filterOrNot;
+            let filtered = this.filters.filter((element) => element.name === filterName);
+            return filtered.length !== 0;
         }
 
         addFilter (filterName, value) {
             this.removeFilter(filterName, false);
             this.filters.push({
-                "name": filterName,
+                "name":  filterName,
                 "value": value
             });
             this.search();
@@ -81,30 +81,32 @@ app.factory('DataHandler', ($q, apiReq) => {
         }
 
         delete (name, index) {
-            apiReq.request('DELETE', this.path, {}).then(function (data) {
+            apiReq.request('DELETE', this.path, {})
+            .then(function (data) {
                 this.items.splice(index, 1);
             }.bind(this));
         }
 
         search () {
-            var requestData;
-            this.end = false;
-            this.busy = false;
+            let requestData;
+            this.end       = false;
+            this.busy      = false;
             this.sortValue = '';
 
             requestData = {
                 offset: 0,
-                limit: this.initialBatch
+                limit:  this.initialBatch
+            };
+
+            for(let filter of this.filters){
+                if (filter.value !== '') requestData[filter.name] = filter.value;
             }
 
-            angular.forEach(this.filters, function (filter) {
-                if (filter.value != '') requestData[filter.name] = filter.value;
-            });
-
-            apiReq.request('GET', this.path, requestData).then(function (data) {
+            apiReq.request('GET', this.path, requestData)
+            .then(function (data) {
                 this.items = [];
-                var items = data.data.data.items;
-                for (var i = 0; i < items.length; i++) {
+                let items  = data.data.data.items;
+                for (let i = 0,len = items.length; i < len; i++) {
                     this.items.push(items[i]);
                     this.items[i].selected = false;
                 }
@@ -114,18 +116,19 @@ app.factory('DataHandler', ($q, apiReq) => {
 
         sort(by) {
             this.sortValue = by;
-            this.sortDir = !this.sortDir;
+            this.sortDir   = !this.sortDir;
         }
 
         reset() {
-            this.items = [];
-            this.filters = [];
-            this.offset = 0;
+            this.items     = [];
+            this.filters   = [];
+            this.offset    = 0;
             this.sortValue = '';
-            this.initial = true;
-            this.end = false;
-            this.busy = false;
+            this.initial   = true;
+            this.end       = false;
+            this.busy      = false;
         }
     }
+
     return DataHandler;
 });
