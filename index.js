2e3eebd6bd01b46ba152eafc1b9284110ac96d1a
Javier Castro <javier.castro@wazuh.com>
2017-11-13 10:15:44 -0500

    Adding 'uses' to inject Kibana dependencies

--- index.js
+++ index.js
@@ -9,29 +9,17 @@ module.exports = (kibana) => new kibana.Plugin({
             description: 'Wazuh App for Kibana',
             icon:        'plugins/wazuh/img/icon.png',
             main:        'plugins/wazuh/app',
-            injectVars: (server, options) => {
-                const serverConfig     = server.config();
-                const configuredUrl    = server.config().get('tilemap.url');
-                const isOverridden     = typeof configuredUrl === 'string' && configuredUrl !== '';
-                const tilemapConfig    = serverConfig.get('tilemap');
-                const regionmapsConfig = serverConfig.get('regionmap');
-                const mapConfig        = serverConfig.get('map');
-                
-                regionmapsConfig.layers = (regionmapsConfig.layers) ? regionmapsConfig.layers : [];
-                
-                return {
-                    kbnIndex:         serverConfig.get('kibana.index'),
-                    esApiVersion:     serverConfig.get('elasticsearch.apiVersion'),
-                    esShardTimeout:   serverConfig.get('elasticsearch.shardTimeout'),
-                    regionmapsConfig: regionmapsConfig,
-                    mapConfig:        mapConfig,
-                    tilemapsConfig: {
-                        deprecated: {
-                            isOverridden: isOverridden,
-                            config:       tilemapConfig
-                        }
-                    }
-                };
+            uses: [
+              'visTypes',
+              'visResponseHandlers',
+              'visRequestHandlers',
+              'visEditorTypes',
+              'savedObjectTypes',
+              'spyModes',
+              'fieldFormats',
+            ],
+            injectVars: (server) => {
+              return server.plugins.kibana.injectVars(server);
             }
         }
     },
