d2e4e4482f457214cfe26381d5a6ae9ebd2c5c18
havidarou <javier.castro@wazuh.com>
2017-11-17 04:14:39 -0500

    Adapting views to the new integrations - overview tab

--- public/controllers/overview.js
+++ public/controllers/overview.js
@@ -3,179 +3,200 @@ import rison from 'rison-node';
 let app = require('ui/modules')
 .get('app/wazuh', [])
 .controller('overviewController', 
-function ($scope, $q, $routeParams, $route, $location, $rootScope, appState, genericReq) {
+function ($scope, $q, $routeParams, $route, $location, $rootScope, appState, genericReq, timefilter) {
+    $rootScope.page = 'overview';
+    $scope.extensions = appState.getExtensions().extensions;
 
-	$rootScope.page       = 'overview';
-	$scope.submenuNavItem = 'general';
-	$scope.extensions     = appState.getExtensions().extensions;
+    // Check the url hash and retrivew the tabView information 
+    if ($location.search().tabView) {
+        $scope.tabView = $location.search().tabView;
+    } else { // If tabView doesn't exist, default it to 'panels' view
+        $scope.tabView = 'panels';
+        $location.search('tabView', 'panels');
+    }
 
-	if ($location.search().tabView){
-		$scope.tabView = $location.search().tabView;
-	} else {
-		$scope.tabView = 'panels';
-		$location.search('tabView', 'panels');
-	}
+    // Check the url hash and retrivew the tab information 
+    if ($location.search().tab) {
+        $scope.tab = $location.search().tab;
+    } else { // If tab doesn't exist, default it to 'general' view
+        $scope.tab = 'general';
+        $location.search('tab', 'general');
 
-	// TODO: We should fix this and take it from AppState
-	$scope.timeGTE = 'now-1d';
-	$scope.timeLT  = 'now';
+        // Now we initialize the implicitFilter
+        $rootScope.currentImplicitFilter = "";
+    }
 
-	// Object for matching nav items and Wazuh groups
-	let tabGroups = {
-		"general": {
-			"group": "*"
-		},
-		"fim": {
-			"group": "syscheck"
-		},
-		"pm": {
-			"group": "rootcheck"
-		},
-		"oscap": {
-			"group": "oscap"
-		},
-		"audit": {
-			"group": "audit"
-		},
-		"pci": {
-			"group": "*"
-		}
-	};
+    // Getting the timefilter, defaulting it otherwise
+    if (timefilter.time) {
+    	$scope.timeGTE = timefilter.time.from;
+    	$scope.timeLT  = timefilter.time.to;
+    } else {
+    	$scope.timeGTE = "now-15m";
+    	$scope.timeLT  = "now";
+    }
 
-	$scope.hideRing = (items) => {
-		return $(".vis-container").length >= items;
-	};
+    // Object for matching nav items and Wazuh groups
+    let tabFilters = {
+        "general": {
+            "group": ""
+        },
+        "fim": {
+            "group": "syscheck"
+        },
+        "pm": {
+            "group": "rootcheck"
+        },
+        "oscap": {
+            "group": "oscap"
+        },
+        "audit": {
+            "group": "audit"
+        },
+        "pci": {
+            "group": ""
+        }
+    };
 
-	// Switch tab
-	$scope.switchTab = (tab) => {
-		$scope.loading 		  = true;
-		$scope.submenuNavItem = tab;
-		$scope.checkAlerts()
-		.then((data) => {
-			$scope.results = data;
-			$scope.loading = false;
-		})
-		.catch(() => {
-			$scope.results = false;
-			$scope.loading = false;
-		});
-	};
+    $scope.hideRing = (items) => {
+        return $(".vis-container").length >= items;
+    };
 
-	// Check if there are alerts.
-	$scope.checkAlerts = () => {
-		let group   = tabGroups[$scope.submenuNavItem].group;
-		let payload = {};
-		let fields  = {
-			"fields": [{
-				"field": "rule.groups",
-				"value": group
-			}]
-		};
+    // Switch tab
+    $scope.switchTab = (tab) => {
+        // Deleing app state traces in the url
+        $location.search('_a', null);
 
-		// No filter needed for general/pci
-		if (group === '*'){
-			fields = {
-				"fields": []
-			};
-		}
+        $scope.loading = true;
+        $scope.tab = tab;
+        $scope.checkAlerts()
+        .then((data) => {
+            $scope.results = data;
+            $scope.loading = false;
+        })
+        .catch(() => {
+            $scope.results = false;
+            $scope.loading = false;
+        });
+    };
 
-		let clusterName = {
-			"cluster": appState.getClusterInfo().cluster
-		};
+    // Check if there are alerts.
+    $scope.checkAlerts = () => {
+        let group = null;
+        if (tabFilters[$scope.tab].group === '')
+            group = "*";
+        else group = tabFilters[$scope.tab].group;
+        let payload = {};
+        let fields  = {
+            "fields": [{
+                "field": "rule.groups",
+                "value": group
+            }]
+        };
 
-		let timeInterval = {
-			"timeinterval": {
-				"gte": $scope.timeGTE,
-				"lt":  $scope.timeLT
-			}
-		};
-		
-		angular.extend(payload, fields, clusterName, timeInterval);
+        // No filter needed for general/pci
+        if (group === '*'){
+            fields = {
+                "fields": []
+            };
+        }
 
-		let deferred = $q.defer();
+        let clusterName = {
+            "cluster": appState.getClusterInfo().cluster
+        };
 
-		genericReq
-			.request('POST', '/api/wazuh-elastic/alerts-count/', payload)
-			.then((data) => {
-				if (data.data.data !== 0){
-					deferred.resolve(true);
-				} else {
-					deferred.resolve(false);
-				}
-			});
+        let timeInterval = {
+            "timeinterval": {
+                "gte": $scope.timeGTE,
+                "lt":  $scope.timeLT
+            }
+        };
+        
+        angular.extend(payload, fields, clusterName, timeInterval);
 
-		return deferred.promise;
-	};
+        let deferred = $q.defer();
+        genericReq
+            .request('POST', '/api/wazuh-elastic/alerts-count/', payload)
+            .then((data) => {
+                if (data.data.data !== 0){
+                    deferred.resolve(true);
+                } else {
+                    deferred.resolve(false);
+                }
+            });
 
-	// Watch for timefilter changes
-	$scope.$on('$routeUpdate', () => {
-		if ($location.search()._g && $location.search()._g !== '()') {
-			let currentTimeFilter = rison.decode($location.search()._g);
-			// Check if timefilter has changed and update values
-			if (currentTimeFilter.time && 
-				($scope.timeGTE != currentTimeFilter.time.from || 
-				$scope.timeLT != currentTimeFilter.time.to)) {
+        return deferred.promise;
+    };
 
-				$scope.timeGTE = currentTimeFilter.time.from;
-				$scope.timeLT  = currentTimeFilter.time.to;
-				$scope.checkAlerts()
-				.then((data) =>	$scope.results = data)
-				.catch(() => $scope.results = false);
+    // Watch for timefilter changes
+    $scope.$on('$routeUpdate', () => {
+        if ($location.search()._g && $location.search()._g !== '()') {
+            let currentTimeFilter = rison.decode($location.search()._g);
+            // Check if timefilter has changed and update values
+            if (currentTimeFilter.time && 
+                ($scope.timeGTE != currentTimeFilter.time.from || 
+                $scope.timeLT != currentTimeFilter.time.to)) {
 
-			}
-		}
-	});
+                $scope.timeGTE = currentTimeFilter.time.from;
+                $scope.timeLT  = currentTimeFilter.time.to;
+                $scope.checkAlerts()
+                .then((data) => $scope.results = data)
+                .catch(() => $scope.results = false);
 
-	// Watchers
-	$scope.$watch('tabView', () => $location.search('tabView', $scope.tabView));
+            }
+        }
+    });
 
-	// Check alerts
-	$scope.checkAlerts()
-	.then((data) => {
-		$scope.results = data;
-		$scope.loading = false;
-	})
-	.catch(() => {
-		$scope.results = false;
-		$scope.loading = false;
-	});
+    // Watchers
+    $scope.$watch('tabView', () => $location.search('tabView', $scope.tabView));
+    $scope.$watch('tab', () => {
+        $location.search('tab', $scope.tab);
+
+        // Update the implicit filter
+        if (tabFilters[$scope.tab].group === "") $rootScope.currentImplicitFilter = "";
+        else $rootScope.currentImplicitFilter = 'rule.groups : "' + tabFilters[$scope.tab].group + '"';
+    });
+
+    // Check alerts
+    $scope.checkAlerts()
+    .then((data) => {
+        $scope.results = data;
+        $scope.loading = false;
+    })
+    .catch(() => {
+        $scope.results = false;
+        $scope.loading = false;
+    });
 });
 
 app.controller('overviewGeneralController', function ($scope) {
-
 });
 
 app.controller('overviewFimController', function ($scope) {
-
 });
 
 app.controller('overviewPMController', function ($scope) {
-
 });
 
 app.controller('overviewOSCAPController', function ($scope) {
-
 });
 
 app.controller('overviewAuditController', function ($scope) {
-
 });
 
 app.controller('overviewPCIController', function ($scope, genericReq) {
+    let tabs = [];
+    
+    genericReq
+        .request('GET', '/api/wazuh-api/pci/all')
+        .then((data) => {
+            angular.forEach(data.data, (value, key) => {
+                tabs.push({
+                    "title":   key,
+                    "content": value
+                });
+            });
+        });
 
-	let tabs = [];
-	
-	genericReq
-		.request('GET', '/api/wazuh-api/pci/all')
-		.then((data) => {
-			angular.forEach(data.data, (value, key) => {
-				tabs.push({
-					"title":   key,
-					"content": value
-				});
-			});
-		});
-
-	$scope.tabs 		 = tabs;
-	$scope.selectedIndex = 0;
+    $scope.tabs = tabs;
+    $scope.selectedIndex = 0;
 });
