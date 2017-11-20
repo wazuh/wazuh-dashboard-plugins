035c251dde86fbe2fb03bc31aa37bc65bb312c0f
Jesús <jesusgonzaleznovez@gmail.com>
2017-11-20 10:06:54 +0100

    Minor refactoring. Added catch and promise chaining

--- public/kibana-integrations/kibanaVisualizationDirective.js
+++ public/kibana-integrations/kibanaVisualizationDirective.js
@@ -1,51 +1,53 @@
 import $ from 'jquery';
 import { getVisualizeLoader } from 'ui/visualize/loader';
 
-var app = require('ui/modules').get('apps/webinar_app', [])
-    .directive('kbnVis', [function () {
-        return {
-            restrict: 'E',
-            scope: {
-                visID: '=visId',
-            },
-            controller: function VisController($scope, $rootScope, savedVisualizations) {
+const app = require('ui/modules')
+	.get('apps/webinar_app', [])
+	.directive('kbnVis', [
+		function() {
+			return {
+				restrict: 'E',
+				scope: {
+					visID: '=visId',
+	            },
+				controller: function VisController($scope, $rootScope, savedVisualizations) {
+					$scope.implicitFilter = '';
+					$scope.visTitle       = '';
+					$scope.fullFilter     = '';
 
-                $scope.implicitFilter = '';
-                $scope.visTitle = '';
-                $scope.fullFilter = '';
-
-                // Listen for changes
-                var updateSearchSource = $scope.$on('updateVis', function (event, query, filters) {
-                    if (query.query == '') {
-                        $scope.fullFilter = $scope.implicitFilter;
-                    }
-                    else {
-                        if ($scope.implicitFilter != '') {
-                            $scope.fullFilter = $scope.implicitFilter + ' AND ' + query.query;
-                        }
-                        else {
-                            $scope.fullFilter = query.query;
-                        }
-                    }
-                    $scope.savedObj.searchSource.set('query',  {
-                        language: 'lucene',
-                        query: $scope.fullFilter 
-                    });
-                    $scope.savedObj.searchSource.set('filter', filters);
-                });
-
-                // Initializing the visualization
-                getVisualizeLoader().then(loader => {
-                    savedVisualizations.get($scope.visID).then(savedObj => {
-                        $scope.implicitFilter = savedObj.searchSource.get('query')['query'];
-                        $scope.visTitle = savedObj.vis.title;
-                        $scope.savedObj = savedObj;
-                        loader.embedVisualizationWithSavedObject($("#"+$scope.visID), $scope.savedObj, {})
-                        .then(handler => {
-                            console.log('render complete', $scope.visTitle);
-                        });
-                    });
-                });
-            }
-        }
-    }]);
+					// Listen for changes
+					let updateSearchSource = $scope.$on('updateVis', function(event, query, filters) {
+						if (!query.query) {
+							$scope.fullFilter = $scope.implicitFilter;
+						} else {
+							if ($scope.implicitFilter) {
+								$scope.fullFilter = $scope.implicitFilter + ' AND ' + query.query;
+							} else {
+								$scope.fullFilter = query.query;
+							}
+						}
+						$scope.savedObj.searchSource.set('query', {
+							language: 'lucene',
+							query:    $scope.fullFilter
+						});
+						$scope.savedObj.searchSource.set('filter', filters);
+					});
+                    let loader = null;
+					// Initializing the visualization
+					getVisualizeLoader()
+						.then(data => {
+                            loader = data;
+                            return savedVisualizations.get($scope.visID);
+                        })
+						.then(savedObj => {
+							$scope.implicitFilter = savedObj.searchSource.get('query')['query'];
+							$scope.visTitle = savedObj.vis.title;
+							$scope.savedObj = savedObj;
+							return loader.embedVisualizationWithSavedObject($('#' + $scope.visID), $scope.savedObj, {});
+						})
+                        .then(handler => console.log('render complete', $scope.visTitle))
+                        .catch(err => console.error(err));
+				}
+			};
+		}
+	]);
