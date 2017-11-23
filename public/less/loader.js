4045278ca4e2b1def34fcfe6fd0bd9d6615bb9bf
Juanjo Jiménez <juanjo@wazuh.com>
2017-11-23 11:22:27 +0100

    First approach for CSS reordering
    This commit reorders all the CSS from the app, removing the hard-coded one from the templates, and making a first approach of reordering in the `less` folder.

--- public/less/loader.js
+++ public/less/loader.js
@@ -5,3 +5,8 @@ require('plugins/wazuh/less/main.less');
 require('plugins/wazuh/less/manager.less');
 require('plugins/wazuh/less/media-queries.less');
 require('plugins/wazuh/less/ruleset.less');
+require('plugins/wazuh/less/angular-md.less');
+require('plugins/wazuh/less/wazuh-navbar.less');
+require('plugins/wazuh/less/not-used.less');
+require('plugins/wazuh/less/buttons.less');
+require('plugins/wazuh/less/overview.less');
