5837c30e2da28d8c8063d5cdfec0716b185bfe54
havidarou <javier.castro@wazuh.com>
2017-12-05 10:02:09 -0500

    Working reindex, needs refactoring

--- server/initialize.js
+++ server/initialize.js
@@ -234,14 +234,15 @@ module.exports = (server, options) => {
             } else { // The .wazuh index exists, we now proceed to check whether it's from an older version
                 elasticRequest.callWithInternalUser('get', {
                     index: ".wazuh",
-                    type: "wazuh-setup"
+                    type: "wazuh-setup",
+                    id: "1"
                 })
                 .then((data) => {
                     // Reindex!
                     reindexOldVersion();
                 })
                 .catch((error) => {
-                    server.log([blueWazuh, 'initialize', 'error'], 'Could not check if the index .wazuh belongs to an older version.');
+                    server.log([blueWazuh, 'initialize', 'info'], 'No older .wazuh index found -> no need to reindex.');
                 });
             }
         })
@@ -255,10 +256,10 @@ module.exports = (server, options) => {
             id: "1"
         })
         .then((data) => {
-            server.log([blueWazuh, 'initialize', 'info'], 'Wazuh-configuration document already exists. Nothing to be done.');
+            server.log([blueWazuh, 'initialize', 'info'], 'Wazuh-version document already exists. Nothing to be done.');
         })
         .catch((error) => {
-            server.log([blueWazuh, 'initialize', 'info'], 'Wazuh-configuration document does not exist. Initializating configuration...');
+            server.log([blueWazuh, 'initialize', 'info'], 'Wazuh-version document does not exist. Initializating configuration...');
             configureKibana("install");
         });
     };
@@ -277,7 +278,7 @@ module.exports = (server, options) => {
                 reject(error);
             });
         })
-    }
+    };
 
     // Wait until Kibana server is ready
     const checkKibanaStatus = () => {
@@ -285,6 +286,196 @@ module.exports = (server, options) => {
         .catch((error) => {
             server.log([blueWazuh, 'initialize', 'info'], 'Waiting for Kibana and Elasticsearch servers to be ready...');
             setTimeout(() => checkKibanaStatus(), 3000);
+        });
+    };
+
+    const reachAPI = (wapi_config) => {
+        // Now, let's see whether they have a 2.x or 3.x version
+        let id = wapi_config._id;
+        wapi_config = wapi_config._source;
+
+        server.log([blueWazuh, 'reindex', 'info'], 'Reaching ' + wapi_config.manager);
+        let decoded_password = Buffer.from(wapi_config.api_password, 'base64').toString("ascii");
+        if (wapi_config.cluster_info === undefined) { // No cluster_info in the API configuration data -> 2.x version
+            needle('get', `${wapi_config.url}:${wapi_config.api_port}/version`, {}, {
+                username:           wapi_config.api_user,
+                password:           decoded_password,
+                rejectUnauthorized: !wapi_config.insecure
+            })
+            .then((response) => {
+                server.log([blueWazuh, 'reindex', 'info'], 'API is reachable ' + wapi_config.manager);
+                if (parseInt(response.body.error) === 0 && response.body.data) {
+                    needle('get', `${wapi_config.url}:${wapi_config.api_port}/cluster/status`, {}, { // Checking the cluster status
+                        username:           wapi_config.api_user,
+                        password:           decoded_password,
+                        rejectUnauthorized: !wapi_config.insecure
+                    })
+                    .then((response) => {
+                        if (!response.body.error) {
+                            if (response.body.data.enabled === 'yes') { // If cluster mode is active
+                                needle('get', `${wapi_config.url}:${wapi_config.api_port}/cluster/node`, {}, {
+                                    username:           wapi_config.api_user,
+                                    password:           decoded_password,
+                                    rejectUnauthorized: !wapi_config.insecure
+                                })
+                                .then((response) => {
+                                    if (!response.body.error) {
+                                        wapi_config.cluster_info = {};
+                                        wapi_config.cluster_info.status = 'enabled';
+                                        wapi_config.cluster_info.manager = wapi_config.manager;
+                                        wapi_config.cluster_info.node = response.body.data.node;
+                                        wapi_config.cluster_info.cluster = response.body.data.cluster;
+                                    } else if (response.body.error){
+                                        server.log([blueWazuh, 'reindex', 'error'], 'Could not get cluster/node information for ', wapi_config.manager);
+                                    }
+                                });
+                            }
+                            else { // Cluster mode is not active
+                                wapi_config.cluster_info = {};
+                                wapi_config.cluster_info.status = 'disabled';
+                                wapi_config.cluster_info.cluster = 'Disabled';
+                                wapi_config.cluster_info.manager = wapi_config.manager;
+                            }
+
+                            // We filled data for the API, let's insert it now
+                            elasticRequest.callWithInternalUser('update', { 
+                                index: '.wazuh', 
+                                type: 'wazuh-configuration',
+                                id: id,
+                                body: {
+                                    'doc': {
+                                        "api_user": wapi_config.api_user,
+                                        "api_password": wapi_config.api_password,
+                                        "url": wapi_config.url,
+                                        "api_port": wapi_config.api_port,
+                                        "manager": wapi_config.manager,
+                                        "cluster_info" : {
+                                            "manager" : wapi_config.manager,
+                                            "node" : wapi_config.cluster_info.node,
+                                            "cluster" : wapi_config.cluster_info.cluster,
+                                            "status" : wapi_config.cluster_info.status
+                                        },
+                                    }
+                                } 
+                            })
+                            .then((resp) => {
+                                server.log([blueWazuh, 'reindex', 'info'], 'Successfully updated proper cluster information for ' + wapi_config.manager);
+                            })
+                            .catch((err) => {
+                                server.log([blueWazuh, 'reindex', 'error'], 'Could not update proper cluster information for ' + wapi_config.manager + 'due to ' + err);
+                            });
+                        } else {
+                            server.log([blueWazuh, 'reindex', 'error'], 'Could not get cluster/status information for ' + wapi_config.manager);
+                        }
+                    });
+                } else {
+                    server.log([blueWazuh, 'reindex', 'error'], 'The API responded with some kind of error for ' + wapi_config.manager);
+                }
+            })
+            .catch(error => {
+                server.log([blueWazuh, 'reindex', 'info'], 'API is NOT reachable ' + wapi_config.manager);
+                // We weren't able to reach the API, reorganize data and fill with sample node and cluster name information
+                elasticRequest.callWithInternalUser('update', { 
+                    index: '.wazuh', 
+                    type: 'wazuh-configuration',
+                    id: id,
+                    body: {
+                        'doc': {
+                            "api_user": wapi_config.api_user,
+                            "api_password": wapi_config.api_password,
+                            "url": wapi_config.url,
+                            "api_port": wapi_config.api_port,
+                            "manager": wapi_config.manager,
+                            "cluster_info" : {
+                                "manager" : wapi_config.manager,
+                                "node" : "nodata",
+                                "cluster" : "nodata",
+                                "status" : "disabled"
+                            },
+                        }
+                    } 
+                })
+                .then((resp) => {
+                    server.log([blueWazuh, 'reindex', 'info'], 'Successfully updated sample cluster information for ' + wapi_config.manager);
+                })
+                .catch((err) => {
+                    server.log([blueWazuh, 'reindex', 'error'], 'Could not update sample cluster information ' + wapi_config.manager + 'due to ' + err);
+                });
+            });
+        } else { // 3.x version
+            // Nothing to be done, cluster_info is present
+            server.log([blueWazuh, 'reindex', 'info'], 'Nothing to be done for ' + wapi_config.manager + ' as it is already a 3.x version.');
+        }
+    };
+
+    // Reindex a .wazuh index from 2.x-5.x or 3.x-5.x to .wazuh and .wazuh-version in 3.x-6.x
+    const reindexOldVersion = () => {
+        server.log([blueWazuh, 'reindex', 'info'], `Old version detected. Proceeding to reindex.`);
+
+        let configuration = {
+          "source": {
+            "index": ".wazuh",
+            "type": "wazuh-configuration"
+          },
+          "dest": {
+            "index": ".old-wazuh"
+          }
+        };
+
+        // Backing up .wazuh index
+        elasticRequest.callWithInternalUser('reindex', { body: configuration })
+        .then((result) => {
+            // And...this response does not take into acount new index population so...let's wait for it
+            server.log([blueWazuh, 'reindex', 'info'], 'Successfully backed up .wazuh index');
+            setTimeout(() => swapIndex(), 3000);
+        })
+        .catch((error) => {
+            server.log([blueWazuh, 'reindex', 'error'], 'Could not begin the reindex process: ' + error);
+        });
+    };
+
+    const swapIndex = () => {
+        // Deleting old .wazuh index
+        server.log([blueWazuh, 'reindex', 'info'], 'Deleting old .wazuh index.');
+
+        elasticRequest.callWithInternalUser('indices.delete', { index: ".wazuh" })
+        .then((data) => {
+            let configuration = {
+              "source": {
+                "index": ".old-wazuh",
+                "type": "wazuh-configuration"
+              },
+              "dest": {
+                "index": ".wazuh"
+              }
+            };
+
+            server.log([blueWazuh, 'reindex', 'info'], 'Reindexing into the new .wazuh');
+            // Reindexing from .old-wazuh where the type of document is wazuh-configuration into the new index .wazuh
+            elasticRequest.callWithInternalUser('reindex', { body: configuration })
+            .then((result) => {
+                // Now we need to properly replace the cluster_info into the configuration -> improvement: pagination?
+                // And...this response does not take into acount new index population so...let's wait for it
+                setTimeout(() => reachAPIs(), 3000);
+            })
+            .catch((error) => {
+                server.log([blueWazuh, 'reindex', 'error'], 'Could not reindex the new .wazuh: ' + error);
+            });
+        })
+        .catch((error) => {
+            server.log([blueWazuh, 'reindex', 'error'], 'Could not delete the old .wazuh index: ' + error);
+        });
+    };
+
+    const reachAPIs = () => {
+        elasticRequest.callWithInternalUser('search', { index: ".wazuh"} )
+        .then((data) => {
+            for (var i = 0; i < data.hits.hits.length; i++) {
+                reachAPI(data.hits.hits[i]);
+            }
+        })
+        .catch((error) => {
+            server.log([blueWazuh, 'reindex', 'error'], 'Something happened while getting old API configuration data: ' + error);
         });
     };
 
