f1895ad49337ceedb3f1ff2c2e56807330562777
Jesús Ángel González <jesusgonzalez@wazuh.com>
2017-12-12 17:06:08 +0100

    Added new directive to handle enter key event

--- public/controllers/common/directives.js
+++ public/controllers/common/directives.js
@@ -12,6 +12,19 @@ app
 			},
 		};
 	})
+	.directive('myEnter', function () {
+		return function (scope, element, attrs) {
+			element.bind("keydown keypress", function (event) {
+				if(event.which === 13) {
+					scope.$apply(function (){
+						scope.$eval(attrs.myEnter);
+					});
+	
+					event.preventDefault();
+				}
+			});
+		};
+	})
 	.directive('menuTop',function(){
 		return{
 			template:
