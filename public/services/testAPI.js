6947a58d8d47c0ba9649d0188db3edfa1c50be84
Jesús Ángel González <jesusgonzalez@wazuh.com>
2017-12-28 11:25:35 +0100

    Better error handling

--- public/services/testAPI.js
+++ public/services/testAPI.js
@@ -4,8 +4,9 @@ require('ui/modules').get('app/wazuh', [])
     return {
         check_stored: data => {
             let defered = $q.defer();
-            $http.post(chrome.addBasePath('/api/wazuh-api/checkStoredAPI'), data,{timeout: 4000})
-            .then((response) => {
+            $http
+            .post(chrome.addBasePath('/api/wazuh-api/checkStoredAPI'), data,{timeout: 4000})
+            .then(response => {
                 if (response.error) {
                     defered.reject(response);
                 } else {
@@ -15,7 +16,7 @@ require('ui/modules').get('app/wazuh', [])
             .catch(error => {
                 if(error.status && error.status === -1){
                     defered.reject({data: 'request_timeout_checkstored'});
-                } else if (error.error) {
+                } else {
                     defered.reject(error);
                 }
             });
@@ -23,8 +24,10 @@ require('ui/modules').get('app/wazuh', [])
         },
         check: data => {
             let defered = $q.defer();
-            $http.post(chrome.addBasePath("/api/wazuh-api/checkAPI"), data, {timeout: 4000})
-            .then((response) => {
+            const url = chrome.addBasePath("/api/wazuh-api/checkAPI");
+            $http
+            .post(url, data, {timeout: 4000})
+            .then(response => {
                 if (response.error) {
                     defered.reject(response);
                 } else {
@@ -40,7 +43,9 @@ require('ui/modules').get('app/wazuh', [])
                     defered.reject({data: 'request_timeout_checkapi'});
                 } else if (error.data && error.data.message && error.data.message === 'wrong_credentials') {
                     defered.reject({data: 'wrong_credentials'});
-                } else if (error.error) {
+                } else if(error.data && ((error.data.message && error.data.message === 'socket hang up') || (parseInt(error.data.error) === 5))) {
+                    defered.reject({data:'socket_hang_up',https: (data.url && data.url.includes('https'))});
+                } else {
                     defered.reject(error);
                 }
             });
