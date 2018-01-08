c4cfe6737278f3376d2d2eb9d77549744e19f923
Juanjo Jiménez <juanji0503@gmail.com>
2018-01-08 15:07:56 +0100

    Properly reloading of the Agent/Configuration tab

--- public/controllers/agentConfiguration.js
+++ public/controllers/agentConfiguration.js
@@ -6,18 +6,27 @@ app.controller('agentConfigurationController', function($scope, $rootScope, $loc
     const notify   = new Notifier({ location: 'Agent - Configuration' });
     $scope.load    = true;
     $scope.isArray = angular.isArray;
-    $scope.groupName = $scope.agent.group;
 
-    //Functions
-    const load = () => {
-        apiReq
-        .request('GET', `/agents/groups/${$scope.groupName}/configuration`, {})
-        .then(data => {
-            $scope.groupConfiguration = data.data.data.items[0];
-            $scope.load = false;
-        })
-        .catch(error => notify.error(error.message));
-    };
+    const getAgent = (newAgentId) => {
+		let id = null;
+
+		// They passed an id
+		if (newAgentId) {
+			id = newAgentId;
+			$location.search('agent', id);
+		} else {
+			if ($location.search().agent) { // There's one in the url
+				id = $location.search().agent;
+			} else { // We pick the one in the rootScope
+				id = $rootScope.globalAgent;
+				$location.search('agent', id);
+				delete $rootScope.globalAgent;
+			}
+		}
+
+		return apiReq
+        .request('GET', `/agents/${id}`, {});
+	};
 
     $scope.showGroups = () => {
         $location.path('manager/groups');
@@ -25,8 +34,21 @@ app.controller('agentConfigurationController', function($scope, $rootScope, $loc
 
     //Load
     try {
-        load();
+        getAgent()
+        .then(data => {
+			$scope.agent = data.data.data;
+            $scope.groupName = $scope.agent.group;
+
+            return apiReq
+            .request('GET', `/agents/groups/${$scope.groupName}/configuration`, {});
+		})
+        .then(data => {
+            $scope.groupConfiguration = data.data.data.items[0];
+            $scope.load = false;
+        })
+        .catch(error => notify.error(error.message));
+
     } catch (e) {
-        notify.error("Unexpected exception loading controller");
+        notify.error("Unexpected exception loading controller: " + e);
     }
 });
