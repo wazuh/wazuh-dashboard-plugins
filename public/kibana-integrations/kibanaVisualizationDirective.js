72068c8e42c7e3fb83246ed82374b0d96cdc3964
havidarou <javier.castro@wazuh.com>
2018-01-17 10:13:14 -0500

    Process pending updates after rendering

--- public/kibana-integrations/kibanaVisualizationDirective.js
+++ public/kibana-integrations/kibanaVisualizationDirective.js
@@ -18,49 +18,33 @@ var app = require('ui/modules').get('apps/webinar_app', [])
                 let visualization  = null;
                 let visHandler     = null;
                 let pendingUpdates = [];
+                let realPending = true;
+                let realPendingUpdates = [];
 
                 // Listen for changes
                 $scope.$on('updateVis', function (event, query, filters) {
-                    console.log("update, rendered", rendered);
                     if (rendered) {
                         if (visTitle !== 'Wazuh App Overview General Agents status') { // We don't want to filter that visualization as it uses another index-pattern
 
-                            if (pendingUpdates.length != 0) {
-                                for (let i = 0; i < pendingUpdates.length; i++) {
-                                    for (let j = 0; j < implicitFilters.loadFilters().length; j++) {
-                                        console.log("comparing", pendingUpdates[i].query, implicitFilters.loadFilters()[j]);
-                                        if (pendingUpdates[i].query == implicitFilters.loadFilters()[j]) {
-                                            console.log("this is the same", )
-                                        }
-                                    }
-                                }
+                            if (query.query === '') {
+                                fullFilter = implicitFilter;
                             }
-
-                            if (query != null) {
-                                if (query.query === '') {
-                                    fullFilter = implicitFilter;
+                            else {
+                                if (implicitFilter !== '') {
+                                    fullFilter = implicitFilter + ' AND ' + query.query;
                                 }
                                 else {
-                                    if (implicitFilter !== '') {
-                                        fullFilter = implicitFilter + ' AND ' + query.query;
-                                    }
-                                    else {
-                                        fullFilter = query.query;
-                                    }
+                                    fullFilter = query.query;
                                 }
-
-                                console.log("updating inside", visTitle,filters);
-                                visualization.searchSource
-                                .query({ language: 'lucene', query: fullFilter })
-                                .set('filter', filters);
-                            } else { // Update with pending
-
                             }
+
+                            visualization.searchSource
+                            .query({ language: 'lucene', query: fullFilter })
+                            .set('filter', filters);
+
                         }
                     } else {
-                        pendingUpdates.push({"query": query, "filters": filters});
-                            console.log("pushing inside", visTitle, pendingUpdates);
-                            console.log("the implicits", implicitFilters.loadFilters());
+                        pendingUpdates.push.apply(pendingUpdates, filters);
                     }
                 });
 
@@ -68,8 +52,32 @@ var app = require('ui/modules').get('apps/webinar_app', [])
                     rendered = true;
 
                     if (pendingUpdates.length != 0) {
-                        console.log("emitting, pending updates");
-                        $rootScope.$broadcast('updateVis', null, null);
+
+                        realPendingUpdates = implicitFilters.loadFilters();
+
+                        for (let i = 0; i < pendingUpdates.length; i++) {
+                            for (let j = 0; j < implicitFilters.loadFilters().length; j++) {
+                                if (JSON.stringify(pendingUpdates[i].query) === JSON.stringify(implicitFilters.loadFilters()[j].query)) {
+                                    realPending = false;
+                                }
+                            }
+
+                            // There was a real pending update?
+                            if (realPending) {
+                                realPendingUpdates.push(pendingUpdates[i]) 
+                            }
+                            realPending = true;
+                        }
+
+                        //
+                        pendingUpdates = [];
+                        if (realPendingUpdates.length != 0) {
+                            visualization.searchSource
+                            .query({ language: 'lucene', query: implicitFilter })
+                            .set('filter', realPendingUpdates);
+                            $rootScope.$broadcast('fetch');
+                            realPendingUpdates = [];
+                        }
                     }
 
                     $rootScope.loadedVisualizations.push(true);
