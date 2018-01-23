17f37d76f26c40eca2306dc6b0692c733ad70254
Jesús Ángel González <jesusgonzalez@wazuh.com>
2018-01-23 13:10:43 +0100

    Added realtime on the logs tab

--- public/controllers/osseclog.js
+++ public/controllers/osseclog.js
@@ -1,4 +1,4 @@
-let app = require('ui/modules').get('app/wazuh', []);
+const app = require('ui/modules').get('app/wazuh', []);
 
 // Logs controller
 app.controller('managerLogController', function ($scope, Logs, apiReq,Notifier) {
@@ -6,14 +6,50 @@ app.controller('managerLogController', function ($scope, Logs, apiReq,Notifier) 
     $scope.searchTerm  = '';
     $scope.loading     = true;
     $scope.logs        = Logs;
+    $scope.realtime    = false;
+    let intervalId     = null;
 
-    $scope.logs.nextPage('')
-    .then(() => apiReq.request('GET', '/manager/logs/summary', {}))
-    .then(data => {
-        $scope.summary = data.data.data;
-        $scope.loading = false;
-    })
-    .catch(error => notify.error(error.message));
+    const getRealLogs = async () => {
+        try{
+            const data = await apiReq.request('GET', '/manager/logs', {limit:20});
+            $scope.realLogs = data.data.data.items;
+            if(!$scope.$$phase) $scope.$digest();
+            return;
+        } catch (error) {
+            if(error.data && error.data.errorData && error.data.errorData.error === 1000){
+                console.log(error.data.errorData.message);
+            } else {
+                notify.error(error.message);
+            }
+        }
+    };
+
+    $scope.playRealtime = async () => {
+        $scope.realtime = true;
+        await getRealLogs();
+        intervalId = setInterval(getRealLogs,2500);
+    };    
+
+    $scope.stopRealtime = () => {
+        $scope.realtime   = false;
+        clearInterval(intervalId);
+        if(!$scope.$$phase) $scope.$digest();
+    }
+
+    const initialize = async () => {
+        try{
+            await $scope.logs.nextPage();
+            const data = await apiReq.request('GET', '/manager/logs/summary', {});
+            $scope.summary = data.data.data;
+            $scope.loading = false;
+            if(!$scope.$$phase) $scope.$digest();
+            return;
+        } catch (error) {
+            notify.error(error.message)
+        }
+    }
+
+    initialize();
 
     // Resetting the factory configuration
     $scope.$on("$destroy", () => $scope.logs.reset());
