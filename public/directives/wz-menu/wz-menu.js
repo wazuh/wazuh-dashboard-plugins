f282ed1993a43bb0385e2382cfb33f17fdf0c90b
Jesús Ángel González <jesusgonzalez@wazuh.com>
2018-02-15 13:18:00 +0100

    Separated each directive by folder

---
+++ public/directives/wz-menu/wz-menu.js
@@ -0,0 +1,82 @@
+import menuTemplate from './wz-menu.html'
+
+const app = require('ui/modules').get('app/wazuh', []);
+
+app.directive('wzMenu',function(){
+    return {
+        controller: function ($scope,$window, $rootScope, appState, patternHandler, courier, errorHandler) {
+
+            if(appState.getCurrentAPI()) {
+                $scope.theresAPI = true;
+                $scope.currentAPI = JSON.parse(appState.getCurrentAPI()).name;
+            }
+            else {
+                $scope.theresAPI = false;
+            }
+            $scope.goToClick = path => {
+                $window.location.href = path;
+            }
+            const load = async () => {
+                try {
+                    const data = await courier.indexPatterns.get(appState.getCurrentPattern());
+                    $scope.theresPattern = true;
+                    $scope.currentPattern = data.title;
+
+                    const list = await patternHandler.getPatternList();
+
+                    // Getting the list of index patterns
+                    if(list) {
+                        $scope.patternList = list;
+                        $scope.currentSelectedPattern = appState.getCurrentPattern();
+                    }
+                    if(!$scope.$$phase) $scope.$digest();
+                    return;
+                } catch (error) {
+                    errorHandler.handle(error,'Directives - Menu');
+                    $scope.theresPattern = false;
+                    if(!$rootScope.$$phase) $rootScope.$digest();
+                }
+            }
+
+            load();
+
+            // Function to change the current index pattern on the app
+            $scope.changePattern = async selectedPattern => {
+                try{
+                    $scope.currentSelectedPattern = await patternHandler.changePattern(selectedPattern);
+                    if(!$scope.$$phase) $scope.$digest();
+                    $window.location.reload();
+                    return;
+                }catch(error){
+                    errorHandler.handle(error,'Directives - Menu');
+                    if(!$rootScope.$$phase) $rootScope.$digest();
+                }                    
+            }
+
+            $scope.$on('updateAPI', () => {
+                if(appState.getCurrentAPI())
+                {
+                    $scope.theresAPI = true;
+                    $scope.currentAPI = JSON.parse(appState.getCurrentAPI()).name;
+                }
+                else {
+                    $scope.theresAPI = false;
+                }
+            });
+
+            $scope.$on('updatePattern', () => {
+                courier.indexPatterns.get(appState.getCurrentPattern())
+                .then(data => {
+                    $scope.theresPattern = true;
+                    $scope.currentSelectedPattern = appState.getCurrentPattern();
+                })
+                .catch(error => {
+                    errorHandler.handle(error,'Directives - Menu');
+                    if(!$rootScope.$$phase) $rootScope.$digest();
+                    $scope.theresPattern = false;
+                });
+            });
+        },
+        template: menuTemplate
+    };
+});
---
+++ public/directives/wz-table/wz-table.js
@@ -0,0 +1,48 @@
+import tableTemplate from './wz-table.html'
+
+const app = require('ui/modules').get('app/wazuh', []);
+
+app.directive('wzTable',function(){
+    return {
+        restrict: 'E',
+        scope: {
+            data      : '=data',
+            keys      : '=keys',
+            func      : '&',
+            noscroll  : '=noscroll',
+            nopointer : '=nopointer',
+            full      : '=full',
+            noheight  : '=noheight',
+            isruleset : '=isruleset',
+            isdecoders: '=isdecoders',
+            activeitem: '=activeitem'
+        },
+        link: function(scope,ele,attrs){
+            scope.clickAction = index => {
+                const obj = {};
+                if(scope.full){
+                    obj[scope.full] = index;
+                } else {
+                    obj.index = index;
+                }
+    
+                scope.func(obj);
+            }   
+            scope.parseItem = (item,key) => {
+
+                let tmp = key;
+                if(key.col) {
+                    tmp = key.col;
+                }
+                if(tmp && tmp.includes('.')){
+                    return item[tmp.split('.')[0]][tmp.split('.')[1]];
+                }
+                if(Array.isArray(item[tmp])){
+                    return item[tmp].length ? (item[tmp].join(', ')) : '';
+                }
+                return item[tmp];
+            }
+        },
+        template: tableTemplate
+    }
+});
