55bd42507ff1c18e1fbd5b305e3bbfba3b0275ed
Jesús Ángel González <jesusgonzalez@wazuh.com>
2018-03-06 12:33:54 +0100

    More cleaning

--- public/less/loader.js
+++ public/less/loader.js
@@ -6,8 +6,6 @@ require('plugins/wazuh/less/manager.less');
 require('plugins/wazuh/less/media-queries.less');
 require('plugins/wazuh/less/ruleset.less');
 require('plugins/wazuh/less/angular-md.less');
-require('plugins/wazuh/less/wazuh-navbar.less');
-require('plugins/wazuh/less/not-used.less');
 require('plugins/wazuh/less/buttons.less');
 require('plugins/wazuh/less/overview.less');
 require('plugins/wazuh/less/settings.less');
