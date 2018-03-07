77caf4911dca8a4b9c4b9b455e1f712889514cfa
havidarou <javier.castro@wazuh.com>
2018-03-07 06:51:04 -0500

    Reducing and merging CSS - In progress

--- public/less/loader.js
+++ public/less/loader.js
@@ -1,10 +1,7 @@
 require('plugins/wazuh/less/agents.less');
 require('plugins/wazuh/less/angular-md.less');
-require('plugins/wazuh/less/buttons.less');
 require('plugins/wazuh/less/common.less');
 require('plugins/wazuh/less/heights.less');
-require('plugins/wazuh/less/kbn.less');
-require('plugins/wazuh/less/main.less');
 require('plugins/wazuh/less/manager.less');
 require('plugins/wazuh/less/media-queries.less');
 require('plugins/wazuh/less/tables.less');
