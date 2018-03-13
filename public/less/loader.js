4537e9199c4ae969940591e87790e6091057d1de
Juanjo Jiménez <juanjo@wazuh.com>
2018-03-13 17:53:55 +0100

    More CSS cleanup and new typography stylesheet

--- public/less/loader.js
+++ public/less/loader.js
@@ -1,5 +1,10 @@
+/* -------------------------------------------------------------------------- */
+/* ------------------------ Wazuh stylesheets loader ------------------------ */
+/* -------------------------------------------------------------------------- */
+
 require('plugins/wazuh/less/common.less');
 require('plugins/wazuh/less/heights.less');
 require('plugins/wazuh/less/cleaned.less');
+require('plugins/wazuh/less/typography.less');
 require('plugins/wazuh/less/media-queries.less');
 import 'ui/styles/theme.less';
