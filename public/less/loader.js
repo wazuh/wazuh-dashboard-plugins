817353a763755e06e2369baa36a8c3170de936a8
Juanjo Jiménez <juanjo@wazuh.com>
2018-03-14 16:21:32 +0100

    Completions to first big milestone of HTML/CSS refactoring

--- public/less/loader.js
+++ public/less/loader.js
@@ -7,7 +7,4 @@ require('plugins/wazuh/less/layout.less');
 require('plugins/wazuh/less/height.less');
 require('plugins/wazuh/less/typography.less');
 require('plugins/wazuh/less/media-queries.less');
-
-require('plugins/wazuh/less/cleaned.less');
-
 import 'ui/styles/theme.less';
