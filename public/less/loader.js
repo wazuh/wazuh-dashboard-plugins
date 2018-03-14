b625b6a1343123037a14a4d7ef5b65b15d6310e9
Juanjo Jiménez <juanjo@wazuh.com>
2018-03-14 11:45:36 +0100

    More HTML/CSS cleaning and refactoring

--- public/less/loader.js
+++ public/less/loader.js
@@ -3,8 +3,11 @@
 /* -------------------------------------------------------------------------- */
 
 require('plugins/wazuh/less/common.less');
-require('plugins/wazuh/less/heights.less');
-require('plugins/wazuh/less/cleaned.less');
+require('plugins/wazuh/less/layout.less');
+require('plugins/wazuh/less/height.less');
 require('plugins/wazuh/less/typography.less');
 require('plugins/wazuh/less/media-queries.less');
+
+require('plugins/wazuh/less/cleaned.less');
+
 import 'ui/styles/theme.less';
