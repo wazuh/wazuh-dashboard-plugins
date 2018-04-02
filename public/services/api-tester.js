890a4c3b452ded7e0e5bb7019024489f4aa821dc
Jesús Ángel González <jesusgonzalez@wazuh.com>
2018-04-02 13:01:10 +0200

    Refactored check_stored function from api tester service

--- public/services/api-tester.js
+++ public/services/api-tester.js
@@ -1,57 +1,53 @@
 import chrome from 'ui/chrome';
 const app = require('ui/modules').get('app/wazuh', []);
 
-app.service('testAPI', function ($q, $http, $location, $rootScope, appState, genericReq) {
+app.service('testAPI', function ($http, $location, $rootScope, appState, genericReq) {
     return {
-        check_stored: data => {
-            
-            let defered = $q.defer();
-            
-            const headers = {headers:{ "Content-Type": 'application/json' },timeout: $rootScope.userTimeout || 8000};
-            
-            /** Checks for outdated cookies */
-            const current     = appState.getCreatedAt();
-            const lastRestart = $rootScope.lastRestart;
-
-            if(current && lastRestart && lastRestart > current){
-                appState.removeCurrentPattern();
-                appState.removeCurrentAPI();
-                appState.removeClusterInfo();
-                appState.removeCreatedAt();
-                delete $rootScope.lastRestart;
-                genericReq.request('GET', '/api/wazuh-api/configuration', {})
-                .then(configuration => {
-                    appState.setPatternSelector(typeof configuration.data.data['ip.selector'] !== 'undefined' ? configuration.data.data['ip.selector'] : true)
-                    defered.resolve('cookies_outdated');
-                })
-                .catch(error => defered.reject(error));            
-
-                /** End of checks for outdated cookies */
-            } else {
-                if(appState.getUserCode()) headers.headers.code = appState.getUserCode();
+        check_stored: async data => {
+            try {
+                const headers = {headers:{ "Content-Type": 'application/json' },timeout: $rootScope.userTimeout || 8000};
                 
-                Promise.all([
-                    genericReq.request('GET', '/api/wazuh-api/configuration', {}),
-                    $http.post(chrome.addBasePath('/api/wazuh-api/checkStoredAPI'), data,headers)
-                ])
-                .then(data => {
-                    appState.setPatternSelector(typeof data[0].data.data['ip.selector'] !== 'undefined' ? data[0].data.data['ip.selector'] : true)
-                    if (data[1].error) {
-                        defered.reject(data[1]);
-                    } else {
-                        defered.resolve(data[1]);
+                /** Checks for outdated cookies */
+                const current     = appState.getCreatedAt();
+                const lastRestart = $rootScope.lastRestart;
+    
+                if(current && lastRestart && lastRestart > current){
+                    appState.removeCurrentPattern();
+                    appState.removeCurrentAPI();
+                    appState.removeClusterInfo();
+                    appState.removeCreatedAt();
+                    delete $rootScope.lastRestart;
+                    
+                    const configuration = await genericReq.request('GET', '/api/wazuh-api/configuration', {})
+
+                    appState.setPatternSelector(typeof configuration.data.data['ip.selector'] !== 'undefined' ? configuration.data.data['ip.selector'] : true)
+                    
+                    return 'cookies_outdated';
+                    /** End of checks for outdated cookies */
+         
+                } else {
+                    if(appState.getUserCode()) headers.headers.code = appState.getUserCode();
+                    
+                    const result = await Promise.all([
+                        genericReq.request('GET', '/api/wazuh-api/configuration', {}),
+                        $http.post(chrome.addBasePath('/api/wazuh-api/checkStoredAPI'), data,headers)
+                    ]);
+
+                    appState.setPatternSelector(typeof result[0].data.data['ip.selector'] !== 'undefined' ? result[0].data.data['ip.selector'] : true)
+
+                    if(result[1].error) {
+                        return Promise.reject(result[1])
                     }
-                })
-                .catch(error => {
-                    if(error.status && error.status === -1){
-                        $rootScope.apiIsDown = true;
-                        defered.reject({data: 'request_timeout_checkstored'});
-                    } else {
-                        defered.reject(error);
-                    }
-                });
+                    return result[1];
+                }
+            } catch (error) {
+                if(error.status && error.status === -1){
+                    $rootScope.apiIsDown = true;
+                    return Promise.reject({data: 'request_timeout_checkstored'});
+                } else {
+                    return Promise.reject(error);
+                }
             }
-            return defered.promise;
         },
         check: async data => {
             try {
