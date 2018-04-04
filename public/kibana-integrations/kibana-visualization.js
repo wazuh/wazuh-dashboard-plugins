3b8ed0ce229ad2544e11757fce92d87e528be867
Jesús Ángel González <jesusgonzalez@wazuh.com>
2018-04-04 18:02:08 +0200

    Fix typo

--- public/kibana-integrations/kibana-visualization.js
+++ public/kibana-integrations/kibana-visualization.js
@@ -22,7 +22,6 @@ var app = require('ui/modules').get('apps/webinar_app', [])
                 let renderInProgress       = false;
 
                 const myRender = function() {
-                    console.log($rootScope.visTimestamp,$rootScope.resultState)
                     if ($rootScope.visTimestamp) { // Only render if we already have the timestamp for it
                         if (($rootScope.discoverPendingUpdates && $rootScope.discoverPendingUpdates.length != 0) || $scope.visID.includes('Ruleset') ){ // There are pending updates from the discover (which is the one who owns the true app state)
 
