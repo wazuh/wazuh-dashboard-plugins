4b25b6c6695ec506314b601fe0f86e7038b8dea2
Jesús Ángel González <jesusgonzalez@wazuh.com>
2018-04-11 17:59:53 +0200

    Prevents from render visualizations on configuration tab

--- public/controllers/agents.js
+++ public/controllers/agents.js
@@ -112,7 +112,7 @@ app.controller('agentsController',
         // Switch subtab
         $scope.switchSubtab = subtab => {
             if($scope.tabView === subtab) return;
-            if(subtab === 'panels'){
+            if(subtab === 'panels' && $scope.tab !== 'configuration'){
                 if(!$rootScope.visTimestamp) {
                     $rootScope.visTimestamp = new Date().getTime();
                     if(!$rootScope.$$phase) $rootScope.$digest();
@@ -145,21 +145,22 @@ app.controller('agentsController',
                 $rootScope.visTimestamp = new Date().getTime();
                 if(!$rootScope.$$phase) $rootScope.$digest();
             }
-            
-            // Create current tab visualizations
-            genericReq.request('GET',`/api/wazuh-elastic/create-vis/agents-${tab}/${$rootScope.visTimestamp}/${appState.getCurrentPattern()}`)
-            .then(() => {
-    
-                // Render visualizations
-                $rootScope.$broadcast('updateVis');
-    
-                checkMetrics(tab, 'panels');
-    
-                // Deleting app state traces in the url
-                $location.search('_a', null);
-    
-            })
-            .catch(error => errorHandler.handle(error, 'Agents'));
+            if(tab !== 'configuration') {
+                // Create current tab visualizations
+                genericReq.request('GET',`/api/wazuh-elastic/create-vis/agents-${tab}/${$rootScope.visTimestamp}/${appState.getCurrentPattern()}`)
+                .then(() => {
+        
+                    // Render visualizations
+                    $rootScope.$broadcast('updateVis');
+        
+                    checkMetrics(tab, 'panels');
+        
+                    // Deleting app state traces in the url
+                    $location.search('_a', null);
+        
+                })
+                .catch(error => errorHandler.handle(error, 'Agents'));
+            }
         };
 
         // Watchers
@@ -470,15 +471,16 @@ app.controller('agentsController',
             }
         }
         /** End of agent configuration */
+        if($scope.tab !== 'configuration'){
+            // Create visualizations for controller's first execution
+            genericReq.request('GET',`/api/wazuh-elastic/create-vis/agents-${$scope.tab}/${$rootScope.visTimestamp}/${appState.getCurrentPattern()}`)
+            .then(() => {
 
-        // Create visualizations for controller's first execution
-        genericReq.request('GET',`/api/wazuh-elastic/create-vis/agents-${$scope.tab}/${$rootScope.visTimestamp}/${appState.getCurrentPattern()}`)
-        .then(() => {
-
-            // Render visualizations
-            $rootScope.$broadcast('updateVis');
-      
-            checkMetrics($scope.tab,'panels');
-        })
-        .catch(error => errorHandler.handle(error, 'Agents'));
+                // Render visualizations
+                $rootScope.$broadcast('updateVis');
+        
+                checkMetrics($scope.tab,'panels');
+            })
+            .catch(error => errorHandler.handle(error, 'Agents'));
+        }
     });
