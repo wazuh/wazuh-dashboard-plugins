0bd8699dab808637ca69c0d63a7578d7b0b6cfab
Jesús Ángel González <jesusgonzalez@wazuh.com>
2018-04-11 15:51:12 +0200

    New cron job to delete any visualization which was not removed properly

---
+++ server/vis-deletion-cron.js
@@ -0,0 +1,28 @@
+const ElasticWrapper = require('./lib/elastic-wrapper');
+const cron           = require('node-cron');
+const { log }        = require('./logger');
+
+module.exports = server => {
+    console.log('Hello')
+    let abort = 0;
+    const wzWrapper = new ElasticWrapper(server);
+
+    const clean = async () => {
+        try {
+            if(abort < 3) {
+                const date = new Date();
+                date.setTime(date.getTime() - (60 * 60 * 1000))
+                await wzWrapper.deleteVisualizationByDescription(date.getTime(),true);
+                log('vis-deletion-cron.js',`Deleted visualizations with a timestamp lower than ${date.getTime()}`,'info');
+                return;
+            } 
+        } catch (error) {
+            abort++;
+            log('vis-deletion-cron.js', error.message || error);
+        }
+    }
+
+ 
+    cron.schedule('* * */1 * * *',clean, true);
+
+}
