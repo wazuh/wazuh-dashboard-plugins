273e44c137ba252580aea7169680f7f95cc6ae19
Jesús Ángel González <jesusgonzalez@wazuh.com>
2018-04-18 16:17:43 +0200

    Removed vis-deletion-cron since it's no longer needed

--- init.js
+++ init.js
@@ -4,6 +4,5 @@ module.exports = (server, options) => {
     require('./server/routes/wazuh-elastic')(server, options);
     require('./server/routes/wazuh-api-elastic')(server, options);
     require('./server/monitoring')(server, options);
-    require('./server/vis-deletion-cron')(server);
     require('./server/routes/wazuh-api')(server, options);
 };
