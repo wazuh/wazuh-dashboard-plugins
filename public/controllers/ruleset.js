a0f1bbc2b3f65376d0303cfbe04ae6bfe5347b41
Juanjo Jiménez <juanji0503@gmail.com>
2018-04-24 20:00:42 +0200

    Added colouring to regex and order on Decoders

--- public/controllers/ruleset.js
+++ public/controllers/ruleset.js
@@ -23,6 +23,8 @@ app.controller('rulesController', function ($scope, $rootScope, Rules, RulesAuto
     $scope.rulesAutoComplete = RulesAutoComplete;
     $scope.setRulesTab('rules');
     $rootScope.tabVisualizations = { ruleset: 4 };
+    $scope.isArray = angular.isArray;
+
     $scope.analizeRules = async search => {
         try {
             $scope.rulesAutoComplete.filters = [];
@@ -158,7 +160,7 @@ app.controller('rulesController', function ($scope, $rootScope, Rules, RulesAuto
     });
 });
 
-app.controller('decodersController', function ($scope, $rootScope, Decoders, DecodersAutoComplete, errorHandler, genericReq, appState) {
+app.controller('decodersController', function ($scope, $rootScope, $sce, Decoders, DecodersAutoComplete, errorHandler, genericReq, appState) {
     $scope.setRulesTab = tab => $rootScope.globalsubmenuNavItem2 = tab;
 
     //Initialization
@@ -168,6 +170,37 @@ app.controller('decodersController', function ($scope, $rootScope, Decoders, Dec
     $scope.typeFilter = "all";
     $scope.setRulesTab('decoders');
     $rootScope.tabVisualizations = { ruleset: 1 };
+    $scope.isArray = angular.isArray;
+
+    const colors = [
+        '#3F6833', '#967302', '#2F575E', '#99440A', '#58140C', '#052B51', '#511749', '#3F2B5B', //6
+        '#508642', '#CCA300', '#447EBC', '#C15C17', '#890F02', '#0A437C', '#6D1F62', '#584477', //2
+        '#629E51', '#E5AC0E', '#64B0C8', '#E0752D', '#BF1B00', '#0A50A1', '#962D82', '#614D93', //4
+        '#7EB26D', '#EAB839', '#6ED0E0', '#EF843C', '#E24D42', '#1F78C1', '#BA43A9', '#705DA0', // Normal
+        '#9AC48A', '#F2C96D', '#65C5DB', '#F9934E', '#EA6460', '#5195CE', '#D683CE', '#806EB7', //5
+        '#B7DBAB', '#F4D598', '#70DBED', '#F9BA8F', '#F29191', '#82B5D8', '#E5A8E2', '#AEA2E0', //3
+        '#E0F9D7', '#FCEACA', '#CFFAFF', '#F9E2D2', '#FCE2DE', '#BADFF4', '#F9D9F9', '#DEDAF7' //7
+    ];
+
+    $scope.colorRegex = regex => {
+        regex = regex.toString();
+        let valuesArray   = regex.match(/\(((?!<\/span>).)*?\)(?!<\/span>)/gmi);
+        let coloredString = regex;
+        for (let i = 0, len = valuesArray.length; i < len; i++) {
+            coloredString = coloredString.replace(/\(((?!<\/span>).)*?\)(?!<\/span>)/mi, '<span style="color: ' + colors[i] + ' ">' + valuesArray[i] + '</span>');
+        }
+        return $sce.trustAsHtml(coloredString);
+    };
+
+    $scope.colorOrder = order => {
+        order = order.toString();
+        let valuesArray   = order.split(',');
+        let coloredString = order;
+        for (let i = 0, len = valuesArray.length; i < len; i++) {
+            coloredString = coloredString.replace(valuesArray[i], '<span style="color: ' + colors[i] + ' ">' + valuesArray[i] + '</span>');
+        }
+        return $sce.trustAsHtml(coloredString);
+    };
 
     let timesOpened = 0;
     let lastName = false;
