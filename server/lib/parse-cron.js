e17c5be6083f665e74facc771ad16fe73c962248
Jesús Ángel González <jesusgonzalez@wazuh.com>
2018-05-07 16:21:51 +0200

    Module to transform seconds interval to cron readable format

---
+++ server/lib/parse-cron.js
@@ -0,0 +1,35 @@
+/*
+ * Wazuh app - Module to transform seconds interval to cron readable format
+ * Copyright (C) 2018 Wazuh, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Find more information about this on the LICENSE file.
+ */
+import log from '../logger'
+import cron from 'node-cron'
+
+export default interval => {
+    try {
+        if(!interval) throw new Error('Interval not found');
+
+        const parsed = parseInt(interval);
+
+        if(!parsed || typeof parsed !== 'number') throw new Error('Interval not valid');
+        if(parsed < 60) throw new Error('Interval too low');
+        if(parsed >= 84600) throw new Error('Interval too high');
+
+        const minutes = parseInt(parsed / 60);
+
+        const cronstr = `0 */${minutes} * * * *`
+        
+        return cronstr;
+
+    } catch (error) {
+        log('[cron][parse-interval]',`Using default value due to: ${error.message || error}`);
+        return '0 1 * * * *';
+    }
+}
