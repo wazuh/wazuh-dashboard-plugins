a9e40023f8cd57650c5f3ffa75d16b649bc89bd6
Juanjo Jiménez <juanji0503@gmail.com>
2018-05-09 16:07:51 +0200

    Rule description arguments are now coloured

--- public/controllers/ruleset.js
+++ public/controllers/ruleset.js
@@ -14,11 +14,12 @@ import CsvGenerator from './csv-generator'
 
 const app = modules.get('app/wazuh', []);
 
-app.controller('rulesController', function ($scope, $rootScope, Rules, RulesRelated, RulesAutoComplete, errorHandler, genericReq, appState, csvReq) {
+app.controller('rulesController', function ($scope, $rootScope, $sce, Rules, RulesRelated, RulesAutoComplete, errorHandler, genericReq, appState, csvReq) {
 
     $scope.setRulesTab = tab => $rootScope.globalsubmenuNavItem2 = tab;
 
     //Initialization
+    $scope.searchTerm = '';
     $scope.loading = true;
     $scope.viewingDetail = false;
     $scope.rules   = Rules;
@@ -26,6 +27,33 @@ app.controller('rulesController', function ($scope, $rootScope, Rules, RulesRela
     $scope.rulesAutoComplete = RulesAutoComplete;
     $scope.setRulesTab('rules');
     $scope.isArray = angular.isArray;
+
+    const colors = [
+        '#3F6833', '#967302', '#2F575E', '#99440A', '#58140C', '#052B51', '#511749', '#3F2B5B', //6
+        '#508642', '#CCA300', '#447EBC', '#C15C17', '#890F02', '#0A437C', '#6D1F62', '#584477', //2
+        '#629E51', '#E5AC0E', '#64B0C8', '#E0752D', '#BF1B00', '#0A50A1', '#962D82', '#614D93', //4
+        '#7EB26D', '#EAB839', '#6ED0E0', '#EF843C', '#E24D42', '#1F78C1', '#BA43A9', '#705DA0', // Normal
+        '#9AC48A', '#F2C96D', '#65C5DB', '#F9934E', '#EA6460', '#5195CE', '#D683CE', '#806EB7', //5
+        '#B7DBAB', '#F4D598', '#70DBED', '#F9BA8F', '#F29191', '#82B5D8', '#E5A8E2', '#AEA2E0', //3
+        '#E0F9D7', '#FCEACA', '#CFFAFF', '#F9E2D2', '#FCE2DE', '#BADFF4', '#F9D9F9', '#DEDAF7' //7
+    ];
+
+    $scope.colorRuleArg = ruleArg => {
+        ruleArg = ruleArg.toString();
+        let valuesArray   = ruleArg.match(/\$\(((?!<\/span>).)*?\)(?!<\/span>)/gmi);
+        let coloredString = ruleArg;
+
+        // If valuesArray is empty, means that the description doesn't have any arguments
+        // In this case, then simply return the string
+        // In other case, then colour the string and return
+        if (valuesArray && valuesArray.length) {
+            for (let i = 0, len = valuesArray.length; i < len; i++) {
+                coloredString = coloredString.replace(/\$\(((?!<\/span>).)*?\)(?!<\/span>)/mi, '<span style="color: ' + colors[i] + ' ">' + valuesArray[i] + '</span>');
+            }
+        }
+
+        return $sce.trustAsHtml(coloredString);
+    };
 
     // Reloading watcher initialization
     const reloadWatcher = $rootScope.$watch('rulesetIsReloaded',() => {
@@ -155,6 +183,12 @@ app.controller('rulesController', function ($scope, $rootScope, Rules, RulesRela
         $scope.rules.reset();
         $scope.rulesRelated.reset();
         $scope.rulesAutoComplete.reset();
+        if($rootScope.ownHandlers){
+            for(let h of $rootScope.ownHandlers){
+                h._scope.$destroy();
+            }
+        }
+        $rootScope.ownHandlers = [];
         reloadWatcher();
     });
 });
@@ -163,6 +197,7 @@ app.controller('decodersController', function ($scope, $rootScope, $sce, Decoder
     $scope.setRulesTab = tab => $rootScope.globalsubmenuNavItem2 = tab;
 
     //Initialization
+    $scope.searchTerm = '';
     $scope.loading  = true;
     $scope.viewingDetail = false;
     $scope.decoders = Decoders;
@@ -181,13 +216,6 @@ app.controller('decodersController', function ($scope, $rootScope, $sce, Decoder
         '#B7DBAB', '#F4D598', '#70DBED', '#F9BA8F', '#F29191', '#82B5D8', '#E5A8E2', '#AEA2E0', //3
         '#E0F9D7', '#FCEACA', '#CFFAFF', '#F9E2D2', '#FCE2DE', '#BADFF4', '#F9D9F9', '#DEDAF7' //7
     ];
-
-    // Reloading watcher initialization
-    const reloadWatcher = $rootScope.$watch('rulesetIsReloaded',() => {
-        delete $rootScope.rulesetIsReloaded;
-        $scope.viewingDetail = false;
-        if(!$scope.$$phase) $scope.$digest();
-    });
 
     $scope.colorRegex = regex => {
         regex = regex.toString();
@@ -208,6 +236,13 @@ app.controller('decodersController', function ($scope, $rootScope, $sce, Decoder
         }
         return $sce.trustAsHtml(coloredString);
     };
+
+    // Reloading watcher initialization
+    const reloadWatcher = $rootScope.$watch('rulesetIsReloaded',() => {
+        delete $rootScope.rulesetIsReloaded;
+        $scope.viewingDetail = false;
+        if(!$scope.$$phase) $scope.$digest();
+    });
 
     $scope.checkEnter = search => {
         $scope.searchTerm = '';
@@ -322,6 +357,12 @@ app.controller('decodersController', function ($scope, $rootScope, $sce, Decoder
         $scope.decoders.reset();
         $scope.decodersRelated.reset();
         $scope.decodersAutoComplete.reset();
+        if($rootScope.ownHandlers){
+            for(let h of $rootScope.ownHandlers){
+                h._scope.$destroy();
+            }
+        }
+        $rootScope.ownHandlers = [];
         reloadWatcher();
     });
 });
