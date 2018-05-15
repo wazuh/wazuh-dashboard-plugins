e1bc3fa0a64eb2cfe7d12246a41a609cd71c5b67
Jesús Ángel González <jesusgonzalez@wazuh.com>
2018-05-15 16:11:35 +0200

    Added discover pending updates factory

---
+++ public/factories/discover-pending-updates.js
@@ -0,0 +1,37 @@
+/*
+ * Wazuh app - Factory to store pending updates from Discover
+ * 
+ * Copyright (C) 2018 Wazuh, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Find more information about this on the LICENSE file.
+ */
+import * as modules from 'ui/modules'
+
+const app = modules.get('app/wazuh', []);
+
+app.factory('discoverPendingUpdates', function() {
+    let pendingUpdates = [];
+
+    const addItem = (query, filters) => {
+        pendingUpdates.push(query, filters);
+    }
+
+    const getList = () => {
+        return pendingUpdates;
+    }
+
+    const removeAll = () => {
+        pendingUpdates = [];
+    }
+  
+    return {
+      addItem    : addItem,
+      getList    : getList,
+      removeAll  : removeAll
+    };
+});
--- public/factories/index.js
+++ public/factories/index.js
@@ -13,3 +13,4 @@ import 'plugins/wazuh/factories/data-handler-composer';
 import 'plugins/wazuh/factories/raw-visualizations';
 import 'plugins/wazuh/factories/loaded-visualizations';
 import 'plugins/wazuh/factories/tab-visualizations';
+import 'plugins/wazuh/factories/discover-pending-updates';
