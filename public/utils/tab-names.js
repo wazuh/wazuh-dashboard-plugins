ad95eac96b58ea6de9b7d52fb9c9b6cf68ad3fcc
Jesús Ángel González <jesusgonzalez@wazuh.com>
2018-06-12 13:37:20 +0200

    Moved common data out of controllers

---
+++ public/utils/tab-names.js
@@ -0,0 +1,24 @@
+/*
+ * Wazuh app - Tab name equivalence
+ * Copyright (C) 2018 Wazuh, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Find more information about this on the LICENSE file.
+ */
+export default {
+    welcome   : 'Welcome',
+    general   : 'General',
+    fim       : 'File integrity',
+    pm        : 'Policy monitoring',
+    vuls      : 'Vulnerabilities',
+    oscap     : 'Open SCAP',
+    audit     : 'Audit',
+    pci       : 'PCI DSS',
+    gdpr      : 'GDPR',
+    aws       : 'AWS',
+    virustotal: 'VirusTotal'
+}
