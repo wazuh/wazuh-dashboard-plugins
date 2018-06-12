a2f9f9b451e66b29364ba586b5de6a1c1aceb7e5
Jesús Ángel González <jesusgonzalez@wazuh.com>
2018-06-12 15:22:37 +0200

    Reporting now has its own service

---
+++ public/services/reporting.js
@@ -0,0 +1,69 @@
+/*
+ * Wazuh app - Reporting service
+ * Copyright (C) 2018 Wazuh, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Find more information about this on the LICENSE file.
+ */
+import * as modules from 'ui/modules'
+import $            from 'jquery'
+
+modules.get('app/wazuh', [])
+.service('reportingService', function ($rootScope, vis2png, rawVisualizations, visHandlers, genericReq, errorHandler) {
+    return {
+        startVis2Png: async (tab,isAgents = false) => {
+            try {
+                if(vis2png.isWorking()){
+                    errorHandler.handle('Report in progress', 'Reporting',true);
+                    return;
+                }
+                $rootScope.reportBusy = true;
+                $rootScope.reportStatus = 'Generating report...0%'
+                if(!$rootScope.$$phase) $rootScope.$digest();
+
+                vis2png.clear();
+
+                const idArray = rawVisualizations.getList().map(item => item.id);
+
+                for(const item of idArray) {
+                    const tmpHTMLElement = $(`#${item}`);
+                    vis2png.assignHTMLItem(item,tmpHTMLElement)
+                }
+
+                const appliedFilters = visHandlers.getAppliedFilters();
+
+                const array = await vis2png.checkArray(idArray)
+                const name  = `wazuh-${isAgents ? 'agents' : 'overview'}-${tab}-${Date.now() / 1000 | 0}.pdf`
+
+                const data = {
+                    array,
+                    name,
+                    title: isAgents ? `Agents ${tab}` : `Overview ${tab}`,
+                    filters: appliedFilters.filters,
+                    time: appliedFilters.time,
+                    searchBar: appliedFilters.searchBar,
+                    tab,
+                    section: isAgents ? 'agents' : 'overview',
+                    isAgents
+                };
+
+                const request = await genericReq.request('POST','/api/wazuh-api/report',data)
+
+                $rootScope.reportBusy = false;
+                $rootScope.reportStatus = false;
+
+                errorHandler.info('Success. Go to Management -> Reporting', 'Reporting')
+
+                return;
+            } catch (error) {
+                $rootScope.reportBusy = false;
+                $rootScope.reportStatus = false;
+                errorHandler.handle(error, 'Reporting')
+            }
+        }
+    }
+});
