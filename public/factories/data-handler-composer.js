3f950e36fcde88258cf391144d237770c7c9f525
Jesús Ángel González <jesusgonzalez@wazuh.com>
2018-06-18 18:10:38 +0200

    Removed old logic from legacy data handler

--- public/factories/data-handler-composer.js
+++ public/factories/data-handler-composer.js
@@ -24,28 +24,12 @@ const compose = (DataHandler, path) => {
     return dataHandler;
 };
 
-const groups   = DataHandler => compose(DataHandler, '/agents/groups');
+
 const agents   = DataHandler => compose(DataHandler, '/agents');
-const logs     = DataHandler => compose(DataHandler, '/manager/logs');
-const rules    = DataHandler => compose(DataHandler, '/rules');
-const decoders = DataHandler => compose(DataHandler, '/decoders');
-const nodes    = DataHandler => compose(DataHandler, '/cluster/nodes');
 const simple   = DataHandler => new DataHandler();
 
 app
-    .factory('Groups', groups)
-    .factory('GroupAgents', simple)
-    .factory('GroupFiles', simple)
     .factory('AgentsAutoComplete', agents)
-    .factory('Agents', agents)
-    .factory('Logs', logs)
-    .factory('Rules', rules)
-    .factory('RulesRelated', rules)
-    .factory('RulesAutoComplete', rules)
-    .factory('Decoders', decoders)
-    .factory('DecodersRelated', simple)
-    .factory('DecodersAutoComplete', decoders)
-    .factory('ClusterNodes', nodes)
     .directive('lazyLoadData', function($compile) {
         return {
             link: (scope, el, attrs) => {
