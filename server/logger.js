84630bd61f3ef431912add0b1d05f978485c53c0
Jesús Ángel González <jesusgonzalez@wazuh.com>
2018-07-09 10:10:01 +0200

    Added message when rotating log file

--- server/logger.js
+++ server/logger.js
@@ -87,6 +87,12 @@ const checkFiles = () => {
                 path.join(__dirname, '../../../optimize/wazuh-logs/wazuhapp.log'),
                 path.join(__dirname, `../../../optimize/wazuh-logs/wazuhapp.${new Date().getTime()}.log`)
             );
+            fs.writeFileSync(path.join(__dirname, '../../../optimize/wazuh-logs/wazuhapp.log'),JSON.stringify({
+                date    : new Date(),
+                level   : 'info',
+                location: 'logger',
+                message : 'Rotated log file'
+            }) + '\n');
         }
     }
 };
