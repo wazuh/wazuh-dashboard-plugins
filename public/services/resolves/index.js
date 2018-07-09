8c35d9db13ab4b7e7602b31a721b7d40f02bcc15
Jesús Ángel González <jesusgonzalez@wazuh.com>
2018-07-09 10:41:35 +0200

    Exporting new resolve function

--- public/services/resolves/index.js
+++ public/services/resolves/index.js
@@ -9,12 +9,13 @@
  *
  * Find more information about this on the LICENSE file.
  */
-import checkTimestamp from './check-timestamp'
-import healthCheck    from './health-check'
-import settingsWizard from './settings-wizard'
-import getSavedSearch from './get-saved-search'
-import goToKibana     from './go-to-kibana'
-import getIp          from './get-ip'
-import totalRAM       from './check-ram'
+import checkTimestamp from './check-timestamp';
+import healthCheck    from './health-check';
+import settingsWizard from './settings-wizard';
+import getSavedSearch from './get-saved-search';
+import goToKibana     from './go-to-kibana';
+import getIp          from './get-ip';
+import totalRAM       from './check-ram';
+import getWzConfig    from './get-config';
 
-export { checkTimestamp, healthCheck, settingsWizard, getSavedSearch, goToKibana, getIp, totalRAM }
+export { checkTimestamp, healthCheck, settingsWizard, getSavedSearch, goToKibana, getIp, totalRAM, getWzConfig };
