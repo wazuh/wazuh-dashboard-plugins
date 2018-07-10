b7050e95c89439b03dede68794074111941da26d
Jesús Ángel González <jesusgonzalez@wazuh.com>
2018-07-10 17:28:26 +0200

    Checking if filter is negated

--- server/controllers/wazuh-reporting.js
+++ server/controllers/wazuh-reporting.js
@@ -139,8 +139,8 @@ export default class WazuhReportingCtrl {
         for (let i = 0; i < len; i++) {
             const filter = filters[i];
             str += i === len - 1 ?
-                filter.meta.key + ': ' + filter.meta.value :
-                filter.meta.key + ': ' + filter.meta.value + ' AND ';
+                (filter.meta.negate ? 'NOT ' : '' ) + filter.meta.key + ': ' + filter.meta.value :
+                (filter.meta.negate ? 'NOT ' : '' ) + filter.meta.key + ': ' + filter.meta.value + ' AND ';
         }
 
         if (searchBar) {
