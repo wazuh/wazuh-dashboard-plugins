c744232f17f434c815b29221f72ed18a7cb2609c
Jesús Ángel <jesus.gonzalez@wazuh.com>
2018-07-16 09:05:53 +0200

    Removed rootScope

--- public/services/resolves/health-check.js
+++ public/services/resolves/health-check.js
@@ -9,11 +9,11 @@
  *
  * Find more information about this on the LICENSE file.
  */
-export default ($window, $rootScope) => {
+export default $window => {
     if (!$window.sessionStorage.getItem('healthCheck')) { // New session, execute health check
         $window.sessionStorage.setItem('healthCheck', 'executed');
         return true;
-    } else {
-        return false;
-    }
-}
+    } 
+        
+    return false;
+};
