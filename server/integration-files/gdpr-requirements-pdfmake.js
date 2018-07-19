d6a875be4364c7ce13f215f5c8e840769bd5d8a0
Jesús Ángel <jesus.gonzalez@wazuh.com>
2018-07-19 15:18:18 +0200

    Added pdfmake format to pci/gdpr

--- server/integration-files/gdpr-requirements-pdfmake.js
+++ server/integration-files/gdpr-requirements-pdfmake.js
@@ -1,5 +1,5 @@
 /*
- * Wazuh app - Module for GDPR requirements
+ * Wazuh app - Module for GDPR requirements (Reporting)
  * Copyright (C) 2018 Wazuh, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -10,7 +10,7 @@
  * Find more information about this on the LICENSE file.
  */
 export default {
-    "II_5.1.f": "Ensure the ongoing confidentiality, integrity, availability and resilience of processing systems and services, verifying its modifications, accesses, locations and guarantee the safety of them.<br>File sharing protection and file sharing technologies that meet the requirements of data protection.",
+    "II_5.1.f": "Ensure the ongoing confidentiality, integrity, availability and resilience of processing systems and services, verifying its modifications, accesses, locations and guarantee the safety of them. File sharing protection and file sharing technologies that meet the requirements of data protection.",
     "III_14.2.c": " Restrict the processing of personal data temporarily.",
     "III_17": " Permanently erase personal information of a subject.",
     "IV_24.2": "Be able to demonstrate compliance with the GDPR by complying with data protection policies.",
@@ -20,5 +20,5 @@ export default {
     "IV_32.2": "Account management tools that closely monitor actions taken by standard administrators and users who use standard or privileged account credentials are required to control access to data. ",
     "IV_33": " Notify the supervisory authority of a violation of the data in 72 hours and in certain cases, the injured parties.",
     "IV_35.1": "Perform a data protection impact evaluation for high risk processes. Implement appropriate technical measures to safeguard the rights and freedoms of data subjects, informed by an assessment of the risks to these rights and freedoms.",
-    "IV_35.7.d": "Capabilities for identification, blocking and forensic investigation of data breaches by malicious actors, through compromised credentials, unauthorized network access, persistent threats and verification of the correct operation of all components.<br>Network perimeter and endpoint security tools to prevent unauthorized access to the network, prevent the entry of unwanted data types and malicious threats. Anti-malware and anti-ransomware to prevent malware and ransomware threats from entering your devices.<br>A behavioral analysis that uses machine intelligence to identify people who do anomalous things on the network, in order to give early visibility and alert employees who start to become corrupt."
+    "IV_35.7.d": "Capabilities for identification, blocking and forensic investigation of data breaches by malicious actors, through compromised credentials, unauthorized network access, persistent threats and verification of the correct operation of all components. Network perimeter and endpoint security tools to prevent unauthorized access to the network, prevent the entry of unwanted data types and malicious threats. Anti-malware and anti-ransomware to prevent malware and ransomware threats from entering your devices. A behavioral analysis that uses machine intelligence to identify people who do anomalous things on the network, in order to give early visibility and alert employees who start to become corrupt."
 };
