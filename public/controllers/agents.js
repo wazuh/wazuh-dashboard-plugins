a0a7c2e692efb440a691a11fe77d29425029d4c1
Jesús Ángel <jesus.gonzalez@wazuh.com>
2018-07-26 15:46:05 +0200

    Renamed mergedSum and configSum fields

--- public/controllers/agents.js
+++ public/controllers/agents.js
@@ -24,7 +24,7 @@ app.controller('agentsController',
 function (
     $timeout, $scope, $location, $rootScope,
     appState, apiReq, AgentsAutoComplete, errorHandler,
-    tabVisualizations, vis2png, shareAgent, commonData,
+    tabVisualizations, shareAgent, commonData,
     reportingService, visFactoryService, csvReq,
     wzTableFilter
 ) {
@@ -310,7 +310,7 @@ function (
             $scope.groupMergedSum = (groupMergedSum.length) ? groupMergedSum[0].mergedSum : 'Unknown';
 
             const agentMergedSum  = agentGroups[1].data.data.items.filter(item => item.id === $scope.agent.id);
-            $scope.agentMergedSum = (agentMergedSum.length) ? agentMergedSum[0].merged_sum : 'Unknown';
+            $scope.agentMergedSum = (agentMergedSum.length) ? agentMergedSum[0].mergedSum : 'Unknown';
 
             $scope.isSynchronized = (($scope.agentMergedSum === $scope.groupMergedSum) && !([$scope.agentMergedSum,$scope.groupMergedSum].includes('Unknown')) ) ? true : false;
 
