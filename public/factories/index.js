1595773d96a64f8933ec4c988b056266cd7f4b13
JuanjiJG <juanji0503@gmail.com>
2018-08-02 15:24:43 +0200

    Fixed some dependencies

--- public/factories/index.js
+++ public/factories/index.js
@@ -9,7 +9,6 @@
  *
  * Find more information about this on the LICENSE file.
  */
-import './data-handler-composer';
 import './raw-visualizations';
 import './loaded-visualizations';
 import './tab-visualizations';
