09d5c16da86bf6783155e7ecece0fe6746b7fa4a
Jesús Ángel <jesus.gonzalez@wazuh.com>
2018-08-21 17:24:14 +0200

    Adapted global timepicker

--- public/kibana-integrations/kbn-global-timepicker/kbn_global_timepicker.js
+++ public/kibana-integrations/kbn-global-timepicker/kbn_global_timepicker.js
@@ -18,51 +18,76 @@
  */
 
 import { uiModules } from 'ui/modules';
-import { once, clone } from 'lodash';
 
 import toggleHtml from './kbn_global_timepicker.html';
 import { timeNavigation } from './time_navigation';
+import { timefilter } from 'ui/timefilter';
+import { prettyDuration } from 'ui/timepicker/pretty_duration';
+import { prettyInterval } from 'ui/timepicker/pretty_interval';
 
 // WAZUH. Renamed from kbnGlobalTimepicker to wzKbnGlobalTimepicker
 uiModules
   .get('kibana')
-  .directive('wzKbnGlobalTimepicker', (timefilter, globalState, $rootScope) => {
-    const listenForUpdates = once($scope => {
-      $scope.$listen(timefilter, 'update', () => {
-        globalState.time = clone(timefilter.time);
-        globalState.refreshInterval = clone(timefilter.refreshInterval);
-        globalState.save();
+  .directive('wzKbnGlobalTimepicker', (globalState, config, $rootScope) => {
+    const getConfig = (...args) => config.get(...args);
+
+    const listenForUpdates = ($scope) => {
+      $scope.$listenAndDigestAsync(timefilter, 'refreshIntervalUpdate', () => {
+        setTimefilterValues($scope);
       });
-    });
+      $scope.$listenAndDigestAsync(timefilter, 'timeUpdate', () => {
+        setTimefilterValues($scope);
+      });
+      $scope.$listenAndDigestAsync(timefilter, 'enabledUpdated', () => {
+        setTimefilterValues($scope);
+      });
+    };
+
+    function setTimefilterValues($scope) {
+      console.log('setTimefilterValues')
+      $rootScope.$broadcast('updateVis')
+      const time = timefilter.getTime();
+      const refreshInterval = timefilter.getRefreshInterval();
+      $scope.timefilterValues = {
+        refreshInterval: refreshInterval,
+        time: time,
+        display: {
+          time: prettyDuration(time.from, time.to, getConfig),
+          refreshInterval: prettyInterval(refreshInterval.value),
+        },
+        isAutoRefreshSelectorEnabled: timefilter.isAutoRefreshSelectorEnabled,
+        isTimeRangeSelectorEnabled: timefilter.isTimeRangeSelectorEnabled,
+      };
+    }
 
     return {
       template: toggleHtml,
       replace: true,
       require: '^wzKbnTopNav',
       link: ($scope, element, attributes, wzKbnTopNav) => {
-        listenForUpdates($rootScope);
+        listenForUpdates($scope);
 
-        $rootScope.timefilter = timefilter;
-        $rootScope.toggleRefresh = () => {
-          timefilter.refreshInterval.pause = !timefilter.refreshInterval.pause;
+        setTimefilterValues($scope);
+
+        $scope.toggleRefresh = () => {
+          timefilter.toggleRefresh();
         };
 
         $scope.forward = function () {
-          timefilter.time = timeNavigation.stepForward(timefilter.getBounds());
+          timefilter.setTime(timeNavigation.stepForward(timefilter.getBounds()));
         };
 
         $scope.back = function () {
-          timefilter.time = timeNavigation.stepBackward(timefilter.getBounds());
+          timefilter.setTime(timeNavigation.stepBackward(timefilter.getBounds()));
         };
 
-        $scope.updateFilter = function (from, to) {
-          timefilter.time.from = from;
-          timefilter.time.to = to;
+        $scope.updateFilter = function (from, to, mode) {
+          timefilter.setTime({ from, to, mode });
           wzKbnTopNav.close('filter');
         };
 
         $scope.updateInterval = function (interval) {
-          timefilter.refreshInterval = interval;
+          timefilter.setRefreshInterval(interval);
           wzKbnTopNav.close('interval');
         };
 
