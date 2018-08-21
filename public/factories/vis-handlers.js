16bf5db67c55bca1fdcda745d66adb2ce38751c3
Jesús Ángel <jesus.gonzalez@wazuh.com>
2018-08-21 17:26:52 +0200

    Adapting vis handler factory (in progress)

--- public/factories/vis-handlers.js
+++ public/factories/vis-handlers.js
@@ -44,45 +44,32 @@ class VisHandlers {
         }
         
         // Check raw response from all rendered tables
-        const tables = this.list.filter(item => item._scope &&
-                                           item._scope.savedObj &&
-                                           item._scope.savedObj.vis &&
-                                           item._scope.savedObj.vis._state &&
-                                           item._scope.savedObj.vis._state.type === 'table'
-                            )
+        
+        const tables = this.list
+                            .filter(item => item.vis && item.vis._state && item.vis._state.type === 'table')
                             .map(item => {
                                 const columns = []; 
-                                for(const agg of item._scope.savedObj._source.visState.aggs){
-                                    if(agg.type === 'count') continue;
-                                    if(agg.params && agg.params.customLabel){
-                                        columns.push(agg.params.customLabel);
-                                    } else {
-                                        columns.push('Column');
-                                    }
+                                for(const table of item.dataLoader.visData.tables){
+                                    columns.push(...table.columns.map(t => t.title));
                                 }
-                                columns.push('Count');
 
-                                return (item._scope &&
-                                        item._scope.savedObj &&
-                                        item._scope.savedObj.vis &&
-                                        item._scope.savedObj.vis.searchSource &&
-                                        item._scope.savedObj.vis.searchSource.rawResponse) ? 
+                                return (item.vis &&
+                                        item.vis.searchSource &&
+                                        item.vis.searchSource.rawResponse) ? 
                                         {
-                                            rawResponse: item._scope.savedObj.vis.searchSource.rawResponse,
-                                            title: (item._scope && item._scope.savedObj && item._scope.savedObj.title) ?
-                                                    item._scope.savedObj.title :
-                                                    'Table',
-                                            columns
+                                            rawResponse: item.vis.searchSource.rawResponse,
+                                            title: item.vis.title || 'Table',
+                                            columns: columns
                                         } : 
                                         false;
                             });
         
         if(this.list && this.list.length) {
             // Parse applied filters for the first visualization
-            const filters = this.list[0]._scope.savedObj.vis.API.queryFilter.getFilters();
+            const filters = this.list[0].vis.API.queryFilter.getFilters();
      
             // Parse current time range
-            const { from, to } = this.list[0]._scope.savedObj.vis.API.timeFilter.time;
+            const { from, to } = this.list[0].vis.API.timeFilter.getTime();
 
             Object.assign(appliedFilters, {
                 filters,
@@ -90,9 +77,7 @@ class VisHandlers {
                     from: dateMath.parse(from),
                     to: dateMath.parse(to)
                 },
-                searchBar: this.list[0] && this.list[0]._scope && this.list[0]._scope.appState && this.list[0]._scope.appState.query && this.list[0]._scope.appState.query.query ? 
-                           this.list[0]._scope.appState.query.query :
-                           false,
+                searchBar: false,
                 tables
             });
         }
@@ -102,25 +87,22 @@ class VisHandlers {
 
     hasData() {
         for(const item of this.list) {
-            if(item && item._scope && item._scope.savedObj && item._scope.savedObj.searchSource && 
-                item._scope.savedObj.searchSource.rawResponse &&
-                item._scope.savedObj.searchSource.rawResponse.hits &&
-                item._scope.savedObj.searchSource.rawResponse.hits.total){
-                    return true;
-                }
+            if(
+                item &&
+                item.vis &&
+                item.vis.title !== 'Agents status' &&
+                item.vis.searchSource && 
+                item.vis.searchSource.rawResponse &&
+                item.vis.searchSource.rawResponse.hits &&
+                item.vis.searchSource.rawResponse.hits.total
+            ){
+                return true;
+            }
         }
         return false;
     }
 
     removeAll() {
-        for(const item of this.list){
-            if(item && item._scope){
-                item._scope.$destroy();
-            }
-            if(item && item._scope && item._scope.savedObj){
-                item._scope.savedObj.destroy();
-            }
-        }
         this.list = [];
     }
 }
