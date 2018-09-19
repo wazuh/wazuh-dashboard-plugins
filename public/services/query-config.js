7c1a01a428a563d151b035bb670a6b47fa7b8b40
Jesús Ángel <jesus.gonzalez@wazuh.com>
2018-09-19 12:57:51 +0200

    Added queryConfig function

---
+++ public/services/query-config.js
@@ -0,0 +1,55 @@
+/*
+ * Wazuh app - Query config on-demand
+ * Copyright (C) 2018 Wazuh, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Find more information about this on the LICENSE file.
+ */
+
+/**
+ * Returns applied configuration for specific agent and specific section
+ * @param {string} agentId Agent ID
+ * @param {Array<object>} sections Array that includes sections to be fetched
+ * @param {object} apiReq API request service reference
+ */
+export async function queryConfig(agentId, sections, apiReq) {
+  try {
+    if (
+      !agentId ||
+      typeof agentId !== 'string' ||
+      !sections ||
+      !sections.length ||
+      typeof sections !== 'object' ||
+      !Array.isArray(sections)
+    ) {
+      throw new Error('Invalid parameters');
+    }
+
+    const result = {};
+    for (const section of sections) {
+      const { component, configuration } = section;
+      if (
+        !component ||
+        typeof component !== 'string' ||
+        !configuration ||
+        typeof configuration !== 'string'
+      ) {
+        throw new Error('Invalid section');
+      }
+      const partialResult = await apiReq.request(
+        'GET',
+        `/agents/${agentId}/config/${component}/${configuration}`,
+        {}
+      );
+      result[`${component}-${configuration}`] = partialResult.data.data;
+    }
+
+    return result;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
