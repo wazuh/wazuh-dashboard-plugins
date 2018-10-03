1ba41812f5bc3ae934ab4574a7973e8387f02c5e
Jesús Ángel <jesus.gonzalez@wazuh.com>
2018-10-03 16:56:40 +0200

    Fix wrong condition

--- public/controllers/agent/agents.js
+++ public/controllers/agent/agents.js
@@ -256,30 +256,12 @@ class AgentsController {
       this.$scope.tabView = subtab;
 
       if (
-        subtab === 'panels' &&
-        this.$scope.tab !== 'configuration' &&
-        this.$scope.tab !== 'welcome' &&
-        this.$scope.tab !== 'syscollector'
-      ) {
-        const condition =
-          (!this.changeAgent && localChange) ||
-          (!this.changeAgent && preserveDiscover);
-        await this.visFactoryService.buildAgentsVisualizations(
-          this.filterHandler,
-          this.$scope.tab,
-          subtab,
-          condition,
-          this.$scope.agent.id
-        );
-        this.changeAgent = false;
-      } else if (
-        this.targetLocation &&
+        ((subtab === 'panels') || 
+        (this.targetLocation &&
         typeof this.targetLocation === 'object' &&
         this.targetLocation.subTab === 'discover' &&
-        subtab === 'discover' &&
-        this.$scope.tab !== 'configuration' &&
-        this.$scope.tab !== 'welcome' &&
-        this.$scope.tab !== 'syscollector'
+        subtab === 'discover')) &&
+        !['configuration', 'welcome', 'syscollector'].includes(this.$scope.tab)
       ) {
         const condition =
           (!this.changeAgent && localChange) ||
