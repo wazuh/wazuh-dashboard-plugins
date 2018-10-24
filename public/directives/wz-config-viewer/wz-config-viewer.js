307fd3189817440fae5cd544c34605fbb2a8add0
Jesús Ángel <jesus.gonzalez@wazuh.com>
2018-10-24 09:05:42 +0200

    Added wz-config-viewer directive

---
+++ public/directives/wz-config-viewer/wz-config-viewer.js
@@ -0,0 +1,39 @@
+/*
+ * Wazuh app - Wazuh config viewer directive
+ * Copyright (C) 2018 Wazuh, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Find more information about this on the LICENSE file.
+ */
+
+import template from './wz-config-viewer.html';
+import { uiModules } from 'ui/modules';
+
+const app = uiModules.get('app/wazuh', []);
+
+class WzConfigViewer {
+  constructor() {
+    this.restrict = 'E';
+    this.scope = {
+      getjson: '&',
+      getxml: '&',
+      jsoncontent: '=jsoncontent',
+      xmlcontent: '=xmlcontent'
+    };
+    this.replace = true;
+    this.template = template;
+  }
+
+  link(scope, elm, attrs) {
+    scope.callgetjson = () => {
+      scope.getjson();
+    };
+    scope.callgetxml = () => scope.getxml();
+  }
+}
+
+app.directive('wzConfigViewer', () => new WzConfigViewer());
