e8cebd377c790690b08e1199d00b9d8d814d41a0
Jesús Ángel <jesus.gonzalez@wazuh.com>
2018-11-02 19:28:31 +0100

    Exporting palette color externally

--- public/controllers/management/decoders.js
+++ public/controllers/management/decoders.js
@@ -11,38 +11,7 @@
  */
 import * as FileSaver from '../../services/file-saver';
 
-const colors = [
-  '#004A65',
-  '#00665F',
-  '#BF4B45',
-  '#BF9037',
-  '#1D8C2E',
-  'BB3ABF',
-  '#00B1F1',
-  '#00F2E2',
-  '#7F322E',
-  '#7F6025',
-  '#104C19',
-  '7C267F',
-  '#0079A5',
-  '#00A69B',
-  '#FF645C',
-  '#FFC04A',
-  '#2ACC43',
-  'F94DFF',
-  '#0082B2',
-  '#00B3A7',
-  '#401917',
-  '#403012',
-  '#2DD947',
-  '3E1340',
-  '#00668B',
-  '#008C83',
-  '#E55A53',
-  '#E5AD43',
-  '#25B23B',
-  'E045E5'
-];
+import { colors } from './colors';
 
 export class DecodersController {
   constructor($scope, $sce, errorHandler, appState, csvReq, wzTableFilter) {
