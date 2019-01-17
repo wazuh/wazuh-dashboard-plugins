2dbc1e4a64c356c3b9882efff572ae89edba7746
Jesús Ángel <jesus.gonzalez@wazuh.com>
2019-01-17 10:05:39 +0100

    Removed "rule.id" direct filter feature

--- public/directives/wz-table/lib/init.js
+++ public/directives/wz-table/lib/init.js
@@ -9,42 +9,15 @@
  *
  * Find more information about this on the LICENSE file.
  */
-import { FilterHandler } from '../../../utils/filter-handler';
+
 export async function initTable(
   $scope,
   fetch,
   wzTableFilter,
   instance,
-  errorHandler,
-  appState,
-  globalState,
-  $window
+  errorHandler
 ) {
   try {
-    if ($scope.path.includes('/rules')) {
-      try {
-        const filterHandler = new FilterHandler(appState.getCurrentPattern());
-
-        const checkGlobalFilters = () => {
-          if (!globalState.filters || !Array.isArray(globalState.filters)) {
-            globalState.filters = [];
-          }
-        };
-
-        $scope.searchRuleId = (e, ruleId) => {
-          e.stopPropagation();
-          checkGlobalFilters();
-          const ruleIdFilter = filterHandler.ruleIdQuery(ruleId);
-          if (globalState.filters.length) {
-            globalState.filters = globalState.filters.filter(
-              item => item && item.meta && item.meta.key !== 'rule.id'
-            );
-          }
-          globalState.filters.push(ruleIdFilter);
-          $window.location.href = '#/wazuh-discover';
-        };
-      } catch (error) {} // eslint-disable-line
-    }
     $scope.error = false;
     $scope.wazuh_table_loading = true;
     await fetch();
