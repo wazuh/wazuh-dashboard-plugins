f27f645bedf4f553f84490e4020884522250db17
Jesús Ángel <jesus.gonzalez@wazuh.com>
2019-01-23 14:53:49 +0100

    Allowing the same query multiple times for Dev Tools

--- public/controllers/dev-tools/dev-tools.js
+++ public/controllers/dev-tools/dev-tools.js
@@ -153,7 +153,7 @@ export class DevToolsController {
 
       let start = 0;
       let end = 0;
-
+      let starts = [];
       const slen = splitted.length;
       for (let i = 0; i < slen; i++) {
         let tmp = splitted[i].split('\n');
@@ -164,6 +164,23 @@ export class DevToolsController {
 
         if (cursor.findNext()) start = cursor.from().line;
         else return [];
+        
+        /**
+         * Prevents from user frustation when there are duplicated queries.
+         * We want to look for the next query when available, even if it 
+         * already exists but it's not the selected query.
+         */
+        if(tmp.length) {
+          // It's a safe loop since findNext method returns null if there is no next query.
+          while (this.apiInputBox.getLine(cursor.from().line) !== tmp[0] && cursor.findNext()) {
+            start = cursor.from().line;
+          }
+          // It's a safe loop since findNext method returns null if there is no next query.
+          while(starts.includes(start) && cursor.findNext()) {
+            start = cursor.from().line;
+          }
+        }
+        starts.push(start);
 
         end = start + tmp.length;
 
@@ -203,7 +220,7 @@ export class DevToolsController {
           end
         });
       }
-
+      starts = [];
       return tmpgroups;
     } catch (error) {
       return [];
@@ -433,15 +450,16 @@ export class DevToolsController {
   calculateWhichGroup(firstTime) {
     try {
       const selection = this.apiInputBox.getCursor();
-
+      console.log('SELECTION',selection)
       const desiredGroup = firstTime
         ? this.groups.filter(item => item.requestText)
         : this.groups.filter(
-            item =>
-              item.requestText &&
-              (item.end >= selection.line && item.start <= selection.line)
+            item => item.requestText &&
+              (item.end >= selection.line && item.start <= selection.line)            
           );
 
+      console.log('THIS GROUPS',this.groups)
+      console.log('DESIRED GROUP',desiredGroup)
       // Place play button at first line from the selected group
       const cords = this.apiInputBox.cursorCoords({
         line: desiredGroup[0].start,
