cd0302d6df04e09df4fd9c20603a3e8b3ae6a60a
Jesús Ángel <jesus.gonzalez@wazuh.com>
2019-01-30 18:28:08 +0100

    Fix bug with CSV download (6.6 related)

--- server/controllers/wazuh-api.js
+++ server/controllers/wazuh-api.js
@@ -800,7 +800,7 @@ export class WazuhApiCtrl {
           }
         }
 
-        return (csv).type('text/csv');
+        return reply.response(csv).type('text/csv');
       } else if (
         output &&
         output.body &&
