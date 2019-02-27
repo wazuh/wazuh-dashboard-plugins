69fd92af75a181fb156f624902559eb5913633b1
JuanCarlos <juanca.romo9@gmail.com>
2019-02-27 14:00:03 +0100

    Refactor files controller

--- public/controllers/management/files.js
+++ public/controllers/management/files.js
@@ -9,82 +9,23 @@
  *
  * Find more information about this on the LICENSE file.
  */
-import * as FileSaver from '../../services/file-saver';
 
 export class FilesController {
   constructor(
     $scope,
-    errorHandler,
-    appState,
-    csvReq,
-    wzTableFilter,
-    $location,
-    apiReq,
-    wazuhConfig,
-    rulesetHandler
+    wazuhConfig
   ) {
     this.$scope = $scope;
-    this.errorHandler = errorHandler;
-    this.appState = appState;
-    this.csvReq = csvReq;
-    this.wzTableFilter = wzTableFilter;
-    this.$location = $location;
-    this.apiReq = apiReq;
     this.wazuhConfig = wazuhConfig;
-    this.rulesetHandler = rulesetHandler;
-
     this.appliedFilters = [];
     this.searchTerm = '';
-    this.viewingDetail = false;
-    this.isArray = Array.isArray;
-    this.newKey = '';
-    this.newValue = '';
   }
 
   $onInit() {
     const configuration = this.wazuhConfig.getConfig();
     this.adminMode = !!(configuration || {}).admin;
-
-    // Reloading event listener
-    this.$scope.$on('rulesetIsReloaded', () => {
-      this.viewingDetail = false;
-      if (!this.$scope.$$phase) this.$scope.$digest();
-    });
-
-    this.$scope.$on('closeListView', () => {
-      this.closeDetailView();
-    });
-
-
-    const currentLocation = this.$location.search();
-    if ((currentLocation || {}).listname) {
-      const incomingList = this.$location.search().listname;
-      this.$location.search('listname', null);
-      this.apiReq
-        .request('get', `/cdblists/${incomingList}`, {})
-        .then(data => {
-          this.currentList = data.data.data.items[0];
-          this.$scope.$emit('setCurrentList', {
-            currentList: this.currentList
-          });
-          if (
-            !(Object.keys((this.currentList || {}).details || {}) || []).length
-          ) {
-            this.currentList.details = false;
-          }
-          this.viewingDetail = true;
-          if (!this.$scope.$$phase) this.$scope.$digest();
-        })
-        .catch(() =>
-          this.errorHandler.handle(
-            `Error fetching list: ${incomingList} from the Wazuh API`,
-            'CDB Lists'
-          )
-        );
-    }
     this.filesSubTab = 'rules';
   }
-
 
   switchFilesSubTab(tab) {
     this.filesSubTab = tab;
@@ -92,40 +33,5 @@ export class FilesController {
 
   search(term) {
     this.$scope.$broadcast('wazuhSearch', { term, removeFilters: 0 });
-  }
-
-  /**
-   * Get full data on CSV format
-   */
-  async downloadCsv() {
-    try {
-      this.errorHandler.info(
-        'Your download should begin automatically...',
-        'CSV'
-      );
-      const currentApi = JSON.parse(this.appState.getCurrentAPI()).id;
-      const output = await this.csvReq.fetch(
-        '/cdblists',
-        currentApi,
-        this.wzTableFilter.get()
-      );
-      const blob = new Blob([output], { type: 'text/csv' }); // eslint-disable-line
-
-      FileSaver.saveAs(blob, 'cdblists.csv');
-    } catch (error) {
-      this.errorHandler.handle(error, 'Download CSV');
-    }
-    return;
-  }
-
-  /**
-   * This function changes to the lists list view
-   */
-  closeDetailView() {
-
-  }
-
-  deleteFile() {
-
   }
 }
