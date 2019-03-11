5ac87d26417354dc58424d367a05b79a257931d7
Juanka Rodríguez <juancarlos.rodriguez@wazuh.com>
2019-03-11 09:35:01 +0100

    Management view UI improvements (#1290)
    * Edit files in File tab
    
    * Handle if rule or decoder ID doesnt exists
    
    * Fix array exception
    
    * Refresh related rules/decoders
    
    * Fixed decoder breadcrumb

--- public/controllers/management/files.js
+++ public/controllers/management/files.js
@@ -11,9 +11,12 @@
  */
 
 export class FilesController {
-  constructor($scope, wazuhConfig) {
+  constructor($scope, wazuhConfig, rulesetHandler, errorHandler, appState) {
     this.$scope = $scope;
     this.wazuhConfig = wazuhConfig;
+    this.rulesetHandler = rulesetHandler;
+    this.errorHandler = errorHandler;
+    this.appState = appState;
     this.appliedFilters = [];
     this.searchTerm = '';
   }
@@ -22,6 +25,65 @@ export class FilesController {
     const configuration = this.wazuhConfig.getConfig();
     this.adminMode = !!(configuration || {}).admin;
     this.filesSubTab = 'rules';
+
+    this.$scope.$on('editFile', (ev, params) => {
+      this.editFile(params);
+      this.$scope.$applyAsync();
+    });
+
+    this.$scope.closeEditingFile = () => {
+      this.$scope.editingFile = false;
+      this.$scope.fetchedXML = null;
+      this.$scope.$applyAsync();
+    };
+
+    this.$scope.doSaveConfig = () => {
+      const clusterInfo = this.appState.getClusterInfo();
+      const showRestartManager =
+        clusterInfo.status === 'enabled' ? 'cluster' : 'manager';
+      this.$scope.doingSaving = true;
+      const objParam = { showRestartManager };
+      this.$scope.currentFile.type === 'rule' ?
+        objParam.rule = this.$scope.currentFile :
+        objParam.decoder = this.$scope.currentFile;
+      this.$scope.$broadcast('saveXmlFile', objParam);
+      this.$scope.$applyAsync();
+    };
+
+    this.$scope.toggleSaveConfig = () => {
+      this.$scope.doingSaving = false;
+      this.$scope.$applyAsync();
+    };
+
+    this.$scope.toggleRestartMsg = () => {
+      this.$scope.restartMsg = false;
+      this.$scope.$applyAsync();
+    };
+
+    this.$scope.$on('showRestartMsg', () => {
+      this.$scope.restartMsg = true;
+      this.$scope.$applyAsync();
+    });
+
+    this.$scope.restart = () => {
+      this.$scope.$emit('performRestart', {});
+    };
+  }
+
+  async editFile(params) {
+    this.$scope.editingFile = true;
+    try {
+      this.$scope.currentFile = params.file;
+      this.$scope.currentFile.type = params.path.includes('rules') ? 'rule' : 'decoder';
+      this.$scope.fetchedXML = this.$scope.currentFile.type === 'rule' ?
+        await this.rulesetHandler.getRuleConfiguration(this.$scope.currentFile.file) :
+        await this.rulesetHandler.getDecoderConfiguration(this.$scope.currentFile.file);
+      this.$scope.$applyAsync();
+      this.$scope.$broadcast('fetchedFile', { data: this.$scope.fetchedXML });
+    } catch (error) {
+      this.$scope.fetchedXML = null;
+      this.errorHandler.handle(error, 'Fetch file error');
+    }
   }
 
   switchFilesSubTab(tab) {
