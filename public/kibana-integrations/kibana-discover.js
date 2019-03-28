d8e29560c97dd02b25606d77267282f05cd8465d
JuanCarlos <juanca.romo9@gmail.com>
2019-03-28 12:27:44 +0100

    Adapt kibana app for elastic 6.7.0

--- public/kibana-integrations/kibana-discover.js
+++ public/kibana-integrations/kibana-discover.js
@@ -31,8 +31,6 @@ import 'ui/doc_table/components/table_row';
 
 // Research added (further checks needed)
 import 'ui/doc_table/doc_table';
-import 'ui/styles/sidebar.less';
-import 'ui/styles/table.less';
 import 'ui/doc_viewer/doc_viewer';
 import 'ui/doc_title/doc_title';
 import 'ui/style_compile/style_compile';
