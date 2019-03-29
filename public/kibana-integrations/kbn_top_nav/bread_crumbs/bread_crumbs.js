2e0e2f745d2bc5dfb5267d02d482202ef1658203
JuanCarlos <juanca.romo9@gmail.com>
2019-03-29 08:37:56 +0100

    Prettier

--- public/kibana-integrations/kbn_top_nav/bread_crumbs/bread_crumbs.js
+++ public/kibana-integrations/kbn_top_nav/bread_crumbs/bread_crumbs.js
@@ -4,7 +4,7 @@ import uiRouter from 'ui/routes';
 
 const module = uiModules.get('kibana');
 
-module.directive('wzBreadCrumbs', function () {
+module.directive('wzBreadCrumbs', function() {
   return {
     restrict: 'E',
     scope: {
@@ -27,38 +27,35 @@ module.directive('wzBreadCrumbs', function () {
       useLinks: '='
     },
     template: breadCrumbsTemplate,
-    controller: function ($scope, config) {
-      config.watch('k7design', (val) => $scope.showPluginBreadcrumbs = !val);
+    controller: function($scope, config) {
+      config.watch('k7design', val => ($scope.showPluginBreadcrumbs = !val));
 
       function omitPagesFilter(crumb) {
-        return (
-          !$scope.omitPages ||
-          !$scope.omitPages.includes(crumb.id)
-        );
+        return !$scope.omitPages || !$scope.omitPages.includes(crumb.id);
       }
 
       function omitCurrentPageFilter(crumb) {
         return !($scope.omitCurrentPage && crumb.current);
       }
 
-      $scope.$watchMulti([
-        '[]omitPages',
-        'omitCurrentPage'
-      ], function getBreadcrumbs() {
-        $scope.breadcrumbs = (
-          uiRouter
+      $scope.$watchMulti(
+        ['[]omitPages', 'omitCurrentPage'],
+        function getBreadcrumbs() {
+          $scope.breadcrumbs = uiRouter
             .getBreadcrumbs()
             .filter(omitPagesFilter)
-            .filter(omitCurrentPageFilter)
-        );
+            .filter(omitCurrentPageFilter);
 
-        const newBreadcrumbs = $scope.breadcrumbs
-          .map(b => ({ text: b.display, href: b.href }));
+          const newBreadcrumbs = $scope.breadcrumbs.map(b => ({
+            text: b.display,
+            href: b.href
+          }));
 
-        if ($scope.pageTitle) {
-          newBreadcrumbs.push({ text: $scope.pageTitle });
+          if ($scope.pageTitle) {
+            newBreadcrumbs.push({ text: $scope.pageTitle });
+          }
         }
-      });
+      );
     }
   };
 });
