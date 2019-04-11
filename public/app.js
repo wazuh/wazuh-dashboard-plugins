bde4830eb13c3929d14df762c47faf15a8a99822
Jesús González <jesus.gonzalez@wazuh.com>
2019-04-11 13:39:17 +0200

    Yarn prettier

--- public/app.js
+++ public/app.js
@@ -34,14 +34,14 @@ import chrome from 'ui/chrome';
 import './components';
 
 // angular-charts.js
-import 'angular-chart.js'
+import 'angular-chart.js';
 
 // Set up Wazuh app
 const app = uiModules.get('app/wazuh', ['ngCookies', 'ngMaterial', 'chart.js']);
 
 app.config([
   '$compileProvider',
-  function ($compileProvider) {
+  function($compileProvider) {
     $compileProvider.aHrefSanitizationWhitelist(
       /^\s*(https?|ftp|mailto|data|blob):/
     );
@@ -50,12 +50,12 @@ app.config([
 
 app.config([
   '$httpProvider',
-  function ($httpProvider) {
+  function($httpProvider) {
     $httpProvider.useApplyAsync(true);
   }
 ]);
 
-app.run(function ($rootScope, $route, $location, appState, $window) {
+app.run(function($rootScope, $route, $location, appState, $window) {
   appState.setNavigation({ status: false });
   appState.setNavigation({
     reloaded: false,
@@ -128,17 +128,17 @@ app.run(function ($rootScope, $route, $location, appState, $window) {
                 {
                   page: chrome.addBasePath(
                     'wazuh#' +
-                    navigation.discoverPrevious +
-                    '?agent=' +
-                    $location.search().agent
+                      navigation.discoverPrevious +
+                      '?agent=' +
+                      $location.search().agent
                   )
                 },
                 '',
                 chrome.addBasePath(
                   'wazuh#' +
-                  navigation.discoverPrevious +
-                  '?agent=' +
-                  $location.search().agent
+                    navigation.discoverPrevious +
+                    '?agent=' +
+                    $location.search().agent
                 )
               );
             } else {
