003913f9eaf537b28f32e369ec0f016f2747eb31
JuanCarlos <juanca.romo9@gmail.com>
2019-05-06 17:25:18 +0200

    Dark theme completely removed

--- public/less/loader.js
+++ public/less/loader.js
@@ -13,9 +13,6 @@
 /* ------------------------ Wazuh stylesheets loader ------------------------ */
 /* -------------------------------------------------------------------------- */
 
-import chrome from 'ui/chrome';
-const IS_DARK_THEME = chrome.getUiSettingsClient().get('theme:darkMode');
-
 import './common.less';
 import './component.less';
 import './height.less';
@@ -24,6 +21,24 @@ import './media-queries.less';
 import './typography.less';
 import './ui_framework.css';
 import './jquery-ui.css';
-import './wz_theme_dark.css';
-import './bootstrap_light.less';
-import './kui_light.css';
+
+//import './dark_theme/wz_theme_dark.css';
+import chrome from 'ui/chrome';
+const IS_DARK_THEME = chrome.getUiSettingsClient().get('theme:darkMode');
+
+if (IS_DARK_THEME) {
+  var newSS = document.createElement('link');
+  newSS.rel = 'stylesheet';
+  newSS.href = '../plugins/wazuh/less/dark_theme/wz_theme_dark.css';
+  document.getElementsByTagName('head')[0].appendChild(newSS);
+
+  newSS = document.createElement('link');
+  newSS.rel = 'stylesheet';
+  newSS.href = '../plugins/wazuh/less/dark_theme/bootstrap_light.less';
+  document.getElementsByTagName('head')[0].appendChild(newSS);
+
+  newSS = document.createElement('link');
+  newSS.rel = 'stylesheet';
+  newSS.href = '../plugins/wazuh/less/dark_theme/kui_light.css';
+  document.getElementsByTagName('head')[0].appendChild(newSS);
+}
