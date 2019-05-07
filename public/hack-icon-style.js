34072373af8999c3d9f0adfac6b383f96621ab14
Juanka Rodríguez <juancarlos.rodriguez@wazuh.com>
2019-05-07 15:25:18 +0200

    Elastic 7 dark theme fix (#1430)

--- public/hack-icon-style.js
+++ public/hack-icon-style.js
@@ -1,1 +1,14 @@
-import './less/icon-style.less';
+import chrome from 'ui/chrome';
+const IS_DARK_THEME = chrome.getUiSettingsClient().get('theme:darkMode');
+
+if (IS_DARK_THEME) {
+  var newSS = document.createElement('link');
+  newSS.rel = 'stylesheet';
+  newSS.href = '../plugins/wazuh/less/icon-style-w.css';
+  document.getElementsByTagName('head')[0].appendChild(newSS);
+} else {
+  var newSS = document.createElement('link');
+  newSS.rel = 'stylesheet';
+  newSS.href = '../plugins/wazuh/less/icon-style.css';
+  document.getElementsByTagName('head')[0].appendChild(newSS);
+}
