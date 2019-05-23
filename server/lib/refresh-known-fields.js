0fe895ee5c1475253353faa0781a18401075364d
Jesús Ángel <jesus.gonzalez@wazuh.com>
2019-05-23 16:05:00 +0200

    No longer using "ansicolors" as a dependency (#1470)

--- server/lib/refresh-known-fields.js
+++ server/lib/refresh-known-fields.js
@@ -9,8 +9,7 @@
  *
  * Find more information about this on the LICENSE file.
  */
-import colors from 'ansicolors';
-const blueWazuh = colors.blue('wazuh');
+const blueWazuh = '\u001b[34mwazuh\u001b[39m';
 
 /**
  * Refresh known fields for all valid index patterns.
