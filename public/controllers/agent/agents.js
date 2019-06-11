c7faa083acc4abbe6f5601d504bdb23f926f8aa5
JuanCarlos <juanca.romo9@gmail.com>
2019-06-11 17:28:11 +0200

    Hide OS not compatible modules

--- public/controllers/agent/agents.js
+++ public/controllers/agent/agents.js
@@ -557,7 +557,7 @@ export class AgentsController {
           (((agentInfo || {}).data || {}).data || {}).status ||
           this.$scope.agent.status;
       }
-    } catch (error) {} // eslint-disable-line
+    } catch (error) { } // eslint-disable-line
 
     try {
       this.$scope.showSyscheckFiles = false;
@@ -604,7 +604,7 @@ export class AgentsController {
       if (tab === 'syscollector')
         try {
           await this.loadSyscollector(this.$scope.agent.id);
-        } catch (error) {} // eslint-disable-line
+        } catch (error) { } // eslint-disable-line
       if (tab === 'configuration') {
         this.$scope.switchConfigurationTab('welcome');
       } else {
@@ -732,7 +732,7 @@ export class AgentsController {
           {}
         );
         netifaceResponse = ((resultNetiface || {}).data || {}).data || false;
-      } catch (error) {} // eslint-disable-line
+      } catch (error) { } // eslint-disable-line
 
       // This API call may fail so we put it out of Promise.all
       let netaddrResponse = false;
@@ -744,7 +744,7 @@ export class AgentsController {
         );
         netaddrResponse =
           ((resultNetaddrResponse || {}).data || {}).data || false;
-      } catch (error) {} // eslint-disable-line
+      } catch (error) { } // eslint-disable-line
 
       // Before proceeding, syscollector data is an empty object
       this.$scope.syscollector = {};
@@ -760,7 +760,7 @@ export class AgentsController {
       this.$scope.syscollector = {
         hardware:
           typeof hardwareResponse === 'object' &&
-          Object.keys(hardwareResponse).length
+            Object.keys(hardwareResponse).length
             ? { ...hardwareResponse }
             : false,
         os:
@@ -803,7 +803,7 @@ export class AgentsController {
 
       try {
         data[0] = await this.apiReq.request('GET', `/agents/${id}`, {});
-      } catch (error) {} //eslint-disable-line
+      } catch (error) { } //eslint-disable-line
 
       try {
         data[1] = await this.apiReq.request(
@@ -811,7 +811,7 @@ export class AgentsController {
           `/syscheck/${id}/last_scan`,
           {}
         );
-      } catch (error) {} //eslint-disable-line
+      } catch (error) { } //eslint-disable-line
 
       try {
         data[2] = await this.apiReq.request(
@@ -819,7 +819,7 @@ export class AgentsController {
           `/rootcheck/${id}/last_scan`,
           {}
         );
-      } catch (error) {} //eslint-disable-line
+      } catch (error) { } //eslint-disable-line
 
       const result = data.map(item => ((item || {}).data || {}).data || false);
 
@@ -833,9 +833,13 @@ export class AgentsController {
         this.$scope.agent.isLinuxOS = this.$scope.agent.os.uname.includes(
           'Linux'
         );
+        this.$scope.agent.isWindowsOS = this.$scope.agent.os.platform === 'windows';
+        this.$scope.agent.isMacOS = this.$scope.agent.os.platform === 'darwing';
       } else {
         this.$scope.agentOS = '-';
         this.$scope.agent.isLinuxOS = false;
+        this.$scope.agent.isWindowsOS = false;
+        this.$scope.agent.isMacOS = false;
       }
 
       // Syscheck
@@ -1002,7 +1006,7 @@ export class AgentsController {
       );
       this.errorHandler.info(
         `Policy monitoring scan launched successfully on agent ${
-          this.$scope.agent.id
+        this.$scope.agent.id
         }`,
         ''
       );
