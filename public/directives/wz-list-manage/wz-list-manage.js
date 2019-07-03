1c112303a01ef15e1cc5814f344c3cb5bb9510de
Adri Valle <adri9valle@gmail.com>
2019-07-03 14:00:15 +0200

    Fix when removing unique element in the las page CDB list

--- public/directives/wz-list-manage/wz-list-manage.js
+++ public/directives/wz-list-manage/wz-list-manage.js
@@ -59,8 +59,9 @@ app.directive('wzListManage', function() {
       $scope.setPage = function(page = false) {
         if (page === 0 || page === -1) {
           $scope.firstPage();
-        } else {      
+        } else {   
           this.n = page || this.n;
+          $scope.n = this.n;
           $scope.currentPage = this.n;
           $scope.nextPage(this.n); 
         }
