551086148e967b87a6998eda08e229c05334ac11
Jose Sánchez <joseantonio.sanchez@wazuh.com>
2019-07-24 09:47:20 +0200

    Add test for the action of disabling extensions

--- test/functional/apps/overview/_welcome.js
+++ test/functional/apps/overview/_welcome.js
@@ -24,12 +24,13 @@ export default function({ getService, getPageObjects }) {
     });
 
     it('should be enabled the `Amazon AWS` extension', async () => {
-      expect(testSubjects.exists('overviewWelcomeAws')).to.be.ok();
+      expect(await testSubjects.exists('overviewWelcomeAws')).to.be.ok();
     });
 
     it('after reload the browser the `Amazon AWS` extension should be enabled', async () => {
       browser.refresh();
-      expect(testSubjects.exists('overviewWelcomeAws')).to.be.ok();
+      await PageObjects.common.sleep(1000);
+      expect(await testSubjects.exists('overviewWelcomeAws')).to.be.ok();
     });
 
     it('should be marked the switch of `OpenSCAP` extension when clicking this', async () => {
@@ -38,12 +39,13 @@ export default function({ getService, getPageObjects }) {
     });
 
     it('should be enabled the `OpenSCAP` extension', async () => {
-      expect(testSubjects.exists('overviewWelcomeOscap')).to.be.ok();
+      expect(await testSubjects.exists('overviewWelcomeOscap')).to.be.ok();
     });
 
     it('after reload the browser the `OpenSCAP` extension should be enabled', async () => {
       browser.refresh();
-      expect(testSubjects.exists('overviewWelcomeOscap')).to.be.ok();
+      await PageObjects.common.sleep(1000);
+      expect(await testSubjects.exists('overviewWelcomeOscap')).to.be.ok();
     });
 
     it('should be marked the switch of `CIS-CAT` extension when clicking this', async () => {
@@ -52,12 +54,13 @@ export default function({ getService, getPageObjects }) {
     });
 
     it('should be enabled the `CIS-CAT` extension', async () => {
-      expect(testSubjects.exists('overviewWelcomeCiscat')).to.be.ok();
+      expect(await testSubjects.exists('overviewWelcomeCiscat')).to.be.ok();
     });
 
     it('after reload the browser the `CIS-CAT` extension should be enabled', async () => {
       browser.refresh();
-      expect(testSubjects.exists('overviewWelcomeCiscat')).to.be.ok();
+      await PageObjects.common.sleep(1000);
+      expect(await testSubjects.exists('overviewWelcomeCiscat')).to.be.ok();
     });
 
     it('should be marked the switch of `VirusTotal` extension when clicking this', async () => {
@@ -66,12 +69,13 @@ export default function({ getService, getPageObjects }) {
     });
 
     it('should be enabled the `VirusTotal` extension', async () => {
-      expect(testSubjects.exists('overviewWelcomeVirustotal')).to.be.ok();
+      expect(await testSubjects.exists('overviewWelcomeVirustotal')).to.be.ok();
     });
 
     it('after reload the browser the `VirusTotal` extension should be enabled', async () => {
       browser.refresh();
-      expect(testSubjects.exists('overviewWelcomeVirustotal')).to.be.ok();
+      await PageObjects.common.sleep(1000);
+      expect(await testSubjects.exists('overviewWelcomeVirustotal')).to.be.ok();
     });
 
     it('should be marked the switch of `Osquery` extension when clicking this', async () => {
@@ -80,12 +84,13 @@ export default function({ getService, getPageObjects }) {
     });
 
     it('should be enabled the `Osquery` extension', async () => {
-      expect(testSubjects.exists('overviewWelcomeOsquery')).to.be.ok();
+      expect(await testSubjects.exists('overviewWelcomeOsquery')).to.be.ok();
     });
 
     it('after reload the browser the `Osquery` extension should be enabled', async () => {
       browser.refresh();
-      expect(testSubjects.exists('overviewWelcomeOsquery')).to.be.ok();
+      await PageObjects.common.sleep(1000);
+      expect(await testSubjects.exists('overviewWelcomeOsquery')).to.be.ok();
     });
 
     it('should be marked the switch of `Docker listener` extension when clicking this', async () => {
@@ -94,12 +99,13 @@ export default function({ getService, getPageObjects }) {
     });
 
     it('should be enabled the `Docker listener` extension', async () => {
-      expect(testSubjects.exists('overviewWelcomeDocker')).to.be.ok();
+      expect(await testSubjects.exists('overviewWelcomeDocker')).to.be.ok();
     });
 
     it('after reload the browser the `Docker listener` extension should be enabled', async () => {
       browser.refresh();
-      expect(testSubjects.exists('overviewWelcomeDocker')).to.be.ok();
+      await PageObjects.common.sleep(1000);
+      expect(await testSubjects.exists('overviewWelcomeDocker')).to.be.ok();
     });
 
     it('no extension should not be enabled when change the api', async () => {
@@ -126,6 +132,96 @@ export default function({ getService, getPageObjects }) {
       expect(true).to.be.ok();
       while(!await testSubjects.exists('overviewWelcomeDocker')){}
       expect(true).to.be.ok();
+    });
+
+    it('should be unmarked the switch of `Amazon AWS` extension when clicking this again', async () => {
+      const result = await popOver.checkedPopover('switchAws', 'eyePopoverSecurity');
+      expect(result).to.not.be.ok();
+    });
+
+    it('should be disabled the `Amazon AWS` extension', async () => {
+      expect(await testSubjects.exists('overviewWelcomeAws')).to.not.be.ok();
+    });
+
+    it('after reload the browser the `Amazon AWS` extension should be disabled', async () => {
+      browser.refresh();
+      await PageObjects.common.sleep(1000);
+      expect(await testSubjects.exists('overviewWelcomeAws')).to.not.be.ok();
+    });
+
+    it('should be unmarked the switch of `OpenSCAP` extension when clicking this again', async () => {
+      const result = await popOver.checkedPopover('switchOscap', 'eyePopoverAuditing');
+      expect(result).to.not.be.ok();
+    });
+
+    it('should be disabled the `OpenSCAP` extension', async () => {
+      expect(await testSubjects.exists('overviewWelcomeOscap')).to.not.be.ok();
+    });
+
+    it('after reload the browser the `OpenSCAP` extension should be disabled', async () => {
+      browser.refresh();
+      await PageObjects.common.sleep(1000);
+      expect(await testSubjects.exists('overviewWelcomeOscap')).to.not.be.ok();
+    });
+
+    it('should be unmarked the switch of `CIS-CAT` extension when clicking this again', async () => {
+      const result = await popOver.checkedPopover('switchCiscat', 'eyePopoverAuditing');
+      expect(result).to.not.be.ok();
+    });
+
+    it('should be disabled the `CIS-CAT` extension', async () => {
+      expect(await testSubjects.exists('overviewWelcomeCiscat')).to.not.be.ok();
+    });
+
+    it('after reload the browser the `CIS-CAT` extension should be disabled', async () => {
+      browser.refresh();
+      await PageObjects.common.sleep(1000);
+      expect(await testSubjects.exists('overviewWelcomeCiscat')).to.not.be.ok();
+    });
+
+    it('should be unmarked the switch of `VirusTotal` extension when clicking this again', async () => {
+      const result = await popOver.checkedPopover('switchVirustotal', 'eyePopoverThreat');
+      expect(result).to.not.be.ok();
+    });
+
+    it('should be disabled the `VirusTotal` extension', async () => {
+      expect(await testSubjects.exists('overviewWelcomeVirustotal')).to.not.be.ok();
+    });
+
+    it('after reload the browser the `VirusTotal` extension should be disabled', async () => {
+      browser.refresh();
+      await PageObjects.common.sleep(1000);
+      expect(await testSubjects.exists('overviewWelcomeVirustotal')).to.not.be.ok();
+    });
+
+    it('should be unmarked the switch of `Osquery` extension when clicking this again', async () => {
+      const result = await popOver.checkedPopover('switchOsquery', 'eyePopoverThreat');
+      expect(result).to.not.be.ok();
+    });
+
+    it('should be disabled the `Osquery` extension', async () => {
+      expect(await testSubjects.exists('overviewWelcomeOsquery')).to.not.be.ok();
+    });
+
+    it('after reload the browser the `Osquery` extension should be disabled', async () => {
+      browser.refresh();
+      await PageObjects.common.sleep(1000);
+      expect(await testSubjects.exists('overviewWelcomeOsquery')).to.not.be.ok();
+    });
+
+    it('should be unmarked the switch of `Docker listener` extension when clicking this again', async () => {
+      const result = await popOver.checkedPopover('switchDocker', 'eyePopoverThreat');
+      expect(result).to.not.be.ok();
+    });
+
+    it('should be disabled the `Docker listener` extension', async () => {
+      expect(await testSubjects.exists('overviewWelcomeDocker')).to.not.be.ok();
+    });
+
+    it('after reload the browser the `Docker listener` extension should be disabled', async () => {
+      browser.refresh();
+      await PageObjects.common.sleep(1000);
+      expect(await testSubjects.exists('overviewWelcomeDocker')).to.not.be.ok();
     });
 
   });
