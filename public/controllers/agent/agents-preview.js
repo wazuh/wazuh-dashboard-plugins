72b13e51b5649e3f83f26ef8b3c5d04e3fc7cd0b
Adri Valle <adri9valle@gmail.com>
2019-08-02 12:15:48 +0200

    Fix number of agents is not properly updated

--- public/controllers/agent/agents-preview.js
+++ public/controllers/agent/agents-preview.js
@@ -234,6 +234,8 @@ export class AgentsPreviewController {
     this.$scope.$broadcast('wazuhSearch', { term: this.prevSearch || '' });
   }
 
+  
+
   async refreshAgentsStats() {
     try {
       const data = await this.genericReq.request('GET', '/api/agents-unique/' + this.api, {});
