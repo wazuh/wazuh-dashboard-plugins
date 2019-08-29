254e453b6bf08665653adf8a85fd5e38c8bf4396
Jesús Ángel <jesus.gonzalez@wazuh.com>
2019-08-29 14:34:07 +0200

    Set vis tab before doing any other thing

--- public/controllers/overview/overview.js
+++ public/controllers/overview/overview.js
@@ -259,6 +259,7 @@ export class OverviewController {
 
   // Switch tab
   async switchTab(newTab, force = false) {
+    this.tabVisualizations.setTab(newTab);
     this.$rootScope.rendered = false;
     this.$rootScope.$applyAsync();
     this.falseAllExpand();
@@ -301,8 +302,6 @@ export class OverviewController {
 
       if (this.tabHistory.length > 2)
         this.tabHistory = this.tabHistory.slice(-2);
-
-      this.tabVisualizations.setTab(newTab);
 
       if (this.tab === newTab && !force) return;
       
