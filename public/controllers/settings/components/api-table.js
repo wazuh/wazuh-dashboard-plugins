f7f5a23c3e3d678cb9fc64b504b596f5bb3af1fe
Jesús Ángel <jesus.gonzalez@wazuh.com>
2019-09-05 17:46:45 +0200

    Simplified API entries table in Settings

--- public/controllers/settings/components/api-table.js
+++ public/controllers/settings/components/api-table.js
@@ -12,21 +12,12 @@
  */
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { RIGHT_ALIGNMENT } from '@elastic/eui/lib/services';
 import {
   EuiFlexGroup,
   EuiFlexItem,
   EuiBasicTable,
-  EuiPanel,
   EuiButtonIcon,
   EuiToolTip,
-  EuiFormRow,
-  EuiFieldText,
-  EuiFieldPassword,
-  EuiFieldNumber,
-  EuiButton,
-  EuiSpacer,
-  EuiButtonEmpty
 } from '@elastic/eui';
 
 export class ApiTable extends Component {
@@ -51,75 +42,6 @@ export class ApiTable extends Component {
     });
   }
 
-  onChangeEdit(e, field) {
-    this.setState({
-      [field]: e.target.value
-    });
-  }
-
-  toggleDetails(item) {
-    const itemIdToExpandedRowMap = { ...this.state.itemIdToExpandedRowMap };
-    if (itemIdToExpandedRowMap[item._id]) {
-      delete itemIdToExpandedRowMap[item._id];
-    } else {
-      itemIdToExpandedRowMap[item._id] = (
-        <EuiFlexGroup>
-          <EuiFlexItem>
-            <EuiFormRow label="Username">
-              <EuiFieldText
-                onChange={e => this.onChangeEdit(e, 'user')}
-                placeholder="foo"
-              />
-            </EuiFormRow>
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <EuiFormRow label="Password">
-              <EuiFieldPassword
-                onChange={e => this.onChangeEdit(e, 'password')}
-                placeholder="bar"
-              />
-            </EuiFormRow>
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <EuiFormRow label="Host">
-              <EuiFieldText
-                onChange={e => this.onChangeEdit(e, 'url')}
-                placeholder="http://localhost"
-              />
-            </EuiFormRow>
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <EuiFormRow label="Port">
-              <EuiFieldNumber
-                max={99999}
-                onChange={e => this.onChangeEdit(e, 'port')}
-                placeholder={55000}
-              />
-            </EuiFormRow>
-          </EuiFlexItem>
-
-          <EuiFlexItem grow={false}>
-            <EuiFormRow label="Actions">
-              <EuiButton
-                aria-label="Update"
-                iconType="save"
-                color="primary"
-                onClick={() =>
-                  this.props
-                    .updateSettings({ ...this.state, _id: item._id }, true)
-                    .then(result => result !== -1 && this.toggleDetails(item))
-                }
-              >
-                Save
-              </EuiButton>
-            </EuiFormRow>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-      );
-    }
-    this.setState({ itemIdToExpandedRowMap });
-  }
-
   UNSAFE_componentWillReceiveProps(nextProps) {
     this.setState({
       apiEntries: nextProps.apiEntries,
@@ -128,7 +50,6 @@ export class ApiTable extends Component {
   }
 
   render() {
-    const { itemIdToExpandedRowMap } = this.state;
     const items = [...this.state.apiEntries];
     const columns = [
       {
@@ -188,71 +109,19 @@ export class ApiTable extends Component {
                 />
               </EuiToolTip>
             </EuiFlexItem>
-            <EuiFlexItem grow={false}>
-              <EuiToolTip position="bottom" content={<p>Remove</p>}>
-                <EuiButtonIcon
-                  aria-label="Remove manager"
-                  iconType="trash"
-                  onClick={() =>
-                    this.props.removeManager(item).then(apiEntries =>
-                      this.setState({
-                        apiEntries
-                      })
-                    )
-                  }
-                  color="danger"
-                />
-              </EuiToolTip>
-            </EuiFlexItem>
           </EuiFlexGroup>
-        )
-      },
-
-      {
-        align: RIGHT_ALIGNMENT,
-        width: '40px',
-        isExpander: true,
-        render: item => (
-          <EuiToolTip position="bottom" content={<p>Edit</p>}>
-            <EuiButtonIcon
-              onClick={() => this.toggleDetails(item)}
-              aria-label={
-                itemIdToExpandedRowMap[item.id]
-                  ? 'Collapse edition'
-                  : 'Expand edition'
-              }
-              iconType={
-                itemIdToExpandedRowMap[item.id] ? 'arrowUp' : 'arrowDown'
-              }
-            />
-          </EuiToolTip>
         )
       }
     ];
     return (
-      <EuiPanel>
+
+
         <EuiBasicTable
           itemId="_id"
           items={items}
           columns={columns}
-          itemIdToExpandedRowMap={this.state.itemIdToExpandedRowMap}
-          isExpandable={true}
         />
-        <EuiSpacer size="m" />
-        <EuiFlexGroup>
-          <EuiFlexItem />
-          <EuiFlexItem grow={false}>
-            <EuiButtonEmpty
-              aria-label="Add"
-              iconType="plusInCircle"
-              onClick={() => this.props.switch()}
-            >
-              Add new
-            </EuiButtonEmpty>
-          </EuiFlexItem>
-          <EuiFlexItem />
-        </EuiFlexGroup>
-      </EuiPanel>
+
     );
   }
 }
