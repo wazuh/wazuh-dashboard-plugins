c3d65c43aa3c74168b28bf43d4bf309d218cdc0e
Jose Sanchez <josesanxez@gmail.com>
2019-09-12 12:01:35 +0200

    Create two new services to test pie charts

--- test/functional/services/index.js
+++ test/functional/services/index.js
@@ -1,9 +1,13 @@
 import { ExtensionPopoverProvider } from './extension_popover';
 import { AreaChartProvider } from './area_chart';
+import { PieChartsProvider } from './pie_chart';
 import { EsAreaChartProvider } from './es_area_chart';
+import { EsPieChartProvider } from './es_pie_chart';
 
 export const services = {
   popOver: ExtensionPopoverProvider,
   areaChart: AreaChartProvider,
+  pieCharts: PieChartsProvider,
   esAreaChart: EsAreaChartProvider,
+  esPieChart: EsPieChartProvider,
 }
