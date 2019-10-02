9f7fd5b53b03dd66b1a8967bdc4008635f23b701
Adri Valle <adri9valle@gmail.com>
2019-10-02 11:16:57 +0200

    Allow unsigned certs

--- server/lib/api-helper.js
+++ server/lib/api-helper.js
@@ -20,7 +20,7 @@ export function buildOptionsObject(api) {
     },
     username: api.user,
     password: api.password,
-    rejectUnauthorized: api.url.includes('https://')
+    rejectUnauthorized: false
   };
 }
 
