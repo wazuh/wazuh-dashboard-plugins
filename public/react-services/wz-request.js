d20a8157a31db5138468fc36b2217dce3ae0fb60
Adri Valle <adri9valle@gmail.com>
2019-10-15 11:26:00 +0200

    Expand the capability of WzRequest

--- public/react-services/wz-request.js
+++ public/react-services/wz-request.js
@@ -13,33 +13,29 @@ import axios from 'axios';
 import chrome from 'ui/chrome';
 
 export class WzRequest {
-  /**
-   * Perform an API request
-   * @param {String} method Eg. GET, PUT, POST, DELETE
-   * @param {String} path API route
-   * @param {Object} body Request body
+    /**
+   * Permorn a generic request
+   * @param {String} method 
+   * @param {String} path 
+   * @param {Object} payload 
    */
-  static async request(method, path, body) {
+  static async genericReq(method, path, payload = null) {
     try {
-      if (!method || !path || !body) {
+      if (!method || !path) {
         throw new Error('Missing parameters');
       }
-      const url = chrome.addBasePath('/api/request');
-      const id = this.getCookie('API').id;
-      const requestData = { method, path, body, id };
+      const url = chrome.addBasePath(path);
       const options = {
-        method: 'POST',
+        method: method,
         headers: { 'Content-Type': 'application/json', 'kbn-xsrf': 'kibana' },
-        data: requestData,
         url: url,
+        data: payload,
         timeout: 20000
       };
-      
       const data = await axios(options);
       if (data.error) {
         throw new Error(data.error);
       }
-      
       return Promise.resolve(data);
     } catch (error) {
       return ((error || {}).data || {}).message || false
@@ -49,7 +45,50 @@ export class WzRequest {
   }
 
   /**
-   * Gets the current API in order to get the credentials from it
+   * Perform a request to the Wazuh API
+   * @param {String} method Eg. GET, PUT, POST, DELETE
+   * @param {String} path API route
+   * @param {Object} body Request body
+   */
+  static async apiReq(method, path, body) {
+    try {
+      if (!method || !path || !body) {
+        throw new Error('Missing parameters');
+      }
+      const id = this.getCookie('API').id;
+      const requestData = { method, path, body, id };
+      const data = await this.genericReq('POST', '/api/request', requestData);
+      return Promise.resolve(data);
+    } catch (error) {
+      return ((error || {}).data || {}).message || false
+        ? Promise.reject(error.data.message)
+        : Promise.reject(error.message || error);
+    }
+  }
+
+  /**
+   * Perform a request to generate a CSV
+   * @param {String} path 
+   * @param {Object} filters 
+   */
+  static async csvReq(path, filters){
+    try {
+      if (!path || !filters) {
+        throw new Error('Missing parameters');
+      }
+      const id = this.getCookie('API').id;
+      const requestData = { path, id, filters };
+      const data = await this.genericReq('POST', '/api/csv', requestData);
+      return Promise.resolve(data);
+    } catch (error) {
+      return ((error || {}).data || {}).message || false
+        ? Promise.reject(error.data.message)
+        : Promise.reject(error.message || error);
+    }
+  }
+
+  /**
+   * Gets the value from a cookie
    * @param {String} cookieName
    */
   static getCookie(cookieName) {
@@ -61,7 +100,7 @@ export class WzRequest {
         const decode = decodeURIComponent(cookie);
         try {
           return JSON.parse(JSON.parse(decode));
-        } catch(error) {
+        } catch (error) {
           return decode;
         }
       } else {
