9497702e798ac6ec7a4edb8fbebca1da514aecdc
Jose Sanchez <josesanxez@gmail.com>
2019-10-21 12:18:58 +0200

    Rebuild the agentTable component

--- public/controllers/agent/components/agents-table.js
+++ public/controllers/agent/components/agents-table.js
@@ -38,169 +38,96 @@ import {
 } from '@elastic/eui';
 import { WzFilterBar } from '../../../components/wz-filter-bar/wz-filter-bar'
 
-class WzInMemoryTable extends EuiInMemoryTable {
-
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidUpdate() {
-    if ((this.state || {}).selectPage !== undefined){
-      if ((this.state || {}).selectPage !== (this.state || {}).pageIndex){
-        this.setState({ pageIndex: this.state.selectPage });        
-      }
-    }
-  }
-
-  getItems() {
-    const result = super.getItems()
-    result.totalItemCount = this.props.totalitems
-    return result;
-  }
-
-
-  onTableChange = ({ page = {}, sort = {} }) => {
-
-    const { index: pageIndex, size: pageSize } = page;
-
-    let { field: sortName, direction: sortDirection } = sort;
-    let reportedSortName = sortName;
-
-    if (
-      this.state.allowNeutralSort &&
-      this.state.sortName === sortName &&
-      this.state.sortDirection === 'desc' &&
-      sortDirection === 'asc'
-    ) {
-      sortName = '';
-      reportedSortName = '';
-      sortDirection = '';
-    }
-
-    if (this.props.onTableChange) {
-      this.props.onTableChange({
-        page,
-        sort: {
-          field: reportedSortName,
-          direction: sortDirection,
-        },
-      });
-    }
-    if(this.props.items.length < this.state.pageSize){
-      this.setState({pageIndex: 0});
-    }
-    this.setState({
-      selectPage: page.index,
-      pageIndex,
-      pageSize,
-      sortName,
-      sortDirection,
-    });
-  };
-
-
-
-  renderSearchBar() {
-    const { search } = this.props;
-    if (search) {
-      const {
-        onChange, // eslint-disable-line no-unused-vars
-        ...searchBarProps
-      } = search;
-
-      if (searchBarProps.box && searchBarProps.box.schema === true) {
-        searchBarProps.box.schema = this.resolveSearchSchema();
-      }
-      this.wzFilterProps = { 
-        model: [
-          {
-            label: 'Status',
-            options: [
-              {
-                label: 'Active',
-                group: 'status'
-              },
-              {
-                label: 'Disconnected',
-                group: 'status'
-              },
-              {
-                label: 'Never connected',
-                group: 'status'
-              }
-            ]
-          },
-        ],
-        clickAction: this.props.clickAction
-      }
-
-      return <WzFilterBar {...this.wzFilterProps} />;
-    }
-  }
-  
-}
-
-
 export class AgentsTable extends Component {
 
   constructor(props) {
     super(props);
-    this.onTableChange = this.onTableChange.bind(this);
-    this.onQueryChange = this.onQueryChange.bind(this);
     this.state = {
-      "pageIndex": 0,
-      "previusIndex": 0,
-      "previusSort": "",
-      "previusSortDirection": "",
-      "previusQ": "",
-      "totalItems": 0,
-      "isLoading": false
+      agents: [],
+      pageIndex: 0,
+      pageSize: 10,
+      sortField: 'id',
+      sortDirection: 'asc',
+      isProcessing: true,
+      totalItems: 0,
+      q: '',
+      search: '',
     }
   }
-  
+
+  onTableChange = ({ page = {}, sort = {} }) => {
+    const { index: pageIndex, size: pageSize } = page;
+    const { field: sortField, direction: sortDirection } = sort;
+    this.setState({
+      pageIndex,
+      pageSize,
+      sortField,
+      sortDirection,
+      isProcessing: true,
+    });
+  };
+
+  onQueryChange = ({q={}, search={}}) => {
+    this.setState({
+      q,
+      search,
+      isProcessing: true,
+    });
+  };
 
   async componentDidMount() {
-    await this.getAgents();
-    this.setState({isLoading: false});
-
+    await this.getItems();
   }
-
   async componentDidUpdate() {
-    if(this.isFilterChange()){
-      await this.getAgents();
-      await this.getAgents();
-      this.setState({previusIndex: this.state.pageIndex});
-      this.setState({previusSort: this.state.sortField});
-      this.setState({previusSortDirection: this.state.sortDirection});
-      this.setState({previusQ: this.state.q});
-      this.setState({isLoading: false});
+    if (this.state.isProcessing) {
+      await this.getItems();
     }
   }
 
-  isFilterChange () {
-    const pageIndexChange = this.state.pageIndex !== this.state.previusIndex;
-    const sortChange = this.state.sortField !== this.state.previusSort 
-      || this.state.sortDirection !== this.state.previusSortDirection;
-    const qChange = this.state.q !== this.state.previusQ;
-    
-    return (pageIndexChange || sortChange || qChange);
-  }
 
-  async getAgents() {
+  async getItems() {
     const rawAgents = await this.props.wzReq(
       'GET',
       '/agents',
-      this.createFilter()
+      this.buildFilter()
     );
-    const agentsWithoutMaster = (((rawAgents || {}).data || {}).data || {}).items.filter(
-      (agent) => { return (agent.id !== '000'); }
-    )
-    const formatedAgents = agentsWithoutMaster.map(this.formatAgent);
 
-    this.setState({totalItems: (((rawAgents || {}).data || {}).data || {}).totalItems - 1});
+    const formatedAgents = (((rawAgents || {}).data || {}).data || {}).items.map(this.formatAgent);
+    this.setState({
+      agents: formatedAgents,
+      totalItems: (((rawAgents || {}).data || {}).data || {}).totalItems - 1,
+      isProcessing: false,
+    });
+  }
 
-    const {beforeEmptyAgents, afterEmptyAgents} = this.generateEmpties(formatedAgents.length);
-    this.setState({agents: [...beforeEmptyAgents, ...formatedAgents, ...afterEmptyAgents] });
+  buildFilter() {
+    const { pageIndex, pageSize, search } = this.state;
+
+    const filter = {
+      offset: pageIndex * pageSize,
+      limit: pageSize,
+      q: this.buildQFilter(),
+      sort: this.buildSortFilter(),
+    };
+
+    if (search !== '') {
+      filter.search = search;
+    }
+    return filter;
+  }
+
+  buildSortFilter() {
+    const {sortField, sortDirection} = this.state;
+
+    const field = (sortField === 'os_name') ? '' : sortField;
+    const direction = (sortDirection === 'asc') ? '+' : '-';
+    
+    return direction+field;
+  }
+
+  buildQFilter() {
+    const { q } = this.state;
+    return (q === '') ? `id!=000` :`id!=000;${q}`;
   }
 
   formatAgent(agent) {
@@ -219,59 +146,15 @@ export class AgentsTable extends Component {
       "actions": agent.id
     }
   }
-
-  generateEmpties(nAgents) {
-    if(this.nAgents < this.state.pageSize){
-      return{beforeEmptyAgents: [], afterEmptyAgents: []};
-    }
-    const beforeEmptyAgents = ((this.state || {}).pageIndex)
-      ? [...Array(this.state.pageIndex * this.state.pageSize).keys()]
-      : [];
-
-    const afterElements = this.state.totalItems - beforeEmptyAgents.length - nAgents;
-    const afterEmptyAgents = ((this.state || {}).pageIndex)
-      ? [...Array((afterElements >= 0) ? afterElements : 0).keys()]
-      : [...Array(nAgents > 0 ? this.state.totalItems - nAgents: 0).keys()];
-    return {beforeEmptyAgents, afterEmptyAgents};
-  }
-
-  createFilter() {
-    const offset = ((this.state || {}).pageIndex !== undefined) ? (this.state || {}).pageIndex : 0;
-    const limit = ((this.state || {}).pageSize !== undefined) ? (this.state || {}).pageSize : 10;
-    const filter = {
-      "offset": (offset * limit),
-      "limit": limit + 1
-    };
-    this.addQFilter(filter);
-    this.addSortFilter(filter);
-    return filter;
-  }
-
-  addSortFilter(filter) {
-    const sortFieldExists = (this.state || {}).sortField !== undefined;
-    const sortFieldNotEmpty = (this.state || {}).sortField !== "";
-    if (sortFieldExists && sortFieldNotEmpty) {
-      const fields = this.columns().filter((field) => {
-        return (field.field === this.state.sortField || field.name === this.state.sortField);
-      })
-      if(fields.length < 0) {
-        return;
-      }
-      const direction = (this.state || {}).sortDirection === 'asc' ? '+' : '-';
-      if (fields[0].field === 'os_name') {
-        filter.sort = direction + 'os.name,os.version';            
-      } else {
-        filter.sort = direction + fields[0].field;
-      }
-    }
-  }
-
-  addQFilter(filter) {
-    const qFieldExists = (this.state || {}).q !== undefined;
-    const qFieldNotEmpty = (this.state || {}).q !== "";
-    if (qFieldExists && qFieldNotEmpty) {
-      filter.q = this.state.q;
-    }
+  
+  formattedButton() {
+    return (
+      <EuiFlexItem grow={false}>
+        <EuiButtonEmpty iconType="importAction" onClick={() => this.refresh()}>
+          Formatted          
+        </EuiButtonEmpty>
+      </EuiFlexItem>
+    );
   }
 
 
@@ -330,115 +213,102 @@ export class AgentsTable extends Component {
     ];
   }
 
-  actions(item) {
-    const style = {
-      marginRight: 5
-    }
-    if(item){
-      return (
-        <div>
-          <EuiIcon type="discoverApp" style={style} />
-          <EuiIcon type="wrench" />
-        </div>
-      );
-    } else {
-      return (
-      <div>
-        <EuiIcon type="wrench" style={{fillOpacity: 0}} />
-      </div>
-      );
-    }
-  };
-
-  formattedButton() {
+  title() {
+    const formattedButton = this.formattedButton()
     return (
-      <EuiFlexItem grow={false}>
-        <EuiButtonEmpty iconType="importAction" onClick={() => this.refresh()}>
-          Formatted          
-        </EuiButtonEmpty>
-      </EuiFlexItem>
+      <EuiFlexGroup>
+        <EuiFlexItem>
+          <EuiFlexGroup>
+            <EuiFlexItem>
+              <EuiTitle>
+                <h2>Agents</h2>
+              </EuiTitle>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiFlexItem>
+        {formattedButton}
+        <EuiFlexItem grow={false}>
+          <EuiButtonEmpty iconType="plusInCircle" onClick={() => this.refresh()}>
+            Add new agent
+          </EuiButtonEmpty>
+        </EuiFlexItem>
+      </EuiFlexGroup>
     );
   }
-  onTableChange ({page = {}, sort = {}})  {
-    this.setState({
-      pageSize: page.size,
-      pageIndex: page.index,
-      isLoading:true,
-      sortField: sort.field,
-      sortDirection: sort.direction
-    })
-  }
 
-  onQueryChange ({q, search}) {
-    if ((this.state || {}).q !== q) {
-      this.setState({q: q});
-    }
-
-    if((this.state || {}).search !== search) {
-      this.setState({search: search});
-    }
-  }
-
-  
-  render() {
-
-    const search = {
-      box: {
-        incremental: false,
-        schema: {
-          "strict": true,
-          "fields":{
-            "status": {
-              "type":"string"
-            }
+  filterBar() {
+    const model = [
+      {
+        label: 'Status',
+        options: [
+          {
+            label: 'Active',
+            group: 'status'
+          },
+          {
+            label: 'Disconnected',
+            group: 'status'
+          },
+          {
+            label: 'Never connected',
+            group: 'status'
           }
-        },
-      }
-    };
-
-    const pagination = {
-      pageIndex: this.state.pageIndex,
-      pageSize: 10,
-      totalItemCount: this.state.totalItems,
-      hidePerPageOptions: true
-    }
+        ]
+      },
+    ];
 
     return (
-      
+      <EuiFlexGroup>
+        <EuiFlexItem>
+          <WzFilterBar
+            model={model}
+            clickAction={this.onQueryChange}
+          />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    );
+  }
+
+  table() {
+    const {pageIndex, pageSize, totalItems, agents, sortField, sortDirection} = this.state
+    const columns = this.columns();
+    const pagination = {
+      pageIndex: pageIndex,
+      pageSize: pageSize,
+      totalItemCount: totalItems,
+      hidePerPageOptions: true,
+    }
+    const sorting = {
+      sort: {
+        field: sortField,
+        direction: sortDirection,
+      },
+    };
+    return (
+      <EuiFlexGroup>
+        <EuiFlexItem>
+          <EuiBasicTable
+            items={agents}
+            columns={columns}
+            pagination={pagination}
+            onChange={this.onTableChange}
+            sorting={sorting}
+          />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    )
+  }
+
+  render() {
+    const title = this.title();
+    const filter = this.filterBar();
+    const table = this.table();
+
+    return (
       <EuiPanel paddingSize="l">
-        <EuiFlexGroup>
-          <EuiFlexItem>
-            <EuiFlexGroup>
-              <EuiFlexItem>
-                <EuiTitle>
-                  <h2>Agents</h2>
-                </EuiTitle>
-              </EuiFlexItem>
-            </EuiFlexGroup>
-          </EuiFlexItem>
-          <this.formattedButton />
-          <EuiFlexItem grow={false}>
-            <EuiButtonEmpty iconType="plusInCircle" onClick={() => this.refresh()}>
-              Add new agent
-            </EuiButtonEmpty>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-        <EuiFlexGroup>
-          <EuiFlexItem>
-            <WzInMemoryTable
-              itemId="id"
-              items={(this.state || {}).agents}
-              loading={this.state.isLoading}
-              columns={this.columns()}
-              sorting={true}
-              search={search}
-              pagination={pagination}
-              totalitems={this.state.totalItems}
-              onTableChange={this.onTableChange}
-              clickAction={this.onQueryChange}
-            />
-          </EuiFlexItem>
-        </EuiFlexGroup>
+        {title}
+        {filter}
+        {table}
       </EuiPanel>
     );
   }
@@ -447,7 +317,3 @@ export class AgentsTable extends Component {
 AgentsTable.propTypes = {
   wzReq: PropTypes.func
 };
-
-
-
-
