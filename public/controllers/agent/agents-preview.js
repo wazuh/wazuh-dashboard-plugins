ceec2eaab30f4d2c9b233a2456fe62665a689547
Jose Sanchez Robles <josesanxez@gmail.com>
2019-11-13 12:16:54 +0100

    Migrate the agents table to react component (#1886)
    * Add the new component `AgentsTable`
    
    * Prettier
    
    * Remove console log
    
    * Bump version
    
    * Fix API call
    
    * Add arial-label
    
    * Fix when checking admin mode
    
    * Fix ruleset open files
    
    * Fix the hidden content in management>configuration
    
    * Insert the controller's data in the component
    
    * Improvement the designed of the component
    
    * Move the logic to the agents-table component
    
    * Fix the pageIndex of the table
    
    * Add loading animation to the table
    
    * New Filter bar as React component
    
    * Fix the sort problem
    
    * Remove the agent 000 and improvement the code
    
    * Refactor component
    
    * Remove the old table
    
    * Integrate the WzFilterBar component
    
    * Fix infinite loop error
    
    * Rebuild the agentTable component
    
    * Improvement the wzFilterBar
    
    * Add the filters to the agentsTable component
    
    * Add more filters and remove style
    
    * Improvement the style of the WzFilterBar
    
    * Delete tags from the wzFilterBar options
    
    * Remove deprectaed code
    
    * Add downloadCsv function
    
    * Add the action buttons to the agent table
    
    * Update branch for 7.4.2
    
    * Update README.md
    
    * Apply UX/UI changes
    
    * Add `pageSize` option
    
    * Add persistance to the filters
    
    * Update de TimeServiceProvider with the moment lib
    
    * Add timezone format
    
    * Quick fixes
    
    * Update package.json
    
    * Add message when no agents found

--- public/controllers/agent/agents-preview.js
+++ public/controllers/agent/agents-preview.js
@@ -10,8 +10,10 @@
  * Find more information about this on the LICENSE file.
  */
 import * as FileSaver from '../../services/file-saver';
+import { DataFactory } from '../../services/data-factory';
 import { timefilter } from 'ui/timefilter';
 import { version } from '../../../package.json';
+import { clickAction } from '../../directives/wz-table/lib/click-action';
 
 export class AgentsPreviewController {
   /**
@@ -103,8 +105,33 @@ export class AgentsPreviewController {
       getCurrentApiAddress: () => this.getCurrentApiAddress(),
       needsPassword: () => this.needsPassword()
     };
+
     this.hasAgents = true;
     this.init = false;
+    const instance = new DataFactory(
+      this.apiReq,
+      '/agents',
+      false,
+      false
+    )
+    //Props
+    this.tableAgentsProps = {
+      wzReq: (method, path, body) => this.apiReq.request(method, path, body),
+      addingNewAgent: () => this.addNewAgent(true),
+      downloadCsv: (filters = []) => this.downloadCsv(filters),
+      clickAction: (item, openAction= false) => {
+        clickAction(
+          item,
+          openAction,
+          instance,
+          this.shareAgent,
+          this.$location,
+          this.$scope,
+          this.appState
+        )
+      },
+      timeService: (date) => this.timeService.offset(date),
+    } 
     //Load
     this.load();
   }
@@ -131,7 +158,7 @@ export class AgentsPreviewController {
   /**
    * Exports the table in CSV format
    */
-  async downloadCsv() {
+  async downloadCsv(filters) {
     try {
       this.errorHandler.info(
         'Your download should begin automatically...',
@@ -140,7 +167,7 @@ export class AgentsPreviewController {
       const output = await this.csvReq.fetch(
         '/agents',
         this.api,
-        this.wzTableFilter.get()
+        filters
       );
       const blob = new Blob([output], { type: 'text/csv' }); // eslint-disable-line
 
@@ -180,55 +207,7 @@ export class AgentsPreviewController {
       const [agentsUnique, agentsTop] = data;
       const unique = agentsUnique.data.result;
 
-      this.searchBarModel = {
-        name: [],
-        status: ['Active', 'Disconnected', 'Never connected'],
-        group: unique.groups.sort((a, b) => {
-          return a.toString().localeCompare(b.toString());
-        }),
-        version: unique.versions.sort((a, b) => {
-          return a.toString().localeCompare(b.toString(), undefined, {
-            numeric: true,
-            sensitivity: 'base'
-          });
-        }),
-        'os.platform': unique.osPlatforms
-          .map(x => x.platform)
-          .sort((a, b) => {
-            return a.toString().localeCompare(b.toString());
-          }),
-        'os.version': unique.osPlatforms
-          .map(x => x.version)
-          .sort((a, b) => {
-            return a.toString().localeCompare(b.toString(), undefined, {
-              numeric: true,
-              sensitivity: 'base'
-            });
-          }),
-        'os.name': unique.osPlatforms
-          .map(x => x.name)
-          .sort((a, b) => {
-            return a.toString().localeCompare(b.toString());
-          })
-      };
 
-      if (clusterInfo.status === 'enabled' && unique.nodes) {
-        this.searchBarModel.node_name = unique.nodes;
-      }
-
-      this.searchBarModel['os.name'] = Array.from(
-        new Set(this.searchBarModel['os.name'])
-      );
-      this.searchBarModel['os.version'] = Array.from(
-        new Set(this.searchBarModel['os.version'])
-      );
-      this.searchBarModel['os.platform'] = Array.from(
-        new Set(this.searchBarModel['os.platform'])
-      );
-      this.groups = unique.groups;
-      this.nodes = unique.nodes.map(item => ({ id: item }));
-      this.versions = unique.versions.map(item => ({ id: item }));
-      this.osPlatforms = unique.osPlatforms;
       this.lastAgent = unique.lastAgent;
       this.summary = unique.summary;
       if (!this.lastAgent || !this.lastAgent.id) {
@@ -260,6 +239,7 @@ export class AgentsPreviewController {
     }
     this.loading = false;
     this.$scope.$applyAsync();
+    
     return;
   }
 
