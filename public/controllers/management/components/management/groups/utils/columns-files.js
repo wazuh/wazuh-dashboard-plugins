e974526c9ae76685888089460dd7ef11f34d330c
Alberto Andújar <josealbertoandujar@gmail.com>
2019-11-29 15:59:45 +0100

    Add autoFormat to edit XML

--- public/controllers/management/components/management/groups/utils/columns-files.js
+++ public/controllers/management/components/management/groups/utils/columns-files.js
@@ -44,9 +44,16 @@ export default class GroupsFilesColumns {
                       );
 
                       const isEditable = item.filename === 'agent.conf';
-                      const data = !isEditable ? beautifier.prettyPrint(result) : result;
+                      const data = !isEditable
+                        ? beautifier.prettyPrint(result)
+                        : this.autoFormat(result);
 
-                      const file = { name: item.filename, content: data, isEditable: isEditable, groupName: itemDetail.name };
+                      const file = {
+                        name: item.filename,
+                        content: data,
+                        isEditable: isEditable,
+                        groupName: itemDetail.name,
+                      };
                       this.tableProps.updateFileContent(file);
                     }}
                     color="primary"
@@ -61,4 +68,61 @@ export default class GroupsFilesColumns {
 
     this.buildColumns();
   }
+
+  autoFormat = xml => {
+    var reg = /(>)\s*(<)(\/*)/g;
+    var wsexp = / *(.*) +\n/g;
+    var contexp = /(<.+>)(.+\n)/g;
+    xml = xml
+      .replace(reg, '$1\n$2$3')
+      .replace(wsexp, '$1\n')
+      .replace(contexp, '$1\n$2');
+    var formatted = '';
+    var lines = xml.split('\n');
+    var indent = 0;
+    var lastType = 'other';
+    var transitions = {
+      'single->single': 0,
+      'single->closing': -1,
+      'single->opening': 0,
+      'single->other': 0,
+      'closing->single': 0,
+      'closing->closing': -1,
+      'closing->opening': 0,
+      'closing->other': 0,
+      'opening->single': 1,
+      'opening->closing': 0,
+      'opening->opening': 1,
+      'opening->other': 1,
+      'other->single': 0,
+      'other->closing': -1,
+      'other->opening': 0,
+      'other->other': 0,
+    };
+
+    for (var i = 0; i < lines.length; i++) {
+      var ln = lines[i];
+      if (ln.match(/\s*<\?xml/)) {
+        formatted += ln + '\n';
+        continue;
+      }
+      var single = Boolean(ln.match(/<.+\/>/)); // is this line a single tag? ex. <br />
+      var closing = Boolean(ln.match(/<\/.+>/)); // is this a closing tag? ex. </a>
+      var opening = Boolean(ln.match(/<[^!].*>/)); // is this even a tag (that's not <!something>)
+      var type = single ? 'single' : closing ? 'closing' : opening ? 'opening' : 'other';
+      var fromTo = lastType + '->' + type;
+      lastType = type;
+      var padding = '';
+
+      indent += transitions[fromTo];
+      for (var j = 0; j < indent; j++) {
+        padding += '\t';
+      }
+      if (fromTo == 'opening->closing')
+        formatted = formatted.substr(0, formatted.length - 1) + ln + '\n';
+      // substr removes line break (\n) from prev loop
+      else formatted += padding + ln + '\n';
+    }
+    return formatted.trim();
+  };
 }
