4136b2d5cc6d76b00baba4217b7e737020c6c78f
Alberto Andújar <josealbertoandujar@gmail.com>
2019-12-10 08:48:13 +0100

    Code cleaning for Groups-overview

--- public/controllers/management/components/management/groups/groups-overview.js
+++ public/controllers/management/components/management/groups/groups-overview.js
@@ -13,8 +13,6 @@
 import React, { Component } from 'react';
 import { EuiFlexItem, EuiFlexGroup, EuiPanel, EuiTitle, EuiText, EuiPage } from '@elastic/eui';
 
-import { connect } from 'react-redux';
-
 // Wazuh components
 import WzGroupsTable from './groups-table';
 import WzGroupsActionButtons from './actions-buttons-main';
@@ -23,45 +21,6 @@ export class WzGroupsOverview extends Component {
   _isMounted = false;
   constructor(props) {
     super(props);
-
-    this.state = {
-      items: this.props.items,
-      originalItems: this.props.items,
-      pageIndex: 0,
-      pageSize: 10,
-      showPerPageOptions: true,
-      showConfirm: false,
-      newGroupName: '',
-      isPopoverOpen: false,
-      msg: false,
-      isLoading: false,
-    };
-
-    this.filters = { name: 'search', value: '' };
-  }
-
-  /**
-   * Refresh the groups entries
-   */
-  async refresh() {
-    try {
-      this.setState({ refreshingGroups: true });
-      await this.props.refresh();
-      this.setState({
-        originalItems: this.props.items,
-        refreshingGroups: false,
-      });
-    } catch (error) {
-      this.setState({
-        refreshingGroups: false,
-      });
-    }
-  }
-
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    this.setState({
-      items: nextProps.items,
-    });
   }
 
   componentDidMount() {
@@ -73,29 +32,6 @@ export class WzGroupsOverview extends Component {
   componentWillUnmount() {
     this._isMounted = false;
   }
-
-  showConfirm(groupName) {
-    this.setState({
-      showConfirm: groupName,
-    });
-  }
-
-  onQueryChange = ({ query }) => {
-    if (query) {
-      this.setState({ isLoading: true });
-      const filter = query.text || '';
-      this.filters.value = filter;
-      const items = filter
-        ? this.state.originalItems.filter(item => {
-            return item.name.toLowerCase().includes(filter.toLowerCase());
-          })
-        : this.state.originalItems;
-      this.setState({
-        isLoading: false,
-        items: items,
-      });
-    }
-  };
 
   render() {
     return (
@@ -131,10 +67,4 @@ export class WzGroupsOverview extends Component {
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    state: state.rulesetReducers,
-  };
-};
-
-export default connect(mapStateToProps)(WzGroupsOverview);
+export default WzGroupsOverview;
