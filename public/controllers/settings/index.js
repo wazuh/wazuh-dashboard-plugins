afc2e94f3aebaec5e53991845661298c86e6465a
Alberto Andújar <josealbertoandujar@gmail.com>
2019-12-13 15:03:49 +0100

    Redesign blues (#1954)
    * Redesign top Menu
    
    * Redesign settings section
    
    * Redesign visualization cards
    
    * Revert redesign
    
    * Merge revert redesign
    
    * First version configuration as React component
    
    * Full version of App settings / Configuration to React components
    
    * Add control to settings-side-menu
    
    * Code cleaning for App settings / Configuration
    
    * Minors Fix

--- public/controllers/settings/index.js
+++ public/controllers/settings/index.js
@@ -17,9 +17,15 @@ const app = uiModules.get('app/wazuh', []);
 import { ApiTable } from './components/api-table';
 import { AddApi } from './components/add-api';
 import { ApiIsDown } from './components/api-is-down';
+import WzSettingsSideMenu from './components/settings-side-menu';
+import WzConfigurationSettings from './components/configuration/configuration';
+import SettingsLogs from './components/logs';
 
 app
   .controller('settingsController', SettingsController)
+  .value('WzSettingsSideMenu', WzSettingsSideMenu)
+  .value('WzConfigurationSettings', WzConfigurationSettings)
+  .value('SettingsLogs', SettingsLogs)
   .value('ApiTable', ApiTable)
   .value('AddApi', AddApi)
   .value('ApiIsDown', ApiIsDown);
