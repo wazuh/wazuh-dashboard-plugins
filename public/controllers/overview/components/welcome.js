5cc55d618d097b070ea112983e17a526613f7b9b
JuanCarlos <juanca.romo9@gmail.com>
2019-12-13 15:16:16 +0100

    Adapt Kibana for 7.5.0

--- public/controllers/overview/components/welcome.js
+++ public/controllers/overview/components/welcome.js
@@ -126,7 +126,7 @@ export class WelcomeScreen extends Component {
               </EuiFlexGroup>
               <EuiFlexGrid columns={2}>
                 {this.buildTabCard('general', 'dashboardApp')}
-                {this.buildTabCard('fim', 'loggingApp')}
+                {this.buildTabCard('fim', 'filebeatApp')}
                 {this.props.extensions.aws &&
                   this.buildTabCard('aws', 'logoAWSMono')}
               </EuiFlexGrid>
