6f4ac5a070bb7e75fe25ee6dd6962f2a2d4e912a
Jose Sanchez <josesanxez@gmail.com>
2019-12-17 14:56:48 +0100

    Fix the FIM icon

--- public/controllers/overview/components/welcome.js
+++ public/controllers/overview/components/welcome.js
@@ -137,7 +137,7 @@ export class WelcomeScreen extends Component {
                   <EuiSpacer size='m' />
                   <EuiFlexGrid columns={2}>
                     {this.buildTabCard('general', 'dashboardApp')}
-                    {this.buildTabCard('fim', 'loggingApp')}
+                    {this.buildTabCard('fim', 'filebeatApp')}
                     {this.props.extensions.aws &&
                       this.buildTabCard('aws', 'logoAWSMono')}
                   </EuiFlexGrid>
