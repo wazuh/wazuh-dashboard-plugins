202055f342c5a3339dec53e34f2671ff7b3b3c8b
JuanCarlos <juanca.romo9@gmail.com>
2019-12-18 10:49:09 +0100

    Add mitre to welcome popover

--- public/controllers/overview/components/welcome.js
+++ public/controllers/overview/components/welcome.js
@@ -172,6 +172,7 @@ export class WelcomeScreen extends Component {
                       {this.buildPopover('popoverThreat', [
                         'virustotal',
                         'osquery',
+                        'mitre',
                         'docker'
                       ])}
                     </EuiFlexItem>
