e823236648fcdcbd79a24cc34f6d05e1ad339cf1
Alberto Andújar <josealbertoandujar@gmail.com>
2020-01-08 08:16:01 +0100

    Add overview-general to loop

--- public/templates/overview/visualizations.js
+++ public/templates/overview/visualizations.js
@@ -11,41 +11,50 @@
  */
 
 export const visualizations = {
-    overview: {
-        metrics: [
-            { id: 'Wazuh-App-Overview-General-Metric-alerts' },
-            { id: 'Wazuh-App-Overview-General-Level-12-alerts' },
-            { id: 'Wazuh-App-Overview-General-Authentication-failure' },
-            { id: 'Wazuh-App-Overview-General-Authentication-success' }
+  general: {
+    metrics: [
+      { id: 'Wazuh-App-Overview-General-Metric-alerts' },
+      { id: 'Wazuh-App-Overview-General-Level-12-alerts' },
+      { id: 'Wazuh-App-Overview-General-Authentication-failure' },
+      { id: 'Wazuh-App-Overview-General-Authentication-success' },
+    ],
+    rows: [
+      {
+        height: 360,
+        vis: [
+          {
+            title: 'Alert level evolution',
+            id: 'Wazuh-App-Overview-General-Alert-level-evolution',
+            width: 50,
+          },
+          { title: 'Alerts', id: 'Wazuh-App-Overview-General-Alerts', width: 50 },
         ],
-        rows: [
-            {
-                height: 360,
-                vis: [
-                    { title: 'Alert level evolution', id: 'Wazuh-App-Overview-General-Alert-level-evolution', width: 50 },
-                    { title: 'Alerts', id: 'Wazuh-App-Overview-General-Alerts', width: 50 },
-                ]
-            },
-            {
-                height: 270,
-                vis: [
-                    { title: 'Top 5 agents', id: 'Wazuh-App-Overview-General-Top-5-agents', width: 30 },
-                    { title: 'Top 5 rule groups', id: 'Wazuh-App-Overview-General-Top-5-rule-groups', width: 30 },
-                    { title: 'Agents status', id: 'Wazuh-App-Overview-General-Agents-status', width: 30 },
-                ]
-            },
-            {
-                height: 360,
-                vis: [
-                    { title: 'Alerts evolution - Top 5 agents', id: 'Wazuh-App-Overview-General-Alerts-evolution-Top-5-agents' },
-                ]
-            },
-            {
-                height: 570,
-                vis: [
-                    { title: 'Alerts summary', id: 'Wazuh-App-Overview-General-Alerts-summary' },
-                ]
-            },
-        ]
-    }
+      },
+      {
+        height: 270,
+        vis: [
+          { title: 'Top 5 agents', id: 'Wazuh-App-Overview-General-Top-5-agents', width: 33 },
+          {
+            title: 'Top 5 rule groups',
+            id: 'Wazuh-App-Overview-General-Top-5-rule-groups',
+            width: 33,
+          },
+          { title: 'Agents status', id: 'Wazuh-App-Overview-General-Agents-status', width: 33 },
+        ],
+      },
+      {
+        height: 360,
+        vis: [
+          {
+            title: 'Alerts evolution - Top 5 agents',
+            id: 'Wazuh-App-Overview-General-Alerts-evolution-Top-5-agents',
+          },
+        ],
+      },
+      {
+        height: 570,
+        vis: [{ title: 'Alerts summary', id: 'Wazuh-App-Overview-General-Alerts-summary' }],
+      },
+    ],
+  },
 };
