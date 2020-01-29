96535353fa2cef30d09310bb8a79b7fad3925cc0
Pablo <pablotr9@correo.ugr.es>
2020-01-29 15:03:06 +0100

    Allow disabling known fields health check

--- public/controllers/misc/health-check.js
+++ public/controllers/misc/health-check.js
@@ -226,6 +226,7 @@ export class HealthCheck {
       this.checks.template = configuration['checks.template'];
       this.checks.api = configuration['checks.api'];
       this.checks.setup = configuration['checks.setup'];
+      this.checks.fields = configuration['checks.fields'];
 
       this.results.push(
         {
@@ -251,7 +252,7 @@ export class HealthCheck {
         {
           id: 4,
           description: 'Check index pattern known fields',
-          status: 'Checking...'
+          status: this.checks.fields ? 'Checking...' : 'disabled'
         }
       );
 
@@ -263,12 +264,14 @@ export class HealthCheck {
 
       this.checksDone = true;
 
-      try {
-        await this.genericReq.request('GET', '/elastic/known-fields/all', {});
-        this.results[this.results.length - 1].status = 'Ready';
-      } catch (error) {
-        this.results[this.results.length - 1].status = 'Error';
-        this.handleError(error);
+      if(this.checks.fields){
+        try {
+          await this.genericReq.request('GET', '/elastic/known-fields/all', {});
+          this.results[this.results.length - 1].status = 'Ready';
+        } catch (error) {
+          this.results[this.results.length - 1].status = 'Error';
+          this.handleError(error);
+        }
       }
 
       if (!this.errors || !this.errors.length) {
