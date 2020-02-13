5464c5501ee898dfe2d0708b499d12d7b53ce0c1
Jose Sanchez <josesanxez@gmail.com>
2020-02-13 09:19:46 +0100

    Added a new class for making API requests
    The old monitoring task has a specific logic to obtain data from the agents
    of the API.

---
+++ server/lib/cron-scheduler/apiRequest.ts
@@ -0,0 +1,58 @@
+
+import Axios, { AxiosRequestConfig, AxiosResponse }from 'axios';
+import { log } from '../../logger';
+
+export interface IApi {
+  id: string
+  user: string
+  password: string
+  url: string
+  port: number
+  cluster_info: {
+    manager: string
+    cluster: 'Disabled' | 'Enabled'
+    status: 'disabled' | 'enabled'
+  }
+}
+
+export class ApiRequest {
+  private api: IApi;
+  private request: string;
+  private params: {};
+
+  constructor(request:string, api:IApi, params:{}={}, ) {
+    this.request = request;
+    this.api = api;
+    this.params = params;
+  }
+
+  private async makeRequest():AxiosResponse {
+    const {url, port, user, password} = this.api;
+    
+    const requestConfig: AxiosRequestConfig = {
+      method: 'GET',
+      baseURL: `${url}:${port}`,
+      url: this.request,
+      params: this.params,
+      headers: { 'Content-Type': 'application/json', 'kbn-xsrf': 'kibana' },
+      auth: {username: user, password: password},
+    }
+    
+    const response = await Axios(requestConfig);
+    return response;
+  }
+
+  public async getData() {
+    try {
+      const response = await this.makeRequest();
+      return response.data;
+    } catch (error) {
+      if(error.response.status === 401){
+        log('apiRequest', `Wrong Wazuh API credentials used`);
+        throw error.response;
+      }
+      console.log(error.response.data)
+      return error.response.data;
+    }
+  }
+}
