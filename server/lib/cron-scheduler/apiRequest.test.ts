599b4a80ccdb1d0f38797192ab380f649014cb24
Jose Sanchez <josesanxez@gmail.com>
2020-02-13 09:19:46 +0100

    Added a new class for making API requests
    The old monitoring task has a specific logic to obtain data from the agents
    of the API.

---
+++ server/lib/cron-scheduler/apiRequest.test.ts
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import { ApiRequest, IApi } from './index'
+jest.mock('axios');
+
+describe('ApiRequest', () => {
+  const apiExample1:IApi = {
+    id: 'default',
+    user: 'foo',
+    password: 'bar',
+    url: 'http://localhost',
+    port: 55000,
+    cluster_info: {
+      manager: 'master',
+      cluster: 'Disabled',
+      status: 'disabled',
+    },
+  }
+  beforeEach(() => {
+  })
+
+  afterEach(() => {
+  })
+
+  test('should return the object with the data of the request ', async () => {
+    const mockResponse = {
+      data: { error: 0, data: 'v3.12.0' },
+      status: 200,
+      statusText: 'OK',
+      headers: {},
+      config: {},
+    }
+    axios.mockResolvedValue(mockResponse);
+    
+    const apiRequest = new ApiRequest('/version', apiExample1);
+    const response = await apiRequest.getData();
+
+    expect(response).toEqual(mockResponse.data);    
+  });
+
+  test('should return the object with the error when the path is invalid', async () => {
+    const mockResponse = {
+      response: {
+        data: { 
+          error: 603,
+          message: 'The requested URL was not found on this server' },
+          status: 404,
+          statusText: 'Not Found',
+          headers: {},
+          config: {},
+      }
+    }
+    axios.mockRejectedValue(mockResponse);
+
+    const apiRequest = new ApiRequest('/versio', apiExample1);
+    const response = await apiRequest.getData();
+
+    expect(response).toEqual(mockResponse.data);
+  })
+  
+  test('should throw an error when the api user are invalid', () => {
+    expect(true).toBe(false);
+  })
+  
+  test('should throw an error when the port api are invalid', () => {
+    expect(true).toBe(false);
+  })
+  
+  test('should throw an error when the url api are invalid', () => {
+    expect(true).toBe(false);
+  })
+
+})
