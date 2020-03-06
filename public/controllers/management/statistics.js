e1cb63896d8e0c716026c7e5866ebe4e410dc5a8
Jose Sanchez <josesanxez@gmail.com>
2020-03-06 11:28:06 +0100

    Fixed the timepicker

--- public/controllers/management/statistics.js
+++ public/controllers/management/statistics.js
@@ -11,6 +11,8 @@
  */
 import { AppState } from '../../react-services/app-state';
 import { GenericRequest } from '../../react-services/generic-request';
+import { FilterHandler } from '../../utils/filter-handler';
+import { timefilter } from 'ui/timefilter';
 
 export function StatisticsController(
     $scope,
@@ -27,12 +29,126 @@ export function StatisticsController(
     visHandlers,
     tabVisualizations,
 ) {
-    tabVisualizations.setTab('statistics');
-    $location.search('tabView', 'statistics');
-    const clusterEnabled = AppState.getClusterInfo()
-      && AppState.getClusterInfo().status === 'enabled';
-    $scope.isClusterEnabled = clusterEnabled;
-    $scope.IsClusterRunning = true;
-    $scope.tabView = 'statistics';
+  timefilter.setRefreshInterval({ pause: true, value: 0 });
+  const filterHandler = new FilterHandler(AppState.getCurrentPattern());
+  tabVisualizations.setTab('statistics');
+  $location.search('tabView', 'statistics');
+  const clusterEnabled = AppState.getClusterInfo()
+    && AppState.getClusterInfo().status === 'enabled';
+  $scope.isClusterEnabled = clusterEnabled;
+  $scope.IsClusterRunning = true;
+  $scope.tabView = 'statistics';
+  $scope.showConfig = false;
+  $scope.showNodes = false;
+  $scope.currentNode = null;
+  $scope.nodeSearchTerm = '';
 
+  /**
+   * This set default boolean flags for a given component
+   * @param {String} component
+   */
+  const setBooleans = component => {
+    $scope.showConfig = component === 'showConfig';
+    $scope.showNodes = component === 'showNodes';
+    $scope.currentNode = null;
+  };
+
+  /**
+   * This navigates to agents preview
+   */
+  $scope.goAgents = () => {
+    $window.location.href = '#/agents-preview';
+  };
+
+
+  /**
+   * This navigates to nodes
+   */
+  $scope.goNodes = () => {
+    setBooleans('showNodes');
+    tabVisualizations.assign({
+      monitoring: 1
+    });
+    assignFilters();
+    $rootScope.$broadcast('updateVis');
+  };
+
+  /**
+   * This navigates back
+   */
+  $scope.goBack = () => {
+    setBooleans(null);
+    tabVisualizations.assign({
+      monitoring: 2
+    });
+    assignFilters();
+    $rootScope.$broadcast('updateVis');
+  };
+
+  //listeners
+  $scope.$on('wazuhShowClusterNode', async (event, parameters) => {
+    try {
+      $rootScope.$broadcast('updateVis');
+
+      $scope.$applyAsync();
+    } catch (error) {
+      errorHandler.handle(error, 'Cluster');
+    }
+  });
+
+  let filters = discoverPendingUpdates.getList()[1];
+  /**
+   * This creatie custom filters for visualizations for a given node
+   * @param {Object} node
+   */
+  const assignFilters = (node = false) => {
+    try {
+      filters = discoverPendingUpdates.getList()[1];
+
+      $rootScope.$emit('wzEventFilters', { filters, localChange: false });
+      if (!$rootScope.$$listenerCount['wzEventFilters']) {
+        $timeout(100).then(() => assignFilters(node));
+      }
+    } catch (error) {
+      errorHandler.handle(
+        'An error occurred while creating custom filters for visualizations',
+        'Cluster',
+        true
+      );
+    }
+  };
+
+  /**
+   * This set some required settings at init
+   */
+  const load = async () => {
+    try {
+      visHandlers.removeAll();
+      discoverPendingUpdates.removeAll();
+      rawVisualizations.removeAll();
+      loadedVisualizations.removeAll();
+
+ 
+      assignFilters();
+      $rootScope.$broadcast('updateVis');
+
+      $scope.loading = false;
+      $scope.$applyAsync();
+      return;
+    } catch (error) {
+      $scope.loading = false;
+      errorHandler.handle(error, 'Cluster');
+    }
+  };
+
+  load();
+
+  //listeners
+  $scope.$on('$destroy', () => {
+    discoverPendingUpdates.removeAll();
+    tabVisualizations.removeAll();
+    rawVisualizations.removeAll();
+    loadedVisualizations.removeAll();
+    visHandlers.removeAll();
+  });
 }
