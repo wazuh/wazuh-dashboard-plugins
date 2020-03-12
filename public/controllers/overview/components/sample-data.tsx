65049b9ec8aca752b8ef69a02de7939c92221c16
victorst79 <victorst79@gmail.com>
2020-03-12 09:18:21 +0100

    added new component to add sample data in dashboards

--- public/controllers/overview/components/sample-data.tsx
+++ public/controllers/overview/components/sample-data.tsx
@@ -1,5 +1,5 @@
 /*
- * Wazuh app - React component for show search and filter
+ * Wazuh app - React component for add sample data
  * Copyright (C) 2015-2020 Wazuh, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -12,44 +12,99 @@
 
 import React, { Component, Fragment } from 'react';
 import {
-	EuiFlexItem,
-	EuiCard,
-	EuiSpacer,
-	EuiFlexGrid
+    EuiFlexItem,
+    EuiCard,
+    EuiSpacer,
+    EuiFlexGrid,
+    EuiFlexGroup,
+    EuiButton,
 } from '@elastic/eui';
+import { toastNotifications } from 'ui/notify';
 
 export class SampleData extends Component {
-	render() {
-		return(
-			<Fragment>
-				<EuiSpacer />
-					<EuiFlexGrid columns={3}>
-						<EuiFlexItem key={1}>
-							<EuiCard
-								layout="horizontal"
-								title={'Example'}
-								description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, quas enim! Commodi, obcaecati quis ea ducimus vel reprehenderit, dolor distinctio quaerat, dignissimos voluptas officiis autem est quasi nemo voluptatum tempora.'}
-								onClick={() => console.log('L')}
-							/>
-						</EuiFlexItem>
-						<EuiFlexItem key={1}>
-							<EuiCard
-								layout="horizontal"
-								title={'Example'}
-								description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, quas enim! Commodi, obcaecati quis ea ducimus vel reprehenderit, dolor distinctio quaerat, dignissimos voluptas officiis autem est quasi nemo voluptatum tempora.'}
-								onClick={() => console.log('L')}
-							/>
-						</EuiFlexItem>
-						<EuiFlexItem key={1}>
-							<EuiCard
-								layout="horizontal"
-								title={'Example'}
-								description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, quas enim! Commodi, obcaecati quis ea ducimus vel reprehenderit, dolor distinctio quaerat, dignissimos voluptas officiis autem est quasi nemo voluptatum tempora.'}
-								onClick={() => console.log('L')}
-							/>
-						</EuiFlexItem>
-				</EuiFlexGrid>
-			</Fragment>
-		)
-	}
+  showToast = (color, title, text, time) => {
+      toastNotifications.add({
+          color: color,
+          title: title,
+          text: text,
+          toastLifeTimeMs: time,
+      });
+  };
+
+  generateSampleData(index) {
+
+  }
+
+  tabIndex() {
+    return [
+      {
+        id: 'security',
+        title: 'Security Information',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, quas enim! Commodi, obcaecati quis ea ducimus vel reprehenderit, dolor distinctio quaerat.',
+        footer: this.renderActionButtons()
+      },
+      {
+        title: 'Auditing and Policy Monitoring',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, quas enim! Commodi, obcaecati quis ea ducimus vel reprehenderit, dolor distinctio quaerat.',
+        footer: this.renderActionButtons()
+      },
+      {
+        title: 'Threat detection and response',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, quas enim! Commodi, obcaecati quis ea ducimus vel reprehenderit, dolor distinctio quaerat.',
+        footer: this.renderActionButtons()
+      },
+      {
+        title: 'Regulatory Compliance',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, quas enim! Commodi, obcaecati quis ea ducimus vel reprehenderit, dolor distinctio quaerat.',
+        footer: this.renderActionButtons()
+      },
+    ]
+  }
+
+  rendenderTabs() {
+    const tabs = this.tabIndex();
+    let auxTabs: any = [];
+
+    tabs.map((tab, index)=> {
+      auxTabs.push(
+        <EuiFlexItem key={index}>
+          <EuiCard
+            textAlign={'left'}
+            title={tab.title}
+            description={tab.description}
+            footer={tab.footer}
+          />
+        </EuiFlexItem>
+      )
+    });
+
+    return auxTabs;
+  }
+
+  renderActionButtons() {
+    return (
+      <EuiFlexGroup justifyContent="flexEnd">
+        <EuiFlexItem grow={false}>
+          <EuiButton
+            isLoading={false}
+            onClick={() => this.showToast('success', `Sample logs instaled`, '', 5000)}>
+                Add Data
+          </EuiButton>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    )
+  }
+
+  render() {
+    const tabsSection = this.rendenderTabs();
+    
+    return(
+      <Fragment>
+        <EuiSpacer />
+        <EuiFlexGrid columns={3}>
+          {tabsSection.map(tab => tab)}
+        </EuiFlexGrid>
+      </Fragment>
+    )
+  }
 }
