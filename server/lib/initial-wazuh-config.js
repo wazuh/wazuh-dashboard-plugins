6ad9e6d9425c39871b6fb9a75cab7760a948b5ea
Pablo <pablotr9@correo.ugr.es>
2020-03-12 09:35:51 +0100

    Add checks.fields to initial default config

--- server/lib/initial-wazuh-config.js
+++ server/lib/initial-wazuh-config.js
@@ -43,6 +43,7 @@
 #checks.template: true
 #checks.api     : true
 #checks.setup   : true
+#checks.fields   : true
 #
 # --------------------------------- Extensions ---------------------------------
 #
