4b32882a25054e24b1c57b8545916bb4a102b147
Pablo <pablotr9@correo.ugr.es>
2020-03-23 11:11:16 +0100

    Allow \< in our xml validator

--- public/directives/wz-xml-file-editor/wz-xml-file-editor.js
+++ public/directives/wz-xml-file-editor/wz-xml-file-editor.js
@@ -88,6 +88,7 @@ app.directive('wzXmlFileEditor', function() {
           const text = $scope.xmlCodeBox.getValue();
           let xml = replaceIllegalXML(text);
           xml = xml.replace(/..xml.+\?>/, '');
+          xml = xml.replace(/\\</, '\ <');
           xml = xml.replace(/\\</gm, '');
           xml = xml.replace(/<!--[\s\S\n]*?-->/gm, '');
           const xmlDoc = parser.parseFromString(
