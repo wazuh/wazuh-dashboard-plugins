977bc16d789474c96212fb61d60aa1b9565f0187
Alberto Andújar <josealbertoandujar@gmail.com>
2020-04-16 12:54:47 -0400

    Add new vulnerabilityDatas

--- server/lib/generate-alerts-script.js
+++ server/lib/generate-alerts-script.js
@@ -100,8 +100,45 @@ const vulnerabilitySeverity = ["Low", "Medium", "High", "Critical"];
 const vulnerabilityPackageName = ["bluez", "gcc-mingw-w64", "squashfs-tools", "util-linux", "accountsservice", "git", "node", "zip", "kernel"];
 const vulnerabilityCve = ["CVE-2017-11671", "CVE-2008-7320", "CVE-2011-1011", "CVE-2012-0881", "CVE-2012-1093"];
 const vulnerabilityCwe_reference = ["CWE-527", "CWE-911", "CWE-409", "CWE-102", "CWE-120", "CWE-420", "CWE-322", "CWE-789"];
-const vulnerabilityReference = ["Sample vulnerability reference 1", "Sample vulnerability reference 2", "Sample vulnerability reference 3", "Sample vulnerability reference 4"];
-const vulnerabilityTitle = ["Sample vulnerability 1", "Sample vulnerability 2", "Sample vulnerability 3", "Sample vulnerability 4"];
+
+const vulnerabilityData = [
+    {
+        title: 'An issue was discovered in zlib_decompress_extra in modules/demux/mkv/util.cpp in VideoLAN VLC media player 3.x through 3.0.7. The Matroska demuxer, while parsing a malformed MKV file type, has a double free.',
+        reference: 'http://git.videolan.org/?p=vlc.git;a=commit;h=81023659c7de5ac2637b4a879195efef50846102'
+    },
+    {
+        title: 'bash: BASH_CMD is writable in restricted bash shells',
+        reference: 'https://access.redhat.com/security/cve/CVE-2019-9924'
+    },
+    {
+        title: 'CVE-2017-16808',
+        reference: 'https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2017-16808'
+    },
+    {
+        title: 'CVE-2017-9763 on Ubuntu 18.04 LTS (bionic) - medium.',
+        reference: 'https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2017-9763'
+    },
+    {
+        title: 'CVE-2018-10105',
+        reference: 'https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2018-10105'
+    },
+    {
+        title: 'CVE-2018-14462',
+        reference: 'https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2018-14462'
+    },
+    {
+        title: 'CVE-2018-14463',
+        reference: 'https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2018-14463'
+    },
+    {
+        title: 'CVE-2018-14466',
+        reference: 'https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2018-14466'
+    },
+    {
+        title: 'CVE-2018-5710 on Ubuntu 18.04 LTS (bionic) - low.',
+        reference: 'https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2018-5710'
+    }
+]
 
 // Authenticathin
 const sshRuleDescription = ['sshd: authentication failed.', 'sshd: Multiple authentication failures.']
@@ -414,8 +451,9 @@ function generateAlert(params) {
         alert.data.vulnerability.package.name = getRandomFromArray(vulnerabilityPackageName);
         alert.data.vulnerability.cve = getRandomFromArray(vulnerabilityCve);
         alert.data.vulnerability.cwe_reference = getRandomFromArray(vulnerabilityCwe_reference);
-        alert.data.vulnerability.reference = getRandomFromArray(vulnerabilityReference);
-        alert.data.vulnerability.title = getRandomFromArray(vulnerabilityTitle);
+        const dataVulnerability = getRandomFromArray(vulnerabilityData);
+        alert.data.vulnerability.reference = dataVulnerability.reference;
+        alert.data.vulnerability.title = dataVulnerability.title;
     }
     
     if (params.osquery) {
