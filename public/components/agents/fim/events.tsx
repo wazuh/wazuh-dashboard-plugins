6cf3f8cc709ee90db5b3fe663e15690fe200e092
Jose Sanchez <josesanxez@gmail.com>
2020-04-24 13:52:18 +0200

    Fix console error, state update on an unmounted component EventsFim

--- public/components/agents/fim/events.tsx
+++ public/components/agents/fim/events.tsx
@@ -17,6 +17,7 @@ import { EuiOverlayMask } from '@elastic/eui';
 
 
 export class EventsFim extends Component {
+  _isMount = false;
   state: {
     isFlyoutVisible: Boolean,
     currentFile: string,
@@ -39,6 +40,45 @@ export class EventsFim extends Component {
     };
     this.modulesHelper = ModulesHelper;
     this.getRowsField.bind(this);
+  }
+
+  async componentDidMount() {
+    this._isMount = true;
+    const scope = await this.modulesHelper.getDiscoverScope();
+    this.fetchWatch = scope.$watchCollection('fetchStatus',
+      () => {
+        const {fetchStatus} = this.state;
+        if (fetchStatus !== scope.fetchStatus){
+          const rows = scope.fetchStatus === 'complete' ? scope.rows.length : 0;
+          this._isMount && this.setState({fetchStatus: scope.fetchStatus, rows})
+        }
+      });
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    const {fetchStatus, isFlyoutVisible, rows} = this.state;
+    if (nextState.isFlyoutVisible !== isFlyoutVisible ){
+      return true;
+    }
+    if (nextState.fetchStatus !== fetchStatus){
+      return true;
+    }
+    if (nextState.rows !== rows) {
+      return true;
+    }
+    return false;
+  }
+
+  componentDidUpdate() {
+    const {fetchStatus, rows} = this.state;
+    if(fetchStatus === 'complete' && rows){
+      this.getRowsField();
+    }
+  }
+
+  componentWillUnmount() {
+    this._isMount = false;
+    if (this.fetchWatch) this.fetchWatch();
   }
 
   getRowsField = async () => {
@@ -93,43 +133,6 @@ export class EventsFim extends Component {
           }
         })
     }
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    const {fetchStatus, isFlyoutVisible, rows} = this.state;
-    if (nextState.isFlyoutVisible !== isFlyoutVisible ){
-      return true;
-    }
-    if (nextState.fetchStatus !== fetchStatus){
-      return true;
-    }
-    if (nextState.rows !== rows) {
-      return true;
-    }
-    return false;
-  }
-
-  componentDidUpdate() {
-    const {fetchStatus, rows} = this.state;
-    if(fetchStatus === 'complete' && rows){
-      this.getRowsField();
-    }
-  }
-
-  async componentDidMount() {
-    const scope = await this.modulesHelper.getDiscoverScope();
-    this.fetchWatch = scope.$watchCollection('fetchStatus',
-      () => {
-        const {fetchStatus} = this.state;
-        if (fetchStatus !== scope.fetchStatus){
-          const rows = scope.fetchStatus === 'complete' ? scope.rows.length : 0;
-          this.setState({fetchStatus: scope.fetchStatus, rows})
-        }
-      });
-  }
-
-  componentWillUnmount() {
-    if (this.fetchWatch) this.fetchWatch();
   }
 
   showFlyout(file) {
