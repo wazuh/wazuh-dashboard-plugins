021b54de1450713c0ed895567d770b4c1cfa036b
Pablo <pablotr9@correo.ugr.es>
2020-05-04 12:05:06 +0200

    Hide 'Inspect in discover' button if we are already in Discover

--- public/components/agents/fim/inventory/fileDetail.tsx
+++ public/components/agents/fim/inventory/fileDetail.tsx
@@ -309,18 +309,19 @@ export class FileDetails extends Component {
           buttonContent={
             <EuiTitle size="s">
               <h3> 
-              Recent events
-              <EuiToolTip
-                position="top"
-                content="Inspect in discover">
-                <EuiIcon
-                  className='euiButtonIcon euiButtonIcon--primary'
-                  onClick={this.viewInEvents}
-                  type="link"
-                  style={{marginLeft: 16}}
-                  aria-label="Inspect in discover"
-                />
-              </EuiToolTip>
+              Recent events {this.props.view !== 'events' && (
+                <EuiToolTip
+                  position="top"
+                  content="Inspect in discover">
+                  <EuiIcon
+                    className='euiButtonIcon euiButtonIcon--primary'
+                    onClick={this.viewInEvents}
+                    type="link"
+                    style={{marginLeft: 16}}
+                    aria-label="Inspect in discover"
+                  />
+                </EuiToolTip>
+              )}
               </h3>
             </EuiTitle>
           }
