930fdf7e0c86daefd4ccd72fa2640e9b9312308b
Desvelao <iamdesvelao@gmail.com>
2020-06-01 16:20:34 +0200

    replaced agent id by agent name + tooltip in some components

--- public/components/common/globalBreadcrumb/globalBreadcrumb.tsx
+++ public/components/common/globalBreadcrumb/globalBreadcrumb.tsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { EuiBreadcrumbs } from '@elastic/eui';
+import { EuiBreadcrumbs, EuiToolTip } from '@elastic/eui';
 import { connect } from 'react-redux';
 import './globalBreadcrumb.less';
+import chrome from 'ui/chrome';
+import { AppNavigate } from '../../../react-services/app-navigate';
 
 class WzGlobalBreadcrumb extends Component {
   props: { state: { breadcrumb: [] } };
@@ -11,8 +13,17 @@ class WzGlobalBreadcrumb extends Component {
     this.props = props;
   }
 
+  async componentDidMount() {
+    const $injector = await chrome.dangerouslyGetActiveInjector();
+    this.router = $injector.get('$route');
+    $('#breadcrumbNoTitle').attr("title","");
+  }
+  componnedDidUpdate(){
+    $('#breadcrumbNoTitle').attr("title","");
+  }
   render() {
     const container = document.getElementsByClassName('euiBreadcrumbs');
+    console.log('breadrender',this.props)
     return (
       <div>
         {!!this.props.state.breadcrumb.length && (
@@ -23,7 +34,19 @@ class WzGlobalBreadcrumb extends Component {
               truncate={false}
               max={6}
               showMaxPopover
-              breadcrumbs={this.props.state.breadcrumb}
+              breadcrumbs={this.props.state.breadcrumb.map(breadcrumb => breadcrumb.agent ? {
+                text: (
+                  <a 
+                    style={{marginRight: 0, marginBottom: 3 }}
+                    className="euiLink euiLink--subdued euiBreadcrumb"
+                    onClick={(ev) =>  {ev.stopPropagation(); AppNavigate.navigateToModule(ev, 'agents', {"tab": "welcome", "agent": breadcrumb.agent.id  } ); this.router.reload();}}
+                    id="breadcrumbNoTitle"
+                    >
+                    <EuiToolTip position="top" content={"View agent summary"}>
+                      <span>{breadcrumb.agent.name}</span>
+                    </EuiToolTip>
+                  </a> )
+              } : breadcrumb)}
               aria-label="Wazuh global breadcrumbs"
             />,
             container[0])
