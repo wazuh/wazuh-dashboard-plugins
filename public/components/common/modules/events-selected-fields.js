662b04d388fa558c9d8adc5a6004b6a26c7c32df
Pablo <pablotr9@correo.ugr.es>
2020-06-11 09:49:42 +0200

    Change selected columns Docker, aws and vuls

--- public/components/common/modules/events-selected-fields.js
+++ public/components/common/modules/events-selected-fields.js
@@ -38,10 +38,9 @@ export const EventsSelectedFiles = {
   ],
   aws: [
     'data.aws.source',
-    'data.aws.type',
-    'data.aws.severity',
-    'data.aws.region',
-    'data.aws.description',
+    'rule.description',
+    'rule.level',
+    'rule.id',
   ],
   gcp: [
     'data.jsonPayload.vmInstanceName',
@@ -86,12 +85,11 @@ export const EventsSelectedFiles = {
     'data.cis.result',
   ],
   vuls: [
+    'data.vulnerability.package.name',
+    'data.vulnerability.state',
+    'data.vulnerability.severity',
     'data.vulnerability.cve',
     'data.vulnerability.cwe_reference',
-    'data.vulnerability.reference',
-    'data.vulnerability.severity',
-    'data.cis.unknown',
-    'data.vulnerability.state',
   ],
   virustotal: [
     'data.virustotal.source.file',
@@ -107,16 +105,11 @@ export const EventsSelectedFiles = {
     'data.osquery.subquery',
   ],
   docker: [
-    'data.docker.Actor.Attributes.name',
     'data.docker.from',
     'data.docker.Type',
     'data.docker.Action',
-  ],
-  docker: [
-    'data.docker.Actor.Attributes.name',
-    'data.docker.from',
-    'data.docker.Type',
-    'data.docker.Action',
+    'rule.description',
+    'rule.level',
   ],
   pci: [
     'rule.pci_dss',
