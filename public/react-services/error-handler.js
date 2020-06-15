4b74f1bd9e77c53905ff2cf6cf88af164c395a75
Desvelao <iamdesvelao@gmail.com>
2020-06-15 18:26:55 +0200

    added condition when error is a object to extract message function of ErrorHandler

--- public/react-services/error-handler.js
+++ public/react-services/error-handler.js
@@ -67,7 +67,7 @@ export class ErrorHandler {
     if (typeof error === 'string'){
       return error
     };
-    if (typeof error === 'object'){
+    if (typeof error === 'object' && error !== null){
       return JSON.stringify(error)
     };
     return error || 'Unexpected error';
