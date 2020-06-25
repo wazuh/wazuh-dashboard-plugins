64a6c78c2192d3ef8df917db61e1ca0ca47a8af1
JuanCarlos <juanca.romo9@gmail.com>
2020-06-25 16:23:19 +0200

    Fix edit configuration height

--- public/controllers/management/components/management/configuration/edit-configuration/edit-configuration.js
+++ public/controllers/management/components/management/configuration/edit-configuration/edit-configuration.js
@@ -405,7 +405,7 @@ const WzEditorConfiguration = compose(
                 value={editorValue}
                 onChange={value => onChange(value)}
                 minusHeight={
-                  wazuhNotReadyYet || infoChangesAfterRestart ? 270 : 215
+                  wazuhNotReadyYet || infoChangesAfterRestart ? 270 : 220
                 }
               />
               )}
