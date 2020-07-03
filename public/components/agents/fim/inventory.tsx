3b7d4a2c59df1624e04a3de97e831de8806db466
Alberto Andújar <josealbertoandujar@gmail.com>
2020-07-03 12:15:02 +0200

    Minors fix

--- public/components/agents/fim/inventory.tsx
+++ public/components/agents/fim/inventory.tsx
@@ -41,7 +41,6 @@ import exportCsv from '../../../react-services/wz-csv';
 import { toastNotifications } from 'ui/notify';
 import { ICustomBadges } from '../../wz-search-bar/components';
 import { filtersToObject } from '../../wz-search-bar';
-import { catch } from 'fetch-mock';
 
 export class Inventory extends Component {
   _isMount = false;
@@ -331,7 +330,7 @@ export class Inventory extends Component {
       );
   
       return (((response.data || {}).data).syscheck || {}).disabled === 'no';
-    } catch {
+    } catch (error) {
       return false;
     }
   }
