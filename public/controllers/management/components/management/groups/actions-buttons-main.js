46d205acf34e5642c14a61fd8bd350feee52a1ca
victorst79 <victorst79@gmail.com>
2020-07-10 10:01:03 +0200

    some changes in popover new group

--- public/controllers/management/components/management/groups/actions-buttons-main.js
+++ public/controllers/management/components/management/groups/actions-buttons-main.js
@@ -248,29 +248,30 @@ class WzGroupsActionButtons extends Component {
               isOpen={this.state.isPopoverOpen}
               closePopover={() => this.closePopover()}
             >
-              <EuiFormRow label="Introduce the group name" id="">
-                <EuiFieldText
-                  className="groupNameInput"
-                  value={this.state.newGroupName}
-                  onChange={this.onChangeNewGroupName}
-                  aria-label=""
-                />
-              </EuiFormRow>
-              <EuiSpacer size="xs" />
-              <EuiFlexGroup>
+            <EuiFlexGroup direction={'column'}>
                 <EuiFlexItem>
-                  <EuiButton
-                    iconType="save"
-                    isDisabled={!this.isOkNameGroup(this.state.newGroupName)}
-                    fill
-                    onClick={async () => {
-                      await this.createGroup();
-                    }}
-                  >
-                    Save new group
-                  </EuiButton>
-                </EuiFlexItem>
-              </EuiFlexGroup>
+                  <EuiFormRow label="Introduce the group name" id="">
+                    <EuiFieldText
+                      className="groupNameInput"
+                      value={this.state.newGroupName}
+                      onChange={this.onChangeNewGroupName}
+                      aria-label=""
+                    />
+                  </EuiFormRow>
+              </EuiFlexItem>
+              <EuiFlexItem>
+                <EuiButton
+                  iconType="save"
+                  isDisabled={!this.isOkNameGroup(this.state.newGroupName)}
+                  fill
+                  onClick={async () => {
+                    await this.createGroup();
+                  }}
+                >
+                  Save new group
+                </EuiButton>
+              </EuiFlexItem>
+            </EuiFlexGroup>
             </EuiPopover>
           </EuiFlexItem>
         )}
