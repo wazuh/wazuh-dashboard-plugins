115885ae4aa3fceccbacec1f37446e471d8f8007
Desvelao <iamdesvelao@gmail.com>
2020-07-31 08:26:41 +0200

    fix useWindowSize hook first state

--- public/components/common/hooks/useWindowSize.ts
+++ public/components/common/hooks/useWindowSize.ts
@@ -1,3 +1,14 @@
+/*
+ * Wazuh app - React hook for get query of Kibana searchBar
+ * Copyright (C) 2015-2020 Wazuh, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Find more information about this on the LICENSE file.
+ */
 import React, { useState, useEffect } from 'react';
 
 export const useWindowSize = () => {
@@ -10,7 +21,7 @@ export const useWindowSize = () => {
     };
   }
 
-  const [windowSize, setWindowSize] = useState(getSize);
+  const [windowSize, setWindowSize] = useState(getSize());
 
   useEffect(() => {
     if (!isClient) {
