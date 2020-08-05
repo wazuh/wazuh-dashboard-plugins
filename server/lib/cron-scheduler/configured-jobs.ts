a73ef66ff3e2f8ffd38df5e31b8cf91fa0cb503d
Jose Sanchez <josesanxez@gmail.com>
2020-08-05 12:13:27 +0200

    Added checkConfiguration method

--- server/lib/cron-scheduler/configured-jobs.ts
+++ server/lib/cron-scheduler/configured-jobs.ts
@@ -1,29 +1,60 @@
 import { jobs } from './index';
 import { IApi } from './apiRequest';
 import { IJob } from './predefined-jobs';
+import { getConfiguration } from '../get-configuration';
 
-export const configuredJobs = (params:{jobName?:string, host?: IApi}) => {
+export const configuredJobs = (params: { jobName?: string, host?: IApi }) => {
   const { host, jobName } = params;
-  return checkCluster(getJobs({jobName, host}))
+  return checkCluster(checkConfiguration(getJobs({ jobName, host })))
 }
 
-const getJobs = (params:{jobName?:string, host?: IApi}) => {
+const getJobs = (params: { jobName?: string, host?: IApi }) => {
   const { host, jobName } = params;
-  if (!jobName) return {jobObj:jobs, host};
-  return {jobObj:{[jobName]:jobs[jobName]}, host}
+  if (!jobName) return { jobObj: jobs, host };
+  return { jobObj: { [jobName]: jobs[jobName] }, host }
 }
 
-const checkCluster = (params: {jobObj:{[key:string]: IJob}, host?: IApi}) => {
-  const {host} = params;
+const checkCluster = (params: { jobObj: { [key: string]: IJob }, host?: IApi }) => {
+  const { host } = params;
   const newJobObj = JSON.parse(JSON.stringify(params.jobObj));
-  if(host && host.cluster_info.status === 'enabled'){
+  if (host && host.cluster_info && host.cluster_info.status === 'enabled') {
     ['manager-stats-remoted', 'manager-stats-analysisd'].forEach(item => {
       newJobObj[item] && (newJobObj[item].status = false);
     });
-  } else if (host && host.cluster_info.status === 'disabled') {
+  } else if (host && host.cluster_info && host.cluster_info.status === 'disabled') {
     ['cluster-stats-remoted', 'cluster-stats-analysisd'].forEach(item => {
       newJobObj[item] && (newJobObj[item].status = false);
     })
+  } else if (host && !host.cluster_info) {
+    Object.keys(newJobObj).forEach(key => newJobObj[key].status = false);
   }
   return newJobObj;
 }
+
+const checkConfiguration = (params: { jobObj: { [key: string]: IJob }, host?: IApi }) => {
+  const {jobObj, host} = params;
+  const config = getConfiguration();
+  const cronSettigns = Object.keys(config).filter(checkSetting);
+  cronSettigns.forEach(setting => applySettings(setting, config[setting], jobObj))
+  return { jobObj, host };
+}
+const cronRegx = /cron.(?<task>remoted|analysisd).((?<index>\w+)\.)?(?<config>\w+)$/;
+
+const checkSetting = (setting) => cronRegx.test(setting);
+
+const applySettings = (setting, value, jobObj: { [key: string]: IJob }) => {
+  const { task, index, config } = cronRegx.exec(setting).groups;
+  Object.keys(jobObj).forEach(key => {
+    if (!key.includes(task)) return;
+    applySetting(jobObj[key], index, config, value)
+  })
+}
+
+const applySetting = (job, index, config, value) => {
+  if (index) {
+    job[index][config] = value;
+  } else {
+    job[config] = value;
+  }
+}
+
