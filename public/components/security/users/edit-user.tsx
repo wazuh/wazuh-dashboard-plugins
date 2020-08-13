aef7770ff347c7a9b4e688c742760cdcbba737c1
Pablo <pablotr9@correo.ugr.es>
2020-08-13 08:17:47 +0200

    Remove users creation/edition

--- public/components/security/users/edit-user.tsx
+++ public/components/security/users/edit-user.tsx
@@ -19,64 +19,24 @@ import {
     EuiComboBox
 } from '@elastic/eui';
 
-import { WazuhSecurity } from '../../../factories/wazuh-security'
 import { useApiRequest } from '../../common/hooks/useApiRequest';
-import { AppState } from '../../../react-services/app-state';
 import { ErrorHandler } from '../../../react-services/error-handler';
 
 
 export const EditUser = ({ currentUser, closeFlyout }) => {
-    const [wazuhSecurity, setWazuhSecurity] = useState(new WazuhSecurity());
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [fullName, setFullName] = useState(currentUser.full_name);
-    const [emailAddress, setEmailAddress] = useState(currentUser.email);
     const userRoles = currentUser.roles ? currentUser.roles.map(item => { return { label: item } }) : [];
     const [selectedRoles, setSelectedRole] = useState(userRoles);
-    const [editPassword, setEditPassword] = useState(false);
-    const [password, setPassword] = useState('');
-    const [passwordError, setPasswordError] = useState(false);
-    const [passwordConfirmation, setPasswordConfirmation] = useState('');
-    const [passwordConfirmationError, setPasswordConfirmationError] = useState(false);
     const roles = useApiRequest('GET', '/security/roles', {}, (result) => { return ((result || {}).data || {}).data || {}; });
     const rolesOptions = roles.data.affected_items ? roles.data.affected_items.map(item => { return { label: item.name } }) : [];
 
 
-    const closeModal = () => setIsModalVisible(false);
-    const showModal = () => setIsModalVisible(true);
 
 
     const editUser = async () => {
-        if (editPassword) {
-            if (!password || password.length < 6) {
-                setPasswordError(true);
-                return;
-            } else {
-                setPasswordError(false);
-            }
-            if (!passwordConfirmation || passwordConfirmation.length < 6 || passwordConfirmation !== password) {
-                setPasswordConfirmationError(true);
-                return;
-            } else {
-                setPasswordConfirmationError(false);
-            }
-        }
-
-        const currentApi = JSON.parse(AppState.getCurrentAPI()).id;
         const formattedRoles = selectedRoles.map(item => {
-            return `wazuh_${currentApi}_${item.label}`
+            return item.label;
         });
-        formattedRoles.push('wazuh_user'); // TODO: documentation should show a guide of adding a role necessary for all wazuh users with minimum permissions
-
-        try {
-            if (editPassword) {
-                await wazuhSecurity.security.editUser(currentUser.user, { username: currentUser.user, password, roles: formattedRoles, full_name: fullName, email: emailAddress });
-            } else {
-                await wazuhSecurity.security.editUser(currentUser.user, { username: currentUser.user, roles: formattedRoles, full_name: fullName, email: emailAddress });
-            }
-            ErrorHandler.info('User was successfully edited', '');
-        } catch (error) {
-            ErrorHandler.handle(error, 'Error editing user');
-        }
+        
         closeFlyout(false)
     }
 
@@ -84,52 +44,8 @@ export const EditUser = ({ currentUser, closeFlyout }) => {
     const onChangeRoles = selectedRoles => {
         setSelectedRole(selectedRoles);
     };
-    const onPasswordChange = e => {
-        setPassword(e.target.value);
-    }
-    const onPasswordConfChange = e => {
-        setPasswordConfirmation(e.target.value);
-    }
-    const onEmailChange = e => {
-        setEmailAddress(e.target.value);
-    }
-    const onFullNameChange = e => {
-        setFullName(e.target.value);
-    }
 
 
-    const removeUser = async (confirmed) => {
-        if (!confirmed) {
-            showModal();
-        } else {
-            try {
-                await wazuhSecurity.security.deleteUser(currentUser.user);
-                ErrorHandler.info('User was successfully deleted', '');
-            } catch (error) {
-                ErrorHandler.handle(error, 'Error deleting user');
-            }
-            closeFlyout(false)
-        }
-    };
-
-    let modal;
-
-    if (isModalVisible) {
-        modal = (
-            <EuiOverlayMask>
-                <EuiConfirmModal
-                    title={`Delete user ${currentUser.user}`}
-                    onCancel={closeModal}
-                    onConfirm={async () => { closeModal(); await removeUser(true) }}
-                    cancelButtonText="Cancel"
-                    confirmButtonText="Delete"
-                    buttonColor="danger"
-                    defaultFocusedButton="confirm">
-                    <p>This operation can't be undone</p>
-                </EuiConfirmModal>
-            </EuiOverlayMask>
-        )
-    }
 
     return (
 
@@ -142,20 +58,6 @@ export const EditUser = ({ currentUser, closeFlyout }) => {
             </EuiFlyoutHeader>
             <EuiFlyoutBody>
                 <EuiForm component="form" style={{ padding: 24 }}>
-                    <EuiFormRow label="Full name">
-                        <EuiFieldText
-                            value={fullName}
-                            onChange={e => onFullNameChange(e)}
-                            aria-label="Use aria labels when no actual label is in use"
-                        />
-                    </EuiFormRow>
-                    <EuiFormRow label="Email address">
-                        <EuiFieldText
-                            value={emailAddress}
-                            onChange={e => onEmailChange(e)}
-                            aria-label="Use aria labels when no actual label is in use"
-                        />
-                    </EuiFormRow>
                     <EuiFormRow label="Roles"
                         helpText="Assign roles to the user from the roles available in your security plugin.">
 
@@ -169,51 +71,16 @@ export const EditUser = ({ currentUser, closeFlyout }) => {
                             data-test-subj="demoComboBox"
                         />
                     </EuiFormRow>
-                    {!editPassword &&
-                        <EuiButtonEmpty style={{ marginTop: 15 }} size="s" onClick={() => setEditPassword(true)}>
-                            Change password
-                        </EuiButtonEmpty>
-                    }
-                    {editPassword &&
-                        <>
-                            <EuiHorizontalRule margin="xs" />
-                            <EuiFormRow label="Password"
-                                isInvalid={passwordError}
-                                error={'Password must be at least 6 characters'}>
-                                <EuiFieldPassword
-                                    value={password}
-                                    onChange={e => onPasswordChange(e)}
-                                    aria-label="Use aria labels when no actual label is in use"
-                                />
-                            </EuiFormRow>
-                            <EuiFormRow label="Password confirmation"
-                                isInvalid={passwordConfirmationError}
-                                error={'Passwords do not match'}>
-                                <EuiFieldPassword
-                                    value={passwordConfirmation}
-                                    onChange={e => onPasswordConfChange(e)}
-                                    aria-label="Use aria labels when no actual label is in use"
-                                />
-                            </EuiFormRow>
-                        </>
-                    }
-
                     <EuiSpacer />
                     <EuiFlexGroup>
                         <EuiFlexItem grow={false}>
                             <EuiButton type="submit" fill onClick={editUser}>
-                                Edit user
+                                Apply
                             </EuiButton>
-                        </EuiFlexItem>
-                        <EuiFlexItem grow={false}>
-                            <EuiButtonEmpty type="submit" fill color="danger" onClick={async () => await removeUser(false)}>
-                                Delete user
-                            </EuiButtonEmpty>
                         </EuiFlexItem>
                     </EuiFlexGroup>
                 </EuiForm>
             </EuiFlyoutBody>
-            {modal}
         </EuiFlyout>
 
     )
--- public/components/security/users/users.tsx
+++ public/components/security/users/users.tsx
@@ -12,32 +12,19 @@ import {
 import { UsersTable } from './users-table';
 
 import {WazuhSecurity} from '../../../factories/wazuh-security'
-import {useApiRequest} from '../../common/hooks/useApiRequest';
-import { CreateUser } from './create-user';
 import { AppState } from '../../../react-services/app-state'
 import { EditUser } from './edit-user';
-import { ErrorHandler } from '../../../react-services/error-handler';
 
 export const Users = ({setSecurityError}) => {
-    const [isFlyoutVisible, setIsFlyoutVisible] = useState(false);
     const [isEditFlyoutVisible, setIsEditFlyoutVisible] = useState(false);
     const [editingUser, setEditingUser] = useState(false);
     const [users,setUsers] = useState([]);
     const getUsers = async() => {
         try{
-            const currentApi = JSON.parse(AppState.getCurrentAPI()).id;
             const wazuhSecurity = new WazuhSecurity();
-            const response = await wazuhSecurity.security.getUsers();
-            // Filter by users with the role "wazuh_user"
-            const wazuh_users = response.filter(item => { 
-                return item.roles.includes('wazuh_user')
-            });
-            // Show only roles that starts with "wazuh_<currentAPI>"
+            const wazuh_users = await wazuhSecurity.security.getUsers();
             const users = wazuh_users.map((item,idx) => {
-                const filteredRoles = item.roles.filter(role => {
-                    return role.startsWith(`wazuh_${currentApi}_`);
-                }).map(item => {return item.replace(`wazuh_${currentApi}_`,"")});
-                return {id: idx, user: item.username, roles: filteredRoles, full_name: item.full_name, email: item.email}
+                return {id: idx, user: item.username, roles: [], full_name: item.full_name, email: item.email}
             });
             setUsers(users)
         }catch(error){
@@ -48,30 +35,12 @@ export const Users = ({setSecurityError}) => {
         getUsers();
       }, []);
 
-
-
-    const closeFlyout = async() => {
-        setIsFlyoutVisible(false);
-        await getUsers();
-    };
-
-    let flyout;
-    if (isFlyoutVisible) {
-        flyout = (
-            <EuiOverlayMask onClick={(e) => {
-                e.target.className === 'euiOverlayMask' &&
-                    setIsFlyoutVisible(false)
-            }}>
-                <CreateUser closeFlyout={closeFlyout}/> 
-            </EuiOverlayMask >
-        );
-    }
+  
     let editFlyout;
     const closeEditFlyout = async() => {
         await getUsers();
         setIsEditFlyoutVisible(false);
     }
-
     if (isEditFlyoutVisible) {
         editFlyout = (
             <EuiOverlayMask onClick={(e) => {
@@ -97,19 +66,12 @@ export const Users = ({setSecurityError}) => {
                     </EuiTitle>
                 </EuiPageContentHeaderSection>
                 <EuiPageContentHeaderSection>
-                    <div>
-                        <EuiButton
-                            onClick={() => setIsFlyoutVisible(true)}>
-                            Create user
-                                </EuiButton>
-                        {flyout}
-                        {editFlyout}
-                    </div>
                 </EuiPageContentHeaderSection>
             </EuiPageContentHeader>
             <EuiPageContentBody>
                 <UsersTable users={users} editUserFlyover={showEditFlyover}></UsersTable>
             </EuiPageContentBody>
+            {editFlyout}
         </EuiPageContent>
     );
 };
