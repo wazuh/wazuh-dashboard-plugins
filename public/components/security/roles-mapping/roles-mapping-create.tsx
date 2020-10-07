8283597f5f323f9ec4eec5bf9646e5fe66a4af76
Pablo <pablotr9@correo.ugr.es>
2020-10-07 15:44:47 +0200

    Add requested changes

--- public/components/security/roles-mapping/roles-mapping-create.tsx
+++ public/components/security/roles-mapping/roles-mapping-create.tsx
@@ -1,139 +1,110 @@
-
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
-    EuiToolTip,
-    EuiButtonIcon,
-    EuiButton,
-    EuiTitle,
-    EuiFlyout,
-    EuiFlyoutHeader,
-    EuiFlyoutBody,
-    EuiForm,
-    EuiFormRow,
-    EuiSpacer,
-    EuiFlexGroup,
-    EuiFlexItem,
-    EuiComboBox,
-    EuiFieldText,
+  EuiTitle,
+  EuiFlyout,
+  EuiFlyoutHeader,
+  EuiFlyoutBody,
+  EuiForm,
+  EuiFormRow,
+  EuiSpacer,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiComboBox,
+  EuiFieldText,
 } from '@elastic/eui';
 import { WzRequest } from '../../../react-services/wz-request';
 import { ErrorHandler } from '../../../react-services/error-handler';
 import { RuleEditor } from './rule-editor';
 
 export const RolesMappingCreate = ({ closeFlyout, rolesEquivalences, roles }) => {
-    const [selectedRoles, setSelectedRoles] = useState([]);
-    const [ruleName, setRuleName] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
+  const [selectedRoles, setSelectedRoles] = useState([]);
+  const [ruleName, setRuleName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        initData();
-    }, []);
+  const getRolesList = (roles) => {
+    const list = roles.map(item => {
+      return { label: rolesEquivalences[item.id], id: item.id }
+    });
+    return list;
+  };
 
-    const initData = () => {
-    }
-
-    const getEquivalences = (roles) => {
-        const list = roles.map(item => {
-            return { label: rolesEquivalences[item], id: item }
-        });
-        return list;
-    };
-
-    const getRolesList = (roles) => {
-        const list = roles.map(item => {
-            return { label: rolesEquivalences[item.id], id: item.id }
-        });
-        return list;
-    };
-
-    const onChangeComboBox = (roles) => {
-        setSelectedRoles(roles);
-    }
-
-    const onChangeRuleName = (e) => {
-        setRuleName(e.target.value)
-    }
-
-    const createRule = async (toSaveRule) => {
-        try {
-            setIsLoading(true);
-            const formattedRoles = selectedRoles.map(item => {
-                return item.id;
-            });
-            const result = await WzRequest.apiReq(
-                'POST',
-                `/security/rules`,
-                {
-                    "name": ruleName,
-                    "rule": toSaveRule
-                }
-            );
-            const ruleId = ((((result.data || {}).data || {}).affected_items || [])[0] || {}).id;
-            
-            await Promise.all(formattedRoles.map(async (role) => {  
-                const data = await WzRequest.apiReq(
-                    'POST',
-                    `/security/roles/${role}/rules`,
-                    {
-                        params: {
-                            rule_ids: ruleId
-                        }
-                    }
-                );
-            }));
-            const msg = (result.data || {}).message || "Role mapping was successfully created"; 
-            ErrorHandler.info(msg);
-        } catch (error) {
-            ErrorHandler.handle(error, "There was an error");
+  const createRule = async (toSaveRule) => {
+    try {
+      setIsLoading(true);
+      const formattedRoles = selectedRoles.map(item => {
+        return item.id;
+      });
+      const result = await WzRequest.apiReq(
+        'POST',
+        `/security/rules`,
+        {
+          "name": ruleName,
+          "rule": toSaveRule
         }
-        closeFlyout(false);
-    }
+      );
+      const ruleId = ((((result.data || {}).data || {}).affected_items || [])[0] || {}).id;
 
-    return (
-        <EuiFlyout
-            onClose={() => closeFlyout(false)}>
-            <EuiFlyoutHeader hasBorder={false}>
-                <EuiTitle size="m">
-                    <h2>
-                       Create new role mapping &nbsp;
+      await Promise.all(formattedRoles.map(async (role) => await WzRequest.apiReq(
+          'POST',
+          `/security/roles/${role}/rules`,
+          {
+            params: {
+              rule_ids: ruleId
+            }
+          }
+        )));
+      const msg = (result.data || {}).message || "Role mapping was successfully created";
+      ErrorHandler.info(msg);
+    } catch (error) {
+      ErrorHandler.handle(error, "There was an error");
+    }
+    closeFlyout(false);
+  }
+
+  return (
+    <EuiFlyout
+      onClose={() => closeFlyout(false)}>
+      <EuiFlyoutHeader hasBorder={false}>
+        <EuiTitle size="m">
+          <h2>
+            Create new role mapping &nbsp;
                     </h2>
-                </EuiTitle>
-            </EuiFlyoutHeader>
-            <EuiFlyoutBody>
-                <EuiForm component="form" style={{ padding: 24 }}>
-                    <EuiFormRow label="Role name"
-                        isInvalid={false}
-                        error={'Please provide a role name'}
-                        helpText="Introduce a name for this role mapping.">
-                        <EuiFieldText
-                            placeholder=""
-                            value={ruleName}
-                            onChange={e => onChangeRuleName(e)}
-                            aria-label=""
-                        />
-                    </EuiFormRow>
-                    <EuiFormRow label="Roles"
-                        isInvalid={false}
-                        error={'At least one role must be selected.'}
-                        helpText="Assign roles to your users.">
-                        <EuiComboBox
-                            placeholder="Select policies"
-                            options={getRolesList(roles)}
-                            isDisabled={false}
-                            selectedOptions={selectedRoles}
-                            onChange={onChangeComboBox}
-                            isClearable={true}
-                            data-test-subj="demoComboBox"
-                        />
-                    </EuiFormRow>
-                    <EuiSpacer />
-                </EuiForm>
-                <EuiFlexGroup style={{ padding: "0px 24px 24px 24px" }}>
-                    <EuiFlexItem>
-                        <RuleEditor save={(rule) => createRule(rule)} initialRule={false} isReserved={false} isLoading={isLoading}></RuleEditor>
-                    </EuiFlexItem>
-                </EuiFlexGroup>
-            </EuiFlyoutBody>
-        </EuiFlyout>
-    );
+        </EuiTitle>
+      </EuiFlyoutHeader>
+      <EuiFlyoutBody>
+        <EuiForm component="form" style={{ padding: 24 }}>
+          <EuiFormRow label="Role name"
+            isInvalid={false}
+            error={'Please provide a role name'}
+            helpText="Introduce a name for this role mapping.">
+            <EuiFieldText
+              placeholder="Role name"
+              value={ruleName}
+              onChange={e => setRuleName(e.target.value)}
+            />
+          </EuiFormRow>
+          <EuiFormRow label="Roles"
+            isInvalid={false}
+            error={'At least one role must be selected.'}
+            helpText="Assign roles to your users.">
+            <EuiComboBox
+              placeholder="Select policies"
+              options={getRolesList(roles)}
+              isDisabled={false}
+              selectedOptions={selectedRoles}
+              onChange={(roles) => {setSelectedRoles(roles)}}
+              isClearable={true}
+              data-test-subj="demoComboBox"
+            />
+          </EuiFormRow>
+          <EuiSpacer />
+        </EuiForm>
+        <EuiFlexGroup style={{ padding: "0px 24px 24px 24px" }}>
+          <EuiFlexItem>
+            <RuleEditor save={(rule) => createRule(rule)} initialRule={false} isReserved={false} isLoading={isLoading}></RuleEditor>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </EuiFlyoutBody>
+    </EuiFlyout>
+  );
 };
--- public/components/security/roles-mapping/roles-mapping.tsx
+++ public/components/security/roles-mapping/roles-mapping.tsx
@@ -1,112 +1,102 @@
 import React, { useState, useEffect } from 'react';
 import {
-    EuiPageContent,
-    EuiPageContentHeader,
-    EuiPageContentHeaderSection,
-    EuiPageContentBody,
-    EuiButton,
-    EuiTitle,
-    EuiOverlayMask,
+  EuiPageContent,
+  EuiPageContentHeader,
+  EuiPageContentHeaderSection,
+  EuiPageContentBody,
+  EuiButton,
+  EuiTitle,
+  EuiOverlayMask,
 } from '@elastic/eui';
 import { RolesMappingTable } from './roles-mapping-table';
 import { RolesMappingEdit } from './roles-mapping-edit';
 import { RolesMappingCreate } from './roles-mapping-create';
 import { WzRequest } from '../../../react-services/wz-request';
+import { ErrorHandler } from '../../../react-services/error-handler';
 
 export const RolesMapping = () => {
-    const [isEditingRule, setIsEditingRule] = useState(false);
-    const [isCreatingRule, setIsCreatingRule] = useState(false);
-    const [rules,setRules] = useState([]);
-    const [loadingTable, setLoadingTable] = useState(true);
-    const [selectedRule, setSelectedRule] = useState({});
-    const [rolesEquivalences, setRolesEquivalences] = useState({});
-    const [roles, setRoles] = useState([]);
+  const [isEditingRule, setIsEditingRule] = useState(false);
+  const [isCreatingRule, setIsCreatingRule] = useState(false);
+  const [rules, setRules] = useState([]);
+  const [loadingTable, setLoadingTable] = useState(true);
+  const [selectedRule, setSelectedRule] = useState({});
+  const [rolesEquivalences, setRolesEquivalences] = useState({});
+  const [roles, setRoles] = useState([]);
 
-    useEffect(() => {
-        initData();
-    }, []);
+  useEffect(() => {
+    initData();
+  }, []);
 
 
-    const initData = async() => {
-        setLoadingTable(true);
-        const roles = await WzRequest.apiReq(
-            'GET',
-            '/security/roles',
-            {}
-        );
-        const roles_data = (((roles || {}).data || {}).data || {}).affected_items || [];
-        setRoles(roles_data);
-
-        const rolesObject = {};
-        roles_data.forEach((item) => {
-            rolesObject[item.id] = item.name;
-        })
-        setRolesEquivalences(rolesObject);
-
-        const request = await WzRequest.apiReq(
-            'GET',
-            '/security/rules',
-            {}
-        );
-        const rules = (((request || {}).data || {}).data || {}).affected_items || [];
-        setRules(rules);
-
-        
-
-        setLoadingTable(false);
+  const initData = async () => {
+    try{
+      setLoadingTable(true);
+      const roles = await WzRequest.apiReq(
+        'GET',
+        '/security/roles',
+        {}
+      );
+      const roles_data = (((roles || {}).data || {}).data || {}).affected_items || [];
+      setRoles(roles_data);
+  
+      const rolesObject = {};
+      roles_data.forEach((item) => {
+        rolesObject[item.id] = item.name;
+      })
+      setRolesEquivalences(rolesObject);
+  
+      const request = await WzRequest.apiReq(
+        'GET',
+        '/security/rules',
+        {}
+      );
+      const rules = (((request || {}).data || {}).data || {}).affected_items || [];
+      setRules(rules);      
+    }catch(error){
+      ErrorHandler.error(error);
     }
+    setLoadingTable(false);
+  }
 
+  let editFlyout;
+  if (isEditingRule) {
+    editFlyout = (<EuiOverlayMask onClick={() => {
+      setIsEditingRule(false)
+    }}>
+      <RolesMappingEdit rule={selectedRule} closeFlyout={(isVisible) => { setIsEditingRule(isVisible); initData() }} rolesEquivalences={rolesEquivalences} roles={roles} />
+    </EuiOverlayMask>)
+  }
+  let createFlyout;
+  if (isCreatingRule) {
+    editFlyout = (<EuiOverlayMask onClick={() => {
+      setIsCreatingRule(false)
+    }}>
+      <RolesMappingCreate closeFlyout={(isVisible) => { setIsCreatingRule(isVisible); initData() }} rolesEquivalences={rolesEquivalences} roles={roles} />
+    </EuiOverlayMask>)
+  }
 
-    const setIsFlyoutVisible = (isVisible) => {
-        setIsEditingRule(isVisible);
-    }
-    
-    const setIsCreateFlyoutVisible = (isVisible) => {
-        setIsCreatingRule(isVisible);
-    }
-
-
-    let editFlyout;
-    if(isEditingRule){
-        editFlyout = ( <EuiOverlayMask onClick={(e) => {
-            e.target.className === 'euiOverlayMask' &&
-            setIsFlyoutVisible(false)
-        }}>
-            <RolesMappingEdit rule={selectedRule} closeFlyout={(isVisible) => {setIsFlyoutVisible(isVisible); initData() }} rolesEquivalences={rolesEquivalences} roles={roles}/>
-        </EuiOverlayMask>)
-    }
-    let createFlyout;
-    if(isCreatingRule){
-        editFlyout = ( <EuiOverlayMask onClick={(e) => {
-            e.target.className === 'euiOverlayMask' &&
-            setIsCreateFlyoutVisible(false)
-        }}>
-            <RolesMappingCreate  closeFlyout={(isVisible) => {setIsCreateFlyoutVisible(isVisible); initData() }} rolesEquivalences={rolesEquivalences} roles={roles}/>
-        </EuiOverlayMask>)
-    }
-   
-    return (
-        <EuiPageContent>
-            <EuiPageContentHeader>
-                <EuiPageContentHeaderSection>
-                    <EuiTitle>
-                        <h2>Roles mapping</h2>
-                    </EuiTitle>
-                </EuiPageContentHeaderSection>
-                <EuiPageContentHeaderSection>
-                    <div>
-                        <EuiButton
-                            onClick={() => setIsCreateFlyoutVisible(true)}>
-                            Create role mapping
-                                </EuiButton>
-                        {createFlyout}
-                        {editFlyout}
-                    </div>
-                </EuiPageContentHeaderSection>
-            </EuiPageContentHeader>
-            <EuiPageContentBody>
-               <RolesMappingTable rolesEquivalences={rolesEquivalences} loading={loadingTable} rules={rules} editRule={(item) => {setSelectedRule(item); setIsEditingRule(true);}} updateRules={async() => await initData()}></RolesMappingTable>
-            </EuiPageContentBody>
-        </EuiPageContent>
-    );
+  return (
+    <EuiPageContent>
+      <EuiPageContentHeader>
+        <EuiPageContentHeaderSection>
+          <EuiTitle>
+            <h2>Roles mapping</h2>
+          </EuiTitle>
+        </EuiPageContentHeaderSection>
+        <EuiPageContentHeaderSection>
+          <div>
+            <EuiButton
+              onClick={() => setIsCreatingRule(true)}>
+              Create role mapping
+            </EuiButton>
+            {createFlyout}
+            {editFlyout}
+          </div>
+        </EuiPageContentHeaderSection>
+      </EuiPageContentHeader>
+      <EuiPageContentBody>
+        <RolesMappingTable rolesEquivalences={rolesEquivalences} loading={loadingTable} rules={rules} editRule={(item) => { setSelectedRule(item); setIsEditingRule(true); }} updateRules={async () => await initData()}></RolesMappingTable>
+      </EuiPageContentBody>
+    </EuiPageContent>
+  );
 };
