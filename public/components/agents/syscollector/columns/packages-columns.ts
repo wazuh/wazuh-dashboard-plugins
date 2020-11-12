d95223582006d2f488c89520527bd74a7cc30ec4
Jose Sanchez <josesanxez@gmail.com>
2020-11-12 09:00:12 +0100

    Prettier

--- public/components/agents/syscollector/columns/packages-columns.ts
+++ public/components/agents/syscollector/columns/packages-columns.ts
@@ -1,22 +1,22 @@
 const windowsColumns = [
-    { id: 'name' },
-    { id: 'architecture', width: "10%" },
-    { id: 'version' },
-    { id: 'vendor', width: '30%' }
+  { id: 'name' },
+  { id: 'architecture', width: "10%" },
+  { id: 'version' },
+  { id: 'vendor', width: '30%' }
 ];
 const linuxColumns = [
-    { id: 'name' },
-    { id: 'architecture', width: '10%' },
-    { id: 'version' },
-    { id: 'vendor', width: "30%" },
-    { id: 'description', width: '30%' }
+  { id: 'name' },
+  { id: 'architecture', width: '10%' },
+  { id: 'version' },
+  { id: 'vendor', width: "30%" },
+  { id: 'description', width: '30%' }
 ];
 const MacColumns = [
-    { id: 'name' },
-    { id: 'version' },
-    { id: 'format' },
-    { id: 'location', width: "30%" },
-    { id: 'description', width: '20%' }
+  { id: 'name' },
+  { id: 'version' },
+  { id: 'format' },
+  { id: 'location', width: "30%" },
+  { id: 'description', width: '20%' }
 ];
 
 export const packagesColumns = {
--- public/components/agents/syscollector/columns/ports-columns.ts
+++ public/components/agents/syscollector/columns/ports-columns.ts
@@ -1,19 +1,19 @@
 const windowsColumns = [
-    { id: "process" },
-    { id: "local.ip", sortable: false },
-    { id: "local.port", sortable: false },
-    { id: "state" },
-    { id: "protocol" }
+  { id: "process" },
+  { id: "local.ip", sortable: false },
+  { id: "local.port", sortable: false },
+  { id: "state" },
+  { id: "protocol" }
 ];
 const defaultColumns = [
-    { id: "local.ip", sortable: false },
-    { id: "local.port", sortable: false },
-    { id: "state" },
-    { id: "protocol" }
+  { id: "local.ip", sortable: false },
+  { id: "local.port", sortable: false },
+  { id: "state" },
+  { id: "protocol" }
 ];
 
 export const portsColumns = {
-    'windows': windowsColumns,
-    'linux': defaultColumns,
-    'apple': defaultColumns
+  'windows': windowsColumns,
+  'linux': defaultColumns,
+  'apple': defaultColumns
 }
