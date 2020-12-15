e8dd674f2df6f7c696e126a5ad9bc4b95bab8f2a
gabiwassan <gabriel.wassan@wazuh.com>
2020-12-15 10:41:34 -0300

    Added header Wazuh Copyright.

--- public/react-services/wz-user-permissions.test.ts
+++ public/react-services/wz-user-permissions.test.ts
@@ -1,20 +1,13 @@
 /*
- * Licensed to Elasticsearch B.V. under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch B.V. licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Wazuh app - React hook for get query of Kibana searchBar
+ * Copyright (C) 2015-2020 Wazuh, Inc.
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Find more information about this on the LICENSE file.
  */
 
 import { WzUserPermissions } from './wz-user-permissions';
