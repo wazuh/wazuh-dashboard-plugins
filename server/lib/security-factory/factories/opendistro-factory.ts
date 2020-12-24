bc7ca9b52749890ab5393385b0ef0357c31937ea
Harduino <mail@harduino.com>
2020-12-24 09:50:39 +0300

    siemonster :: fix route in getCurrentUser when AWS Elasticsearch is used

--- server/lib/security-factory/factories/opendistro-factory.ts
+++ server/lib/security-factory/factories/opendistro-factory.ts
@@ -11,7 +11,7 @@ export class OpendistroFactory implements ISecurityFactory {
     try {
       const elasticRequest = this.server.plugins.elasticsearch.getCluster('data');
       const params = {
-        path: `_opendistro/_security/api/account`,
+        path: `/_opendistro/_security/api/account`,
         method: 'GET',
       };
       
