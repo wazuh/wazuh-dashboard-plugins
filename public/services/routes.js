94a7837b4577a22a4ec33db6d902eb8ea001174e
Pablo Martínez <pablomarga@hotmail.com>
2021-01-08 11:05:11 +0100

    Fixed reload when changing section

--- public/services/routes.js
+++ public/services/routes.js
@@ -42,7 +42,7 @@ import { AppState } from '../react-services/app-state';
 import { getAngularModule } from '../kibana-services';
 
 const assignPreviousLocation = ($rootScope, $location) => {
-  const path = $location.path();	
+  const path = $location.path();
   const params = $location.search();
   // Save current location if we aren't performing a health-check, to later be able to come back to the same tab
   if (!path.includes('/health-check')) {
@@ -162,7 +162,8 @@ app.config(($routeProvider) => {
   })
   .when('/settings', {
     template: settingsTemplate,
-    resolve: { enableWzMenu, nestedResolve, ip, savedSearch }
+    resolve: { enableWzMenu, nestedResolve, ip, savedSearch },
+    reloadOnSearch: false
   })
   .when('/security', {
     template: securityTemplate,
