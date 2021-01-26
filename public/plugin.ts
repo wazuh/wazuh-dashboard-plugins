74a11c31aeb4ca61e2f6804d23d173c2df543b53
Franco Charriol <franco.charriol@wazuh.com>
2021-01-26 14:53:07 -0300

    [4.0-7.10] Removed kibana alert for security (#2806)
    * Removed kibana alert for security
    
    * Added validation for security_oss
    
    * Rename plugin dependency to securityOss
    
    * Added securityOss as required plugin

--- public/plugin.ts
+++ public/plugin.ts
@@ -64,6 +64,11 @@ export class WazuhPlugin implements Plugin<WazuhSetup, WazuhStart, WazuhSetupPlu
   }
 
   public async start(core: CoreStart, plugins: AppPluginStartDependencies): Promise<WazuhStart> {
+    // hide security alert 
+    if(plugins.securityOss) {
+      plugins.securityOss.insecureCluster.hideAlert(true);
+    }
+    
     // we need to register the application service at setup, but to render it
     // there are some start dependencies necessary, for this reason
     // initializeInnerAngular + initializeServices are assigned at start and used
@@ -83,6 +88,7 @@ export class WazuhPlugin implements Plugin<WazuhSetup, WazuhStart, WazuhSetupPlu
       setAngularModule(module);
       this.innerAngularInitialized = true;
     };
+
 
     setCore(core);
     setPlugins(plugins);
