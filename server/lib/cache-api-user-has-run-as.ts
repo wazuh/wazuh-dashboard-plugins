763a4c8ca8d56e8b2ea41d9001bef44efdf58b98
gabiwassan <gabriel.wassan@wazuh.com>
2021-01-28 15:06:11 -0300

    Changed const name to avoid confusion.

--- server/lib/cache-api-user-has-run-as.ts
+++ server/lib/cache-api-user-has-run-as.ts
@@ -51,11 +51,11 @@ export const APIUserAllowRunAs = {
         {},
         { apiHostID: apiId }
       );
-      const APIUserAllowRunAs = response.data.data.affected_items[0].allow_run_as ? API_USER_STATUS_RUN_AS.ENABLED : API_USER_STATUS_RUN_AS.NOT_ALLOWED;
-      
+      const statusUserAllowRunAs = response.data.data.affected_items[0].allow_run_as ? API_USER_STATUS_RUN_AS.ENABLED : API_USER_STATUS_RUN_AS.NOT_ALLOWED;
+
       // Cache the run_as for the API user
-      CacheInMemoryAPIUserAllowRunAs.set(apiId, api.username, APIUserAllowRunAs);
-      return APIUserAllowRunAs;
+      CacheInMemoryAPIUserAllowRunAs.set(apiId, api.username, statusUserAllowRunAs);
+      return statusUserAllowRunAs;
     }catch(error){
       log('APIUserAllowRunAs:check', error.message || error);
       return API_USER_STATUS_RUN_AS.DISABLED;
