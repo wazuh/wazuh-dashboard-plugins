2b8984789ffe5ab50a0705ebdbbefe53d34ea668
Toni <34042064+Desvelao@users.noreply.github.com>
2021-02-18 18:26:45 +0100

    Fix a non-stop error in Manage agents when the user has no permissions (#2976)
    * fix(frontend): Fix non-stop error in Manage agents when the user has not permissions
    
      - Manage the error loading the selected agent while loop
      - Added a prompt to th the required permission to the group detail view
    
    * fix(frontend): Added a PR to changelog

--- public/controllers/management/components/management/groups/actions-buttons-agents.js
+++ public/controllers/management/components/management/groups/actions-buttons-agents.js
@@ -89,8 +89,8 @@ class WzGroupsActionButtonsAgents extends Component {
   showManageAgents() {
     const { itemDetail } = this.props.state;
 
-    this.props.updateShowAddAgents(true);
     this.props.groupsProps.showAddingAgents(true, itemDetail);
+    this.props.updateShowAddAgents(true);
   }
 
   closePopover() {
