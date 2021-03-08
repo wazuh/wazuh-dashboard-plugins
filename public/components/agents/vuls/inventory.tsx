e14d6f32ae061260dc61e1dbc74a1d7b39bdab90
Federico Rodriguez <federico.rodriguez@wazuh.com>
2021-03-08 18:03:34 -0300

    draft CVEs table view

---
+++ public/components/agents/vuls/inventory.tsx
@@ -0,0 +1,307 @@
+/*
+ * Wazuh app - Integrity monitoring components
+ * Copyright (C) 2015-2021 Wazuh, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Find more information about this on the LICENSE file.
+ */
+
+import React, { Component, Fragment } from 'react';
+import {
+  EuiPanel,
+  EuiPage,
+  EuiTabs,
+  EuiTab,
+  EuiTitle,
+  EuiLoadingSpinner,
+  EuiEmptyPrompt,
+  EuiSpacer,
+  EuiProgress,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiLink,
+  EuiHorizontalRule,
+  EuiButtonEmpty
+} from '@elastic/eui';
+import {
+  InventoryTable,
+  FilterBar
+} from './inventory/';
+import { WzRequest } from '../../../react-services/wz-request';
+import exportCsv from '../../../react-services/wz-csv';
+import { getToasts }  from '../../../kibana-services';
+import { ICustomBadges } from '../../wz-search-bar/components';
+import { filtersToObject } from '../../wz-search-bar';
+import { WzEmptyPromptNoPermissions } from "../../common/permissions/prompt";
+
+import mockup from './vuls-mockup';
+
+export class Inventory extends Component {
+  _isMount = false;
+  state: {
+    filters: [];
+    // selectedTabId: 'files' | 'registry';
+    totalItemsFile: number;
+    totalItemsRegistry: number;
+    isLoading: Boolean;
+    syscheck: [];
+    customBadges: ICustomBadges[];
+    isConfigured: Boolean;
+  };
+
+  props: any;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      filters: [],
+      syscheck: [],
+      // selectedTabId: 'files',
+      totalItemsFile: 0,
+      totalItemsRegistry: 0,
+      isLoading: true,
+      customBadges: [],
+      isConfigured: false
+    }
+    this.onFiltersChange.bind(this);
+  }
+
+  async componentDidMount() {
+    this._isMount = true;
+    await this.loadAgent();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (JSON.stringify(this.props.agent) !== JSON.stringify(prevProps.agent)){
+      this.setState({isLoading: true}, this.loadAgent)
+    }
+  }
+
+  componentWillUnmount() {
+    this._isMount = false;
+  }
+
+  async loadAgent() {
+    const agentPlatform  = ((this.props.agent || {}).os || {}).platform;
+    const {totalItemsFile, syscheck} = await this.getItemNumber('file');
+    const totalItemsRegistry = agentPlatform === 'windows' ? await this.getItemNumber('registry') : 0;
+    const isConfigured = await this.isConfigured();
+    if (this._isMount){
+      this.setState({ totalItemsFile, totalItemsRegistry, syscheck, isLoading: false, isConfigured });
+    }
+  }
+
+  // Do not load the localStorage filters when changing tabs
+  // componentDidUpdate(prevProps, prevState) {
+  //   const { selectedTabId } = this.state;
+  //   if (selectedTabId !== prevState.selectedTabId) {
+  //     const filters = this.getStoreFilters(this.props);
+  //     this.setState({ filters });
+  //   }
+  // }
+
+  // tabs() {
+  //   let auxTabs = [
+  //     {
+  //       id: 'vulnerabilities',
+  //       name: `Vulnerabilities ${this.state.isLoading === true ? '' : '(' + this.state.totalItemsFile + ')'}`,
+  //       disabled: false,
+  //     },
+  //   ];
+  //   // const platform = (this.props.agent.os || {}).platform || "other";
+  //   //  platform  === 'windows' ? auxTabs.push(
+  //   //   {
+  //   //     id: 'registry',
+  //   //     name: `Windows Registry ${this.state.isLoading === true ? '' : '(' + this.state.totalItemsRegistry + ')'}`,
+  //   //     disabled: false,
+  //   //   },
+  //   // ) : null;
+  //   return (auxTabs);
+  // }
+
+  getStoreFilters(props) {
+    const { section, selectView, agent } = props;
+    const filters = JSON.parse(window.localStorage.getItem(`wazuh-${section}-${selectView}-vulnerabilities-${agent['id']}`) || '{}');
+    return filters;
+  }
+
+  setStoreFilters(filters) {
+    const { section, selectView, agent } = this.props;
+    window.localStorage.setItem(`wazuh-${section}-${selectView}-vulnerabilities-${agent['id']}`, JSON.stringify(filters))
+  }
+
+  onFiltersChange = (filters) => {
+    // this.setStoreFilters(filters);
+    this.setState({ filters });
+  }
+
+  onTotalItemsChange = (totalItems: number) => {
+    this.setState({ totalItemsFile: totalItems });
+  }
+
+  // onSelectedTabChanged = id => {
+  //   this.setState({ selectedTabId: id });
+  // }
+
+  buildFilter() {
+    const filters = filtersToObject(this.state.filters);
+    const filter = {
+      ...filters,
+      limit: '15' 
+    };
+    return filter;
+  }
+
+  async getItemNumber() {
+    try {
+      const agentID = this.props.agent.id;
+      // const response = await WzRequest.apiReq('GET', `/syscheck/${agentID}`, {
+      //   params: this.buildFilter(),
+      // });
+      const response = mockup;
+      // if (type === 'file') {
+        return {
+          totalItemsFile: ((response.data || {}).data || {}).total_affected_items || 0,
+          syscheck: ((response.data || {}).data || {}).affected_items || [],
+        };
+      // }
+      // return ((response.data || {}).data || {}).total_affected_items || 0;
+    } catch (error) {
+      this.setState({ isLoading: false });
+      this.showToast('danger', error, 3000);
+    }
+  }
+
+  renderTabs() {
+    // const tabs = this.tabs();
+    const { isLoading } = this.state;
+    
+      return (
+        <EuiFlexGroup>
+          <EuiFlexItem>
+            <EuiTitle size="s">
+              <h1>Vulnerabilities&nbsp;{isLoading === true && <EuiLoadingSpinner size="s" />}</h1>
+            </EuiTitle>
+          </EuiFlexItem>
+          <EuiFlexItem grow={false}>
+            <EuiButtonEmpty iconType="importAction" onClick={() => this.downloadCsv()}>
+              Export formatted
+            </EuiButtonEmpty>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      )
+  }
+
+  showToast = (color, title, time) => {
+    getToasts().add({
+      color: color,
+      title: title,
+      toastLifeTimeMs: time,
+    });
+  };
+
+  async downloadCsv() {
+    const { filters } = this.state;
+    try {
+      const filtersObject = filtersToObject(filters);
+      const formatedFilters = Object.keys(filtersObject).map(key => ({name: key, value: filtersObject[key]}));
+      this.showToast('success', 'Your download should begin automatically...', 3000);
+      await exportCsv(
+        '/syscheck/' + this.props.agent.id,
+        [
+          { name: 'type', value: 'vulnerabilities' },
+          ...formatedFilters
+        ],
+        `vuls-vulnerabilities`
+      );
+    } catch (error) {
+      this.showToast('danger', error, 3000);
+    }
+  }
+
+  renderTable() {
+    const { filters, syscheck,  totalItemsFile } = this.state;
+    return (
+      <div>
+        <FilterBar
+          filters={filters}
+          onFiltersChange={this.onFiltersChange}
+          // selectView={selectedTabId}
+          agent={this.props.agent} />
+          <InventoryTable
+            {...this.props}
+            filters={filters}
+            items={syscheck}
+            totalItems={totalItemsFile}
+            onFiltersChange={this.onFiltersChange}
+            onTotalItemsChange={this.onTotalItemsChange}/>
+      </div>
+    )
+  }
+
+  noConfigured() {
+    return (
+      <EuiEmptyPrompt
+        iconType="filebeatApp"
+        title={<h2>Vulnerabilities is not configured for this agent</h2>}
+        body={<Fragment>
+          <EuiHorizontalRule margin='s' />
+          <EuiLink
+            href='https://documentation.wazuh.com/current/user-manual/capabilities/file-integrity/index.html'
+            target="_blank"
+            style={{ textAlign: "center" }}
+          >
+            https://documentation.wazuh.com/current/user-manual/capabilities/file-integrity/index.html
+          </EuiLink>
+          <EuiHorizontalRule margin='s' />
+        </Fragment>}
+      />);
+  }
+
+  loadingInventory() {
+    return <EuiPage>
+      <EuiFlexGroup>
+        <EuiFlexItem>
+          <EuiProgress size="xs" color="primary" />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </EuiPage>;
+  }
+
+  async isConfigured() {
+    try {
+      const response = await WzRequest.apiReq(
+        'GET',
+        `/agents/${this.props.agent.id}/config/syscheck/syscheck`,
+        {}
+      );
+
+      return (((response.data || {}).data).syscheck || {}).disabled === 'no';
+    } catch (error) {
+      return false;
+    }
+  }
+
+  render() {
+    const { isLoading, isConfigured } = this.state;
+    if (isLoading) {
+      return this.loadingInventory()
+    }
+    const table = this.renderTable();
+    const tabs = this.renderTabs();
+
+    return isConfigured
+      ? (<EuiPage>
+        <EuiPanel>
+          {tabs}
+          <EuiSpacer size={(((this.props.agent || {}).os || {}).platform || false) === 'windows' ? 's' : 'm'} />
+          {table}
+        </EuiPanel>
+      </EuiPage>)
+      : this.noConfigured()
+  }
+}
---
+++ public/components/agents/vuls/inventory/table.tsx
@@ -0,0 +1,270 @@
+/*
+ * Wazuh app - Integrity monitoring table component
+ * Copyright (C) 2015-2021 Wazuh, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Find more information about this on the LICENSE file.
+ */
+
+import React, { Component } from 'react';
+import {
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiBasicTable,
+  Direction,
+  EuiOverlayMask,
+} from '@elastic/eui';
+import { WzRequest } from '../../../../react-services/wz-request';
+import { FlyoutDetail } from './flyout';
+import { filtersToObject, IFilter } from '../../../wz-search-bar';
+// import { TimeService } from '../../../../react-services/time-service';
+
+export class InventoryTable extends Component {
+  state: {
+    syscheck: []
+    error?: string
+    pageIndex: number
+    pageSize: number
+    totalItems: number
+    sortField: string
+    isFlyoutVisible: Boolean
+    sortDirection: Direction
+    isLoading: boolean
+    currentItem: {
+      vul: string
+    },
+    syscheckItem: {}
+  };
+
+  props!: {
+    filters: IFilter[]
+    agent: any
+    items: []
+    totalItems: number,
+    onTotalItemsChange: Function
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      syscheck: props.items,
+      pageIndex: 0,
+      pageSize: 15,
+      totalItems: 0,
+      sortField: 'file',
+      sortDirection: 'asc',
+      isLoading: false,
+      isFlyoutVisible: false,
+      currentItem: {
+        vul: ""
+      },
+      syscheckItem: {}
+    }
+  }
+
+  async componentDidMount() {
+    const regex = new RegExp('file=' + '[^&]*');
+    const match = window.location.href.match(regex);
+    this.setState({totalItems: this.props.totalItems});
+    if (match && match[0]) {
+      const file = match[0].split('=')[1];
+      this.showFlyout(decodeURIComponent(file), true);
+    }
+  }
+
+  closeFlyout() {
+    this.setState({ isFlyoutVisible: false, currentItem: {} });
+  }
+
+  async showFlyout(file, item, redirect = false) {
+    
+    let fileData = false;
+    if (!redirect) {
+      fileData = this.state.syscheck.filter(item => {
+        return item.file === file;
+      })
+    } else {
+      const response = await WzRequest.apiReq('GET', `/syscheck/${this.props.agent.id}`, {
+        params: { 
+          'file': file 
+        }
+      });
+      fileData = ((response.data || {}).data || {}).affected_items || [];
+    }
+    if (!redirect)
+      window.location.href = window.location.href += `&file=${file}`;
+    //if a flyout is opened, we close it and open a new one, so the components are correctly updated on start.
+    this.setState({ isFlyoutVisible: false }, () => this.setState({ isFlyoutVisible: true, currentItem: file, syscheckItem: item }));
+  }
+
+  async componentDidUpdate(prevProps) {
+    const { filters } = this.props;
+    if (JSON.stringify(filters) !== JSON.stringify(prevProps.filters)) {
+      this.setState({ pageIndex: 0, isLoading: true }, this.getSyscheck);
+    }
+  }
+
+  async getSyscheck() {
+    const agentID = this.props.agent.id;
+    try {
+      const syscheck = await WzRequest.apiReq(
+      'GET',
+      `/syscheck/${agentID}`,
+      { params: this.buildFilter()},
+      );
+
+      this.props.onTotalItemsChange((((syscheck || {}).data || {}).data || {}).total_affected_items);
+      
+      this.setState({
+        syscheck: (((syscheck || {}).data || {}).data || {}).affected_items || {},
+        totalItems: (((syscheck || {}).data || {}).data || {}).total_affected_items - 1,
+        isLoading: false,
+        error: undefined
+      });
+    } catch (error) {
+      this.setState({error, isLoading: false})
+    }
+}
+
+  buildSortFilter() {
+    const { sortField, sortDirection } = this.state;
+
+    const field = (sortField === 'os_name') ? '' : sortField;
+    const direction = (sortDirection === 'asc') ? '+' : '-';
+
+    return direction + field;
+  }
+
+  buildFilter() {
+    const { pageIndex, pageSize } = this.state;
+    const filters = filtersToObject(this.props.filters);
+    const filter = {
+      ...filters,
+      offset: pageIndex * pageSize,
+      limit: pageSize,
+      sort: this.buildSortFilter(),
+      type: 'file'
+    };
+    return filter;
+  }
+
+  onTableChange = ({ page = {}, sort = {} }) => {
+    const { index: pageIndex, size: pageSize } = page;
+    const { field: sortField, direction: sortDirection } = sort;
+    this.setState({
+      pageIndex,
+      pageSize,
+      sortField,
+      sortDirection,
+      isLoading: true,
+    },
+      () => this.getSyscheck()
+    );
+  };
+
+  columns() {
+    let width;
+    (((this.props.agent || {}).os || {}).platform || false) === 'windows' ? width = '60px' : width = '80px';
+    return [
+      {
+        field: 'name',
+        name: 'Name',
+        sortable: true,
+        width: '100px'
+      },
+      {
+        field: 'cve',
+        name: 'CVE',
+        sortable: true,
+        truncateText: true,
+        width: `${width}`
+      },
+      {
+        field: 'architecture',
+        name: 'Architecture',
+        sortable: true,
+        width: '100px'
+      },      
+      {
+        field: 'version',
+        name: 'Version',
+        sortable: true,
+        truncateText: true,
+        width: `${width}`
+      }
+    ]
+  }
+
+  renderTable() {
+    const getRowProps = item => {
+      const { itemDetails } = item;
+      return {
+        'data-test-subj': `row-${itemDetails}`,
+        onClick: () => this.showFlyout(itemDetails, item),
+      };
+    };
+
+    const { syscheck, pageIndex, pageSize, totalItems, sortField, sortDirection, isLoading, error } = this.state;
+    const columns = this.columns();
+    const pagination = {
+      pageIndex: pageIndex,
+      pageSize: pageSize,
+      totalItemCount: totalItems,
+      pageSizeOptions: [15, 25, 50, 100],
+    }
+    const sorting = {
+      sort: {
+        field: sortField,
+        direction: sortDirection,
+      },
+    };
+
+    return (
+      <EuiFlexGroup>
+        <EuiFlexItem>
+          <EuiBasicTable
+            items={syscheck}
+            error={error}
+            columns={columns}
+            pagination={pagination}
+            onChange={this.onTableChange}
+            rowProps={getRowProps}
+            sorting={sorting}
+            itemId="vulnerabilitiy"
+            isExpandable={true}
+            loading={isLoading}
+          />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    );
+  }
+
+  render() {
+    const table = this.renderTable();
+    return (
+      <div className='wz-inventory'>
+        {table}
+        {this.state.isFlyoutVisible &&
+          <EuiOverlayMask
+            headerZindexLocation="below"
+            onClick={() => this.closeFlyout() } >
+            <FlyoutDetail
+              vulName={this.state.currentItem}
+              agentId={this.props.agent.id}
+              item={this.state.syscheckItem}
+              closeFlyout={() => this.closeFlyout()}
+              type='vulnerability'
+              view='inventory'
+              showViewInEvents={true}
+              {...this.props} />
+          </EuiOverlayMask>
+        }
+      </div>
+    )
+  }
+}
