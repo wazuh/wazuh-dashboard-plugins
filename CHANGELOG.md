240dd0584ce7676904134a50e16f1fe08e659d1f
Federico Rodriguez <federico.rodriguez@wazuh.com>
2021-03-19 13:55:36 -0300

    Added custom message for each possible run_as setup (#3048)
    * Added custom message for each possible run_as setup
    
    * added new run_as status, refactor enum
    
    * fixed allow_run_as undefined default message
    
    * Code cleaning + cache-api-user-has-run-as fix
    
    * Small message improvement
    
    * Changed enum API_USER_STATUS_RUN_AS INTERFACE_DISABLED to USER_DISABLED + canUse api fix
    
    * Fixed Enum USER_NOT_ALLOWED name
    
    * Added changelog
    
    * fix falsy run_as condition
    
    * fix Changelog
    
    * fix API configuration table warning

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,7 @@ All notable changes to the Wazuh app project will be documented in this file.
 - Creation of index pattern after the default one is changes in Settings [#2985](https://github.com/wazuh/wazuh-kibana-app/pull/2985)
 - Added node name of agent list and detail [#3039](https://github.com/wazuh/wazuh-kibana-app/pull/3039)
 - Added loading view while the user is logging to prevent permissions prompts [#3041](https://github.com/wazuh/wazuh-kibana-app/pull/3041)
+- Added custom message for each possible run_as setup [#3048](https://github.com/wazuh/wazuh-kibana-app/pull/3048)
 
 ### Fixed
 
--- public/components/security/main.tsx
+++ public/components/security/main.tsx
@@ -117,11 +117,31 @@ export const WzSecurity = compose(
   };
 
 
-  const isNotRunAs = () => {
+  const isNotRunAs = (allowRunAs) => {
+    let runAsWarningTxt = '';
+    switch (allowRunAs) {
+      case API_USER_STATUS_RUN_AS.HOST_DISABLED:
+        runAsWarningTxt =
+          'For the role mapping to take effect, enable run_as in /usr/share/kibana/data/wazuh/config/wazuh.yml configuration file, restart the Kibana service and clear your browser cache and cookies.';
+        break;
+      case API_USER_STATUS_RUN_AS.USER_NOT_ALLOWED:
+        runAsWarningTxt =
+          'The role mapping has no effect because the current Wazuh API user has allow_run_as disabled.';
+        break;
+      case API_USER_STATUS_RUN_AS.ALL_DISABLED:
+        runAsWarningTxt =
+          'For the role mapping to take effect, enable run_as in /usr/share/kibana/data/wazuh/config/wazuh.yml configuration file and set the current Wazuh API user allow_run_as to true. Restart the Kibana service and clear your browser cache and cookies.';
+        break;
+      default:
+        runAsWarningTxt =
+          'The role mapping has no effect because the current Wazuh API user has run_as disabled.';
+        break;
+    }
+      
     return (
       <EuiFlexGroup >
         <EuiFlexItem >
-          <EuiCallOut title=" The role mapping has no effect because the Wazuh API's configured user has not the run_as setting enabled in the configuration or is not allowed to use it. " color="warning" iconType="alert">
+          <EuiCallOut title={runAsWarningTxt} color="warning" iconType="alert">
           </EuiCallOut>
           <EuiSpacer></EuiSpacer>
         </EuiFlexItem >
@@ -147,7 +167,7 @@ export const WzSecurity = compose(
           }
           {selectedTabId === 'roleMapping' &&
             <>
-              {allowRunAs !== API_USER_STATUS_RUN_AS.ENABLED && allowRunAs !== undefined && isNotRunAs()}
+              {(allowRunAs !== undefined && allowRunAs !== API_USER_STATUS_RUN_AS.ENABLED) && isNotRunAs(allowRunAs)}
               <RolesMapping></RolesMapping>
             </>
           }
