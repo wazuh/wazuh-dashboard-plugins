804be1ccf23fda07fbc9f35259324787b240fb2c
pablomarga <pablomarga@hotmail.com>
2021-05-04 18:01:18 +0200

    Change copyright

--- public/components/common/util/wz-overlay-mask-interface.tsx
+++ public/components/common/util/wz-overlay-mask-interface.tsx
@@ -1,25 +1,13 @@
 /*
- * Licensed to Elasticsearch B.V. under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch B.V. licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Wazuh app - Simple description for each App tabs
+ * Copyright (C) 2015-2021 Wazuh, Inc.
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-/**
- * NOTE: We can't test this component because Enzyme doesn't support rendering
- * into portals.
+ * Find more information about this on the LICENSE file.
  */
 
 import React, {
