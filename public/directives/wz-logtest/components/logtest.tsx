01ef8f93ea00f4952f557d76108fed8249b1d739
CPAlejandro <cuellarpeinado@gmail.com>
2021-05-27 09:56:20 +0200

    Added buttom to clear session in logtest and add control of logtest token to Redux

--- public/directives/wz-logtest/components/logtest.tsx
+++ public/directives/wz-logtest/components/logtest.tsx
@@ -29,6 +29,9 @@ import {
 import { WzRequest } from '../../../react-services';
 import { withReduxProvider, withUserAuthorizationPrompt } from '../../../components/common/hocs';
 import { compose } from 'redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateLogtestToken } from '../../../redux/actions/appStateActions';
+import { WzButtonPermissionsModalConfirm } from '../../../components/common/buttons';
 
 type LogstestProps = {
   openCloseFlyout: () => {};
@@ -44,7 +47,8 @@ export const Logtest = compose(
   const [events, setEvents] = useState([]);
   const [testing, setTesting] = useState(false);
   const [testResult, setTestResult] = useState('');
-  const [sessionToken, setSesstionToken] = useState('');
+  const dispatch = useDispatch();
+  const sessionToken = useSelector((state)=> state.appStateReducers.logtestToken);
 
   const onChange = (e) => {
     setEvents(e.target.value.split('\n').filter((item) => item));
@@ -98,7 +102,7 @@ export const Logtest = compose(
           ...(token ? { token }: {})
         });
         token = response.data.data.token;
-        !sessionToken && !gotToken && token && setSesstionToken(token);
+        !sessionToken && !gotToken && token && dispatch(updateLogtestToken(token));
         token && (gotToken = true);
         responses.push(response);
       };
@@ -120,6 +124,17 @@ export const Logtest = compose(
     }
   };
 
+  const deleteToken = async() =>{
+    try {
+      const response = await WzRequest.apiReq('DELETE', `/logtest/sessions/${sessionToken}`, {});
+      dispatch(updateLogtestToken(''));
+      setTestResult('');
+    }
+    catch(error) {
+      this.showToast('danger', 'Error', `Error trying to delete logtest token due to: ${error.message || error}`);
+    }    
+  }
+
   const buildLogtest = () => {
     return (
       <Fragment>
@@ -132,6 +147,7 @@ export const Logtest = compose(
           onKeyPress={handleKeyPress}
         />
         <EuiSpacer size="m" />
+        <EuiFlexGroup justifyContent="spaceBetween">
         <EuiFlexItem grow={false}>
           <EuiButton
             style={{ maxWidth: '100px' }}
@@ -144,6 +160,28 @@ export const Logtest = compose(
             Test
           </EuiButton>
         </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <WzButtonPermissionsModalConfirm
+            style={{ maxWidth: '150px' }}
+            tooltip={{position: 'top', content: 'Clear current session'}}
+            fill
+            isDisabled={sessionToken === '' ? true : false}
+            aria-label="Clear current session"
+            iconType="broom"
+            onConfirm={async () => {
+              deleteToken();
+            }}
+            color="danger"
+            modalTitle={`Do you want to clear current session?`}
+            modalProps={{
+              buttonColor: 'danger',
+              children: 'Clearing the session means the logs execution history is removed. This affects to rules that fire an alert when similar logs are executed in a specific range of time.'
+            }}
+          > 
+          Clear sesion 
+          </WzButtonPermissionsModalConfirm> 
+        </EuiFlexItem> 
+        </EuiFlexGroup>
         <EuiSpacer size="m" />
         <EuiCodeBlock
           language="json"
