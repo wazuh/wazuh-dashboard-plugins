f9d8f703c50f79feb64bcbc87c50fec2e6830669
gabiwassan <gabriel.wassan@wazuh.com>
2021-06-01 13:07:39 -0300

    feat(errorBoundary): Added ErrorBoundary HOC and component and added loglevel dependency

---
+++ public/components/error-boundary/error-boundary.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { expect } from 'chai';
+import ErrorBoundary from './error-boundary';
+
+describe('Spec ErrorBoundary', function () {
+  it('it exists', () => {
+    expect(ErrorBoundary).to.be.ok;
+  });
+});
