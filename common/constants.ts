857403944b303ccab697761de35de72210275cf9
Ibarra Maximiliano <maximiliano.ibarra@wazuh.com>
2021-06-02 16:33:34 -0300

    Renamed constants

--- common/constants.ts
+++ common/constants.ts
@@ -128,9 +128,9 @@ export const WAZUH_DATA_LOGS_DIRECTORY_PATH = path.join(WAZUH_DATA_ABSOLUTE_PATH
 export const WAZUH_DATA_LOGS_PLAIN_PATH = path.join(WAZUH_DATA_LOGS_DIRECTORY_PATH, 'wazuhapp-plain.log');
 export const WAZUH_DATA_LOGS_RAW_PATH = path.join(WAZUH_DATA_LOGS_DIRECTORY_PATH, 'wazuhapp.log');
 
-// Wazuh data path - frontend logs
-export const WAZUH_FRONTEND_LOGS_PLAIN_PATH = path.join(WAZUH_DATA_LOGS_DIRECTORY_PATH, 'wazuh-frontend-plain.log');
-export const WAZUH_FRONTEND_LOGS_RAW_PATH = path.join(WAZUH_DATA_LOGS_DIRECTORY_PATH, 'wazuh-frontend.log');
+// Wazuh data path - UI logs
+export const WAZUH_UI_LOGS_PLAIN_PATH = path.join(WAZUH_DATA_LOGS_DIRECTORY_PATH, 'wazuh-ui-plain.log');
+export const WAZUH_UI_LOGS_RAW_PATH = path.join(WAZUH_DATA_LOGS_DIRECTORY_PATH, 'wazuh-ui.log');
 
 // Wazuh data path - downloads
 export const WAZUH_DATA_DOWNLOADS_DIRECTORY_PATH = path.join(WAZUH_DATA_ABSOLUTE_PATH, 'downloads');
