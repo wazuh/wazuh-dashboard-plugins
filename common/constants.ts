7fdbe565763f1498488eec8d5a1cde29a74fb4ec
Maximiliano Ibarra <maximilianoaibarra@gmail.com>
2021-06-07 16:23:52 -0300

    Feature/3316 error handler orchestrator (#3327)
    * feat(errorBoundary): Added ErrorBoundary HOC and component and added loglevel dependency
    
    * feature(errorBoundary): Moved with the others HOCs.
    
    * feature(errorBoundary): Typo refactor.
    
    * First attempt LoggerService
    
    * Merged error boundary, integrated loggerService.
    
    * changed logger name, create logger-service test file
    
    * Updated CHANGELOG
    
    * Moved to react-services, changed name, traslates comments
    
    * feat(errorBoundary): Removed old integration
    
    * refactor(loggerService): Changed class for function methods.
    
    * test(logger-service): Added basic unit test to logger-service
    
    * refactor(logger-service): Applied new implementation of error-orchestrator service.
    
    * feature(logger-service): PR comments and some refactors.
    
    Co-authored-by: gabiwassan <gabriel.wassan@wazuh.com>
    Co-authored-by: Ibarra Maximiliano <maximiliano.ibarra@wazuh.com>

--- common/constants.ts
+++ common/constants.ts
@@ -220,3 +220,36 @@ export enum WAZUH_MODULES_ID{
 
 export const AUTHORIZED_AGENTS = 'authorized-agents';
 export const HEALTH_CHECK = 'health-check';
+
+// Health check
+export const HEALTH_CHECK_REDIRECTION_TIME = 300; //ms
+
+// Kibana settings
+// Default timeTilter set by the app
+export const WAZUH_KIBANA_SETTING_TIME_FILTER = {
+  from: "now-24h",
+  to: 'now'
+};
+export const KIBANA_SETTING_NAME_TIME_FILTER = 'timepicker:timeDefaults';
+
+// Default maxBuckets set by the app
+export const WAZUH_KIBANA_SETTING_MAX_BUCKETS = 200000;
+export const KIBANA_SETTING_NAME_MAX_BUCKETS = 'timelion:max_buckets';
+
+// Default metaFields Kibana setting set by the app
+export const WAZUH_KIBANA_SETTING_METAFIELDS = ['_source', '_index'];
+export const KIBANA_SETTING_NAME_METAFIELDS = 'metaFields';
+
+
+// Logger
+export const UI_LOGGER_LEVELS = {
+  WARNING: 'WARNING',
+  INFO: 'INFO',
+  ERROR: 'ERROR',
+};
+
+export const UI_TOAST_COLOR = {
+  SUCCESS: 'success',
+  WARNING: 'warning',
+  DANGER: 'danger',
+};
