517e54756600d67cc1aeb1d62b787a46641aec4b
gabiwassan <gabriel.wassan@wazuh.com>
2021-06-07 13:03:23 -0300

    test(ui-logger-controller): Added simple unit test.

--- server/controllers/wazuh-utils/ui-logs.controller.spec.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { expect } from 'chai';
-import { UiLogsCtrl } from './ui-logs.controller';
-
-describe('Spec UiLogsCtrl', function () {
-  it('it exists', () => {
-    expect(UiLogsCtrl).to.be.ok;
-  });
-});
