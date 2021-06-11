9093ced250e9138f2bc43fc6735b1a9631d0025c
gabiwassan <gabriel.wassan@wazuh.com>
2021-06-11 17:32:03 -0300

    fix(error-orchestrator): PR comments + prettier.

--- server/lib/logger.ts
+++ server/lib/logger.ts
@@ -12,11 +12,11 @@
 import { BaseLogger } from './base-logger';
 import {
   WAZUH_DATA_LOGS_PLAIN_FILENAME,
-  WAZUH_DATA_LOGS_RAW_FILENAME
+  WAZUH_DATA_LOGS_RAW_FILENAME,
 } from '../../common/constants';
 
-const logger = new BaseLogger(WAZUH_DATA_LOGS_PLAIN_FILENAME,WAZUH_DATA_LOGS_RAW_FILENAME);
+const logger = new BaseLogger(WAZUH_DATA_LOGS_PLAIN_FILENAME, WAZUH_DATA_LOGS_RAW_FILENAME);
 
 export const log = (location, message, level) => {
-  logger.log(location, message, level )
-}
+  logger.log(location, message, level);
+};
--- server/routes/wazuh-utils/ui-logs.test.ts
+++ server/routes/wazuh-utils/ui-logs.test.ts
@@ -2,38 +2,41 @@
 // yarn test:jest --testEnvironment node --verbose server/routes/wazuh-utils/ui-logs
 import axios from 'axios';
 
-function buildAxiosOptions(method: string, path: string, data: any = {}, headers: any = {}){
+const buildAxiosOptions = (method: string, path: string, data: any = {}, headers: any = {}) => {
   return {
     method: method,
     headers: { 'Content-Type': 'application/json', 'kbn-xsrf': 'kibana', ...headers },
     url: `http://localhost:5601${path}`,
-    data: data
+    data: data,
   };
 };
-
 
 describe('Wazuh API - /utils/logs/ui', () => {
   test('[200] Get UI Logs', () => {
     const options = buildAxiosOptions('get', '/utils/logs/ui');
-    return axios(options).then(response => {
-      expect(response.status).toBe(200);
-      //expect(typeof response.data.data).toBe('object');
-      //expect(typeof response.data.data.hosts).toBe('object');
-    }).catch(error => {throw error})
-  },6000);
+    return axios(options)
+      .then((response) => {
+        expect(response.status).toBe(200);
+      })
+      .catch((error) => {
+        throw error;
+      });
+  }, 6000);
 });
 
 describe('Wazuh API - /utils/logs/ui', () => {
-  let userToken = null;
- 
   test('[200] Create UI Logs', () => {
     const options = buildAxiosOptions('post', '/utils/logs/ui', {
       message: 'Message test',
       level: 'error',
-      location: 'Location'
+      location: 'Location',
     });
-    return axios(options).then(response => {
-      expect(response.status).toBe(200);
-    }).catch(error => {throw error})
-  },6000);
+    return axios(options)
+      .then((response) => {
+        expect(response.status).toBe(200);
+      })
+      .catch((error) => {
+        throw error;
+      });
+  }, 6000);
 });
