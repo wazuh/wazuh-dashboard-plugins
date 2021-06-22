b010102e721c171b19c0840ed06ae1d8e9103df5
gabiwassan <gabriel.wassan@wazuh.com>
2021-06-22 16:42:46 -0300

    fix(error-orchestrator): Fixed imports.

--- public/controllers/management/components/management/mg-logs/logs.js
+++ public/controllers/management/components/management/mg-logs/logs.js
@@ -40,7 +40,7 @@ import { compose } from 'redux';
 import { WzFieldSearch } from '../../../../../components/wz-field-search-bar/wz-field-search-bar';
 import { UI_LOGGER_LEVELS } from '../../../../../../common/constants';
 import { UI_ERROR_SEVERITIES } from '../../../../../react-services/error-orchestrator/types';
-import { getErrorOrchestrator } from '../../../../../react-services';
+import { getErrorOrchestrator } from '../../../../../react-services/common-services';
 
 const errorContext = 'WzLogs';
 
