f4afbdcada6a66b45c539316b01395dba2991bee
gabiwassan <gabriel.wassan@wazuh.com>
2021-06-28 10:22:09 -0300

    feat(error-orchestrator): PR comments

--- public/react-services/error-orchestrator/error-orchestrator-base.ts
+++ public/react-services/error-orchestrator/error-orchestrator-base.ts
@@ -15,21 +15,6 @@ import { GenericRequest } from '../../react-services/generic-request';
 import { ErrorOrchestrator, UIErrorLog } from './types';
 import { UI_LOGGER_LEVELS } from '../../../common/constants';
 
-/**
- * Default levels for winston
-exports.levels = {
-  error: 0,
-  warn: 1,
-  help: 2,
-  data: 3,
-  info: 4,
-  debug: 5,
-  prompt: 6,
-  verbose: 7,
-  input: 8,
-  silly: 9
-};
- */
 const winstonLevelDict = {
   [UI_LOGGER_LEVELS.ERROR]: 'error',
   [UI_LOGGER_LEVELS.WARNING]: 'warn',
