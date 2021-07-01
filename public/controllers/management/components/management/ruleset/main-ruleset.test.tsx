253e9f4a7be46898e1dd2218fade83f61c5a7f1b
Ibarra Maximiliano <maximiliano.ibarra@wazuh.com>
2021-07-01 11:45:23 -0300

    Added main-ruleset test and component snapshot

---
+++ public/controllers/management/components/management/ruleset/main-ruleset.test.tsx
@@ -0,0 +1,35 @@
+/*
+ * Wazuh app - React test for Stats component.
+ *
+ * Copyright (C) 2015-2021 Wazuh, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Find more information about this on the LICENSE file.
+ *
+ */
+
+import React from 'react';
+import { mount } from 'enzyme';
+import WzRuleset from './main-ruleset';
+
+jest.mock('../../../../../kibana-services', () => ({
+  getAngularModule: jest.fn(),
+  getHttp: () => ({
+    basePath: {
+      prepend: (str) => str,
+    },
+  }),
+}));
+
+describe('Ruleset component', () => {
+  it('renders correctly to match the snapshot', () => {
+    const logtestProps = '';
+    const clusterStatus = '';
+    const wrapper = mount(<WzRuleset logtestProps={logtestProps} clusterStatus={clusterStatus} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
