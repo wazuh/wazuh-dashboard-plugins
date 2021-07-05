bd4896998febb6d64140a15da6a31d8fbf7cc824
pablomarga <pablomarga@hotmail.com>
2021-07-05 15:50:40 +0200

    Add test

---
+++ public/controllers/management/components/management/groups/groups-main.test.tsx
@@ -0,0 +1,38 @@
+/*
+ * Wazuh app - React test for Reporting component.
+ *
+ * Copyright (C) 2015-2021 Wazuh, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Find more information about this on the LICENSE file.
+ *
+ */
+import React from 'react';
+import { mount } from 'enzyme';
+import WzGroups from './groups-main';
+import { Provider } from 'react-redux'
+
+jest.mock('../../../../../kibana-services', () => ({
+  getAngularModule: jest.fn(),
+  getHttp: () => ({
+    basePath: {
+      prepend: (str) => str,
+    },
+  }),
+}));
+const ReduxProvider = ({ children, reduxStore }) => (
+  <Provider store={reduxStore}>{children}</Provider>
+)//prueba
+
+
+
+describe('Reporting component', () => {
+  it('renders correctly to match the snapshot', () => {
+    const wrapper = mount(<WzGroups />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
