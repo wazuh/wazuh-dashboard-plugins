599ac2d1e027250d4d9f1bcf90d601f9118fc702
Pablo Martínez <pablo.martinez@wazuh.com>
2021-07-07 23:14:36 +0200

    Implement try catch strategy in Groups (#3415)
    * Implement try catch groups
    
    * Add test
    
    * test(groups): Added simple snapshot test.
    
    * Add changelog
    
    * Change context
    
    * Change title
    
    Co-authored-by: gabiwassan <gabriel.wassan@wazuh.com>

--- public/controllers/management/components/management/groups/group-agents-table.js
+++ public/controllers/management/components/management/groups/group-agents-table.js
@@ -10,13 +10,11 @@
  * Find more information about this on the LICENSE file.
  */
 import React, { Component } from 'react';
-import {
-  EuiCallOut,
-} from '@elastic/eui';
+import { EuiCallOut } from '@elastic/eui';
 
 import { connect } from 'react-redux';
 import GroupsHandler from './utils/groups-handler';
-import { getToasts }  from '../../../../../kibana-services';
+import { getToasts } from '../../../../../kibana-services';
 
 import {
   updateLoadingStatus,
@@ -27,32 +25,117 @@ import {
   updateListItemsForRemove,
   updateSortDirectionAgents,
   updateSortFieldAgents,
-  updateReload
+  updateReload,
 } from '../../../../../redux/actions/groupsActions';
 
 import { getAgentFilterValues } from './get-agents-filters-values';
 import { TableWzAPI } from '../../../../../components/common/tables';
 import { WzButtonPermissions } from '../../../../../components/common/permissions/button';
 import { WzButtonPermissionsModalConfirm } from '../../../../../components/common/buttons';
+import { UI_LOGGER_LEVELS } from '../../../../../../common/constants';
+import { UI_ERROR_SEVERITIES } from '../../../../../react-services/error-orchestrator/types';
+import { getErrorOrchestrator } from '../../../../../react-services/common-services';
+
 
 class WzGroupAgentsTable extends Component {
   _isMounted = false;
   constructor(props) {
     super(props);
     this.suggestions = [
-      { type: 'q', label: 'status', description: 'Filter by agent connection status', operators: ['=', '!=',], values: ['active', 'disconnected', 'never_connected', 'pending'] },
-      { type: 'q', label: 'os.platform', description: 'Filter by OS platform', operators: ['=', '!=',], values: async (value) => getAgentFilterValues('os.platform', value, {q: `group=${this.props.state.itemDetail.name}`})},
-      { type: 'q', label: 'ip', description: 'Filter by agent IP', operators: ['=', '!=',], values: async (value) => getAgentFilterValues('ip', value,  {q: `group=${this.props.state.itemDetail.name}`})},
-      { type: 'q', label: 'name', description: 'Filter by agent name', operators: ['=', '!=',], values: async (value) => getAgentFilterValues('name', value,  {q: `group=${this.props.state.itemDetail.name}`})},
-      { type: 'q', label: 'id', description: 'Filter by agent id', operators: ['=', '!=',], values: async (value) => getAgentFilterValues('id', value,  {q: `group=${this.props.state.itemDetail.name}`})},
-      { type: 'q', label: 'node_name', description: 'Filter by node name', operators: ['=', '!=',], values: async (value) => getAgentFilterValues('node_name', value,  {q: `group=${this.props.state.itemDetail.name}`})},
-      { type: 'q', label: 'manager', description: 'Filter by manager', operators: ['=', '!=',], values: async (value) => getAgentFilterValues('manager', value,  {q: `group=${this.props.state.itemDetail.name}`})},
-      { type: 'q', label: 'version', description: 'Filter by agent version', operators: ['=', '!=',], values: async (value) => getAgentFilterValues('version', value,  {q: `group=${this.props.state.itemDetail.name}`})},
-      { type: 'q', label: 'configSum', description: 'Filter by agent config sum', operators: ['=', '!=',], values: async (value) => getAgentFilterValues('configSum', value,  {q: `group=${this.props.state.itemDetail.name}`})},
-      { type: 'q', label: 'mergedSum', description: 'Filter by agent merged sum', operators: ['=', '!=',], values: async (value) => getAgentFilterValues('mergedSum', value,  {q: `group=${this.props.state.itemDetail.name}`})},
+      {
+        type: 'q',
+        label: 'status',
+        description: 'Filter by agent connection status',
+        operators: ['=', '!='],
+        values: ['active', 'disconnected', 'never_connected', 'pending'],
+      },
+      {
+        type: 'q',
+        label: 'os.platform',
+        description: 'Filter by OS platform',
+        operators: ['=', '!='],
+        values: async (value) =>
+          getAgentFilterValues('os.platform', value, {
+            q: `group=${this.props.state.itemDetail.name}`,
+          }),
+      },
+      {
+        type: 'q',
+        label: 'ip',
+        description: 'Filter by agent IP',
+        operators: ['=', '!='],
+        values: async (value) =>
+          getAgentFilterValues('ip', value, { q: `group=${this.props.state.itemDetail.name}` }),
+      },
+      {
+        type: 'q',
+        label: 'name',
+        description: 'Filter by agent name',
+        operators: ['=', '!='],
+        values: async (value) =>
+          getAgentFilterValues('name', value, { q: `group=${this.props.state.itemDetail.name}` }),
+      },
+      {
+        type: 'q',
+        label: 'id',
+        description: 'Filter by agent id',
+        operators: ['=', '!='],
+        values: async (value) =>
+          getAgentFilterValues('id', value, { q: `group=${this.props.state.itemDetail.name}` }),
+      },
+      {
+        type: 'q',
+        label: 'node_name',
+        description: 'Filter by node name',
+        operators: ['=', '!='],
+        values: async (value) =>
+          getAgentFilterValues('node_name', value, {
+            q: `group=${this.props.state.itemDetail.name}`,
+          }),
+      },
+      {
+        type: 'q',
+        label: 'manager',
+        description: 'Filter by manager',
+        operators: ['=', '!='],
+        values: async (value) =>
+          getAgentFilterValues('manager', value, {
+            q: `group=${this.props.state.itemDetail.name}`,
+          }),
+      },
+      {
+        type: 'q',
+        label: 'version',
+        description: 'Filter by agent version',
+        operators: ['=', '!='],
+        values: async (value) =>
+          getAgentFilterValues('version', value, {
+            q: `group=${this.props.state.itemDetail.name}`,
+          }),
+      },
+      {
+        type: 'q',
+        label: 'configSum',
+        description: 'Filter by agent config sum',
+        operators: ['=', '!='],
+        values: async (value) =>
+          getAgentFilterValues('configSum', value, {
+            q: `group=${this.props.state.itemDetail.name}`,
+          }),
+      },
+      {
+        type: 'q',
+        label: 'mergedSum',
+        description: 'Filter by agent merged sum',
+        operators: ['=', '!='],
+        values: async (value) =>
+          getAgentFilterValues('mergedSum', value, {
+            q: `group=${this.props.state.itemDetail.name}`,
+          }),
+      },
       //{ type: 'q', label: 'dateAdd', description: 'Filter by add date', operators: ['=', '!=',], values: async (value) => getAgentFilterValues('dateAdd', value,  {q: `group=${this.props.state.itemDetail.name}`})},
       //{ type: 'q', label: 'lastKeepAlive', description: 'Filter by last keep alive', operators: ['=', '!=',], values: async (value) => getAgentFilterValues('lastKeepAlive', value,  {q: `group=${this.props.state.itemDetail.name}`})},
-    ]
+    ];
     this.groupsHandler = GroupsHandler;
 
     this.columns = [
@@ -60,54 +143,62 @@ class WzGroupAgentsTable extends Component {
         field: 'id',
         name: 'Id',
         align: 'left',
-        sortable: true
+        sortable: true,
       },
       {
         field: 'name',
         name: 'Name',
         align: 'left',
-        sortable: true
+        sortable: true,
       },
       {
         field: 'ip',
         name: 'Ip',
         align: 'left',
-        sortable: true
+        sortable: true,
       },
       {
         field: 'status',
         name: 'Status',
         align: 'left',
-        sortable: true
+        sortable: true,
       },
       {
         field: 'os.name',
         name: 'Os name',
         align: 'left',
-        sortable: true
+        sortable: true,
       },
       {
         field: 'os.version',
         name: 'Os version',
         align: 'left',
-        sortable: true
+        sortable: true,
       },
       {
         field: 'version',
         name: 'Version',
         align: 'left',
-        sortable: true
+        sortable: true,
       },
       {
         name: 'Actions',
         align: 'left',
-        render: item => {
+        render: (item) => {
           return (
             <div>
               <WzButtonPermissions
-                buttonType='icon'
-                permissions={[[{action: 'agent:read', resource: `agent:id:${item.id}`}, ...(item.group || []).map(group => ({ action: 'agent:read', resource: `agent:group:${group}` }))]]}
-                tooltip={{position: 'top', content: 'Go to the agent'}}
+                buttonType="icon"
+                permissions={[
+                  [
+                    { action: 'agent:read', resource: `agent:id:${item.id}` },
+                    ...(item.group || []).map((group) => ({
+                      action: 'agent:read',
+                      resource: `agent:group:${group}`,
+                    })),
+                  ],
+                ]}
+                tooltip={{ position: 'top', content: 'Go to the agent' }}
                 aria-label="Go to the agent"
                 iconType="eye"
                 onClick={async () => {
@@ -116,9 +207,17 @@ class WzGroupAgentsTable extends Component {
                 color="primary"
               />
               <WzButtonPermissionsModalConfirm
-                buttonType='icon'
-                permissions={[[{action: 'agent:modify_group', resource: `agent:id:${item.id}`}, ...(item.group || []).map(group => ({ action: 'agent:modify_group', resource: `agent:group:${group}` }))]]}
-                tooltip={{position: 'top', content: 'Remove agent from this group'}}
+                buttonType="icon"
+                permissions={[
+                  [
+                    { action: 'agent:modify_group', resource: `agent:id:${item.id}` },
+                    ...(item.group || []).map((group) => ({
+                      action: 'agent:modify_group',
+                      resource: `agent:group:${group}`,
+                    })),
+                  ],
+                ]}
+                tooltip={{ position: 'top', content: 'Remove agent from this group' }}
                 aria-label="Remove agent from this group"
                 iconType="trash"
                 onConfirm={async () => {
@@ -128,13 +227,13 @@ class WzGroupAgentsTable extends Component {
                 isDisabled={item.name === 'default'}
                 modalTitle={`Remove ${item.file || item.name} agent from this group?`}
                 modalProps={{
-                  buttonColor: 'danger'
+                  buttonColor: 'danger',
                 }}
               />
             </div>
           );
-        }
-      }
+        },
+      },
     ];
   }
 
@@ -146,14 +245,14 @@ class WzGroupAgentsTable extends Component {
     const { error } = this.props.state;
     if (!error) {
       return (
-          <TableWzAPI
-            tableColumns={this.columns}
-            tableInitialSortingField='id'
-            searchBarSuggestions={this.suggestions}
-            endpoint={`/groups/${this.props.state.itemDetail.name}/agents`}
-            reload={this.props.state.reload}
-            searchTable={true}
-          />
+        <TableWzAPI
+          tableColumns={this.columns}
+          tableInitialSortingField="id"
+          searchBarSuggestions={this.suggestions}
+          endpoint={`/groups/${this.props.state.itemDetail.name}/agents`}
+          reload={this.props.state.reload}
+          searchTable={true}
+        />
       );
     } else {
       return <EuiCallOut color="warning" title={error} iconType="gear" />;
@@ -165,61 +264,61 @@ class WzGroupAgentsTable extends Component {
       color: color,
       title: title,
       text: text,
-      toastLifeTimeMs: time
+      toastLifeTimeMs: time,
     });
   };
 
   async removeItems(items) {
     const { itemDetail } = this.props.state;
-
     this.props.updateLoadingStatus(true);
-    const results = items.map(async (item, i) => {
-      await this.groupsHandler.deleteAgent(item.id, itemDetail.name);
-    });
+    try {
+      items.map(async (item) => {
+        await this.groupsHandler.deleteAgent(item.id, itemDetail.name);
+      });
 
-    Promise.all(results).then(
-      completed => {
-        this.props.updateIsProcessing(true);
-        this.props.updateLoadingStatus(false);
-        this.props.updateReload();
-        this.showToast('success', 'Success', 'Deleted successfully', 3000);
-      },
-      error => {
-        this.props.updateIsProcessing(true);
-        this.props.updateLoadingStatus(false);
-        this.props.updateReload();
-        this.showToast('danger', 'Error', error, 3000);
-      }
-    );
+      this.props.updateIsProcessing(true);
+      this.props.updateLoadingStatus(false);
+      this.props.updateReload();
+      this.showToast('success', 'Success', 'Deleted successfully', 3000);
+    } catch (error) {
+      const options = {
+        context: `${WzGroupAgentsTable.name}.removeItems`,
+        level: UI_LOGGER_LEVELS.ERROR,
+        severity: UI_ERROR_SEVERITIES.BUSINESS,
+        store: true,
+        error: {
+          error: error,
+          message: error.message || error,
+          title: error.title || error,
+        },
+      };
+      getErrorOrchestrator().handleError(options);
+      this.props.updateIsProcessing(true);
+      this.props.updateLoadingStatus(false);
+      this.props.updateReload();
+    }
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    state: state.groupsReducers
+    state: state.groupsReducers,
   };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    updateLoadingStatus: status => dispatch(updateLoadingStatus(status)),
-    updateFileContent: content => dispatch(updateFileContent(content)),
-    updateIsProcessing: isProcessing =>
-      dispatch(updateIsProcessing(isProcessing)),
-    updatePageIndexAgents: pageIndexAgents =>
-      dispatch(updatePageIndexAgents(pageIndexAgents)),
-    updateShowModal: showModal => dispatch(updateShowModal(showModal)),
-    updateListItemsForRemove: itemList =>
-      dispatch(updateListItemsForRemove(itemList)),
-    updateSortDirectionAgents: sortDirectionAgents =>
+    updateLoadingStatus: (status) => dispatch(updateLoadingStatus(status)),
+    updateFileContent: (content) => dispatch(updateFileContent(content)),
+    updateIsProcessing: (isProcessing) => dispatch(updateIsProcessing(isProcessing)),
+    updatePageIndexAgents: (pageIndexAgents) => dispatch(updatePageIndexAgents(pageIndexAgents)),
+    updateShowModal: (showModal) => dispatch(updateShowModal(showModal)),
+    updateListItemsForRemove: (itemList) => dispatch(updateListItemsForRemove(itemList)),
+    updateSortDirectionAgents: (sortDirectionAgents) =>
       dispatch(updateSortDirectionAgents(sortDirectionAgents)),
-    updateSortFieldAgents: sortFieldAgents =>
-      dispatch(updateSortFieldAgents(sortFieldAgents)),
-    updateReload: () => dispatch(updateReload())
+    updateSortFieldAgents: (sortFieldAgents) => dispatch(updateSortFieldAgents(sortFieldAgents)),
+    updateReload: () => dispatch(updateReload()),
   };
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(WzGroupAgentsTable);
+export default connect(mapStateToProps, mapDispatchToProps)(WzGroupAgentsTable);
