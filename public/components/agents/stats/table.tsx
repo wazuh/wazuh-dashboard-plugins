88140d32004f3d818e9529cb730f3060fa334caf
gabiwassan <gabriel.wassan@wazuh.com>
2021-07-12 13:41:31 -0300

    fix(syscollector): Added try catch strategy.

--- public/components/agents/stats/table.tsx
+++ public/components/agents/stats/table.tsx
@@ -10,21 +10,28 @@
  * Find more information about this on the LICENSE file.
  */
 import React from 'react';
-import { 
+import {
   EuiButtonEmpty,
-  EuiInMemoryTable,
   EuiFlexGroup,
   EuiFlexItem,
   EuiHorizontalRule,
   EuiIcon,
+  EuiInMemoryTable,
   EuiLoadingSpinner,
   EuiPanel,
   EuiSpacer,
-  EuiText
+  EuiText,
 } from '@elastic/eui';
 import * as FileSaver from '../../../services/file-saver';
-import { getToasts } from '../../../kibana-services';
 import { formatUIDate } from '../../../react-services';
+import {
+  UI_ERROR_SEVERITIES,
+  UIErrorLog,
+  UIErrorSeverity,
+  UILogLevel,
+} from '../../../react-services/error-orchestrator/types';
+import { UI_LOGGER_LEVELS } from '../../../../common/constants';
+import { getErrorOrchestrator } from '../../../react-services/common-services';
 
 export function AgentStatTable({columns, title, start, end, loading, items, exportCSVFilename}){
   return (
@@ -60,22 +67,24 @@ export function AgentStatTable({columns, title, start, end, loading, items, expo
   )
 }
 
-
 function downloadCsv(columns: any[], data: any[], filename: string) {
   try {
-    const header = columns.map(column => column.name).join(',');
-    const body = data
-      .map(row => columns.map(column => row[column.field]).join(','))
-      .join('\n');
-    const result = [header,body].join('\n');
+    const header = columns.map((column) => column.name).join(',');
+    const body = data.map((row) => columns.map((column) => row[column.field]).join(',')).join('\n');
+    const result = [header, body].join('\n');
     const blob = new Blob([result], { type: 'text/csv' }); // eslint-disable-line
     FileSaver.saveAs(blob, `${filename}.csv`);
   } catch (error) {
-    getToasts().add({
-      color: 'danger',
-      title: 'CSV',
-      text: 'Error generating CSV',
-      toastLifeTimeMs: 4000,
-    }); 
+    const options: UIErrorLog = {
+      context: 'downloadCsv',
+      level: UI_LOGGER_LEVELS.ERROR as UILogLevel,
+      severity: UI_ERROR_SEVERITIES.BUSINESS as UIErrorSeverity,
+      error: {
+        error: error,
+        message: `Error generating CSV: ${error.message}`,
+        title: `CSV: ${error.name}`,
+      },
+    };
+    getErrorOrchestrator().handleError(options);
   }
 }
