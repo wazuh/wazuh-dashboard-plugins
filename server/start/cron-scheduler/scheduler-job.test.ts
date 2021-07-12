e949a51ca98fbd32de90cc73f1767fd9e5ceea97
Gabriel Wassan <gabriel.wassan@wazuh.com>
2021-07-12 22:28:58 +0200

    Fixing and updating unit-tests. (#3413)
    * test(unit-test): Fixing and updating unit-tests.
    
    * test(scheduler-job): Fixing suite scheduler-job tests.
    
    * test(unit-test): Fixing updating unit-tests.
    
    * test(check-result): Fixing uTest of check-result, added error case.
    
    * test(unit-test): Skipped uTest with dependency of API.
    
    * test(prettier): Applied prettier, redux-mock-store dependency
    
    * test(jest): Jest config without dependencies.
    
    * test(git-actions): Added workflow for uTest.
    
    * test(check-result): Update workflow.
    
    * test(git-actions): Updated workflow
    
    * test(gitactions): Dependencies for unit test.
    
    * test(gitactions): Added coverage-comment
    
    * test(gitactions): Changed text to text-summary
    
    * test(gitactions): Show summary
    
    * test(gitactions): Show summary + added dependencies
    
    * test(gitactions): Typo
    
    * test(gitactions): Porcents
    
    * test(gitactions): Typo
    
    * test(gitactions): Testing node version with yarn
    
    * test(gitactions): Update
    
    * test(gitactions): Update
    
    * test(gitactions): testing coverage summary
    
    * test(gitactions): testing coverage summary
    
    * test(gitactions): fix param github-token
    
    * test(gitactions): update
    
    * test(gitactions): testing report.json
    
    * test(gitactions): final test.
    
    * test(actions): testing kibana dependencies
    
    * test(actions): testing kibana dependencies
    
    * test(actions): testing kibana dependencies
    
    * test(actions): testing kibana dependencies
    
    * test(actions): testing
    
    * test(actions): testing
    
    * test(actions): test with old config jest.
    
    * test(actions): set node version for jest
    
    * test(actions): testing with bootstrap
    
    * test(actions): rollback dependencies
    
    * test(actions): rollback
    
    * test(groups-main): fixing snapshot test
    
    * test(groups-main): added coverage
    
    * test(groups-main): fix path
    
    * test(groups-main): fix path
    
    * test(git-actions): fix script
    
    * test(git-actions): fix checkout
    
    * test(git-actions): fix checkout
    
    * test(git-actions): fix checkout
    
    * test(git-actions): fix checkout
    
    * test(git-actions): fix checkout
    
    * test(git-actions): fix checkout
    
    * test(git-actions): fix checkout
    
    * test(git-actions): fix checkout
    
    * fix(syscollector): Refactor for agents-sections.
    
    * fix(syscollector): Clean files.
    
    * fix(syscollector): Clean files.
    
    * fix(syscollector): test
    
    * fix(syscollector): test
    
    * fix(syscollector): test
    
    * fix(syscollector): test
    
    * fix(syscollector): test
    
    * fix(syscollector): test
    
    * fix(syscollector): test
    
    * fix(syscollector): test
    
    * fix(syscollector): test
    
    * fix(syscollector): test
    
    * fix(syscollector): test
    
    * fix(syscollector): test
    
    * fix(syscollector): test
    
    * fix(syscollector): test
    
    * fix(syscollector): test comment coverage
    
    * fix(syscollector): test comment coverage
    
    * fix(syscollector): test comment coverage
    
    * fix(actions): test comment coverage
    
    * Create Cypress Workflow for 4.3-7.10 (#3444)
    
    * Fixed dispatch for updateCurrentAgentData (#3453)
    
    * fix(syscollector): Fixed dispatch for updateCurrentAgentData
    
    * fix(syscollector): Refactor for agents-sections.
    
    * fix(actions): add const env
    
    Co-authored-by: Matias Ezequiel Moreno <49887871+matiasmoreno876@users.noreply.github.com>

--- server/start/cron-scheduler/scheduler-job.test.ts
+++ server/start/cron-scheduler/scheduler-job.test.ts
@@ -1,10 +1,7 @@
 //@ts-nocheck
-import {
-  SchedulerJob,
-  IApi,
-  jobs
-} from './index';
+import { IApi, jobs, SchedulerJob } from './index';
 import { WazuhHostsCtrl } from '../../controllers/wazuh-hosts';
+
 jest.mock('../../controllers/wazuh-hosts');
 jest.mock('./save-document');
 jest.mock('./predefined-jobs', () => ({
@@ -24,103 +21,117 @@ jest.mock('./predefined-jobs', () => ({
       params: {},
       interval: '* */2 * * *',
       index: 'manager-status',
-    }
-  }
+    },
+  },
 }));
 
 describe('SchedulerJob', () => {
-  const oneApi = [{
-    url: 'https://localhost',
-    port: 55000,
-    username: 'wazuh',
-    password: 'wazuh',
-    id: 'default',
-    cluster_info: {
-      status: 'disabled',
-      manager: 'master',
-      node: 'node01',
-      cluster: 'Disabled'
-    }
-  }];
-  const twoApi = [
-    {
-      url: 'https://localhost',
-      port: 55000,
-      username: 'wazuh',
-      password: 'wazuh',
-      id: 'internal',
-      cluster_info: {
-        status: 'disabled',
-        manager: 'master',
-        node: 'node01',
-        cluster: 'Disabled'
-      }
+  const oneApi = {
+    body: [
+      {
+        url: 'https://localhost',
+        port: 55000,
+        username: 'wazuh',
+        password: 'wazuh',
+        id: 'default',
+        cluster_info: {
+          status: 'disabled',
+          manager: 'master',
+          node: 'node01',
+          cluster: 'Disabled',
+        },
+      },
+    ],
+  };
+  const twoApi = {
+    body: [
+      {
+        url: 'https://localhost',
+        port: 55000,
+        username: 'wazuh',
+        password: 'wazuh',
+        id: 'internal',
+        cluster_info: {
+          status: 'disabled',
+          manager: 'master',
+          node: 'node01',
+          cluster: 'Disabled',
+        },
+      },
+      {
+        url: 'https://externalhost',
+        port: 55000,
+        username: 'wazuh',
+        password: 'wazuh',
+        id: 'external',
+        cluster_info: {
+          status: 'disabled',
+          manager: 'master',
+          node: 'node01',
+          cluster: 'Disabled',
+        },
+      },
+    ],
+  };
+  const threeApi = {
+    body: [
+      {
+        url: 'https://localhost',
+        port: 55000,
+        username: 'wazuh',
+        password: 'wazuh',
+        id: 'internal',
+        cluster_info: {
+          status: 'disabled',
+          manager: 'master',
+          node: 'node01',
+          cluster: 'Disabled',
+        },
+      },
+      {
+        url: 'https://externalhost',
+        port: 55000,
+        username: 'wazuh',
+        password: 'wazuh',
+        id: 'external',
+        cluster_info: {
+          status: 'disabled',
+          manager: 'master',
+          node: 'node01',
+          cluster: 'Disabled',
+        },
+      },
+      {
+        url: 'https://externalhost',
+        port: 55000,
+        username: 'wazuh',
+        password: 'wazuh',
+        id: 'experimental',
+        cluster_info: {
+          status: 'disabled',
+          manager: 'master',
+          node: 'node01',
+          cluster: 'Disabled',
+        },
+      },
+    ],
+  };
+  const mockContext = {
+    wazuh: {
+      logger: { logger: {} },
+      api: { client: [Object] },
     },
-    {
-      url: 'https://externalhost',
-      port: 55000,
-      username: 'wazuh',
-      password: 'wazuh',
-      id: 'external',
-      cluster_info: {
-        status: 'disabled',
-        manager: 'master',
-        node: 'node01',
-        cluster: 'Disabled'
-      }
-    },
-  ];
-  const threeApi = [
-    {
-      url: 'https://localhost',
-      port: 55000,
-      username: 'wazuh',
-      password: 'wazuh',
-      id: 'internal',
-      cluster_info: {
-        status: 'disabled',
-        manager: 'master',
-        node: 'node01',
-        cluster: 'Disabled'
-      }
-    },
-    {
-      url: 'https://externalhost',
-      port: 55000,
-      username: 'wazuh',
-      password: 'wazuh',
-      id: 'external',
-      cluster_info: {
-        status: 'disabled',
-        manager: 'master',
-        node: 'node01',
-        cluster: 'Disabled'
-      }
-    },
-    {
-      url: 'https://externalhost',
-      port: 55000,
-      username: 'wazuh',
-      password: 'wazuh',
-      id: 'experimental',
-      cluster_info: {
-        status: 'disabled',
-        manager: 'master',
-        node: 'node01',
-        cluster: 'Disabled'
-      }
-    },
-  ];
+  };
 
   let schedulerJob: SchedulerJob;
 
   beforeEach(() => {
-    schedulerJob = new SchedulerJob('testJob1', {});
+    schedulerJob = new SchedulerJob('testJob1', mockContext);
   });
 
   afterEach(() => {
     jest.clearAllMocks();
-  })
+  });
 
   it('should job is assigned ', () => {
     expect(schedulerJob).toBeInstanceOf(SchedulerJob);
@@ -130,27 +141,26 @@ describe('SchedulerJob', () => {
   it('should get API object when no specified the `apis` parameter on the job object', async () => {
     WazuhHostsCtrl.prototype.getHostsEntries.mockResolvedValue(oneApi);
 
-
     const apis: IApi[] = await schedulerJob.getApiObjects();
     expect(apis).not.toBeUndefined();
     expect(apis).not.toBeFalsy();
-    expect(apis).toEqual(oneApi);
+    expect(apis).toEqual(oneApi.body);
   });
 
   it('should get all API objects when no specified the `apis` parameter on the job object', async () => {
-    WazuhHostsCtrl.prototype.getHostsEntries.mockResolvedValue(twoApi)
+    WazuhHostsCtrl.prototype.getHostsEntries.mockResolvedValue(twoApi);
     const apis: IApi[] = await schedulerJob.getApiObjects();
 
     expect(apis).not.toBeUndefined();
     expect(apis).not.toBeFalsy();
-    expect(apis).toEqual(twoApi);
+    expect(apis).toEqual(twoApi.body);
   });
 
   it('should get one of two API object when specified the id in `apis` parameter on the job object', async () => {
-    WazuhHostsCtrl.prototype.getHostsEntries.mockResolvedValue(twoApi)
+    WazuhHostsCtrl.prototype.getHostsEntries.mockResolvedValue(twoApi);
     jobs[schedulerJob.jobName] = { ...jobs[schedulerJob.jobName], apis: ['internal'] };
     const apis: IApi[] = await schedulerJob.getApiObjects();
-    const filteredTwoApi = twoApi.filter(item => item.id === 'internal')
+    const filteredTwoApi = twoApi.body.filter((item) => item.id === 'internal');
 
     expect(apis).not.toBeUndefined();
     expect(apis).not.toBeFalsy();
@@ -158,11 +168,11 @@ describe('SchedulerJob', () => {
   });
 
   it('should get two of three API object when specified the id in `apis` parameter on the job object', async () => {
-    WazuhHostsCtrl.prototype.getHostsEntries.mockResolvedValue(threeApi)
+    WazuhHostsCtrl.prototype.getHostsEntries.mockResolvedValue(threeApi);
     const selectedApis = ['internal', 'external'];
     jobs[schedulerJob.jobName] = { ...jobs[schedulerJob.jobName], apis: selectedApis };
     const apis: IApi[] = await schedulerJob.getApiObjects();
-    const filteredThreeApi = threeApi.filter(item => selectedApis.includes(item.id))
+    const filteredThreeApi = threeApi.body.filter((item) => selectedApis.includes(item.id));
 
     expect(apis).not.toBeUndefined();
     expect(apis).not.toBeFalsy();
@@ -170,18 +180,19 @@ describe('SchedulerJob', () => {
   });
 
   it('should throw an exception when no get APIs', async () => {
-    WazuhHostsCtrl.prototype.getHostsEntries.mockResolvedValue([])
-    await expect(schedulerJob.getApiObjects()).rejects.toEqual(
-      { error: 10001, message: 'No Wazuh host configured in wazuh.yml' }
-    );
+    WazuhHostsCtrl.prototype.getHostsEntries.mockResolvedValue({ body: [] });
+    await expect(schedulerJob.getApiObjects()).rejects.toEqual({
+      error: 10001,
+      message: 'No Wazuh host configured in wazuh.yml',
+    });
   });
 
   it('should throw an exception when no match API', async () => {
-    WazuhHostsCtrl.prototype.getHostsEntries.mockResolvedValue(threeApi)
+    WazuhHostsCtrl.prototype.getHostsEntries.mockResolvedValue(threeApi);
     jobs[schedulerJob.jobName] = { ...jobs[schedulerJob.jobName], apis: ['unkown'] };
-    await expect(schedulerJob.getApiObjects()).rejects.toEqual(
-      { error: 10002, message: 'No host was found with the indicated ID' }
-    );
+    await expect(schedulerJob.getApiObjects()).rejects.toEqual({
+      error: 10002,
+      message: 'No host was found with the indicated ID',
+    });
   });
-
-})
+});
--- test/jest/config.js
+++ test/jest/config.js
@@ -12,12 +12,10 @@ export default {
     `${kbnDir}/node_modules`
   ],
   collectCoverageFrom: [
-    `${kbnDir}/packages/kbn-ui-framework/src/components/**/*.js`,
-    `${kbnDir}/!packages/kbn-ui-framework/src/components/index.js`,
-    `${kbnDir}/!packages/kbn-ui-framework/src/components/**/*/index.js`,
-    `${kbnDir}/packages/kbn-ui-framework/src/services/**/*.js`,
-    `${kbnDir}/!packages/kbn-ui-framework/src/services/index.js`,
-    `${kbnDir}/!packages/kbn-ui-framework/src/services/**/*/index.js`,
+    "./common/**/*.{js,jsx,ts,tsx}",
+    "./public/**/*.{js,jsx,ts,tsx}",
+    "./server/**/*.{js,jsx,ts,tsx}",
+    "./!**/node_modules/**",
   ],
   moduleNameMapper: {
     '^ui/(.*)': `${kbnDir}/src/ui/public/$1`,
@@ -28,9 +26,12 @@ export default {
     `${kbnDir}/src/dev/jest/setup/babel_polyfill.js`,
     `${kbnDir}/src/dev/jest/setup/enzyme.js`,
   ],
-  coverageDirectory: `${kbnDir}/target/jest-coverage`,
+  collectCoverage: true,
+  coverageDirectory: `./target/test-coverage`,
   coverageReporters: [
     'html',
+    'text-summary',
+    'json-summary'
   ],
   globals: {
     'ts-jest': {
@@ -49,7 +50,6 @@ export default {
   ],
   testMatch: [
     '**/*.test.{js,ts,tsx}',
-    '**/*{js,ts,tsx}'
   ],
   transform: {
     '^.+\\.js$': `${kbnDir}/src/dev/jest/babel_transform.js`,
