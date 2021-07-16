71e7ba3d67e0777dd1043766b2575f0767d0e8eb
Gabriel Wassan <gabriel.wassan@wazuh.com>
2021-07-16 17:56:10 +0200

    Refactor try catch in Agent Syscollector/Vuls/Stats (#3462)
    * fix(syscollector): Added try catch strategy.
    
    * Implement try catch strategy in Groups (#3415)
    
    * Implement try catch groups
    
    * Add test
    
    * test(groups): Added simple snapshot test.
    
    * Add changelog
    
    * Change context
    
    * Change title
    
    Co-authored-by: gabiwassan <gabriel.wassan@wazuh.com>
    
    * Create Cypress Workflow for 4.3-7.10 (#3444)
    
    * Fixed dispatch for updateCurrentAgentData (#3453)
    
    * fix(syscollector): Fixed dispatch for updateCurrentAgentData
    
    * fix(syscollector): Refactor for agents-sections.
    
    * doc(changelog): Updated
    
    * Updating test. (#3470)
    
    * test(unittest): Updating test.
    
    * test(unittest): Updating test.
    
    * test(agent-table): Added unit test & fix collectCoverage
    
    Co-authored-by: Pablo Martínez <pablo.martinez@wazuh.com>
    Co-authored-by: Matias Ezequiel Moreno <49887871+matiasmoreno876@users.noreply.github.com>

--- public/components/common/hooks/useGenericRequest.ts
+++ public/components/common/hooks/useGenericRequest.ts
@@ -1,10 +1,18 @@
 import { useState, useEffect } from 'react';
 import { GenericRequest } from '../../../react-services/generic-request';
+import { UI_LOGGER_LEVELS } from '../../../../common/constants';
+import { getErrorOrchestrator } from '../../../react-services/common-services';
+import {
+  UI_ERROR_SEVERITIES,
+  UIErrorLog,
+  UIErrorSeverity,
+  UILogLevel,
+} from '../../../react-services/error-orchestrator/types';
 
 export function useGenericRequest(method, path, params, formatFunction) {
-  const [items, setItems] = useState({}); 
-  const [isLoading, setisLoading] = useState(true); 
-  const [error, setError] = useState(""); 
+  const [items, setItems] = useState({});
+  const [isLoading, setisLoading] = useState(true);
+  const [error, setError] = useState("");
 
   useEffect( () => {
     try{
@@ -13,11 +21,22 @@ export function useGenericRequest(method, path, params, formatFunction) {
           const response = await GenericRequest.request(method, path, params);
           setItems(response);
           setisLoading(false);
-        } 
+        }
         fetchData();
-    }catch(err){
+    } catch(error) {
       setError(error);
       setisLoading(false);
+      const options: UIErrorLog = {
+        context: `${useGenericRequest.name}.fetchData`,
+        level: UI_LOGGER_LEVELS.ERROR as UILogLevel,
+        severity: UI_ERROR_SEVERITIES.UI as UIErrorSeverity,
+        error: {
+          error: error,
+          message: error.message || error,
+          title: error.name || error,
+        },
+      };
+      getErrorOrchestrator().handleError(options);
     }
   }, [params]);
 
