7f672af08287e49a68141b6dc8038897d360ffd3
Pablo Martínez <pablo.martinez@wazuh.com>
2021-07-20 19:03:54 +0200

    Implement try catch in Components > Wz files (#3448)
    * Implement try catch wz files in components
    
    * Fix useEffect
    
    * Update changelog
    
    * Solve comments
    
    * Change some error display options
    
    Co-authored-by: Gabriel Wassan <gabriel.wassan@wazuh.com>
    Co-authored-by: Maximiliano Ibarra <maximilianoaibarra@gmail.com>

--- public/components/wz-menu/wz-menu.js
+++ public/components/wz-menu/wz-menu.js
@@ -46,6 +46,9 @@ import { AppNavigate } from '../../react-services/app-navigate';
 import WzTextWithTooltipIfTruncated from '../../components/common/wz-text-with-tooltip-if-truncated';
 import { getDataPlugin } from '../../kibana-services';
 import { withWindowSize } from '../../components/common/hocs/withWindowSize';
+import { UI_LOGGER_LEVELS } from '../../../common/constants';
+import { UI_ERROR_SEVERITIES } from '../../react-services/error-orchestrator/types';
+import { getErrorOrchestrator } from '../../react-services/common-services'
 
 
 const sections = {
@@ -103,8 +106,21 @@ export const WzMenu = withWindowSize(class WzMenu extends Component {
           }
         }
       }
-    } catch (err) { }
-
+    } catch (error) { 
+      const options = {
+        context: `${WzMenu.name}.componentDidMount`,
+        level: UI_LOGGER_LEVELS.ERROR,
+        severity: UI_ERROR_SEVERITIES.CRITICAL,
+        store: true,
+        display: true,
+        error: {
+          error: error,
+          message: error.message || error,
+          title: error.name || error,
+        },
+      };
+      getErrorOrchestrator().handleError(options);
+    }
   }
 
   showToast = (color, title, text, time) => {
@@ -167,7 +183,7 @@ export const WzMenu = withWindowSize(class WzMenu extends Component {
         });
       }
     } catch (error) {
-      this.showToast('danger', 'Error', error, 4000);
+      throw error;
     }
   }
 
@@ -253,7 +269,19 @@ export const WzMenu = withWindowSize(class WzMenu extends Component {
         });
       }
     } catch (error) {
-      this.showToast('danger', 'Error', error.message || error, 4000);
+      const options = {
+        context: `${WzMenu.name}.load`,
+        level: UI_LOGGER_LEVELS.ERROR,
+        severity: UI_ERROR_SEVERITIES.BUSINESS,
+        store: true,
+        display: true,
+        error: {
+          error: error,
+          message: error.message || error,
+          title: error.name || error,
+        },
+      };
+      getErrorOrchestrator().handleError(options);
     }
     this.isLoading = false;
   }
@@ -272,7 +300,19 @@ export const WzMenu = withWindowSize(class WzMenu extends Component {
         this.updatePatternAndApi();
       }
     } catch (error) {
-      this.showToast('danger', 'Error', error, 4000);
+      const options = {
+        context: `${WzMenu.name}.changePattern`,
+        level: UI_LOGGER_LEVELS.ERROR,
+        severity: UI_ERROR_SEVERITIES.BUSINESS,
+        store: false,
+        display: true,
+        error: {
+          error: error,
+          message: error.message || error,
+          title: `Error changing the Index Pattern`,
+        },
+      };
+      getErrorOrchestrator().handleError(options);
     }
   };
 
@@ -323,7 +363,17 @@ export const WzMenu = withWindowSize(class WzMenu extends Component {
         this.router.reload();
       }
     } catch (error) {
-      this.showToast('danger', 'Error', error, 4000);
+      const options = {
+        context: `${WzMenu.name}.changePattern`,
+        level: UI_LOGGER_LEVELS.ERROR,
+        severity: UI_ERROR_SEVERITIES.BUSINESS,
+        error: {
+          error: error,
+          message: error.message || error,
+          title: `Error changing the selected API`,
+        },
+      };
+      getErrorOrchestrator().handleError(options);
     }
   };
 
