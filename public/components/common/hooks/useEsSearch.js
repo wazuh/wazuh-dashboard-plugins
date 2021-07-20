defc79139e663cc8f34bd3b2329e720207060088
Manuel Gómez Castro <manuel.gomez@wazuh.com>
2021-07-20 09:47:20 +0200

    Finished useEsSearch hook

--- public/components/common/hooks/useEsSearch.js
+++ public/components/common/hooks/useEsSearch.js
@@ -3,30 +3,47 @@ import { getDataPlugin } from '../../../kibana-services';
 import { useQuery, useIndexPattern, useFilterManager } from '../hooks';
 import _ from 'lodash';
 
-const useEsSearch = (preAppliedQuery = {}, preAppliedAggs = {}, size = 10) => {
+/*
+You can find more info on how to use the preAppliedAggs object at https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-bucket-terms-aggregation.html
+You can find more info on how to construct a filter object at https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl.html
+*/
+const useEsSearch = ({ preAppliedFilters = {}, preAppliedAggs = {}, size = 10 }) => {
   const data = getDataPlugin();
   const indexPattern = useIndexPattern();
   const filterManager = useFilterManager();
-  const [esResults, setEsResults] = useState({})
-  useEffect(()=>{
-    search()
-      .then((result)=>{
-        setEsResults(result)
-      })
-  },[indexPattern])
+  const [esResults, setEsResults] = useState({});
+  const [managedFilters, setManagedFilters] = useState([]);
+  const [page, setPage] = useState(0);
+  const [query] = useQuery();
+  useEffect(() => {
+    search().then((result) => {
+      setEsResults(result);
+    });
+  }, [indexPattern, query, managedFilters, page]);
+  useEffect(() => {
+    let filterSubscriber = filterManager.getUpdates$().subscribe(() => {
+      const newFilters = filterManager.getFilters();
+      if (!_.isEqual(managedFilters, newFilters)) {
+        setManagedFilters(newFilters);
+      }
+      return () => {
+        filterSubscriber.unsubscribe();
+      };
+    });
+  });
 
   const search = async () => {
     if (indexPattern) {
       const esQuery = await data.query.getEsQuery(indexPattern);
       const searchSource = await data.search.searchSource.create();
-      const combined = _.merge({ ...preAppliedQuery }, { ...esQuery } || {});
-
+      const combined = [...esQuery.bool.filter, ...preAppliedFilters, ...managedFilters];
       const results = await searchSource
         .setParent(undefined)
-        .setField('filter', combined.bool.filter)
-        .setField('query', combined)
+        .setField('filter', combined)
+        .setField('query', esQuery)
         .setField('aggs', preAppliedAggs)
         .setField('size', size)
+        .setField('from', page * size)
         .setField('index', indexPattern)
         .fetch();
       return results;
@@ -34,8 +51,19 @@ const useEsSearch = (preAppliedQuery = {}, preAppliedAggs = {}, size = 10) => {
       return {};
     }
   };
+
+  const nextPage = () => {
+    setPage(page + 1);
+  };
+  const prevPage = () => {
+    setPage(page - 1 < 0 ? 0 : page - 1);
+  };
+
   return {
-    esResults
+    esResults,
+    setPage,
+    nextPage,
+    prevPage,
   };
 };
 
