a6e16a03bbcb88ed52d77fb4d821a75f5a64cbd5
Federico Rodriguez <federico.rodriguez@wazuh.com>
2021-07-23 14:47:07 +0200

    fixed viscard title

--- public/components/overview/office-panel/components/vis-card.tsx
+++ public/components/overview/office-panel/components/vis-card.tsx
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
 import { EuiFlexGroup, EuiFlexItem, EuiPanel, EuiTitle, EuiButtonIcon } from '@elastic/eui';
+import { RawVisualizations } from '../../../../factories/raw-visualizations';
 import KibanaVis from '../../../../kibana-integrations/kibana-vis';
 
-export const VisCard = ({ changeView = () => { }, title = 'Vis Title', id, width, tab, ...props }) => {
+export const VisCard = ({ changeView = () => { }, id, width, tab, ...props }) => {
 
   const [expandedVis, setExpandedVis] = useState(false);
+  
+  const title = (() => {
+    const visList = new RawVisualizations().getList();
+    const rawVis = visList ? visList.filter((item) => item && item.id === id) : [];
+    return rawVis.length && rawVis[0]?.attributes?.title;
+  })()
 
   const toggleExpand = id => {
     setExpandedVis(expandedVis === id ? false : id);
   };
-  
+
   return <> <EuiFlexItem grow={width}>
     <EuiPanel paddingSize={'s'} className={expandedVis === id ? 'fullscreen h-100' : 'h-100'}>
       <EuiFlexGroup direction={'column'} className={'h-100'}>
