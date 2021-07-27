fa711f8e2544eaa479bfdc50cd8ff08ab2061f1c
eze9252 <eze9252@gmail.com>
2021-07-27 13:50:51 +0200

    fix kbn custom search bar

--- public/components/common/custom-search-bar/custom-search-bar.tsx
+++ public/components/common/custom-search-bar/custom-search-bar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import { getIndexPattern } from '../../overview/mitre/lib'
 import { Filter } from '../../../../../../src/plugins/data/public/';
@@ -9,7 +9,6 @@ import {
     EuiFlexItem,
     EuiComboBox,
     EuiSwitch,
-    EuiSpacer,
   } from '@elastic/eui';
 
 //@ts-ignore
@@ -17,29 +16,34 @@ import { getDataPlugin } from '../../../kibana-services';
 import { KbnSearchBar } from '../../kbn-search-bar';
 import { TimeRange, Query } from '../../../../../../src/plugins/data/common';
 
-
-
 export const CustomSearchBar = ({ ...props }) => {
 
     const KibanaServices = getDataPlugin().query;
     const filterManager = KibanaServices.filterManager;
     const timefilter = KibanaServices.timefilter.timefilter;
+    const indexPattern =  getIndexPattern();
     const [filterParams, setFilterParams] = useState({
         filters: filterManager.getFilters() || [],
         query: { language: 'kuery', query: '' },
         time: timefilter.getTime(),
     });
+    const defaultSelectedOptions = () => {
+        const array = []
+        props.filtersValues.forEach(item =>{
+            array[item.key] = []
+        })
+
+        return array
+    }
     const [isLoading, setLoading] = useState(false);
     const [customFilters, setCustomFilters] = useState(props.filtersValues)
     const [avancedFiltersState, setAvancedFiltersState] = useState(false);
-    const [selectedOptions, setSelectedOptions] = useState([]);
-
-    const [defaultFilters, setDefaultFilters] = useState(filterManager.getFilters());
+    const [selectedOptions, setSelectedOptions] = useState(defaultSelectedOptions);
+    const [currentSelectName, setCurrentSelectName] = useState('');
 
     const onQuerySubmit = (payload: { dateRange: TimeRange, query: Query }) => {
         const { query, dateRange } = payload;
         const filters = { query, time: dateRange, filters: filterParams.filters };
-        setLoading(true);
         setFilterParams(filters);
 
     }
@@ -47,20 +51,8 @@ export const CustomSearchBar = ({ ...props }) => {
     const onFiltersUpdated = (filters: Filter[]) => {
         const { query, time } = filterParams;
         const updatedFilterParams = { query, time, filters };
-        setLoading(true);
-        refreshCustomSelectedFilter()
         setFilterParams(updatedFilterParams);
-    }
-
-    const getCustomFilters = (filters) => {
-        const deleteDefaultFilters = []
-        filters.forEach(filter => {
-                if(!defaultFilters.some(item => item.meta.key === filter.meta.key)){
-                   deleteDefaultFilters.push(filter)
-                }
-         })
-
-         return deleteDefaultFilters
+        refreshCustomSelectedFilter()
     }
 
     const changeSwitch = () => {
@@ -91,45 +83,52 @@ export const CustomSearchBar = ({ ...props }) => {
         return { meta, $state, query };
     };
 
-    const setKibanaFilters = async (values) => {
-        const indexPattern =  await getIndexPattern()
+    const setKibanaFilters = (values) => {
+        setLoading(true)
         const newFilters = []
-        if(!values.length){
-            filterManager.removeAll()
-            filterManager.addFilters(defaultFilters)
+        if(!values.length){          
+                const currentFilters = filterManager.getFilters().filter(item => item.meta.key != currentSelectName)
+                filterManager.removeAll()
+                filterManager.addFilters(currentFilters)
+                
         }else{
+            const currentFilters = filterManager.getFilters().filter(item => item.meta.key != values[0].key)
             filterManager.removeAll()
-            newFilters.push(defaultFilters);
+            filterManager.addFilters(currentFilters)
             values.forEach(element => {
                 const customFilter = buildCustomFilter(false,indexPattern.title,element.label,element.key)
                 newFilters.push(customFilter);
             });
             filterManager.addFilters(newFilters)
-        }   
+        } 
     }
 
     const refreshCustomSelectedFilter = () => {
+        setSelectedOptions(defaultSelectedOptions)
         const filters = filterManager.getFilters()
-        const customFilters = getCustomFilters(filters)
-        const filtersUpdated = []
-        customFilters.forEach(item => {
-            const filterObj = {
-                key: item.meta.key,
-                label: item.meta.params.query,
-            }
-            filtersUpdated.push(filterObj)
+        const filterCustom = filters.map(item => {
+                return  {
+                    key: item.meta.key,
+                    label: item.meta.params.query,
+                }
+        }).filter(element => Object.keys(selectedOptions).includes(element.key))
 
-        })
-        setSelectedOptions(filtersUpdated)
-        console.log(customFilters)
-       
+        if(filterCustom.length != 0){
+            filterCustom.forEach(item => {
+                setSelectedOptions(prevState => ({
+                        ...prevState,
+                        [item.key]: [...prevState[item.key],item],
+                }));
+    
+            })
+        } 
+        setLoading(false)      
     };
 
-    const onChange = async(values) => {
-        await setKibanaFilters(values)
-        refreshCustomSelectedFilter();
+    const onChange = (values) => {
+        setKibanaFilters(values)
+        refreshCustomSelectedFilter(); 
     };
-
 
     const getComponent = (item) => {
         var types = {
@@ -137,9 +136,10 @@ export const CustomSearchBar = ({ ...props }) => {
             'combobox': <EuiComboBox
                         placeholder={"Select "+item.key}
                         options={item.values}
-                        selectedOptions={selectedOptions}
+                        selectedOptions={selectedOptions[item.key] || []}
                         onChange={onChange}
-                        isClearable={true}
+                        onClick={() => setCurrentSelectName(item.key)}
+                        isClearable={false}
                     />
         };
         return types[item.type] || types['default'];
@@ -152,7 +152,7 @@ export const CustomSearchBar = ({ ...props }) => {
                 avancedFiltersState === false ?
                 customFilters.map((item, key) => (
                     <EuiFlexItem grow={2} key={key}>
-                        {getComponent(item)}             
+                            {getComponent(item)}           
                     </EuiFlexItem>
                 ))
                 :
@@ -164,7 +164,7 @@ export const CustomSearchBar = ({ ...props }) => {
                 showQueryInput={avancedFiltersState}
                 onQuerySubmit={onQuerySubmit}
                 onFiltersUpdated={onFiltersUpdated}
-                isLoading={false} 
+                isLoading={isLoading} 
             />
             </EuiFlexItem>
         </EuiFlexGroup>
