2de66ea2b05e1b0966d2381b635fd286fd74d783
gabiwassan <gabriel.wassan@wazuh.com>
2021-07-27 11:04:37 -0300

    feat(useEsSearch): Added copyright

--- public/components/common/hooks/use-es-search.ts
+++ public/components/common/hooks/use-es-search.ts
@@ -1,3 +1,15 @@
+/*
+ * Wazuh app - React hook for search ElasticSearch DB.
+ * Copyright (C) 2015-2021 Wazuh, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Find more information about this on the LICENSE file.
+ */
+
 import { SetStateAction, useEffect, useState } from 'react';
 import { getDataPlugin } from '../../../kibana-services';
 import { useFilterManager, useIndexPattern, useQuery } from '.';
