b90d411bb241a2fb88fa17a894722f4aded27747
Manuel Gómez Castro <manuel.gomez@wazuh.com>
2021-07-28 12:43:30 +0200

    Added copyright

--- public/components/common/hooks/use-value-suggestions.ts
+++ public/components/common/hooks/use-value-suggestions.ts
@@ -1,3 +1,14 @@
+/*
+ * Wazuh app - React hook for getting value suggestions
+ * Copyright (C) 2015-2021 Wazuh, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Find more information about this on the LICENSE file.
+ */
 import { useState, useEffect } from 'react';
 import { getDataPlugin } from '../../../kibana-services';
 import { useIndexPattern } from '.';
