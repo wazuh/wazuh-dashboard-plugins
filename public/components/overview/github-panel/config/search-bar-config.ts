d3cd7211ed9d7d66795fc01182018a26698e0205
CPAlejandro <cuellarpeinado@gmail.com>
2021-08-03 13:59:48 +0200

    Integrating dynamic filters

--- public/components/overview/github-panel/config/search-bar-config.ts
+++ public/components/overview/github-panel/config/search-bar-config.ts
@@ -1,70 +1,33 @@
+/*
+ *   Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License").
+ *   You may not use this file except in compliance with the License.
+ *   A copy of the License is located at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file. This file is distributed
+ *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *   express or implied. See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
+
 export const filtersValues = [
-    {   
-        type: 'combobox',
-        key: 'agent.id',
-        values:[
-            {
-                key:'agent.id',
-                label: '001',
-                
-            },
-            {
-                key:'agent.id',
-                label: '002',
-            },
-            {
-                key:'agent.id',
-                label: '003',
-            },
-            {
-                key:'agent.id',
-                label: '004',
-            },
-            {
-                key:'agent.id',
-                label: '006',
-            }
-        ]
-    },
-    {   
-        type: 'combobox',
-        key: 'agent.name',
-        values:[
-            {
-                key:'agent.name',
-                label: 'Amazon',
-                
-            },
-            {
-                key:'agent.name',
-                label: 'Centos',
-            },
-            {
-                key:'agent.name',
-                label: '003',
-            },
-            {
-                key:'agent.name',
-                label: '004',
-            },
-            {
-                key:'agent.name',
-                label: '006',
-            }
-        ]
-    },
-    {
-        type: 'combobox',
-        key: 'agent.ip',
-        values:[
-            {
-                key:'agent.ip',
-                label: '24.273.97.14',
-            },
-            {
-                key:'agent.ip',
-                label: '197.17.1.4',
-            },
-        ]
-    }
-]
+  {
+    type: 'combobox',
+    key: 'data.github.actor',
+  },
+  {
+    type: 'combobox',
+    key: 'data.github.org',
+  },
+  {
+    type: 'combobox',
+    key: 'data.github.repository',
+  },
+  {
+    type: 'combobox',
+    key: 'data.github.action',
+  },
+];
