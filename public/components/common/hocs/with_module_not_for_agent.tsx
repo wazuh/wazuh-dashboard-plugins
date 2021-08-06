460d5a07768a75bd98244ff2057169439125473a
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2021-08-06 15:11:49 +0200

    fix(frontend): Add license block to `with_module_not_for_agent.tsx` and `with_module_tab_loader.tsx`

--- public/components/common/hocs/with_module_not_for_agent.tsx
+++ public/components/common/hocs/with_module_not_for_agent.tsx
@@ -1,10 +1,20 @@
+/*
+ * Wazuh app - React HOC to show a prompt when a module is not available for agents and let to unpin the agent
+ * Copyright (C) 2015-2021 Wazuh, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Find more information about this on the LICENSE file.
+ */
 
-
+import React from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { withGuard } from './withGuard';
 import { PromptModuleNotForAgent } from '../../agents/prompts';
-import React from 'react';
 
 const mapStateToProps = (state) => ({
   agent: state.appStateReducers.currentAgentData,
