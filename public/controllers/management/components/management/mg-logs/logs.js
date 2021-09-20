92e841cd60be5f26457efe23c3b05c6aab581bf1
CPAlejandro <cuellarpeinado@gmail.com>
2021-09-20 12:18:42 +0200

    Changing a if/else for a ternary operator

--- public/controllers/management/components/management/mg-logs/logs.js
+++ public/controllers/management/components/management/mg-logs/logs.js
@@ -149,8 +149,7 @@ export default compose(
     if (this.state.selectedDaemon !== 'all')
       result['tag'] = this.state.selectedDaemon;
     if (this.state.appliedSearch) result['search'] = this.state.appliedSearch;
-    if (this.state.descendingSort) result['sort'] = '-timestamp';
-    else result['sort'] = '+timestamp';
+    result['sort'] = this.state.descendingSort ? '-timestamp' : '+timestamp';
 
     return result;
   }
