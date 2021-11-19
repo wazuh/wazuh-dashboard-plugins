6b3722bb23cd2c1cfe28914920af6a0dafda9fe4
yenienserrano <ian.serrano@wazuh.com>
2021-11-19 11:54:30 -0300

    feat(health-check logo): change of logo in health-check

--- public/services/resolves/get-config.js
+++ public/services/resolves/get-config.js
@@ -70,7 +70,7 @@ export async function getWzConfig($q, genericReq, wazuhConfig) {
     'enrollment.password': '',
     'customization.logo.app':'logotype.svg',
     'customization.logo.sidebar':'icon_blue.png',
-    'customization.logo.healthcheck':'icon_blue.svg',
+    'customization.logo.healthcheck':'logo.png',
     'customization.logo.reports':'logo.png'
   };
 
