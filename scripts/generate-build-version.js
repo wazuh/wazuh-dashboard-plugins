7845a3ddd1ba7a608407ec757d8801b1f038b796
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2022-01-26 17:28:14 +0100

    fix: Fixed the script to set the current app version in the plugin manifest file

--- scripts/generate-build-version.js
+++ scripts/generate-build-version.js
@@ -2,8 +2,10 @@ const fs = require('fs');
 const packageJson = require('../package.json');
 const { version, revision } = packageJson;
 
-//replaces kibana.json with last
-fs.readFile('kibana.json', 'utf-8', function (err, data) {
+//replaces the plugin manifest file with the current app version when building the plugin package
+const pluginManifestFilename = 'opensearch_dashboards.json';
+
+fs.readFile(pluginManifestFilename, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
     return;
@@ -15,12 +17,12 @@ fs.readFile('kibana.json', 'utf-8', function (err, data) {
 
   const jsonConfig = JSON.stringify(config, null, 2);
 
-  fs.writeFile('kibana.json', jsonConfig, 'utf8', function (err) {
+  fs.writeFile(pluginManifestFilename, jsonConfig, 'utf8', function (err) {
     if (err) {
-      console.log('An error occurred while writing JSON Object to kibana.json');
+      console.log(`An error occurred while writing JSON Object to ${pluginManifestFilename}`);
       return console.log(err);
     }
 
-    console.log('kibana.json file has been saved with latest version number');
+    console.log(`${pluginManifestFilename} file has been saved with latest version number`);
   });
 });
