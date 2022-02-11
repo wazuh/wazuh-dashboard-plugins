bf206826f504dfa7a9047df618642922857a2285
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2022-02-11 09:03:33 +0100

    fix: Fixed copyright year in the initial app configuration file

--- server/lib/initial-wazuh-config.ts
+++ server/lib/initial-wazuh-config.ts
@@ -15,7 +15,7 @@ import { ASSETS_BASE_URL_PREFIX } from "../../common/constants";
 export const initialWazuhConfig: string = `---
 #
 # Wazuh app - App configuration file
-# Copyright (C) 2015-2021 Wazuh, Inc.
+# Copyright (C) 2015-2022 Wazuh, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
