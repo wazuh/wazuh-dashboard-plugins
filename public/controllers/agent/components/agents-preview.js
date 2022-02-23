564f37a08f796769ec342db761c6f94da9a6673a
Machi3mfl <maximilianoaibarra@gmail.com>
2022-02-23 08:06:55 -0300

    Fixed agents link style for mobile

--- public/controllers/agent/components/agents-preview.js
+++ public/controllers/agent/components/agents-preview.js
@@ -290,7 +290,7 @@ export const AgentsPreview = compose(
                         )}
                         <EuiFlexGroup className="mt-0">
                           {this.lastAgent && (
-                            <EuiFlexItem className="flex-45">
+                            <EuiFlexItem className="agents-link-item">
                               <EuiStat
                                 className="euiStatLink last-agents-link"
                                 title={
@@ -307,7 +307,7 @@ export const AgentsPreview = compose(
                             </EuiFlexItem>
                           )}
                           {this.mostActiveAgent && (
-                            <EuiFlexItem className="flex-45">
+                            <EuiFlexItem className="agents-link-item">
                               <EuiStat
                                 className={this.mostActiveAgent.name ? 'euiStatLink' : ''}
                                 title={
