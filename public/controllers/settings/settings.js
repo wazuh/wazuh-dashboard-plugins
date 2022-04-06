cf4d7c99a8020875b5f877ce75e4f7c24c8cb49b
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2022-04-06 09:08:10 +0200

    fix(asset): Fixed the path to Google groups icon in the `Settings/About` section when Kibana is served in a subpath

--- public/controllers/settings/settings.js
+++ public/controllers/settings/settings.js
@@ -26,6 +26,7 @@ import { UI_LOGGER_LEVELS, PLUGIN_PLATFORM_NAME } from '../../../common/constant
 import { UI_ERROR_SEVERITIES } from '../../react-services/error-orchestrator/types';
 import { getErrorOrchestrator } from '../../react-services/common-services';
 import { getAssetURL } from '../../utils/assets';
+import { getHttp } from '../../kibana-services';
 
 export class SettingsController {
   /**
@@ -59,7 +60,7 @@ export class SettingsController {
     this.tabNames = TabNames;
     this.indexPatterns = [];
     this.apiEntries = [];
-    this.$scope.googleGroupsSVG = getAssetURL('images/icons/google_groups.svg');
+    this.$scope.googleGroupsSVG = getHttp().basePath.prepend(getAssetURL('images/icons/google_groups.svg'));
   }
 
   /**
