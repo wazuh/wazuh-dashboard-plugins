80292d35a2ab35f313b3c66663e6882587d83555
Federico Rodriguez <federico.rodriguez@wazuh.com>
2022-05-17 11:36:30 +0200

    Fixed troubleshooting url

--- public/components/visualize/wz-visualize.js
+++ public/components/visualize/wz-visualize.js
@@ -159,7 +159,7 @@ export const WzVisualize = compose(
     reloadToast = () => {
       const toastLifeTimeMs = 300000;
       const [mayor, minor] = appVersion.split('.');
-      const urlTroubleShootingDocs = `https://documentation.wazuh.com/${mayor}.${minor}/user-manual/elasticsearch/elastic-tuning/troubleshooting.html#index-pattern-was-refreshed-toast-keeps-popping-up`;
+      const urlTroubleShootingDocs = `https://documentation.wazuh.com/${mayor}.${minor}/user-manual/elasticsearch/troubleshooting.html#index-pattern-was-refreshed-toast-keeps-popping-up`;
       if(satisfyPluginPlatformVersion('<7.11')){
         getToasts().add({
           color: 'success',
