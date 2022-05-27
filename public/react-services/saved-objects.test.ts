fbf476ba69d89a2d13d60c31f8afaddb8ad1c34d
Machi3mfl <maximilianoaibarra@gmail.com>
2022-05-27 15:20:03 -0300

    Added error factory in saved-objects and tests

---
+++ public/react-services/saved-objects.test.ts
@@ -0,0 +1,76 @@
+import axios, { AxiosResponse } from 'axios';
+import { SavedObject } from './saved-objects';
+import { AppState } from './app-state';
+import { ErrorFactory } from './error-factory';
+
+jest.mock('./app-state');
+
+jest.mock('axios');
+
+jest.mock('../kibana-services', () => ({
+  ...(jest.requireActual('../kibana-services') as object),
+  getHttp: jest.fn().mockReturnValue({
+    basePath: {
+      get: () => {
+        return 'http://localhost:5601';
+      },
+      prepend: (url) => {
+        return `http://localhost:5601${url}`;
+      },
+    },
+  }),
+  getCookies: jest.fn().mockReturnValue({
+    set: (name, value, options) => {
+      return true;
+    },
+  }),
+}));
+
+// mock window location
+const mockResponse = jest.fn();
+Object.defineProperty(window, 'location', {
+  value: {
+    hash: {
+      endsWith: mockResponse,
+      includes: mockResponse,
+    },
+    href: mockResponse,
+    assign: mockResponse,
+  },
+  writable: true,
+});
+
+describe('SavedObjects', () => {
+  const response: AxiosResponse = {
+    data: {
+      statusCode: 500,
+      error: 'Internal Server Error',
+      message: '3099 - ERROR3099 - Wazuh not ready yet',
+    },
+    status: 500,
+    statusText: 'Internal Server Error',
+    headers: {},
+    config: {},
+    request: {},
+  };
+
+  describe('existsIndexPattern', () => {
+    it('Should return ERROR when get request if exist index pattern fails', async () => {
+      try {
+        const mockingError = new Error('Error on genericReq');
+        mockingError['response'] = response;
+
+        (axios as jest.MockedFunction<typeof axios>).mockResolvedValue(
+          Promise.reject(ErrorFactory.createError(mockingError))
+        );
+        const currentApiMock = JSON.stringify({ id: 'mocked-api-id' });
+        AppState.getCurrentAPI = jest.fn().mockReturnValue(currentApiMock);
+        await SavedObject.existsIndexPattern('fake-index-pattern');
+      } catch (error) {
+        expect(error).toBeInstanceOf(Error);
+        expect(typeof error).not.toBe('string');
+        expect(error.message).toBe(response.data.message);
+      }
+    });
+  });
+});
--- public/react-services/saved-objects.ts
+++ public/react-services/saved-objects.ts
@@ -60,7 +60,7 @@ export class SavedObject {
       result = this.validateIndexPatterns(list);
     }
 
-    return result.map((item) => {
+    return result.map((item: { id: string; attributes: { title: string } }) => {
       return { id: item.id, title: item.attributes.title };
     });
   }
@@ -117,7 +117,7 @@ export class SavedObject {
         };
       }
     } catch (error) {
-      return ((error || {}).data || {}).message || false ? new Error(error.data.message) : error;
+      return Promise.reject(error);
     }
   }
 
