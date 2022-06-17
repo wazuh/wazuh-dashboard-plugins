0e9d62df0e42432d97f96d5f8167553f8dc74fd7
Gabriel Diaz <gdiaz@qswarm.com>
2022-06-17 11:53:01 +0200

    Add elastic stack dev environment

---
+++ docker/kbn-dev/dev.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+
+elastic_versions=(
+	'7.16.0'
+	'7.16.1'
+	'7.16.2'
+	'7.16.3'
+	'7.17.0'
+	'7.17.1'
+	'7.17.2'
+	'7.17.3'
+	'7.17.4'
+	'8.0.0'
+	'8.0.1'
+	'8.1.0'
+	'8.1.1'
+	'8.1.2'
+	'8.1.3'
+	'8.2.0'
+	'8.2.1'
+	'8.2.2'
+	'8.2.3'
+	'8.2.4'
+	'8.2.5'
+)
+
+
+usage() {
+	echo 
+	echo "./dev.sh elastic_version /wazuh_app_src action "
+	echo 
+	echo "where"
+	echo "  elastic_version is one of " ${elastic_versions[*]}
+	echo "  wazuh_app_src is the path to the wazuh application source code"
+	echo "  action is one of up | down" 
+	exit -1
+}
+
+
+if [ $# -ne	3 ]
+  then
+  	echo "Incorrect number of arguments " $# 
+  	echo 
+    usage
+fi
+
+if [[ ! " ${elastic_versions[*]} " =~ " ${1} " ]]
+ then 
+ 	echo "Version ${1} not found in ${elastic_versions[*]}"
+ 	echo
+ 	exit -1
+fi
+
+if [[ $2 != /* ]]; then 
+	echo "Source path must be absolute, and start with /"
+	echo
+	usage
+	exit
+fi
+
+
+# Password for the 'kibana_system' user (at least 6 characters)
+export KIBANA_PASSWORD=SecretPassword
+
+# Password for the 'elastic' user (at least 6 characters)
+export ELASTIC_PASSWORD=SecretPassword
+
+# Version of Elastic products
+export STACK_VERSION=$1
+
+# Set the cluster name
+export CLUSTER_NAME=cluster
+
+# Set to 'basic' or 'trial' to automatically start the 30-day trial
+export LICENSE=basic
+#LICENSE=trial
+
+# Port to expose Elasticsearch HTTP API to the host
+export ES_PORT=9200
+#ES_PORT=127.0.0.1:9200
+
+# Port to expose Kibana to the host
+export KIBANA_PORT=5601
+
+# wazuh plugin source from parameter 2
+export SRC=$2
+
+
+case "$3" in
+	up)
+		docker-compose  -f dev.yml up -Vd
+		;;
+	down)
+		docker-compose  -f dev.yml down -v --remove-orphans
+		;;
+	*)
+		echo "Action must be up | down: "
+		echo
+		usage
+		;;
+esac
