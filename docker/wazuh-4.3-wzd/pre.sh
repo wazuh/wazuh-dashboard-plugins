475ff17bfbd4743b7129be6c2c7a71e1cabaf3c1
Gabriel Diaz <gdiaz@qswarm.com>
2022-06-24 12:21:28 +0200

    Add wazuh 4.3 wzd pre environment

---
+++ docker/wazuh-4.3-wzd/pre.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+
+versions=(
+	"4.3.0"
+	"4.3.1"
+	"4.3.2"
+	"4.3.3"
+	"4.3.4"
+	)
+
+wazuh_api_version=(
+	"5"
+	"17"
+)
+
+usage() {
+	echo
+	echo "./pre.sh wazuh_version wazuh_api_version action "
+	echo
+	echo "where"
+	echo "  wazuh_version is one of " ${elastic_versions[*]}
+	echo "  wazuh_api_version is the minor version of wazuh 4.3, for example " ${wazuh_api_version[*]}
+	echo "  action is one of up | down"
+	echo
+	echo "In a minor release, the API should not change the version here bumps the API"
+	echo " string returned for testing. This script generates the file "
+	echo
+	echo "    config/imposter/api_info.json"
+	echo
+	echo "used by the mock server"
+	exit -1
+}
+
+if [ $# -ne 3 ]
+  then
+  	echo "Incorrect number of arguments " $#
+    usage
+fi
+
+if [[ ! " ${versions[*]} " =~ " ${1} " ]]
+ then
+ 	echo "Version ${1} not found in ${versions[*]}"
+ 	exit -1
+fi
+
+[ -n "$2" ] && [ "$2" -eq "$2" ] 2>/dev/null
+if [ $? -ne 0 ]; then
+   echo "$2 is not number"
+   exit -1
+fi
+
+patch_version=$2
+cat << EOF > config/imposter/api_info.json
+{
+  "data": {
+    "title": "Wazuh API REST",
+    "api_version": "4.3.${patch_version}",
+    "revision": 40316,
+    "license_name": "GPL 2.0",
+    "license_url": "https://github.com/wazuh/wazuh/blob/4.3/LICENSE",
+    "hostname": "imposter",
+    "timestamp": "2022-06-13T17:20:03Z"
+  },
+  "error": 0
+}
+EOF
+
+export WAZUH_STACK=${1}
+export KIBANA_PORT=5601
+export COMPOSE_PROJECT_NAME=wazuh-${WAZUH_STACK}
+
+case "$3" in
+	up)
+		# recreate volumes
+		docker-compose -f pre.yml up -Vd
+
+		# This installs Wazuh and integrates with a default elastic stack
+		v=$( echo -n $WAZUH_STACK | sed 's/\.//g' )
+		echo
+		echo Install Wazuh ${WAZUH_STACK} into Wazuh ${WAZUH_STACK} manually with:
+		echo
+		echo 1. Uninstall current version of wazuh app
+		echo docker exec -ti wazuh-${v}_wazuh.dashboard_1  /usr/share/wazuh-dashboard/bin/opensearch-dashboards-plugin remove wazuh
+		echo
+		echo 2. Restart wazuh-dashboard
+		echo docker restart wazuh-${v}_wazuh.dashboard_1
+		echo
+		echo 3. Copy the pre-release pacakge to the running wazuh-dashboard container
+		echo docker cp wazuh-4.3.${patch_version}-1.zip wazuh--${v}_wazuh.dashboard_1:/tmp
+		echo
+		echo 4. Install the the package we have just uploaded
+		echo docker exec -ti  wazuh-${v}_wazuh.dashboard_1  /usr/share/wazuh-dashboard/bin/opensearch-dashboards-plugin install file:///tmp/wazuh--4.3.${patch_version}-1.zip
+		echo
+		echo 5. Restart the wazuh-dashboard container
+		echo docker restart wazuh-${v}_wazuh.dashboard_1
+		echo
+		echo 6. Upload the wazuh app configuration
+		echo docker cp ./config/kibana/wazuh.yml wazuh-${v}_wazuh.dashboard_1:/usr/share/wazuh-dashboard/data/wazuh/config/
+		echo
+		echo 7. Access the running instance in
+		echo http://localhost:${KIBANA_PORT}
+		;;
+	down)
+		# delete volumes
+		docker-compose -f pre.yml down -v --remove-orphans
+		;;
+	*)
+		echo "Action must be either up or down"
+		usage
+		;;
+esac
