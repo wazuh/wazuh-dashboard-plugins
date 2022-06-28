e9e75b9d9f5b6a2217dc300e6e8ab8cd7733aab4
Gabriel Diaz <gdiaz@qswarm.com>
2022-06-29 00:21:13 +0200

    Update opensearch dev environment

--- docker/osd-dev/dev.sh
+++ docker/osd-dev/dev.sh
@@ -1,27 +1,78 @@
 #!/bin/bash
 
-if [ -z "${SRC}" ]; then 
-	echo "Usage:"
-	echo " ./dev.sh up | down"
+
+os_versions=(
+	'1.2.4'
+	'2.0.0'
+	'2.0.1'
+)
+
+osd_versions=(
+	'1.2.0'
+	'2.0.0'
+)
+
+usage() {
+	echo
+	echo "./dev.sh os_version osd_version /wazuh_app_src action "
+	echo
+	echo "where"
+	echo "  os_version is one of " ${os_versions[*]}
+	echo "  osd_version is one of " ${osd_versions[*]}
+	echo "  wazuh_app_src is the path to the wazuh application source code"
+	echo "  action is one of up | down | stop"
+	exit -1
+}
+
+
+if [ $# -lt 4 ]
+  then
+  	echo "Incorrect number of arguments " $#
+  	echo
+    usage
+fi
+
+if [[ ! " ${os_versions[*]} " =~ " ${1} " ]]
+ then
+ 	echo "Version ${1} not found in ${os_versions[*]}"
+ 	echo
+ 	exit -1
+fi
+
+if [[ ! " ${osd_versions[*]} " =~ " ${2} " ]]
+ then
+ 	echo "Version ${1} not found in ${osd_versions[*]}"
+ 	echo
+ 	exit -1
+fi
+
+if [[ $3 != /* ]]; then
+	echo "Source path must be absolute, and start with /"
+	echo
+	usage
 	exit
 fi
 
+export PASSWORD=${PASSWORD:-admin}
+export OS_VERSION=$1
+export OSD_VERSION=$2
+export OSD_PORT=${PORT:-5601}
+export SRC=$3
+export COMPOSE_PROJECT_NAME=os-dev-${OSD_VERSION}
 
-# Password for the 'kibana_system' user (at least 6 characters)
-export KIBANA_PASSWORD=SecretPassword
-
-# Port to expose Kibana to the host
-export KIBANA_PORT=5601
-
-case "$1" in
+case "$4" in
 	up)
-		docker-compose -f dev.yml up -Vd
+		docker compose  -f dev.yml up -Vd
 		;;
 	down)
-		docker-compose -f dev.yml down -v
+		docker compose  -f dev.yml down -v --remove-orphans
+		;;
+	stop)
+		docker compose -f dev.yml stop
 		;;
 	*)
-		echo "Usage:"
-		echo "SRC=~/src/wazuh-kibana-app WAZUH_VERSION=4.3.4 ./dev.sh up | down"
+		echo "Action must be up | down: "
+		echo
+		usage
 		;;
 esac
