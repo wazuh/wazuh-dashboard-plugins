3c1e51cbc8d4c269d08df90f855d1f07e4897fd6
Álex <alejandro.ruiz.becerra@wazuh.com>
2022-07-15 15:12:07 +0200

    Sanitize report's inputs and usernames (#4330)
    * Validate path parameters of the reports endpoint
    
    * Remove leftover prop in the reporting-table component
    
    * Fix typo in report name validtion
    
    * Remove the return of reports path from the /reports endpoint
    
    * Remove absolute file path on report not found

--- server/lib/security-factory/factories/xpack-factory.ts
+++ server/lib/security-factory/factories/xpack-factory.ts
@@ -2,6 +2,7 @@ import { ISecurityFactory } from '../'
 import { SecurityPluginSetup } from 'x-pack/plugins/security/server';
 import { KibanaRequest } from 'src/core/server';
 import { WAZUH_SECURITY_PLUGIN_XPACK_SECURITY } from '../../../../common/constants';
+import md5 from 'md5';
 
 export class XpackFactory implements ISecurityFactory {
   platform: string = WAZUH_SECURITY_PLUGIN_XPACK_SECURITY;
@@ -12,7 +13,7 @@ export class XpackFactory implements ISecurityFactory {
       const authContext = await this.security.authc.getCurrentUser(request);
       if(!authContext) return {username: 'elastic', authContext: { username: 'elastic'}};
       const username = this.getUserName(authContext);
-      return {username, authContext};
+      return { username, authContext, hashUsername: md5(username) };
     } catch (error) {
       throw error; 
     }
