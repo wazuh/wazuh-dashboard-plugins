df6eb8c33767fe5be840e89dd13f47499db325f0
Facundo Mayon <mayonf95@gmail.com>
2022-07-27 15:40:10 -0300

    Fixing Mitre Att&ck module

--- test/performance/modules/mitre-attack.module.js
+++ test/performance/modules/mitre-attack.module.js
@@ -1,19 +1,37 @@
-const { WAIT_TIMEOUT } = require('../common/constants');
+const { WAIT_TIMEOUT, SERVER_URL } = require('../common/constants');
+const logger = require('../common/logger');
 
 module.exports = async function (context, commands) {
+  // Navigate to a URL, but do not measure the URL
+  await commands.navigate(SERVER_URL);
 
   try {
     // Start collecting metrics
-    await commands.measure.start('mitre-attack-module')
+    
+    // Click on Kibana menu to access Wazuh App link
     await commands.wait.bySelector('button[data-test-subj="toggleNavButton"]', WAIT_TIMEOUT)
     await commands.click.bySelector('button[data-test-subj="toggleNavButton"]')
     await commands.wait.bySelector('a[href$="/app/wazuh"]', WAIT_TIMEOUT)
     await commands.click.bySelector('a[href$="/app/wazuh"]')
-    await commands.wait.byXpath('//*[contains(@class,"euiTitle euiTitle--small euiCard__title")]//*[contains(text(),"MITRE ATT&CK")]', WAIT_TIMEOUT)
-    await commands.click.bySelector('[data-test-subj=menuWazuhButton]')
-    await commands.wait.bySelector('[data-test-subj="overviewWelcomeGeneral"]', WAIT_TIMEOUT)
-    await commands.click.bySelector('[data-test-subj="overviewWelcomeGeneral"]')
-    await commands.wait.bySelector('.wz-welcome-page-agent-tabs button.euiTab.euiTab-isSelected:first-child', WAIT_TIMEOUT)
+    
+    //Wait for an Wazuh home page component to be loaded
+    await commands.wait.byXpath('//*[contains(@class,"euiTitle euiTitle--small euiCard__title")]//*[contains(text(),"Security events")]', WAIT_TIMEOUT)
+
+    // Click on Integrity Monitoring module button
+    await commands.wait.byXpath('//*[contains(@class,"euiTitle euiTitle--small euiCard__title")]//*[contains(text(),"MITRE")]', WAIT_TIMEOUT)
+    await commands.click.byXpath('//*[contains(@class,"euiTitle euiTitle--small euiCard__title")]//*[contains(text(),"MITRE")]')
+    
+    await commands.measure.start('MITRE ATT&CK module -dashboard')
+    logger('--- Initiate measures in dashboard module ---');
+
+    logger('Mitre techniques by agent');
+    await commands.wait.bySelector('[data-render-complete="true"][data-title="Mitre techniques by agent"]', WAIT_TIMEOUT)
+
+    logger('Top tactics by agent');
+    await commands.wait.bySelector('[data-render-complete="true"][data-title="Top tactics by agent"]', WAIT_TIMEOUT)
+
+    logger('--- Finish measures ---', 'info');
+    
     // Stop and collect the metrics
     return commands.measure.stop();
   } catch (e) {
