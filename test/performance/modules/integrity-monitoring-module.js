cecb77c74ca2d354d98c267435d6909c4f103095
Facundo Mayon <mayonf95@gmail.com>
2022-07-27 13:40:57 -0300

    Adding a new test for integrity monitoring module

--- test/performance/modules/integrity-monitoring-module.js
+++ test/performance/modules/integrity-monitoring-module.js
@@ -1,19 +1,37 @@
-const { WAIT_TIMEOUT } = require('../common/constants');
+const { WAIT_TIMEOUT, SERVER_URL } = require('../common/constants');
+const logger = require('../common/logger');
 
 module.exports = async function (context, commands) {
+  // Navigate to a URL, but do not measure the URL
+  await commands.navigate(SERVER_URL);
 
   try {
     // Start collecting metrics
-    await commands.measure.start('security-events-module')
+    
+    // Click on Kibana menu to access Wazuh App link
     await commands.wait.bySelector('button[data-test-subj="toggleNavButton"]', WAIT_TIMEOUT)
     await commands.click.bySelector('button[data-test-subj="toggleNavButton"]')
     await commands.wait.bySelector('a[href$="/app/wazuh"]', WAIT_TIMEOUT)
     await commands.click.bySelector('a[href$="/app/wazuh"]')
+    
+    //Wait for an Wazuh home page component to be loaded
     await commands.wait.byXpath('//*[contains(@class,"euiTitle euiTitle--small euiCard__title")]//*[contains(text(),"Security events")]', WAIT_TIMEOUT)
-    await commands.click.bySelector('[data-test-subj=menuWazuhButton]')
-    await commands.wait.bySelector('[data-test-subj="overviewWelcomeGeneral"]', WAIT_TIMEOUT)
-    await commands.click.bySelector('[data-test-subj="overviewWelcomeGeneral"]')
-    await commands.wait.bySelector('.wz-welcome-page-agent-tabs button.euiTab.euiTab-isSelected:first-child', WAIT_TIMEOUT)
+    
+    // Click on the Wazuh menu button
+    await commands.click.bySelectorAndWait('[data-test-subj=menuWazuhButton]', WAIT_TIMEOUT)
+    
+    // Click on Integrity Monitoring module button
+    await commands.wait.bySelector('.WzManagementSideMenu.wz-menu-overview button[data-test-subj="menuModulesFimLink"]', WAIT_TIMEOUT)
+    await commands.click.bySelector('.WzManagementSideMenu.wz-menu-overview button[data-test-subj="menuModulesFimLink"]')
+    
+    await commands.measure.start('Integrity Monitoring module -dashboard')
+    logger('--- Initiate measures in dashboard module ---');
+
+    logger('--- TOP 5 agents ---');
+    await commands.wait.bySelector('[id="Wazuh-App-Overview-FIM-Top-5-agents-pie"] [data-render-complete="true"][data-test-subj="visualizationLoader"]', WAIT_TIMEOUT)
+
+    logger('--- Finish measures ---', 'info');
+    
     // Stop and collect the metrics
     return commands.measure.stop();
   } catch (e) {
