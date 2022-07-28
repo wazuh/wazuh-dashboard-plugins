136a6c496c34775e51d43c17460500a69f128f34
Facundo Mayon <mayonf95@gmail.com>
2022-07-28 17:37:53 -0300

    Add new mitre attack dashboards -Intelligence & Framework

---
+++ test/performance/modules/mitre-attack-module-intelligence.js
@@ -0,0 +1,48 @@
+const { WAIT_TIMEOUT, SERVER_URL } = require('../common/constants');
+const logger = require('../common/logger');
+
+module.exports = async function (context, commands) {
+  // Navigate to a URL, but do not measure the URL
+  await commands.navigate(SERVER_URL);
+
+  try {
+    // Click on Kibana menu to access Wazuh App link
+    await commands.wait.bySelector('button[data-test-subj="toggleNavButton"]', WAIT_TIMEOUT)
+    await commands.click.bySelector('button[data-test-subj="toggleNavButton"]')
+    await commands.wait.bySelector('a[href$="/app/wazuh"]', WAIT_TIMEOUT)
+    await commands.click.bySelector('a[href$="/app/wazuh"]')
+    
+    //Wait for an Wazuh home page component to be loaded
+    await commands.wait.byXpath('//*[contains(@class,"euiTitle euiTitle--small euiCard__title")]//*[contains(text(),"Security events")]', WAIT_TIMEOUT)
+
+    // Click on MITRE module button
+    await commands.wait.byXpath('//*[contains(@class,"euiTitle euiTitle--small euiCard__title")]//*[contains(text(),"MITRE")]', WAIT_TIMEOUT)
+    await commands.click.byXpath('//*[contains(@class,"euiTitle euiTitle--small euiCard__title")]//*[contains(text(),"MITRE")]')
+
+    // Accesing to Intelligence Dashboard
+    await commands.wait.byXpath('//*[contains(@class,"euiTab")]//*[contains(text(),"Intelligence")]', WAIT_TIMEOUT)
+    await commands.click.byXpath('//*[contains(@class,"euiTab")]//*[contains(text(),"Intelligence")]')
+
+    // Start collecting metrics
+    await commands.measure.start('MITRE ATT&CK module -Intelligence Dashboard')
+    logger('--- Initiate measures in Intelligence - Sashboard module ---');
+
+    logger('Groups Table');
+    await commands.wait.bySelector('[class="euiTable euiTable--responsive"]', WAIT_TIMEOUT)
+
+    /*logger('Top tactics by agent');
+    await commands.wait.bySelector('[data-render-complete="true"][data-title="Top tactics by agent"]', WAIT_TIMEOUT)
+
+    logger('--- Finish measures ---', 'info');*/
+    
+    // Stop and collect the metrics
+    return commands.measure.stop();
+  } catch (e) {
+    // We try/catch so we will catch if the the input fields can't be found
+    // The error is automatically logged in Browsertime an rethrown here
+    // We could have an alternative flow ...
+    // else we can just let it cascade since it caught later on and reported in
+    // the HTML
+    throw e;
+  }
+};
