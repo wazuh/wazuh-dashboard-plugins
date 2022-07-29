8ab4a3db058c9a930116fc98efcfeaf324c01e5b
mauceballos <mauricioaceballos@gmail.com>
2022-07-29 16:04:15 -0300

    add new test case for performance test on vulnerabilities module

---
+++ test/performance/modules/vulnerabilities-inventory-modules.js
@@ -0,0 +1,47 @@
+const { WAIT_TIMEOUT, SERVER_URL } = require('../common/constants');
+const logger = require('../common/logger');
+
+module.exports = async function (context, commands) {
+
+  await commands.navigate(SERVER_URL);
+
+  try {
+    await commands.wait.bySelector('button[data-test-subj="toggleNavButton"]', WAIT_TIMEOUT)
+    await commands.click.bySelector('button[data-test-subj="toggleNavButton"]')
+    await commands.wait.bySelector('a[href$="/app/wazuh"]', WAIT_TIMEOUT)
+    await commands.click.bySelector('a[href$="/app/wazuh"]')
+    await commands.wait.byXpath('//*[contains(@class,"euiTitle euiTitle--small euiCard__title")]//*[contains(text(),"Vulnerabilities")]', WAIT_TIMEOUT)
+    await commands.click.byXpath('//*[contains(@class,"euiTitle euiTitle--small euiCard__title")]//*[contains(text(),"Vulnerabilities")]')
+    await commands.wait.byXpath('//button//*[contains(text(),"Select agent")]', WAIT_TIMEOUT)
+    await commands.click.byXpath('//button//*[contains(text(),"Select agent")]')
+    await commands.wait.byXpath('//*[contains(@class,"wz-select-agent-modal")]', WAIT_TIMEOUT)
+    await commands.wait.byXpath('//*[contains(@class,"wz-select-agent-modal")]//tbody//*[contains(@class,"uiTableRow-isClickable")][1]', WAIT_TIMEOUT)
+    await commands.click.byXpath('//*[contains(@class,"wz-select-agent-modal")]//tbody//*[contains(@class,"uiTableRow-isClickable")][1]')
+    await commands.wait.bySelector('.wz-welcome-page-agent-tabs button.euiTab.euiTab-isSelected:first-child', WAIT_TIMEOUT)
+    await commands.click.byXpath('//react-component//button//*[contains(text(),"Inventory")]')
+    // Start collecting metrics
+    await commands.measure.start('vulnerabilities-inventory-module')
+    logger('--- Initiate measures in dashboard module ---');
+
+    logger('--- VULNERABILITIE INVENTORY SEVERITY ---')
+    await commands.wait.bySelector('.euiFlexGroup .euiFlexItem:nth-child(1) .euiCard__content .euiFlexItem', WAIT_TIMEOUT)
+
+    logger('--- VULNERABILITIE INVENTORY DETAILS ---')
+    await commands.wait.bySelector('.euiFlexItem .statWithLink.wz-user-select-none', WAIT_TIMEOUT)
+    
+    logger('--- VULNERABILITIE INVENTORY SUMMARY ---')
+    await commands.wait.bySelector('.euiFlexGroup .euiFlexItem:nth-child(3) .euiCard__content .euiFlexItem', WAIT_TIMEOUT)
+
+    logger('--- VULNERABILITIE TABLE ---')
+    await commands.wait.bySelector('.wz-inventory tbody tr', WAIT_TIMEOUT)
+    // Stop and collect the metrics
+    return commands.measure.stop();
+  } catch (e) {
+    // We try/catch so we will catch if the the input fields can't be found
+    // The error is automatically logged in Browsertime an rethrown here
+    // We could have an alternative flow ...
+    // else we can just let it cascade since it caught later on and reported in
+    // the HTML
+    throw e;
+  }
+};
