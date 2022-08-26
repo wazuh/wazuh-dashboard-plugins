be2ea5aa46c979e5e5cc1ff4eb8ab5249e2a4ce0
mauceballos <mauricioaceballos@gmail.com>
2022-08-26 14:56:18 -0300

    fix scenario

--- cypress/cypress/integration/step-definitions/management/decoders/the-user-creates-a-new-decoder-when.js
+++ cypress/cypress/integration/step-definitions/management/decoders/the-user-creates-a-new-decoder-when.js
@@ -3,9 +3,10 @@ import { fillField, elementIsVisible, getSelector } from '../../../utils/driver'
 import { DECODERS_PAGE as pageName} from '../../../utils/pages-constants';
 const decoderTitleSelector = getSelector('decoderTitleSelector', pageName);
 const codeEditorSelector = getSelector('codeEditorSelector', pageName);
+const testXmlText = '<decoder name="json"><prematch>^{\s*"</prematch><plugin_decoder>JSON_Decoder</plugin_decoder></decoder>';
 
 When('The user writes a new decoder', () => {
   elementIsVisible(decoderTitleSelector);
   fillField(decoderTitleSelector,'Test');
-  fillField(codeEditorSelector,'Test');
+  fillField(codeEditorSelector,testXmlText);
 })
