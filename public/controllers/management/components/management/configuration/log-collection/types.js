bb05f8cdc91352ac3c48ddb0c8d433f476c4a156
Maximiliano Ibarra <maximilianoaibarra@gmail.com>
2022-09-06 11:09:05 -0300

    Added agents windows events config tab (#3905)
    * Added custom renderLabel functionality
    
    * Added Windows Events tab in agent configuration
    
    * Updated CHANGELOG
    
    * Added log collection tests for command and windows event tabs
    
    * Updated some test description
    
    * Removed unused code and redefine some conditions
    
    * Conditionals improves code readability
    
    Co-authored-by: Álex <alejandro.ruiz.becerra@wazuh.com>
    (cherry picked from commit ca1781c52f90c62c51cbdb17ce1810fa40ef1595)

---
+++ public/controllers/management/components/management/configuration/log-collection/types.js
@@ -0,0 +1,7 @@
+
+export const LOGCOLLECTOR_LOCALFILE_PROP = 'logcollector-localfile';
+export const LOGCOLLECTOR_SOCKET_PROP = 'logcollector-socket';
+
+export const LOCALFILE_LOGS_PROP = 'localfile-logs';
+export const LOCALFILE_WINDOWSEVENT_PROP = 'localfile-windowsevent';
+export const LOCALFILE_COMMANDS_PROP = 'localfile-commands';
