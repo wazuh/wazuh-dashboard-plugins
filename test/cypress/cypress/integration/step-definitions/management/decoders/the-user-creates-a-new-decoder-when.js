9619e8dca320b5528da6600962210b4cbca02a67
Facundo Mayon <mayonf95@gmail.com>
2022-09-12 10:41:59 -0300

    Test Fixed, change decoder tittle for correct execution

--- test/cypress/cypress/integration/step-definitions/management/decoders/the-user-creates-a-new-decoder-when.js
+++ test/cypress/cypress/integration/step-definitions/management/decoders/the-user-creates-a-new-decoder-when.js
@@ -3,10 +3,14 @@ import { fillField, elementIsVisible, getSelector } from '../../../utils/driver'
 import { DECODERS_PAGE as pageName} from '../../../utils/pages-constants';
 const decoderTitleSelector = getSelector('decoderTitleSelector', pageName);
 const codeEditorSelector = getSelector('codeEditorSelector', pageName);
-const testXmlText = '<decoder name="json"><prematch>^{\s*"</prematch><plugin_decoder>JSON_Decoder</plugin_decoder></decoder>';
+const testXmlText = '<decoder name="wazuh"><prematch>^wazuh2: </prematch></decoder>';
 
 When('The user writes a new decoder', () => {
   elementIsVisible(decoderTitleSelector);
-  fillField(decoderTitleSelector,'Test');
+  fillField(decoderTitleSelector,'Example decoder');
   fillField(codeEditorSelector,testXmlText);
 })
+
+//Test comments:
+//To the correct execution of this test, the decoder that is going to be created must not exist in the decoders list.
+// If the decoder already exists, the test will fail.
