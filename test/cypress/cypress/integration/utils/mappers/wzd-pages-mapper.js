6aec7cc581449f6548e48c4d1bdd85f45dbbce33
Facundo Mayon <mayonf95@gmail.com>
2022-09-27 16:21:39 -0300

    Adding pages

--- test/cypress/cypress/integration/utils/mappers/wzd-pages-mapper.js
+++ test/cypress/cypress/integration/utils/mappers/wzd-pages-mapper.js
@@ -13,6 +13,7 @@ import { DECODERS_PAGE } from '../../pageobjects/wzd/wazuh-menu/decoders.page';
 import { RULES_PAGE } from '../../pageobjects/wzd/wazuh-menu/rules.page';
 import { WAZUH_MENU_PAGE } from '../../pageobjects/wzd/wazuh-menu/wazuh-menu.page';
 import { MODULES_DIRECTORY_PAGE } from '../../pageobjects/wzd/modules-directory.page';
+import { REPORTING_PAGE } from '../../pageobjects/wzd/reporting/report.page';
 
 import { AGENT_MODULES } from './wzd/agent-modules-mapper';
 import { BASIC_MODULES } from './wzd/basic-modules-mapper';
@@ -36,6 +37,7 @@ export const WZD_PAGES_MAPPER = {
   RULES_PAGE,
   WAZUH_MENU_PAGE,
   MODULES_DIRECTORY_PAGE,
+  REPORTING_PAGE,
 
   AGENT_MODULES,
   BASIC_MODULES,
