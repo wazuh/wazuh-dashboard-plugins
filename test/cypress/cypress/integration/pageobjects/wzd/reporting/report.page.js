3107119d6ce301cfcd5c66964bcc4a45abbc3c5c
Facundo Mayon <mayonf95@gmail.com>
2022-09-28 10:08:05 -0300

    Add new selector and changes for github actions

--- test/cypress/cypress/integration/pageobjects/wzd/reporting/report.page.js
+++ test/cypress/cypress/integration/pageobjects/wzd/reporting/report.page.js
@@ -1,3 +1,4 @@
 export const REPORTING_PAGE = {
     generateReportButton: '.wz-welcome-page-agent-tabs .euiFlexItem .euiFlexItem .euiButtonEmpty[type="button"] .euiButtonEmpty__text:contains("Generate report")',
+    reportGeneratedToast: '.euiToast',
 };
--- test/cypress/cypress/integration/step-definitions/reporting/generate-module-report-when.js
+++ test/cypress/cypress/integration/step-definitions/reporting/generate-module-report-when.js
@@ -1,10 +1,13 @@
 import { When } from 'cypress-cucumber-preprocessor/steps';
-import { clickElement, elementIsVisible, getSelector} from '../../utils/driver';
+import { forceClickElement, elementIsVisible, getSelector} from '../../utils/driver';
 
 import { REPORTING_PAGE as pageName} from '../../utils/pages-constants';
 const generateReportButton = getSelector('generateReportButton', pageName);
+const reportGeneratedToast = getSelector('reportGeneratedToast', pageName);
 
 When('The user generate a module report clicking on the generate report option', () => {
   elementIsVisible(generateReportButton);
-  clickElement(generateReportButton);
+  forceClickElement(generateReportButton);
+  cy.wait(500);
+  elementIsVisible(reportGeneratedToast);
   });
