e30a75223f940248760ac174a0ff2a61ddf45e1c
Facundo Mayon <mayonf95@gmail.com>
2022-09-30 16:46:52 -0300

    Try to capture text

--- test/cypress/cypress/integration/step-definitions/reporting/the-user-agent-report-is-displayed-in-the-table-when.js
+++ test/cypress/cypress/integration/step-definitions/reporting/the-user-agent-report-is-displayed-in-the-table-when.js
@@ -2,10 +2,15 @@ import { Then } from 'cypress-cucumber-preprocessor/steps';
 import { elementIsVisible, elementTextIncludes, getSelector} from '../../utils/driver';
 import { REPORTING_PAGE as pageName} from '../../utils/pages-constants';
 const lastCreatedReport = getSelector('lastCreatedReport', pageName);
-//const agentID = getSelector('lastCreatedReport', pageName);
 
 Then('The agent report is displayed in the table', () => {
   elementIsVisible(lastCreatedReport);
-  cy.wait(500);
-  //elementTextIncludes(lastCreatedReport, "agent-"+agentID);
+  debugger;
+  cy.wait(2000);
+  cy.get('@agentId').then(($agentId) => {
+    expect($agentId).to.be.eq($agentId);
+    const agentID = $agentId;
+    elementTextIncludes(report, "agents-"+agentID);
+  });
 });
+
