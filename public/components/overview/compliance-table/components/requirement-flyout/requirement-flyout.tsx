8024b917bbe5dfe43c19c521a756eefc74554f1c
yenienserrano <ian.serrano@wazuh.com>
2022-10-18 15:03:27 +0200

    Revert "Removed unused wzFlyout"
    This reverts commit 9888c36133c20f896201547ff6e54abf6f2db3f9.

--- public/components/overview/compliance-table/components/requirement-flyout/requirement-flyout.tsx
+++ public/components/overview/compliance-table/components/requirement-flyout/requirement-flyout.tsx
@@ -32,6 +32,7 @@ import { AppState } from '../../../../../react-services/app-state';
 import { requirementGoal } from '../../requirement-goal';
 import { getUiSettings } from '../../../../../kibana-services';
 import { FilterManager } from '../../../../../../../../src/plugins/data/public/';
+import { WzFlyout } from '../../../../../components/common/flyouts';
 
 export class RequirementFlyout extends Component {
   _isMount = false;
@@ -238,9 +239,12 @@ export class RequirementFlyout extends Component {
       size: 'l',
       className: 'flyout-no-overlap wz-inventory wzApp',
       'aria-labelledby': 'flyoutSmallTitle',
-    };
+    }
     return (
-      <EuiFlyout onClose={() => onChangeFlyout(false)} {...flyoutProps}>
+      <EuiFlyout
+        onClose={() => onChangeFlyout(false)}
+        {...flyoutProps }
+      >
         {currentRequirement && this.renderHeader()}
         {this.renderBody()}
         {this.state.loading && this.renderLoading()}
