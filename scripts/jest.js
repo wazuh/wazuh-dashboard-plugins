1051244eda348b534c612ab4c78f43f21a0b515b
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2022-10-19 11:07:26 +0200

    fix(tests): fix the script and configuration to run the unit tests

--- scripts/jest.js
+++ scripts/jest.js
@@ -13,7 +13,6 @@
 const path = require('path');
 process.argv.push('--config', path.resolve(__dirname, '../test/jest/config.js'));
 
-require('../../../src/setup_node_env');
 const jest = require('../../../node_modules/jest');
 
 jest.run(process.argv.slice(2));
