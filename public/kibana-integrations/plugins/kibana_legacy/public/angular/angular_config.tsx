818f71a8752ea1b92df2114f31234da6490d79bb
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2022-11-04 16:02:56 +0100

    fix(compatibility): fix a problem importing a renamed dependency
    Renamed @elastic/safer-lodash-set to @kbn/safer-lodash-set

--- public/kibana-integrations/plugins/kibana_legacy/public/angular/angular_config.tsx
+++ public/kibana-integrations/plugins/kibana_legacy/public/angular/angular_config.tsx
@@ -26,7 +26,7 @@ import {
   IRootScopeService,
 } from 'angular';
 import $ from 'jquery';
-import { set } from '@elastic/safer-lodash-set';
+import { set } from '@kbn/safer-lodash-set';
 import { get } from 'lodash';
 import * as Rx from 'rxjs';
 import { ChromeBreadcrumb, EnvironmentMode, PackageInfo } from 'kibana/public';
