2483d669513f42f258dde242e6d6e9a55c92edfa
Álex <alejandro.ruiz.becerra@wazuh.com>
2022-11-18 17:06:12 +0100

    Bump development Docker environments to 4.5.0 (#4896)

--- docker/imposter/cluster/cluster_restart.js
+++ docker/imposter/cluster/cluster_restart.js
@@ -5,25 +5,25 @@ var callRestart = storeWazuh.load('callRestart');
 storeWazuh.save('callRestart', true);
 
 if (context.request.queryParams.nodes_list !== undefined) {
-    var resp = {
-        "data": {
-            "affected_items":
-                [
-                    context.request.queryParams.nodes_list
-                ],
-            "total_affected_items": 1,
-            "total_failed_items": 0,
-            "failed_items": []
-        },
-        "message": "All selected nodes information was returned",
-        "error": 0
-    }
+  var resp = {
+    "data": {
+      "affected_items":
+        [
+          context.request.queryParams.nodes_list
+        ],
+      "total_affected_items": 1,
+      "total_failed_items": 0,
+      "failed_items": []
+    },
+    "message": "All selected nodes information was returned",
+    "error": 0
+  }
 
-    respond()
-        .withStatusCode(200)
-        .withData(JSON.stringify(resp))
+  respond()
+    .withStatusCode(200)
+    .withData(JSON.stringify(resp))
 } else {
-    respond()
-        .withStatusCode(200)
-        .withFile('cluster/cluster_restart.json')
+  respond()
+    .withStatusCode(200)
+    .withFile('cluster/cluster_restart.json')
 }
