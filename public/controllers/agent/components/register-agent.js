907ed7cd6272a25cfc4c745bb591b50abb726ccf
Nico Guevara <42900763+Tostti@users.noreply.github.com>
2022-12-22 11:17:04 -0300

    [Backport 4.4-7.16] Add length validation to agent name (#5029)
    * Add validation to agent name
    
    * Update changelog
    
    * Add regex to constant
    
    * Reduce number of setState
    
    * Add length validation
    
    * Update changelog

--- public/controllers/agent/components/register-agent.js
+++ public/controllers/agent/components/register-agent.js
@@ -302,18 +302,26 @@ export const RegisterAgent = withErrorBoundary(
 
     setAgentName(event) {
       const validation = /^[a-z0-9-_.]+$/i;
-      this.setState({ agentName: event.target.value });
-      if (validation.test(event.target.value) || event.target.value.length <= 0) {
-        this.setState({ agentNameError: false });
-        this.setState({ badCharacters: [] });
+      if ((validation.test(event.target.value) && event.target.value.length >= 2)
+        || event.target.value.length <= 0) {
+        this.setState({
+          agentName: event.target.value,
+          agentNameError: false,
+          badCharacters: []
+        });
+
       } else {
         let badCharacters = event.target.value.split('').map(char =>
           char.replace(validation, '')).join('');
         badCharacters = badCharacters.split('').map(char =>
           char.replace(/\s/, 'whitespace'));
         const characters = [...new Set(badCharacters)];
-        this.setState({ badCharacters: characters });
-        this.setState({ agentNameError: true });
+        this.setState({
+          agentName: event.target.value,
+          badCharacters: characters,
+          agentNameError: true
+        });
+
       }
     }
 
@@ -880,7 +888,9 @@ export const RegisterAgent = withErrorBoundary(
         <EuiForm>
           <EuiFormRow
             isInvalid={this.state.agentNameError}
-            error={[`The character${this.state.badCharacters.length <= 1 ? ('') : ('s')}
+            error={[this.state.badCharacters.length < 1 ? 'The minimum length is 2 characters.' :
+              `The character${this.state.badCharacters.length <= 1 ? ('') : ('s')}
+
             ${this.state.badCharacters.map(char => ` "${char}"`)}
             ${this.state.badCharacters.length <= 1 ? ('is') : ('are')}
             not valid. Allowed characters are A-Z, a-z, ".", "-", "_"`]}>
