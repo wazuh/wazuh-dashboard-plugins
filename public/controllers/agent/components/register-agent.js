990b854140e1739477703bf5b849dd407674996d
chantal.kelm <chantal.kelm@wazuh.com>
2022-12-29 09:58:06 -0300

    changes

--- public/controllers/agent/components/register-agent.js
+++ public/controllers/agent/components/register-agent.js
@@ -624,7 +624,7 @@ export const RegisterAgent = withErrorBoundary(
         case 'busterorgreater-x86_64':
           return `https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_${this.state.wazuhVersion}${this.addToVersion}_amd64.deb`;
         case 'busterorgreater-powerpc':
-          return `https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_${this.state.wazuhVersion}_ppc64el.deb`;
+          return `https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_${this.state.wazuhVersion}${this.addToVersion}_ppc64el.deb`;
         default:
           return `https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_${this.state.wazuhVersion}${this.addToVersion}_amd64.deb`;
       }
