0232959501fe5321f8ad77161075fb336eacd9f8
Nicolas Guevara <nicolas.guevara@wazuh.com>
2023-01-25 09:22:33 -0300

    improve code

--- public/components/agents/syscollector/columns/packages-columns.ts
+++ public/components/agents/syscollector/columns/packages-columns.ts
@@ -27,18 +27,10 @@ const FreebsdColumns = [
   { id: 'description', width: '30%' },
 ];
 
-const SolarisColumns = [
-  { id: 'name' },
-  { id: 'version' },
-  { id: 'format' },
-  { id: 'architecture', width: '20%' },
-  { id: 'vendor', width: '20%' },
-  { id: 'description', width: '30%' },
-];
 export const packagesColumns = {
   windows: windowsColumns,
   linux: linuxColumns,
   apple: MacColumns,
   freebsd: FreebsdColumns,
-  solaris: SolarisColumns,
+  solaris: linuxColumns,
 };
