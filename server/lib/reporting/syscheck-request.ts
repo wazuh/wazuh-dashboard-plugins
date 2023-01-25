3447a918a744fa1d2cf4353d45bfda0ff3b89ed8
Federico Rodriguez <federico.rodriguez@wazuh.com>
2023-01-25 09:49:27 +0100

    Fix Alerts Summary table truncated results (#5131)
    * Fix query result parsing algorithm
    
    * Added changelog
    
    * Add allowed_agents filter to the report queries
    
    * Fix agentsFilter conditional
    
    Co-authored-by: Antonio <34042064+Desvelao@users.noreply.github.com>
    (cherry picked from commit c54058037361a724126d6bf8b7a6f6254e0a6670)

--- server/lib/reporting/syscheck-request.ts
+++ server/lib/reporting/syscheck-request.ts
@@ -26,12 +26,13 @@ export const top3agents = async (
   gte, 
   lte, 
   filters,
+  allowedAgentsFilter,
   pattern = getSettingDefaultValue('pattern')
 ) => {
   try {
     const base = {};
 
-    Object.assign(base, Base(pattern, filters, gte, lte));
+    Object.assign(base, Base(pattern, filters, gte, lte, allowedAgentsFilter));
 
     Object.assign(base.aggs, {
       '2': {
@@ -78,12 +79,13 @@ export const top3Rules = async (
   gte,
   lte,
   filters,
+  allowedAgentsFilter,
   pattern = getSettingDefaultValue('pattern')
 ) => {
   try {
     const base = {};
 
-    Object.assign(base, Base(pattern, filters, gte, lte));
+    Object.assign(base, Base(pattern, filters, gte, lte, allowedAgentsFilter));
 
     Object.assign(base.aggs, {
       '2': {
@@ -137,12 +139,13 @@ export const lastTenDeletedFiles = async (
   gte,
   lte,
   filters,
+  allowedAgentsFilter,
   pattern = getSettingDefaultValue('pattern')
 ) => {
   try {
     const base = {};
 
-    Object.assign(base, Base(pattern, filters, gte, lte));
+    Object.assign(base, Base(pattern, filters, gte, lte, allowedAgentsFilter));
 
     Object.assign(base.aggs, {
       '2': {
@@ -190,12 +193,13 @@ export const lastTenModifiedFiles = async (
   gte,
   lte,
   filters,
+  allowedAgentsFilter,
   pattern = getSettingDefaultValue('pattern')
 ) => {
   try {
     const base = {};
 
-    Object.assign(base, Base(pattern, filters, gte, lte));
+    Object.assign(base, Base(pattern, filters, gte, lte, allowedAgentsFilter));
 
     Object.assign(base.aggs, {
       '2': {
