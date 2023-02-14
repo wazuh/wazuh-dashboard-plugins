5f54f4453824658e69fe8f0554b8c365da676402
Maxi <maximilianoaibarra@gmail.com>
2023-02-14 10:48:30 -0300

    Added error factory unit test for every error classes

--- public/react-services/error-management/error-factory/error-factory.test.ts
+++ public/react-services/error-management/error-factory/error-factory.test.ts
@@ -9,71 +9,94 @@
  *
  * Find more information about this on the LICENSE file.
  */
-import { AxiosResponse } from 'axios';
+import { AxiosError, AxiosResponse } from 'axios';
 import WazuhApiError from './errors/WazuhApiError';
 import { ErrorFactory } from './error-factory';
+import { ElasticApiError, ElasticError, WazuhReportingError } from './errors';
+import WazuhError from './errors/WazuhError';
+
+const response: AxiosResponse = {
+  data: {
+    statusCode: 500,
+    error: 'Internal Server Error',
+    message: '3099 - ERROR3099 - Wazuh not ready yet',
+  },
+  status: 500,
+  statusText: 'Internal Server Error',
+  headers: {},
+  config: {},
+  request: {},
+};
 
 describe('Error Factory', () => {
-  it('Should return ERROR instance when receive an error', () => {
-    const errorMessage = 'Error message';
-    const error = new Error('Error message');
-    const errorCreated = ErrorFactory.create(Error, { error, message: errorMessage });
-    expect(errorCreated).toBeInstanceOf(Error);
-    expect(errorCreated.name).toBe('Error');
-    expect(errorCreated.message).toEqual(errorMessage);
-    expect(errorCreated.stack).toBeTruthy();
-    expect(typeof errorCreated).not.toBe('string');
-  });
+  it.each([
+    { errorType: WazuhApiError, name: 'WazuhApiError' },
+    { errorType: WazuhReportingError, name: 'WazuhReportingError' },
+    { errorType: ElasticApiError, name: 'ElasticApiError' },
+    { errorType: ElasticError, name: 'ElasticError' },
+  ])(
+    'Should return a $name when receive and error and error type',
+    ({ errorType, name }) => {
+      let error = new Error('Error') as AxiosError;
+      error = {
+        ...error,
+        ...response,
+        stack: error.stack,
+      };
+      const errorCreated = ErrorFactory.create(errorType, {
+        error,
+        message: response.data.message,
+      });
+      expect(errorCreated.name).toBe(name);
+      expect(errorCreated.stack).toBe(error.stack);
+      expect(typeof errorCreated).not.toBe('string');
+    },
+  );
 
-  /*it('Should return ERROR instance when receive a string', () => {
-    const errorMessage = 'String message';
-    const errorCreated = ErrorFactory.createError(errorMessage, Error);
-    expect(errorCreated).toBeInstanceOf(Error);
-    expect(errorCreated.name).toBe('Error');
-    expect(errorCreated.message).toEqual(errorMessage);
-    expect(errorCreated.stack).toBeTruthy();
-    expect(typeof errorCreated).not.toBe('string');
-  });
-  */
-
-  it('Should return ERROR when receive and error with response property', () => {
-    const response: AxiosResponse = {
-      data: {
-        statusCode: 500,
-        error: 'Internal Server Error',
-        message: '3099 - ERROR3099 - Wazuh not ready yet',
-      },
-      status: 500,
-      statusText: 'Internal Server Error',
-      headers: {},
-      config: {},
-      request: {},
-    };
-
+  it('Should return a new ERROR when receive and error and error type and keep the received error Stack Trace', () => {
     // creating an error with response property
-    const error = new Error('Error');
-    error['response'] = response;
-    const errorCreated = ErrorFactory.create(WazuhApiError, 
-      { error, message: response.data.message }
-      )
-    expect(errorCreated).toBeInstanceOf(Error);
+    let error = new Error('Error') as AxiosError;
+    error = {
+      ...error,
+      ...response,
+      stack: error.stack,
+    };
+    const errorCreated = ErrorFactory.create(WazuhApiError, {
+      error,
+      message: response.data.message,
+    });
     expect(errorCreated.name).toBe('WazuhApiError');
-    expect(errorCreated.message).toEqual(response.data.message);
-    expect(errorCreated.stack).toBeTruthy();
+    expect(errorCreated.stack).toBe(error.stack);
     expect(typeof errorCreated).not.toBe('string');
   });
 
-  /* Todo: test that not create new error when receive an error for the same type
-  it('Should return same ERROR instance when receive ErrorResponse, dont create a new error', () => {
-    const errorResponseInstance = new Error('Error message');
-    const errorReceived = ErrorFactory.createError(errorResponseInstance);
-    const error = ErrorFactory.createError(errorReceived);
-    expect(JSON.stringify(error)).toEqual(JSON.stringify(errorReceived));
-    expect(error.message).toEqual(errorReceived.message);
-    expect(error.name).toEqual(errorReceived.name);
-    expect(error.stack).toEqual(errorReceived.stack);
+  it('Should return a new ERROR instance of WazuhError(the parent class)', () => {
+    // creating an error with response property
+    let error = new Error('Error') as AxiosError;
+    error = {
+      ...error,
+      ...response,
+      stack: error.stack,
+    };
+    const errorCreated = ErrorFactory.create(WazuhApiError, {
+      error,
+      message: response.data.message,
+    });
+    expect(errorCreated).toBeInstanceOf(WazuhError);
   });
-  */
 
-
+  it('Should return a new ERROR with the error type received like class name', () => {
+    // creating an error with response property
+    let error = new Error('Error') as AxiosError;
+    error = {
+      ...error,
+      ...response,
+      stack: error.stack,
+    };
+    const errorCreated = ErrorFactory.create(WazuhApiError, {
+      error,
+      message: response.data.message,
+    });
+    expect(errorCreated.name).toBe('WazuhApiError');
+  });
 });
--- public/react-services/error-management/error-factory/errors/WazuhReportingError.ts
+++ public/react-services/error-management/error-factory/errors/WazuhReportingError.ts
@@ -3,9 +3,6 @@ import WazuhError from "./WazuhError";
 export class WazuhReportingError extends WazuhError {
   constructor(error: Error, message: string, code: number) {
     super(error,message, code);
-    // Because we are extending built in class
-    Object.setPrototypeOf(this, WazuhReportingError.prototype);
-    this.name = this.constructor.name;
   }
 
   handleError(){
