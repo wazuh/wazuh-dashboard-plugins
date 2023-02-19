acd9a4ed074c8be23f93d5cfc317a73f02524915
Maxi <maximilianoaibarra@gmail.com>
2023-02-19 12:26:01 -0300

    Added WazuhError abstract class

--- public/react-services/error-management/error-factory/errors/WazuhError.ts
+++ public/react-services/error-management/error-factory/errors/WazuhError.ts
@@ -1,29 +1,17 @@
-import { IWazuhError, IWazuhErrorLogOpts } from "../../types";
+import { IWazuhError, IWazuhErrorInfo, IWazuhErrorLogOpts } from "../../types";
 
-export default class WazuhError extends Error implements IWazuhError  {
+
+export default abstract class WazuhError extends Error {
     error: Error;
-    message: string;
     code?: number;
-    logOptions: IWazuhErrorLogOpts;
-    constructor(error: Error, message: string, logOptions: IWazuhErrorLogOpts, code?: number) {
-        super(message);
+    abstract logOptions: IWazuhErrorLogOpts;
+    constructor(error: Error, info?: IWazuhErrorInfo) {
+        super(info?.message || error.message);
         this.error = error;
-        this.message = message;
-        this.logOptions = logOptions;
-        if(code)
-        this.code = code;
+        this.code = info?.code;
         const childrenName = this.constructor.name; // keep the children class name
         Object.setPrototypeOf(this, WazuhError.prototype); // Because we are extending built in class
         this.name = childrenName;
         this.stack = this.error.stack; // keep the stack trace from children
     }
-
-    /**
-     * This options define how the error will be logged and treated
-     
-    public get logOptions(): IWazuhErrorLogOpts {
-        throw new Error('Log options must be defined!');
-    }
-    */
-
 }
