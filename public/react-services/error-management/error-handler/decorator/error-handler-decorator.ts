0390f3061a0418d580e0f90b2a323e2b2585eb88
Maxi <maximilianoaibarra@gmail.com>
2023-02-22 09:53:53 -0300

    Cleaned decorator

--- public/react-services/error-management/error-handler/decorator/error-handler-decorator.ts
+++ public/react-services/error-management/error-handler/decorator/error-handler-decorator.ts
@@ -1,31 +1,13 @@
 import { ErrorHandler } from '../error-handler';
 
 export const errorHandlerDecorator = (fn: any) => {
-  //if (typeof fn !== 'function') {
-    return function (...args: any) {
-      try {
-        return fn(...args);
-      } catch (error) {
-        if (error instanceof Error) {
-          ErrorHandler.handleError(error);
-        }
+  return function (...args: any) {
+    try {
+      return fn(...args);
+    } catch (error) {
+      if (error instanceof Error) {
+        ErrorHandler.handleError(error);
       }
-    };
-  //}
-  //return fn;
-};
-
-export function errorHandlerWrapper() {
-  return function(target: any, propertyKey: string) {
-    const value = target[propertyKey];
-    const descriptor = {
-      value: function(...args: any[]) {
-        value.apply(this, args);
-        console.log(`${propertyKey} was executed.`);
-      }
-    };
-    Reflect.deleteProperty(target, propertyKey);
-    Reflect.defineProperty(target, propertyKey, descriptor);
+    }
   };
-
-}
+};
