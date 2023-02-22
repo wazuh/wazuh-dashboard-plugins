83ebbb133effaad2a394a20916686d961ebdb6ec
Maxi <maximilianoaibarra@gmail.com>
2023-02-22 10:01:12 -0300

    Cleaned code decorator unit tests

--- public/react-services/error-management/error-handler/decorator/error-handler-decorator.test.tsx
+++ public/react-services/error-management/error-handler/decorator/error-handler-decorator.test.tsx
@@ -1,6 +1,4 @@
-import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { useEffect } from 'react';
 import {
   errorHandlerDecorator
 } from './error-handler-decorator';
