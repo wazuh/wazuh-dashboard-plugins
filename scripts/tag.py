bb319f8ab8961b97051fe10769f90a9afdc75708
Alex Ruiz Becerra <alejandro.ruiz.becerra@wazuh.com>
2023-03-13 11:53:11 +0100

    Prepare 4.4.0-RC2

--- scripts/tag.py
+++ scripts/tag.py
@@ -14,9 +14,9 @@ import subprocess
 # Wazuh version: major.minor.patch
 version = '4.4.0'
 # App's revision number (previous rev + 1)
-revision = '02'
+revision = '04'
 # One of 'pre-alpha', 'alpha', 'beta', 'release-candidate', 'stable'
-stage = 'rc1'
+stage = 'rc2'
 
 # Global variable. Will be set later
 branch = None
