7c863862b7d9b51bc3314f3d3f31b9fba2c048f9
yenienserrano <ian.serrano@wazuh.com>
2023-03-13 15:08:23 +0100

    Change text, elasticsearch to opensearch

--- public/kibana-integrations/discover/application/angular/directives/no_results.js
+++ public/kibana-integrations/discover/application/angular/directives/no_results.js
@@ -157,7 +157,7 @@ export class DiscoverNoResults extends Component {
             <p>
               <FormattedMessage
                 id="discover.noResults.searchExamples.howTosearchForWebServerLogsDescription"
-                defaultMessage="The search bar at the top uses Elasticsearch&rsquo;s support for Lucene {queryStringSyntaxLink}.
+                defaultMessage="The search bar at the top uses OpenSearch&rsquo;s support for Lucene {queryStringSyntaxLink}.
                 Here are some examples of how you can search for web server logs that have been parsed into a few fields."
                 values={{
                   queryStringSyntaxLink: (
