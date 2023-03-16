408848aa08faf857c0113aacf4ff5401322eb649
yenienserrano <ian.serrano@wazuh.com>
2023-03-16 16:58:03 +0100

    Add pending agent, change agents names, add agents

--- docker/imposter/agents/agents.js
+++ docker/imposter/agents/agents.js
@@ -10,13 +10,20 @@ switch (agentId) {
     respond().withStatusCode(200).withFile('agents/agent_active_groups.json');
     break;
   case '003':
+  case '010':
     respond().withStatusCode(200).withFile('agents/agent_disconnected.json');
     break;
   case '004':
+  case '007':
+  case '008':
+  case '009':
     respond().withStatusCode(200).withFile('agents/agent_never_connected.json');
     break;
   case '005':
     respond().withStatusCode(200).withFile('agents/agent_macos.json');
+    break;
+  case '006':
+    respond().withStatusCode(200).withFile('agents/agent_pending.json');
     break;
   default:
     respond().withStatusCode(200).withFile('agents/agent.json');
