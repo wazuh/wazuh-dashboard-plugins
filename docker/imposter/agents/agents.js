bbde38f090fd66b59d6ceb11d7079dd3a1340d17
yenienserrano <ian.serrano@wazuh.com>
2023-03-16 12:10:31 +0100

    Add status, groups and systems

--- docker/imposter/agents/agents.js
+++ docker/imposter/agents/agents.js
@@ -1,12 +1,22 @@
 var agentId = context.request.queryParams.agents_list;
 
+console.log(agentId);
 
 switch (agentId) {
   case undefined:
     respond().withStatusCode(200).withFile('agents/agents.json');
     break;
+  case '001':
+    respond().withStatusCode(200).withFile('agents/agent_active_groups.json');
+    break;
+  case '003':
+    respond().withStatusCode(200).withFile('agents/agent_disconnected.json');
+    break;
   case '004':
     respond().withStatusCode(200).withFile('agents/agent_never_connected.json');
+    break;
+  case '005':
+    respond().withStatusCode(200).withFile('agents/agent_macos.json');
     break;
   default:
     respond().withStatusCode(200).withFile('agents/agent.json');
