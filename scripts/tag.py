4c816fe7455af0b588a255c9b9c44de2aebf845b
Alex Ruiz Becerra <alejandro.ruiz.becerra@wazuh.com>
2023-03-28 13:20:06 +0200

    Wazuh 4.4.0-rc3

--- scripts/tag.py
+++ scripts/tag.py
@@ -14,9 +14,9 @@ import subprocess
 # Wazuh version: major.minor.patch
 version = '4.4.0'
 # App's revision number (previous rev + 1)
-revision = '04'
+revision = '05'
 # One of 'pre-alpha', 'alpha', 'beta', 'release-candidate', 'stable'
-stage = 'rc2'
+stage = 'rc3'
 
 # Global variable. Will be set later
 branch = None
