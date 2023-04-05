582a680635ead26df6f8e558747718310234ed5a
yenienserrano <ian.serrano@wazuh.com>
2023-04-05 10:48:55 +0200

    fix elastic pre 4.4.x

--- docker/wazuh-4.3-es/pre.sh
+++ docker/wazuh-4.3-es/pre.sh
@@ -19,8 +19,19 @@ elastic_versions=(
 )
 
 wazuh_api_version=(
-	"5"
-	"17"
+	"4.3.0"
+	"4.3.1"
+	"4.3.2"
+	"4.3.3"
+	"4.3.4"
+	"4.3.5"
+	"4.3.6"
+	"4.3.7"
+	"4.3.8"
+	"4.3.9"
+	"4.3.10"
+	"4.4.0"
+	"4.4.1"
 )
 
 usage() {
@@ -29,7 +40,7 @@ usage() {
 	echo
 	echo "where"
 	echo "  elastic_version is one of " ${elastic_versions[*]}
-	echo "  wazuh_api_version is the minor version of wazuh 4.3, for example " ${wazuh_api_version[*]}
+	echo "  wazuh_api_version is the version of wazuh, for example " ${wazuh_api_version[*]}
 	echo "  action is one of up | down | stop"
 	echo
 	echo "In a minor release, the API should not change the version here bumps the API"
@@ -53,18 +64,18 @@ if [[ ! " ${elastic_versions[*]} " =~ " ${1} " ]]
  	exit -1
 fi
 
-[ -n "$2" ] && [ "$2" -eq "$2" ] 2>/dev/null
+# [ -n "$2" ] && [ "$2" -eq "$2" ] 2>/dev/null
 if [ $? -ne 0 ]; then
-   echo "$2 is not number"
+   echo "Version ${2} not found in ${wazuh_api_version[*]}"
    exit -1
 fi
 
-patch_version=$2
+wazuh_version=$2
 cat << EOF > config/imposter/api_info.json
 {
   "data": {
     "title": "Wazuh API REST",
-    "api_version": "4.3.${patch_version}",
+    "api_version": "${wazuh_version}",
     "revision": 40316,
     "license_name": "GPL 2.0",
     "license_url": "https://github.com/wazuh/wazuh/blob/4.3/LICENSE",
@@ -94,10 +105,10 @@ case "$3" in
 		echo "Install the pre-release package manually with:"
 		echo
     echo "1. Copy the pre-release package to the running Kibana container:"
-    echo "docker cp wazuh_kibana-4.3.${patch_version}_${ES_VERSION}-1.zip ${COMPOSE_PROJECT_NAME}-kibana-1:/tmp"
+    echo "docker cp wazuh_kibana-${wazuh_version}_${ES_VERSION}-1.zip ${COMPOSE_PROJECT_NAME}-kibana-1:/tmp"
     echo
     echo "2. Install the pre-release package:"
-    echo "docker exec -ti ${COMPOSE_PROJECT_NAME}-kibana-1 /usr/share/kibana/bin/kibana-plugin install file:///tmp/wazuh_kibana-4.3.${patch_version}_${ES_VERSION}-1.zip"
+    echo "docker exec -ti ${COMPOSE_PROJECT_NAME}-kibana-1 /usr/share/kibana/bin/kibana-plugin install file:///tmp/wazuh_kibana-${wazuh_version}_${ES_VERSION}-1.zip"
     echo
     echo "3. Restart Kibana:"
     echo "docker restart ${COMPOSE_PROJECT_NAME}-kibana-1"
