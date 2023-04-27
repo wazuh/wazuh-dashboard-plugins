90b76d80a6053100c1af74d486df751e9accd059
yenienserrano <ian.serrano@wazuh.com>
2023-04-27 10:33:21 +0200

    Update default version in the tags script

--- scripts/tag.py
+++ scripts/tag.py
@@ -15,7 +15,7 @@ import subprocess
 # ======================================================= #
 
 # Wazuh version: major.minor.patch
-version = '4.4.1'
+version = '4.4.2'
 # App's revision number (previous rev + 1)
 revision = '00'
 # One of 'pre-alpha', 'alpha', 'beta', 'release-candidate', 'stable'
