005619de7a1b5ca320fef9123685027fa770ac43
Maximiliano Ibarra <maximiliano.ibarra@wazuh.com>
2023-05-15 15:41:41 -0300

    Add useForm hook types

--- public/components/common/form/hooks.tsx
+++ public/components/common/form/hooks.tsx
@@ -1,91 +1,153 @@
 import { useState, useRef } from 'react';
 import { isEqual } from 'lodash';
 import { EpluginSettingType } from '../../../../common/constants';
+import { IFormFields, IInputForm, IInputFormType, IInputTypes, IInputTypesCustom } from './types';
 
-function getValueFromEvent(event, type){
-  return (getValueFromEventType[type] || getValueFromEventType.default)(event);
-};
+interface IInputFormEvent {
+  [key: string]: (event: any) => any;
+  default: (event: any) => any;
+}
 
-const getValueFromEventType = {
-  [EpluginSettingType.switch] : (event: any) => event.target.checked,
+interface IUseFormFieldsData {
+  value: any;
+  changed: boolean;
+  error: string;
+  setInputRef: (reference: any) => void;
+  inputRef: any;
+  onChange: (event: any) => void;
+  initialValue: any;
+}
+
+interface IUseFormFieldsDataDefault extends IUseFormFieldsData {
+  type: IInputTypes;
+}
+
+interface IUseFormFieldsDataCustom extends IUseFormFieldsData {
+  type: IInputTypesCustom;
+  component: (props: IInputForm) => JSX.Element;
+}
+
+
+
+interface IUseFormFields {
+  [key: string]: IUseFormFieldsDataDefault | IUseFormFieldsDataCustom;
+}
+interface IUseFormResponse {
+  fields: IUseFormFields;
+  changed: { [key: string]: any };
+  errors: { [key: string]: string };
+  undoChanges: () => void;
+  doChanges: () => void;
+}
+
+const getValueFromEventType: IInputFormEvent = {
+  [EpluginSettingType.switch]: (event: any) => event.target.checked,
   [EpluginSettingType.editor]: (value: any) => value,
   [EpluginSettingType.filepicker]: (value: any) => value,
   default: (event: any) => event.target.value,
 };
 
-export const useForm = (fields) => {
-  const [formFields, setFormFields] = useState(Object.entries(fields).reduce((accum, [fieldKey, fieldConfiguration]) => ({
-    ...accum,
-    [fieldKey]: {
-      currentValue: fieldConfiguration.initialValue,
-      initialValue: fieldConfiguration.initialValue,
-    }
-  }), {}));
+function getValueFromEvent(event: any, type: IInputTypes | IInputTypesCustom) {
+  return (getValueFromEventType[type] || getValueFromEventType.default)(event);
+}
+
+
+export const useForm = (fields: IFormFields): IUseFormResponse => {
+  const [formFields, setFormFields] = useState(
+    Object.entries(fields).reduce(
+      (accum, [fieldKey, fieldConfiguration]) => ({
+        ...accum,
+        [fieldKey]: {
+          currentValue: fieldConfiguration.initialValue,
+          initialValue: fieldConfiguration.initialValue,
+        },
+      }),
+      {},
+    ),
+  );
 
   const fieldRefs = useRef({});
 
-  const enhanceFields = Object.entries(formFields).reduce((accum, [fieldKey, {currentValue: value, ...restFieldState}]) => ({
-    ...accum,
-    [fieldKey]: {
-      ...fields[fieldKey],
-      ...restFieldState,
-      type: fields[fieldKey].type,
-      value,
-      changed: !isEqual(restFieldState.initialValue, value),
-      error: fields[fieldKey]?.validate?.(value),
-      setInputRef: (reference) => {fieldRefs.current[fieldKey] = reference},
-      inputRef: fieldRefs.current[fieldKey],
-      onChange: (event) => {
-        const inputValue = getValueFromEvent(event, fields[fieldKey].type);
-        const currentValue = fields[fieldKey]?.transformChangedInputValue?.(inputValue) ?? inputValue;
-        setFormFields(state => ({
-          ...state,
-          [fieldKey]: {
-            ...state[fieldKey],
-            currentValue,
-          }
-        }))
+  const enhanceFields = Object.entries(formFields).reduce(
+    (accum, [fieldKey, { currentValue: value, ...restFieldState }]) => ({
+      ...accum,
+      [fieldKey]: {
+        ...fields[fieldKey],
+        ...restFieldState,
+        type: fields[fieldKey].type,
+        value,
+        changed: !isEqual(restFieldState.initialValue, value),
+        error: fields[fieldKey]?.validate?.(value),
+        setInputRef: reference => {
+          fieldRefs.current[fieldKey] = reference;
+        },
+        inputRef: fieldRefs.current[fieldKey],
+        onChange: event => {
+          const inputValue = getValueFromEvent(event, fields[fieldKey].type);
+          const currentValue =
+            fields[fieldKey]?.transformChangedInputValue?.(inputValue) ??
+            inputValue;
+          setFormFields(state => ({
+            ...state,
+            [fieldKey]: {
+              ...state[fieldKey],
+              currentValue,
+            },
+          }));
+        },
       },
-    }
-  }), {});
+    }),
+    {},
+  ) as IUseFormFields;
 
   const changed = Object.fromEntries(
-    Object.entries(enhanceFields).filter(([, {changed}]) => changed).map(([fieldKey, {value}]) => ([fieldKey, fields[fieldKey]?.transformChangedOutputValue?.(value) ?? value]))
+    Object.entries(enhanceFields)
+      .filter(([, { changed }]) => changed)
+      .map(([fieldKey, { value }]) => [
+        fieldKey,
+        fields[fieldKey]?.transformChangedOutputValue?.(value) ?? value,
+      ]),
   );
 
   const errors = Object.fromEntries(
-    Object.entries(enhanceFields).filter(([, {error}]) => error).map(([fieldKey, {error}]) => ([fieldKey, error]))
+    Object.entries(enhanceFields)
+      .filter(([, { error }]) => error)
+      .map(([fieldKey, { error }]) => [fieldKey, error]),
   );
 
-  function undoChanges(){
-    setFormFields(state => Object.fromEntries(
-      Object.entries(state).map(([fieldKey, fieldConfiguration]) => ([
-        fieldKey,
-        {
-          ...fieldConfiguration,
-          currentValue: fieldConfiguration.initialValue
-        }
-      ]))
-    ));
-  };
+  function undoChanges() {
+    setFormFields(state =>
+      Object.fromEntries(
+        Object.entries(state).map(([fieldKey, fieldConfiguration]) => [
+          fieldKey,
+          {
+            ...fieldConfiguration,
+            currentValue: fieldConfiguration.initialValue,
+          },
+        ]),
+      ),
+    );
+  }
 
-  function doChanges(){
-    setFormFields(state => Object.fromEntries(
-      Object.entries(state).map(([fieldKey, fieldConfiguration]) => ([
-        fieldKey,
-        {
-          ...fieldConfiguration,
-          initialValue: fieldConfiguration.currentValue
-        }
-      ]))
-    ));
-  };
+  function doChanges() {
+    setFormFields(state =>
+      Object.fromEntries(
+        Object.entries(state).map(([fieldKey, fieldConfiguration]) => [
+          fieldKey,
+          {
+            ...fieldConfiguration,
+            initialValue: fieldConfiguration.currentValue,
+          },
+        ]),
+      ),
+    );
+  }
 
   return {
     fields: enhanceFields,
     changed,
     errors,
     undoChanges,
-    doChanges
+    doChanges,
   };
 };
--- public/components/common/form/types.ts
+++ public/components/common/form/types.ts
@@ -1,19 +1,40 @@
-import { TPluginSettingWithKey } from "../../../../common/constants";
+import { TPluginSettingWithKey } from '../../../../common/constants';
+
+export type IInputTypes = 'text' | 'number' | 'switch' | 'editor' | 'filepicker';
+export type IInputTypesCustom = 'custom';
 
 export interface IInputFormType {
-	field: TPluginSettingWithKey
-	value: any
-	onChange: (event: any) => void
-	isInvalid?: boolean
-	options: any
-	setInputRef: (reference: any) => void
-};
+  field: TPluginSettingWithKey;
+  value: any;
+  onChange: (event: any) => void;
+  isInvalid?: boolean;
+  options: any;
+  setInputRef: (reference: any) => void;
+}
 
 export interface IInputForm {
-	field: TPluginSettingWithKey
-	initialValue: any
-	onChange: (event: any) => void
-	label?: string
-	preInput?: ((options: {value: any, error: string | null}) => JSX.Element)
-	postInput?: ((options: {value: any, error: string | null}) => JSX.Element)
-};
+  field: TPluginSettingWithKey;
+  initialValue: any;
+  onChange: (event: any) => void;
+  label?: string;
+  preInput?: (options: { value: any; error: string | null }) => JSX.Element;
+  postInput?: (options: { value: any; error: string | null }) => JSX.Element;
+}
+
+interface IFormField {
+  initialValue: any;
+  validate?: (value: any) => string | undefined;
+}
+interface IFormFieldDefault extends IFormField {
+  type: IInputTypes;
+  initialValue: any;
+}
+
+interface IFormFieldCustom extends IFormField {
+  type: IInputTypesCustom;
+  component: (props: IInputForm) => JSX.Element;
+}
+
+export interface IFormFields {
+  [key: string]: IFormFieldCustom | IFormFieldDefault;
+}
