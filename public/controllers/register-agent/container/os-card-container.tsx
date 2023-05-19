75cce24a327bd28794aa08efd44e9654300737c3
chantal.kelm <chantal.kelm@wazuh.com>
2023-05-19 14:30:21 -0300

    Added a title to the container and updated filenames

--- public/controllers/register-agent/container/os-card-container.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './os-card-container.scss';
-
-const Container: React.FC = () => {
-  return <div className='container'></div>;
-};
-
-export default Container;
---
+++ public/controllers/register-agent/container/register-agent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './register-agent.scss';
+
+export const RegisterAgent: React.FC = () => {
+  return (
+    <div className='container'>
+      <div className='title'>Deploy new agent</div>
+    </div>
+  );
+};
