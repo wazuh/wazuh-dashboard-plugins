e20c365a438028d84ae573d8d6f824df33d97256
Maximiliano Ibarra <maximiliano.ibarra@wazuh.com>
2023-06-27 12:38:24 -0300

    Added new command component white custom copy and language

--- public/controllers/register-agent/components/command-output/command-output.tsx
+++ public/controllers/register-agent/components/command-output/command-output.tsx
@@ -1,18 +1,65 @@
-import { EuiCodeBlock } from '@elastic/eui';
-import React, { Fragment } from 'react';
+import {
+  EuiCodeBlock,
+  EuiCopy,
+  EuiIcon,
+  EuiSpacer,
+  EuiText,
+} from '@elastic/eui';
+import React, { Fragment, useState } from 'react';
 
 interface ICommandSectionProps {
   commandText: string;
   showCommand: boolean;
+  onCopy: () => void;
+  os?: 'WINDOWS' | string;
 }
 
 export default function CommandOutput(props: ICommandSectionProps) {
-  const { commandText, showCommand } = props;
+  const { commandText, showCommand, onCopy, os } = props;
+  const getHighlightCodeLanguage = (os: 'WINDOWS' | string) => {
+    if (os.toLowerCase() === 'windows') {
+      return 'powershell';
+    } else {
+      return 'bash';
+    }
+  };
+
+  const [language, setLanguage] = useState(getHighlightCodeLanguage(os || ''));
+
+  const onHandleCopy = (command: any) => {
+    onCopy && onCopy();
+    return command
+  };
+
+  const [commandToCopy, setCommandToCopy] = useState(commandText);
+
   return (
     <Fragment>
-      <EuiCodeBlock language='shell' isCopyable>
-        {showCommand ? commandText : ''}
-      </EuiCodeBlock>
+      <EuiSpacer />
+      <EuiText>
+        <div className='copy-codeblock-wrapper'>
+          <EuiCodeBlock
+            style={{
+              zIndex: '100',
+            }}
+            language={getHighlightCodeLanguage(os || '')}
+          >
+            {showCommand ? commandText : ''}
+          </EuiCodeBlock>
+          {showCommand && (
+            <EuiCopy textToCopy={commandText}>
+              {commandToCopy => (
+                <div className='copy-overlay' onClick={() => onHandleCopy(commandToCopy)}>
+                  <p>
+                    <EuiIcon type='copy' /> Copy command
+                  </p>
+                </div>
+              )}
+            </EuiCopy>
+          )}
+        </div>
+        <EuiSpacer size='s' />
+      </EuiText>
     </Fragment>
   );
 }
