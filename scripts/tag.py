7ebcbab4cac63e4033eca43582a740750dc34d9a
Ian Yenien Serrano <63758389+yenienserrano@users.noreply.github.com>
2023-07-07 11:29:43 +0200

    [Backport 4.4.5-7.10] Bump Wazuh and platform versions for v4.4.5 (#5639) (#5642)
    * Bump Wazuh and platform versions for v4.4.5 (#5639)
    
    * Update changelog
    
    * Update opensearch_dashboards.json
    
    * Update package.json
    
    * Update readme
    
    * Update tag script
    
    (cherry picked from commit b33c4a5dfcc4349dd0c4635f8b242f5208f17722)
    
    * Bump Wazuh and platform versions for v4.4.5 (#5639)
    
    * Update changelog
    
    * Update opensearch_dashboards.json
    
    * Update package.json
    
    * Update readme
    
    * Update tag script
    
    (cherry picked from commit b33c4a5dfcc4349dd0c4635f8b242f5208f17722)
    
    ---------
    
    Co-authored-by: Nicolas Agustin Guevara Pihen <42900763+Tostti@users.noreply.github.com>

--- scripts/tag.py
+++ scripts/tag.py
@@ -15,13 +15,13 @@ import subprocess
 # ======================================================= #
 
 # Wazuh version: major.minor.patch
-version = '4.4.4'
+version = '4.4.5'
 # App's revision number (previous rev + 1)
 revision = '01'
 # One of 'pre-alpha', 'alpha', 'beta', 'release-candidate', 'stable'
 stage = 'stable'
 # Tag suffix. Usually set to stage + stage iteration.
-tag_suffix = '-rc2'
+tag_suffix = ''
 
 # ================================================ #
 # Constants and global variables                   #
@@ -30,7 +30,7 @@ LOG_FILE = 'output.log'
 TAGS_FILE = 'tags.log'
 # Global variable. Will be set later
 branch = None
-minor = ".".join(version.split('.')[:2])
+minor = version
 
 # Supported versions of Kibana
 kbn_versions = [
