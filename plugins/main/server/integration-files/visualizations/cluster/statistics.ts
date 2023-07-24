178735f819fa1361350e936e1cd45e581637b4d4
chantal.kelm <chantal.kelm@wazuh.com>
2023-07-24 07:46:31 -0300

    create visualization of events decoded summary

--- plugins/main/server/integration-files/visualizations/cluster/statistics.ts
+++ plugins/main/server/integration-files/visualizations/cluster/statistics.ts
@@ -1272,23 +1272,147 @@ export default [
       title: 'Wazuh App Statistics Events Dropped by Node',
       visState: JSON.stringify({
         title: 'Wazuh App Statistics Events Dropped by Node',
-        type: 'timelion',
+        type: 'line',
+        aggs: [
+          {
+            id: '1',
+            enabled: true,
+            type: 'sum',
+            params: {
+              field: 'analysisd.events_dropped',
+              customLabel: 'Count',
+            },
+            schema: 'metric',
+          },
+          {
+            id: '2',
+            enabled: true,
+            type: 'date_histogram',
+            params: {
+              field: 'timestamp',
+              timeRange: {
+                from: 'now-30m',
+                to: 'now',
+              },
+              useNormalizedOpenSearchInterval: true,
+              scaleMetricValues: false,
+              interval: 'auto',
+              drop_partials: false,
+              min_doc_count: 1,
+              extended_bounds: {},
+            },
+            schema: 'segment',
+          },
+          {
+            id: '3',
+            enabled: true,
+            type: 'filters',
+            params: {
+              filters: [
+                {
+                  input: {
+                    query: 'analysisd.events_dropped:*',
+                    language: 'kuery',
+                  },
+                  label: 'Events dropped by Node:',
+                },
+              ],
+            },
+            schema: 'group',
+          },
+          {
+            id: '4',
+            enabled: true,
+            type: 'terms',
+            params: {
+              field: 'nodeName.keyword',
+              orderBy: '1',
+              order: 'desc',
+              size: 5,
+              otherBucket: false,
+              otherBucketLabel: 'Other',
+              missingBucket: false,
+              missingBucketLabel: 'Missing',
+            },
+            schema: 'group',
+          },
+        ],
         params: {
-          expression:
-            ".es(index=wazuh-statistics-*, timefield=timestamp,metric=sum:analysisd.events_dropped, q='*') .label('Total'), .es(index=wazuh-statistics-*, timefield=timestamp,metric=sum:analysisd.events_dropped, q='*', split=nodeName.keyword:5).label('Events dropped by Node: $1','^.* > nodeName.keyword:(\\\\S+) > .*')",
-          interval: '5m',
+          type: 'line',
+          grid: {
+            categoryLines: true,
+          },
+          categoryAxes: [
+            {
+              id: 'CategoryAxis-1',
+              type: 'category',
+              position: 'bottom',
+              show: true,
+              style: {},
+              scale: {
+                type: 'linear',
+              },
+              labels: {
+                show: true,
+                filter: true,
+                truncate: 100,
+              },
+              title: {},
+            },
+          ],
+          valueAxes: [
+            {
+              id: 'ValueAxis-1',
+              name: 'LeftAxis-1',
+              type: 'value',
+              position: 'left',
+              show: true,
+              style: {},
+              scale: {
+                type: 'linear',
+                mode: 'normal',
+              },
+              labels: {
+                show: true,
+                rotate: 0,
+                filter: false,
+                truncate: 100,
+              },
+              title: {
+                text: 'Count',
+              },
+            },
+          ],
+          seriesParams: [
+            {
+              show: true,
+              type: 'line',
+              mode: 'normal',
+              data: {
+                label: 'Count',
+                id: '1',
+              },
+              valueAxis: 'ValueAxis-1',
+              drawLinesBetweenPoints: true,
+              lineWidth: 2,
+              interpolate: 'linear',
+              showCircles: true,
+            },
+          ],
+          addTooltip: true,
+          addLegend: true,
+          legendPosition: 'right',
+          times: [],
+          addTimeMarker: false,
+          labels: {},
+          thresholdLine: {
+            show: false,
+            value: 10,
+            width: 1,
+            style: 'full',
+            color: '#E7664C',
+          },
         },
-        aggs: [],
-      }),
-      visStateByNode: JSON.stringify({
-        title: 'Wazuh App Statistics Events by Node',
-        type: 'timelion',
-        params: {
-          expression:
-            ".es(index=wazuh-statistics-*, timefield=timestamp,metric=sum:analysisd.events_dropped, q='*') .label('Events dropped by Node: NODE_NAME')",
-          interval: '5m',
-        },
-        aggs: [],
       }),
       uiStateJSON: '{}',
       description: '',
