c923029c1bc0639205ba80ba2969e00d054bc5c0
Federico Rodriguez <federico.rodriguez@wazuh.com>
2023-07-24 12:52:59 +0200

    Backport 5714 to 7.16

--- server/lib/reporting/tsc-request.ts
+++ server/lib/reporting/tsc-request.ts
@@ -28,9 +28,10 @@ export const topTSCRequirements = async (
   allowedAgentsFilter,
   pattern = getSettingDefaultValue('pattern')
 ) => {
-  if (filters.includes('rule.tsc: exists')) {
-    filters = filters.replace('AND rule.tsc: exists', '');
-  };
+  // Remove the "rule.tsc" filter
+  filters.bool.filter = filters.bool.filter.filter(filterValue => (
+    JSON.stringify(filterValue) !== '{"exists":{"field":"rule.tsc"}}'
+  ));
 
   try {
     const base = {};
@@ -92,7 +93,7 @@ export const topTSCRequirements = async (
  * @returns {Array<String>}
  */
 export const getRulesByRequirement = async (
-  context, 
+  context,
   gte,
   lte,
   filters,
@@ -100,9 +101,10 @@ export const getRulesByRequirement = async (
   requirement,
   pattern = getSettingDefaultValue('pattern')
 ) => {
-  if (filters.includes('rule.tsc: exists')) {
-    filters = filters.replace('AND rule.tsc: exists', '');
-  };
+  // Remove the "rule.tsc" filter
+  filters.bool.filter = filters.bool.filter.filter(filterValue => (
+    JSON.stringify(filterValue) !== '{"exists":{"field":"rule.tsc"}}'
+  ));
 
   try {
     const base = {};
