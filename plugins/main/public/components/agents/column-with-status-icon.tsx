1d83ec107d02c7073c61a0b7cdb8562f2893eb11
Ian Yenien Serrano <63758389+yenienserrano@users.noreply.github.com>
2023-08-02 20:39:52 +0200

    Show in agent table the disconnection reason (#5680)
    * Add status_code imposter
    
    * Add status_code column in agents table
    
    * Add changelog
    
    * Fix test
    
    * "Add test"
    
    * Add test
    
    * Fix snapshot
    
    * Remove code unnecessary
    
    * Fix comment
    
    * Update agents.json
    
    * fix test
    
    * remove column status code, add tooltip

---
+++ plugins/main/public/components/agents/column-with-status-icon.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { EuiHealth, EuiToolTip } from '@elastic/eui';
+
+interface ColumnWithStatusIconProps {
+  color: string;
+  text: string;
+  tooltip?: string;
+}
+
+export const ColumnWithStatusIcon = ({
+  color,
+  text,
+  tooltip,
+}: ColumnWithStatusIconProps) => {
+  const textTooltip = tooltip ? tooltip : text;
+  return (
+    <>
+      <EuiToolTip position='top' content={textTooltip}>
+        <EuiHealth className='wz-flex' color={color}></EuiHealth>
+      </EuiToolTip>
+      <span className='hide-agent-status'>{text}</span>
+    </>
+  );
+};
