9b2c8ad07104898241d7daa1b96e292402b9548b
Luciano Gorza <lucianogorza@gmail.com>
2023-09-19 17:48:52 -0300

    Fix plugin start types
    Signed-off-by: Luciano Gorza <lucianogorza@gmail.com>

--- plugins/wazuh_check_updates/public/types.ts
+++ plugins/wazuh_check_updates/public/types.ts
@@ -1,16 +1,14 @@
 import { NavigationPublicPluginStart } from '../../../src/plugins/navigation/public';
 import { CurrentUpdateDetailsProps } from './components/currentUpdateDetails';
-import { DismissNotificationCheckProps } from './components/dismissNotificationCheck';
 import { UpToDateStatusProps } from './components/upToDateStatus';
-import { UpdatesNotificationProps } from './components/updatesNotification';
 
 export interface WazuhCheckUpdatesPluginSetup {}
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface WazuhCheckUpdatesPluginStart {
-  UpdatesNotification: (props: UpdatesNotificationProps) => JSX.Element | null;
+  UpdatesNotification: () => JSX.Element | null;
   UpToDateStatus: (props: UpToDateStatusProps) => JSX.Element | null;
   CurrentUpdateDetails: (props: CurrentUpdateDetailsProps) => JSX.Element | null;
-  DismissNotificationCheck: (props: DismissNotificationCheckProps) => JSX.Element | null;
+  DismissNotificationCheck: () => JSX.Element | null;
 }
 
 export interface AppPluginStartDependencies {
