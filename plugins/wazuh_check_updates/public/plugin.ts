f748c81fd3c8065af5c09d94c98bf0d35d04a904
Luciano Gorza <lucianogorza@gmail.com>
2023-09-20 13:19:39 -0300

    Delete commented code
    Signed-off-by: Luciano Gorza <lucianogorza@gmail.com>

--- plugins/wazuh_check_updates/public/plugin.ts
+++ plugins/wazuh_check_updates/public/plugin.ts
@@ -9,21 +9,6 @@ import { DismissNotificationCheck } from './components/dismissNotificationCheck'
 export class WazuhCheckUpdatesPlugin
   implements Plugin<WazuhCheckUpdatesPluginSetup, WazuhCheckUpdatesPluginStart> {
   public setup(core: CoreSetup): WazuhCheckUpdatesPluginSetup {
-    // Register an application into the side navigation menu
-    // core.application.register({
-    //   id: 'wazuhCheckUpdates',
-    //   title: PLUGIN_NAME,
-    //   async mount(params: AppMountParameters) {
-    //     // Load application bundle
-    //     const { renderApp } = await import('./application');
-    //     // Get start services as specified in opensearch_dashboards.json
-    //     const [coreStart, depsStart] = await core.getStartServices();
-    //     // Render the application
-    //     return renderApp(coreStart, depsStart as AppPluginStartDependencies, params);
-    //   },
-    // });
-
-    // Return methods that should be available to other plugins
     return {};
   }
 
