f63b6dafc728cbe7d6a299b122d86a1e984e3f36
Antonio <34042064+Desvelao@users.noreply.github.com>
2023-11-09 18:09:43 +0100

    Fix tests in core plugin (#6111)
    fix(core): tests

--- plugins/wazuh-core/common/services/file-extension.test.ts
+++ plugins/wazuh-core/common/services/file-extension.test.ts
@@ -1,17 +1,20 @@
-import { getFileExtensionFromBuffer } from "./file-extension";
+import { getFileExtensionFromBuffer } from './file-extension';
 import fs from 'fs';
 import path from 'path';
 
 describe('getFileExtensionFromBuffer', () => {
-	it.each`
-		filepath                                                              | extension
-		${'../../server/routes/wazuh-utils/fixtures/fixture_image_small.jpg'} | ${'jpg'}
-		${'../../server/routes/wazuh-utils/fixtures/fixture_image_small.png'} | ${'png'}
-		${'../../server/routes/wazuh-utils/fixtures/fixture_image_big.png'}   | ${'png'}
-		${'../../server/routes/wazuh-utils/fixtures/fixture_image_small.svg'} | ${'svg'}
-		${'../../server/routes/wazuh-utils/fixtures/fixture_file.txt'}        | ${'unknown'}
-		`(`filepath: $filepath expects to get extension: $extension`, ({ extension, filepath }) => {
-			const bufferFile = fs.readFileSync(path.join(__dirname, filepath));
-			expect(getFileExtensionFromBuffer(bufferFile)).toBe(extension);
-    });
+  it.each`
+    filepath                                    | extension
+    ${'./__fixtures__/fixture_image_small.jpg'} | ${'jpg'}
+    ${'./__fixtures__/fixture_image_small.png'} | ${'png'}
+    ${'./__fixtures__/fixture_image_big.png'}   | ${'png'}
+    ${'./__fixtures__/fixture_image_small.svg'} | ${'svg'}
+    ${'./__fixtures__/fixture_file.txt'}        | ${'unknown'}
+  `(
+    `filepath: $filepath expects to get extension: $extension`,
+    ({ extension, filepath }) => {
+      const bufferFile = fs.readFileSync(path.join(__dirname, filepath));
+      expect(getFileExtensionFromBuffer(bufferFile)).toBe(extension);
+    },
+  );
 });
