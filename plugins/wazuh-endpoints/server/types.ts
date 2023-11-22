651a82fb17ccf2efc9011a33e409143563b8e1d3
Luciano Gorza <lucianogorza@gmail.com>
2023-11-22 17:18:05 -0300

    Create new plugin wazuh-endpoints

---
+++ plugins/wazuh-endpoints/server/types.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface AppPluginStartDependencies {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface WazuhEndpointsPluginSetup {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface WazuhEndpointsPluginStart {}
+
+export type PluginSetup = {};
+
+export interface AppPluginStartDependencies {}
