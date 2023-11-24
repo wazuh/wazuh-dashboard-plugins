9201e8b6ba00086c644396c1beaed258b14dbcad
Luciano Gorza <lucianogorza@gmail.com>
2023-11-24 18:03:02 -0300

    Replace div and className by Eui components

--- plugins/main/public/controllers/agent/endpoints-preview.tsx
+++ plugins/main/public/controllers/agent/endpoints-preview.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { EuiProgress, EuiIcon } from '@elastic/eui';
+import { EuiProgress, EuiPage, EuiPageBody, EuiButton, EuiCallOut } from '@elastic/eui';
 import { RegisterAgent } from '../../controllers/register-agent/containers/register-agent/register-agent';
 import { AgentsPreview } from './components/agents-preview';
 
@@ -24,51 +24,52 @@ export const EndpointsPreview = ({
   tableAgentsProps,
   resultState,
 }: EndpointsPreviewProps) => {
+  console.log({
+    init,
+    loading,
+    errorInit,
+    load,
+    addingNewAgent,
+    registerAgentsProps,
+    tableAgentsProps,
+    resultState,
+  });
   if (init || loading) {
     return (
-      <div class="md-padding md-padding-top-16">
-        <EuiProgress size="xs" color="primary" />
-      </div>
+      <EuiPage paddingSize="m">
+        <EuiPageBody>
+          <EuiProgress size="xs" color="primary" />
+        </EuiPageBody>
+      </EuiPage>
     );
   }
 
   if (errorInit) {
-    <div class="wz-margin-top-16" layout="column" layout-align="start space-around">
-      <div flex layout="row" layout-align="center center">
-        <div class="euiPanel euiFlexItem wz-margin-10" flex="50" class="wz-md-card" flex>
-          <md-card-content class="wz-text-center">
-            <span class="embPanel__header embPanel__title embPanel__dragger layout-row wz-headline-title">
-              <EuiIcon type="help" />
-              Error fetching agents
-            </span>
-            <md-divider class="wz-margin-top-10"></md-divider>
-            <div layout="row" class="wz-margin-top-10 layout-align-center-center">
-              <p class="wz-text-gray">{errorInit || 'Internal error'}</p>
-            </div>
-            <div layout="row" class="wz-margin-top-10 layout-align-center-center">
-              <button class="kuiButton kuiButton--secondary height-35" onClick={load}>
-                <EuiIcon type="refresh" />
-                Refresh
-              </button>
-            </div>
-          </md-card-content>
-        </div>
-      </div>
-    </div>;
+    return (
+      <EuiPage paddingSize="m">
+        <EuiPageBody>
+          <EuiCallOut title="Error fetching agents" color="danger" iconType="alert">
+            <p>{errorInit || 'Internal error'}</p>
+            <EuiButton type="secondary" iconType="refresh" onClick={load}>
+              Refresh
+            </EuiButton>
+          </EuiCallOut>
+        </EuiPageBody>
+      </EuiPage>
+    );
   }
 
   return (
-    <div layout="column" layout-align="start space-around">
-      {addingNewAgent ? (
-        <div class="registerAgent">
-          <RegisterAgent registerAgentsProps={registerAgentsProps} />
-        </div>
-      ) : (
-        <div>
-          {/* <kbn-dis class="hide-filter-control"></kbn-dis> */}
+    <EuiPage paddingSize="m">
+      <EuiPageBody>
+        {addingNewAgent ? (
+          <div className="registerAgent">
+            <RegisterAgent registerAgentsProps={registerAgentsProps} />
+          </div>
+        ) : (
           <AgentsPreview tableProps={tableAgentsProps} resultState={resultState} />
-        </div>
-      )}
-    </div>
+        )}
+      </EuiPageBody>
+    </EuiPage>
   );
 };
