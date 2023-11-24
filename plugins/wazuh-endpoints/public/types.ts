67c3e4038508b92905a4fd58a4e7060b6ca865d2
Luciano Gorza <103193307+lucianogorza@users.noreply.github.com>
2023-11-24 12:18:29 -0300

    Create new plugin wazuh-endpoints (#6142)
    * Create new plugin wazuh-endpoints
    
    * Delete import
    
    * Add github action to run plugin test
    
    * Remove github action to run plugin test

---
+++ plugins/wazuh-endpoints/public/types.ts
@@ -0,0 +1,5 @@
+export interface WazuhEndpointsPluginSetup {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface WazuhEndpointsPluginStart {}
+
+export interface AppPluginStartDependencies {}
