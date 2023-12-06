8c00608a94df3f701a543435af5e6ce350fbab66
Luciano Gorza <lucianogorza@gmail.com>
2023-12-06 15:59:59 -0300

    Fix os-card unit test

--- plugins/main/public/components/fleet/register-agent/components/os-selector/os-card/os-card.test.tsx
+++ plugins/main/public/components/fleet/register-agent/components/os-selector/os-card/os-card.test.tsx
@@ -3,35 +3,35 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { OsCard } from './os-card';
 
-// jest.mock('../../../../../../kibana-services', () => ({
-//   ...(jest.requireActual('../../../../../../kibana-services') as object),
-//   getHttp: jest.fn().mockReturnValue({
-//     basePath: {
-//       get: () => {
-//         return 'http://localhost:5601';
-//       },
-//       prepend: url => {
-//         return `http://localhost:5601${url}`;
-//       },
-//     },
-//   }),
-//   getCookies: jest.fn().mockReturnValue({
-//     set: (name, value, options) => {
-//       return true;
-//     },
-//     get: () => {
-//       return '{}';
-//     },
-//     remove: () => {
-//       return;
-//     },
-//   }),
-//   getUiSettings: jest.fn().mockReturnValue({
-//     get: name => {
-//       return true;
-//     },
-//   }),
-// }));
+jest.mock('../../../../../../kibana-services', () => ({
+  ...(jest.requireActual('../../../../../../kibana-services') as object),
+  getHttp: jest.fn().mockReturnValue({
+    basePath: {
+      get: () => {
+        return 'http://localhost:5601';
+      },
+      prepend: url => {
+        return `http://localhost:5601${url}`;
+      },
+    },
+  }),
+  getCookies: jest.fn().mockReturnValue({
+    set: (name, value, options) => {
+      return true;
+    },
+    get: () => {
+      return '{}';
+    },
+    remove: () => {
+      return;
+    },
+  }),
+  getUiSettings: jest.fn().mockReturnValue({
+    get: name => {
+      return true;
+    },
+  }),
+}));
 
 describe('OsCard', () => {
   test('renders three cards with different titles', () => {
