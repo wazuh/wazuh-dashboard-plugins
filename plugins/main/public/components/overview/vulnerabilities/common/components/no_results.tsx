3fe3015959686cc184886f4c2debd51f55355cbf
Maximiliano <maximiliano.ibarra@wazuh.com>
2023-12-07 11:33:23 -0300

    Add new discover children compopnents

--- plugins/main/public/components/overview/vulnerabilities/common/components/no_results.tsx
+++ plugins/main/public/components/overview/vulnerabilities/common/components/no_results.tsx
@@ -26,6 +26,7 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
+ * 
  */
 
 import React from 'react';
