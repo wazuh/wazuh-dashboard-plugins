6e84a16f0761c3af7c468fe8284d7731bae4ff23
Álex Ruiz <alejandro.ruiz.becerra@wazuh.com>
2023-12-13 12:32:15 +0100

    Add Docker envs for 4.6.x and 4.7.x pre-release and release and add server to osd-dev environment (#6132)
    * Add Docker envs for 4.6-production
    
    * feat(environments): remove logging and add dev environments for 4.7.0
    
    * feat(environments): add server to dev environments
    
    - Add real server to osd-dev environments that is enbled through the
      server profile
    - Fix pre-release and release environments for 4.6.x
    - Add pre-release and release environments for 4.7.x
    
    * Fix network name and agent register command
    
    * feat(docker): replace Wazuh server on dev environments
    
    * fix(docker): fix agent version in dev environment
    
    * fix(docker): remove unused files in dev environment
    
    ---------
    
    Co-authored-by: Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
    Co-authored-by: Federico Rodriguez <federico.rodriguez@wazuh.com>

--- docker/osd-dev/dev.sh
+++ docker/osd-dev/dev.sh
@@ -22,13 +22,20 @@ osd_versions=(
   '2.4.0'
   '2.4.1'
   '2.6.0'
+  '2.8.0'
   '4.6.0'
   '4.7.0'
 )
 
+wzs_version=(
+  '4.7.0'
+  '4.7.1'
+  '4.7.2'
+)
+
 usage() {
   echo
-  echo "./dev.sh os_version osd_version /wazuh_app_src action [saml]"
+  echo "./dev.sh os_version osd_version /wazuh_app_src action [saml/server] [server_version]"
   echo
   echo "where"
   echo "  os_version is one of " ${os_versions[*]}
@@ -36,6 +43,7 @@ usage() {
   echo "  wazuh_app_src is the path to the wazuh application source code"
   echo "  action is one of up | down | stop"
   echo "  saml to deploy a saml enabled environment"
+  echo "  server to deploy a real server enabled environment"
   exit -1
 }
 
@@ -96,6 +104,16 @@ if [[ "$5" =~ "saml" ]]; then
   export SEC_CONFIG_FILE=./config/${OSD_MAJOR}/os/config-saml.yml
 fi
 
+if [[ "$5" =~ "server" ]]; then
+  profile="server"
+  if [[ ! " ${wzs_version[*]} " =~ " ${6} " ]]; then
+    echo "Wazuh server version ${6} not found in ${wzs_version[*]}"
+    echo
+    exit -1
+  fi
+  export WAZUH_STACK="${6}"
+fi
+
 export SEC_CONFIG_PATH=/usr/share/opensearch/plugins/opensearch-security/securityconfig
 if [[ "$OSD_MAJOR" == "2.x" ]]; then
   export SEC_CONFIG_PATH=/usr/share/opensearch/config/opensearch-security
@@ -105,6 +123,27 @@ case "$4" in
 up)
   /bin/bash ../scripts/create_docker_networks.sh
   docker compose --profile $profile -f dev.yml up -Vd
+
+  # Display a command to deploy an agent when using the real server
+  if [[ "$5" =~ "server" ]]; then
+    echo
+    echo "**************WARNING**************"
+    echo "The agent version must be a published one. This uses only released versions."
+    echo "If you need to change de version, edit the command as you see fit."
+    echo "***********************************"
+    echo "1. (Optional) Enroll an agent (Ubuntu 20.04):"
+    echo "docker run --name ${COMPOSE_PROJECT_NAME}-agent-\$(date +%s) --network os-dev-${OS_VERSION} --label com.docker.compose.project=${COMPOSE_PROJECT_NAME} --env WAZUH_AGENT_VERSION=${WAZUH_STACK} -d ubuntu:20.04 bash -c '"
+    echo "  apt update -y"
+    echo "  apt install -y curl lsb-release"
+    echo "  curl -so \wazuh-agent-\${WAZUH_AGENT_VERSION}.deb \\"
+    echo "    https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_\${WAZUH_AGENT_VERSION}-1_amd64.deb \\"
+    echo "    && WAZUH_MANAGER='wazuh.manager' WAZUH_AGENT_GROUP='default' dpkg -i ./wazuh-agent-\${WAZUH_AGENT_VERSION}.deb"
+    echo
+    echo "  /etc/init.d/wazuh-agent start"
+    echo "  tail -f /var/ossec/logs/ossec.log"
+    echo "'"
+    echo
+  fi
   ;;
 down)
   docker compose --profile $profile -f dev.yml down -v --remove-orphans
---
+++ docker/wazuh-4.7/rel.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+versions=(
+	"4.7.0"
+  "4.7.1"
+  "4.7.2"
+)
+
+usage() {
+	echo
+	echo "$0 version action [saml]"
+	echo
+	echo "where version is one of ${versions[*]}"
+	echo "action is one of up | down | stop"
+	echo "saml to deploy a saml enabled environment"
+	exit -1
+}
+
+if [ $# -lt 2 ]; then
+	echo "Incorrect number of arguments " $#
+	usage
+fi
+
+if [[ ! " ${versions[*]} " =~ " ${1} " ]]; then
+	echo "Version ${1} not found in ${versions[*]}"
+	exit -1
+fi
+
+export WAZUH_STACK=${1}
+export KIBANA_PORT=5601
+export KIBANA_PASSWORD=${PASSWORD:-SecretPassword}
+export COMPOSE_PROJECT_NAME=wz-rel-${WAZUH_STACK//./}
+
+profile="standard"
+export WAZUH_DASHBOARD_CONF=./config/wazuh_dashboard/wazuh_dashboard.yml
+export SEC_CONFIG_FILE=./config/wazuh_indexer/config.yml
+
+if [[ "$3" =~ "saml" ]]; then
+	profile="saml"
+	export WAZUH_DASHBOARD_CONF=./config/wazuh_dashboard/wazuh_dashboard_saml.yml
+	export SEC_CONFIG_FILE=./config/wazuh_indexer/config-saml.yml
+fi
+
+case "$2" in
+up)
+	docker compose --profile $profile -f rel.yml -p "${COMPOSE_PROJECT_NAME}" up -Vd
+	echo
+	echo "1. (Optional) Enroll an agent (Ubuntu 20.04):"
+	echo "docker run --name ${COMPOSE_PROJECT_NAME}-agent --network ${COMPOSE_PROJECT_NAME} --label com.docker.compose.project=${COMPOSE_PROJECT_NAME} -d ubuntu:20.04 bash -c '"
+	echo "  apt update -y"
+	echo "  apt install -y curl lsb-release"
+	echo "  curl -so \wazuh-agent-${WAZUH_STACK}.deb \\"
+	echo "    https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_${WAZUH_STACK}-1_amd64.deb \\"
+	echo "    && WAZUH_MANAGER='wazuh.manager' WAZUH_AGENT_GROUP='default' dpkg -i ./wazuh-agent-${WAZUH_STACK}.deb"
+	echo
+	echo "  /etc/init.d/wazuh-agent start"
+	echo "  tail -f /var/ossec/logs/ossec.log"
+	echo "'"
+	echo
+	;;
+down)
+	docker compose --profile $profile -f rel.yml -p "${COMPOSE_PROJECT_NAME}" down -v --remove-orphans
+	;;
+stop)
+	docker compose --profile $profile -f rel.yml -p "${COMPOSE_PROJECT_NAME}" stop
+	;;
+*)
+	echo "Action must be either up or down"
+	usage
+	;;
+esac
