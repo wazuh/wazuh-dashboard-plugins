931278e20d1e5c292f828977776cbdea074be194
chantal.kelm <chantal.kelm@wazuh.com>
2023-12-15 12:15:40 -0300

    tab dashboard name change

--- plugins/main/public/components/common/modules/modules-defaults.js
+++ plugins/main/public/components/common/modules/modules-defaults.js
@@ -58,7 +58,7 @@ export const ModulesDefaults = {
     tabs: [
       {
         id: 'dashboard',
-        name: 'dashboard',
+        name: 'Dashboard',
         buttons: [ButtonModuleExploreAgent],
         component: DashboardFim,
       },
