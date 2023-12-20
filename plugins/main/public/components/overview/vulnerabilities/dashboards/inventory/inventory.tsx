34003aaa6b4f1c31b4fd67db33847d8ea41fdfac
Maximiliano <maximiliano.ibarra@wazuh.com>
2023-12-20 09:02:44 -0300

    Replace imports on inventory

--- plugins/main/public/components/overview/vulnerabilities/dashboards/inventory/inventory.tsx
+++ plugins/main/public/components/overview/vulnerabilities/dashboards/inventory/inventory.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import { getPlugins } from '../../../../../kibana-services';
-import useSearchBarConfiguration from '../../search_bar/use_search_bar_configuration';
 import { IntlProvider } from 'react-intl';
 import {
   EuiDataGrid,
@@ -15,34 +13,36 @@ import {
   EuiFlyoutHeader,
   EuiTitle,
   EuiButtonEmpty,
-  EuiCallOut,
-  EuiSpacer,
 } from '@elastic/eui';
 import { IndexPattern } from '../../../../../../../../src/plugins/data/common';
 import { SearchResponse } from '../../../../../../../../src/core/server';
-import DocViewer from '../../doc_viewer/doc_viewer';
-import { DiscoverNoResults } from '../../common/components/no_results';
-import { LoadingSpinner } from '../../common/components/loading_spinner';
-import { useDataGrid } from '../../data_grid/use_data_grid';
-import { MAX_ENTRIES_PER_QUERY, inventoryTableDefaultColumns } from './config';
-import { useDocViewer } from '../../doc_viewer/use_doc_viewer';
-import './inventory.scss';
-import { search, exportSearchToCSV } from './inventory_service';
+import { HitsCounter } from '../../../../../kibana-integrations/discover/application/components/hits_counter/hits_counter';
+import { formatNumWithCommas } from '../../../../../kibana-integrations/discover/application/helpers';
+import { getPlugins } from '../../../../../kibana-services';
 import {
   ErrorHandler,
   ErrorFactory,
   HttpError,
 } from '../../../../../react-services/error-management';
-import { withErrorBoundary } from '../../../../common/hocs';
-import { HitsCounter } from '../../../../../kibana-integrations/discover/application/components/hits_counter/hits_counter';
-import { formatNumWithCommas } from '../../../../../kibana-integrations/discover/application/helpers';
+import './inventory.scss';
+import { MAX_ENTRIES_PER_QUERY, inventoryTableDefaultColumns } from './config';
+import { DiscoverNoResults } from '../../common/components/no_results';
+import { LoadingSpinner } from '../../common/components/loading_spinner';
+// common components/hooks
+import DocViewer from '../../../../common/doc-viewer/doc-viewer';
+import useSearchBar from '../../../../common/search-bar/use-search-bar';
 import { useAppConfig } from '../../../../common/hooks';
+import { useDataGrid } from '../../../../common/data-grid/use-data-grid';
+import { useDocViewer } from '../../../../common/doc-viewer/use-doc-viewer';
+import { withErrorBoundary } from '../../../../common/hocs';
+import { search } from '../../../../common/search-bar/search-bar-service';
+import { exportSearchToCSV } from '../../../../common/data-grid/data-grid-service';
 
 const InventoryVulsComponent = () => {
   const appConfig = useAppConfig();
   const VULNERABILITIES_INDEX_PATTERN_ID =
     appConfig.data['vulnerabilities.pattern'];
-  const { searchBarProps } = useSearchBarConfiguration({
+  const { searchBarProps } = useSearchBar({
     defaultIndexPatternID: VULNERABILITIES_INDEX_PATTERN_ID,
   });
   const { isLoading, filters, query, indexPatterns } = searchBarProps;
