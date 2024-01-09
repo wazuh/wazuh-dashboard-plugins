0c4f64ab93298a2d5d0c7b2c40c3020ce8c21251
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2024-01-09 17:46:46 +0100

    feat(uilib): create buttons that opens flyouts

---
+++ plugins/main/public/components/common/buttons/flyout.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  WzButtonOpenOnClick,
+  WzButtonPermissionsOpenOnClick,
+} from './modal-confirm';
+import { WzFlyout } from '../flyouts';
+import { EuiFlyoutHeader, EuiFlyoutBody, EuiTitle } from '@elastic/eui';
+
+function renderFlyout({ flyoutTitle, flyoutProps, flyoutBody, onClose }) {
+  return (
+    <WzFlyout
+      onClose={onClose}
+      flyoutProps={{
+        maxWidth: '60%',
+        size: 'l',
+        className: 'flyout-no-overlap wz-inventory wzApp',
+        'aria-labelledby': 'flyoutSmallTitle',
+        ...flyoutProps,
+      }}
+    >
+      <EuiFlyoutHeader hasBorder className='flyout-header'>
+        <EuiTitle size='s'>
+          <h2>{flyoutTitle}</h2>
+        </EuiTitle>
+      </EuiFlyoutHeader>
+      <EuiFlyoutBody className='flyout-body'>{flyoutBody}</EuiFlyoutBody>
+    </WzFlyout>
+  );
+}
+
+export const WzButtonOpenFlyout: React.FunctionComponent<any> = ({
+  flyoutTitle,
+  flyoutProps = {},
+  flyoutBody = null,
+  buttonProps = {},
+  ...rest
+}) => (
+  <WzButtonOpenOnClick
+    {...rest}
+    {...buttonProps}
+    render={({ close: onClose }) =>
+      renderFlyout({ flyoutTitle, flyoutProps, flyoutBody, onClose })
+    }
+  />
+);
+
+export const WzButtonPermissionsOpenFlyout: React.FunctionComponent<any> = ({
+  flyoutTitle,
+  flyoutProps = {},
+  flyoutBody = null,
+  buttonProps = {},
+  ...rest
+}) => (
+  <WzButtonPermissionsOpenOnClick
+    {...rest}
+    {...buttonProps}
+    render={({ close: onClose }) =>
+      renderFlyout({ flyoutTitle, flyoutProps, flyoutBody, onClose })
+    }
+  />
+);
