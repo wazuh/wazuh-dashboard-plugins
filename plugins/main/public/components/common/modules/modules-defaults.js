c42fce2eea6b5afbdcedd2eef0ddab79e268e95c
Ian Yenien Serrano <63758389+yenienserrano@users.noreply.github.com>
2024-01-10 19:11:54 +0100

    Replace discover Vulnerability (#6275)
    * Replace discover
    
    * Add explore agent
    
    * Add changelog
    
    * Fix events view with agent
    
    * Fix changelog.md
    
    ---------
    
    Co-authored-by: Federico Rodriguez <federico.rodriguez@wazuh.com>

--- plugins/main/public/components/common/modules/modules-defaults.js
+++ plugins/main/public/components/common/modules/modules-defaults.js
@@ -21,8 +21,9 @@ import { OfficePanel } from '../../overview/office-panel';
 import { GitHubPanel } from '../../overview/github-panel';
 import { DashboardVuls, InventoryVuls } from '../../overview/vulnerabilities';
 import { withModuleNotForAgent } from '../hocs';
-import WazuhDiscover from '../wazuh-discover/wz-discover';
+import { WazuhDiscover } from '../wazuh-discover/wz-discover';
 import { threatHuntingColumns } from '../wazuh-discover/config/data-grid-columns';
+import { vulnerabilitiesColumns } from '../../overview/vulnerabilities/events/vulnerabilities-columns';
 import { DashboardFim } from '../../overview/fim/dashboard/dashboard';
 import { InventoryFim } from '../../overview/fim/inventory/inventory';
 import React from 'react';
@@ -186,7 +187,15 @@ export const ModulesDefaults = {
         name: 'Inventory',
         component: withModuleNotForAgent(InventoryVuls),
       },
-      EventsTab,
+      {
+        ...renderDiscoverTab(ALERTS_INDEX_PATTERN, vulnerabilitiesColumns),
+        component: withModuleNotForAgent(() => (
+          <WazuhDiscover
+            indexPatternName={DEFAULT_INDEX_PATTERN}
+            tableColumns={vulnerabilitiesColumns}
+          />
+        )),
+      },
     ],
     buttons: ['settings'],
     availableFor: ['manager'],
