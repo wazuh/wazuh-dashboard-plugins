21fa7f7ef5bf4002aa0d1b324fd9398e7aec8c56
Ian Yenien Serrano <63758389+yenienserrano@users.noreply.github.com>
2024-01-18 12:31:41 +0100

    Hide discover deprecation notice (#6341)
    * Hide discover deprecation notice
    
    * Update changelog
    
    * Update changelog

--- plugins/main/public/plugin.ts
+++ plugins/main/public/plugin.ts
@@ -57,6 +57,10 @@ export class WazuhPlugin
   private innerAngularInitialized: boolean = false;
   private hideTelemetryBanner?: () => void;
   public async setup(core: CoreSetup, plugins: WazuhSetupPlugins): WazuhSetup {
+    // Hide the discover deprecation notice
+    // After opensearch version 2.11.0 this line may be deleted
+    localStorage.setItem('discover:deprecation-notice:show', 'false');
+
     // Get custom logos configuration to start up the app with the correct logos
     let logosInitialState = {};
     try {
