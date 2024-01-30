4a458b1c4dfa40f9ef02de91ae21715079cc13c3
jbiset <julio.biset@wazuh.com>
2024-01-30 11:25:59 -0300

    Fixed isImplicit filters on hideCloseButtons

--- plugins/main/public/components/common/modules/modules-helper.js
+++ plugins/main/public/components/common/modules/modules-helper.js
@@ -1,4 +1,6 @@
 import { getAngularModule, getDataPlugin } from '../../../kibana-services';
+import { AppState } from '../../../react-services/app-state';
+import { FilterHandler } from '../../../utils/filter-handler';
 
 export class ModulesHelper {
   static async getDiscoverScope() {
@@ -38,11 +40,26 @@ export class ModulesHelper {
     this.activeNoImplicitsFilters();
   };
 
-  static activeNoImplicitsFilters() {
+  static async activeNoImplicitsFilters(generatedImplicitFilters) {
     const { filterManager } = getDataPlugin().query;
-    const allFilters = filterManager.getFilters();
+    const filters = filterManager.getFilters();
+    const implicitFilters = generatedImplicitFilters
+      ? generatedImplicitFilters
+      : filters.filter(filter => filter.$state.isImplicit);
 
-    this.processFilters(allFilters);
+    /*
+    Since the OSD filter definition does not include the "isImplicit" attribute that Wazuh adds, there may be cases where the "isImplicit" attribute is lost, since any action regarding filters that is done with the filterManager ( addFilters, setFilters, setGlobalFilters, setAppFilters) does a mapAndFlattenFilters mapping to the filters that removes any attributes that are not part of the filter definition.
+If this case happens, the implicit filters are regenerated and the function is called again with the generated implicit filters.
+ */
+    if (implicitFilters.length === 0) {
+      const generatedImplicitFilters = ModulesHelper.getImplicitFilter();
+      setTimeout(() => {
+        this.activeNoImplicitsFilters(generatedImplicitFilters);
+      }, 100);
+      return;
+    }
+
+    this.processFilters(implicitFilters);
   }
 
   static processFilters(filters) {
@@ -51,6 +68,7 @@ export class ModulesHelper {
     allFilters.each((_index, filter) => {
       const data = filter.attributes['data-test-subj'];
 
+      /* With the filter classes decide if they are from the module view or not */
       const isImplicitFilter = this.checkFilterValues(data, filters);
 
       this.updateFilterState(isImplicitFilter, filter);
@@ -58,6 +76,10 @@ export class ModulesHelper {
   }
 
   static checkFilterValues(data, filters) {
+    /*
+    Checks if the filter is already in use
+    Check which of the filters are from the module view and which are not pinned filters
+     */
     for (const moduleFilter of filters) {
       if (!moduleFilter.used) {
         const { objKey, objValue } = this.extractKeyAndValue(moduleFilter);
@@ -98,5 +120,53 @@ export class ModulesHelper {
       closeButton.addClass('hide-close-button');
       $(filter).on('click', ev => ev.stopPropagation());
     }
+  }
+
+  static getImplicitFilter() {
+    const tabFilters = {
+      general: { group: '' },
+      welcome: { group: '' },
+      fim: { group: 'syscheck' },
+      pm: { group: 'rootcheck' },
+      vuls: { group: 'vulnerability-detector' },
+      oscap: { group: 'oscap' },
+      ciscat: { group: 'ciscat' },
+      audit: { group: 'audit' },
+      pci: { group: 'pci_dss' },
+      gdpr: { group: 'gdpr' },
+      hipaa: { group: 'hipaa' },
+      nist: { group: 'nist' },
+      tsc: { group: 'tsc' },
+      aws: { group: 'amazon' },
+      gcp: { group: 'gcp' },
+      office: { group: 'office365' },
+      virustotal: { group: 'virustotal' },
+      osquery: { group: 'osquery' },
+      sca: { group: 'sca' },
+      docker: { group: 'docker' },
+      github: { group: 'github' },
+    };
+    const filterHandler = new FilterHandler(AppState.getCurrentPattern());
+    const filters = [];
+    const isCluster = AppState.getClusterInfo().status == 'enabled';
+    filters.push(
+      filterHandler.managerQuery(
+        isCluster
+          ? AppState.getClusterInfo().cluster
+          : AppState.getClusterInfo().manager,
+        isCluster,
+      ),
+    );
+    filters.push(filterHandler.pciQuery());
+    filters.push(filterHandler.gdprQuery());
+    filters.push(filterHandler.hipaaQuery());
+    filters.push(filterHandler.nistQuery());
+    filters.push(filterHandler.tscQuery());
+    filters.push(filterHandler.mitreQuery());
+    Object.keys(tabFilters).forEach(tab => {
+      filters.push(filterHandler.ruleGroupQuery(tabFilters[tab].group));
+    });
+
+    return filters;
   }
 }
