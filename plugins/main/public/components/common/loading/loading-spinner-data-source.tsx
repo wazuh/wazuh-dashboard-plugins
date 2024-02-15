5327ec2e2af92d16744386f462d571af5b5e1907
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2024-02-15 16:58:20 +0100

    fix: loading spinner data source styles

--- plugins/main/public/components/common/loading/loading-spinner-data-source.tsx
+++ plugins/main/public/components/common/loading/loading-spinner-data-source.tsx
@@ -1,4 +1,3 @@
-import './loading_spinner.scss';
 import React from 'react';
 import {
   EuiTitle,
@@ -7,28 +6,31 @@ import {
   EuiLoadingSpinner,
 } from '@elastic/eui';
 import { FormattedMessage } from '@osd/i18n/react';
+import { IntlProvider } from 'react-intl';
 
 export function LoadingSpinnerDataSource() {
   return (
-    <EuiPanel
-      hasBorder={false}
-      hasShadow={false}
-      color='transparent'
-      className='discoverNoResults'
-    >
-      <EuiEmptyPrompt
-        icon={<EuiLoadingSpinner data-test-subj='loadingSpinner' size='xl' />}
-        title={
-          <EuiTitle size='s' data-test-subj='loadingSpinnerText'>
-            <h2>
-              <FormattedMessage
-                id='wazuh.dataSource.checking'
-                defaultMessage='Checking data source'
-              />
-            </h2>
-          </EuiTitle>
-        }
-      />
-    </EuiPanel>
+    <IntlProvider locale='en'>
+      <EuiPanel
+        hasBorder={false}
+        hasShadow={false}
+        color='transparent'
+        style={{ display: 'flex', alignItems: 'center' }}
+      >
+        <EuiEmptyPrompt
+          icon={<EuiLoadingSpinner data-test-subj='loadingSpinner' size='xl' />}
+          title={
+            <EuiTitle size='s' data-test-subj='loadingSpinnerText'>
+              <h2>
+                <FormattedMessage
+                  id='wazuh.dataSource.checking'
+                  defaultMessage='Checking data source'
+                />
+              </h2>
+            </EuiTitle>
+          }
+        />
+      </EuiPanel>
+    </IntlProvider>
   );
 }
