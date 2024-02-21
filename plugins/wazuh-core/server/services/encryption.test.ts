a6ae9459365b627fe6d5a12e450719c0f4d36421
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2024-02-21 11:45:47 +0100

    fix: tests

--- plugins/wazuh-core/server/services/encryption.test.ts
+++ plugins/wazuh-core/server/services/encryption.test.ts
@@ -9,36 +9,30 @@ const mockLogger = {
 };
 
 describe('Encryption service', () => {
-  it('ensure the Encryption throws an error when the password is not defined', () => {
-    expect(() => new Encryption(mockLogger, {})).toThrow(
-      'password must be defined',
-    );
+  it('ensure the Encryption throws an error when the key is not defined', () => {
+    expect(() => new Encryption(mockLogger, {})).toThrow('key must be defined');
   });
 
   it('ensure the Encryption is created', () => {
     expect(
-      () => new Encryption(mockLogger, { password: 'customPassword' }),
+      () => new Encryption(mockLogger, { key: 'customPassword' }),
     ).not.toThrow('');
   });
 });
 
 describe('Encryption service usage', () => {
   it.each`
-    encryptionPassword   | text                                                             | encryptedTextAsHex
-    ${'pass123'}         | ${'custom text'}                                                 | ${'706173733132330000000000ef4496193cb510f07a8395ad895cf7292cccabbe3e91bdf8795893'}
-    ${'custom password'} | ${'custom text'}                                                 | ${'637573746f6d207061737377a8c71e9dc549af7cabba89959a6de263f908f09a9265ec3043bb63'}
-    ${'custom password'} | ${"[{id: 'default',username:'wazuh-wui',password:'wazuh-wui'}]"} | ${'637573746f6d20706173737790c9048d9004a86caba49c522906edd835c614c64616bc26bf38c105509d239bab60ca95e3afc738db7b632f80d94b8166559fef5d94ad337b5d7aa067324001b1b6621486a5e620c2adbd'}
-  `(
-    'encrypt and decrypt',
-    ({ text, encryptionPassword, encryptedTextAsHex }) => {
-      const encryption = new Encryption(mockLogger, {
-        password: encryptionPassword,
-      });
-      const cypherText = encryption.encrypt(text);
-      expect(cypherText).toBe(encryptedTextAsHex);
-      console.log({ encryptedTextAsHex });
-      const decypherText = encryption.decrypt(cypherText);
-      expect(decypherText).toBe(text);
-    },
-  );
+    encryptionKey   | text                                                             | encryptedTextAsHex
+    ${'key123'}     | ${'custom text'}                                                 | ${'6b657931323300000000000069fb9170a1c96951031e53b321126b7bde3718119ac3eebcc8a1f2'}
+    ${'custom key'} | ${'custom text'}                                                 | ${'637573746f6d206b65790000c89f1347f06f5b321bbd75450d6148d45492f0298f99232adbc22c'}
+    ${'custom key'} | ${"[{id: 'default',username:'wazuh-wui',password:'wazuh-wui'}]"} | ${'637573746f6d206b65790000f0910957a5225c221ba3603bf588054a4b535e33cd9d7bb2cd3da87b2a070d9569b929c40998a5e967da1d7138cbff28272c22c95592ce4bf7303fd5d839aa13da076994ae4facab185b4c'}
+  `('encrypt and decrypt', ({ text, encryptionKey, encryptedTextAsHex }) => {
+    const encryption = new Encryption(mockLogger, {
+      key: encryptionKey,
+    });
+    const cypherText = encryption.encrypt(text);
+    expect(cypherText).toBe(encryptedTextAsHex);
+    const decypherText = encryption.decrypt(cypherText);
+    expect(decypherText).toBe(text);
+  });
 });
