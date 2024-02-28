f3fe46b65baa599aeca122aa95706fb4166c01a2
Julio César Biset <43619595+jbiset@users.noreply.github.com>
2024-02-28 12:22:18 -0300

    Refactoring and redesign endpoints summary charts (#6268)
    * Refactor the agent by status and develop other charts
    
    * Update outdated agents card
    
    * Add dynamic colors to graphics and responsive layout
    
    * Fix responsive layout
    
    * Change outdated agents panel max size
    
    * Change agent_distinct imposter endpoint and fix getAgentsByOS service
    
    * Changed agent-by-group and agent-by-OS endpoints to filter agent 000.
    
    * The getAgentsByGroup and getAgentsByOs methods are improved. Unit tests are added to both methods
    
    * fix query when filtering by group and when filtering by outdated agents
    
    * Disabled filter outdated agent when no data and add donut-card test
    
    * Finished donut-card tests and added outdated-agents-card tests
    
    * Adjusted visual styles of chart-legend and outdated-agents-card popover
    
    * Add CHANGELOG
    
    * Fix responsive visualizations
    
    * Changed the services of obtaining agents by group and agents by OS. And fixed filtering by unknown values of both.
    
    * Fix chart legend on tablet-desktop breakpoint
    
    * Change width chart legent to desktop breakpoint
    
    * Fix get-agents-by-group test
    
    * Updated get-agents-by-os to get top 5 agents by OS
    
    * Fixed CSS for both the outdated-agents-card popover and the donut legend

---
+++ plugins/main/public/components/endpoints-summary/dashboard/components/outdated-agents-card.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import {
+  EuiFlexItem,
+  EuiCard,
+  EuiIcon,
+  EuiStat,
+  EuiTextColor,
+  EuiPopover,
+  EuiPopoverFooter,
+  EuiLink,
+  EuiButtonEmpty,
+} from '@elastic/eui';
+import './outdated-agents-card.scss';
+import { getOutdatedAgents } from '../../services/get-outdated-agents';
+import { useApiService } from '../../../common/hooks/useApiService';
+import { webDocumentationLink } from '../../../../../common/services/web_documentation';
+
+interface OutdatedAgentsCardProps {
+  onClick?: (status: any) => void;
+  [key: string]: any;
+}
+
+const OutdatedAgentsCard = ({ onClick, ...props }: OutdatedAgentsCardProps) => {
+  const [loading, data] = useApiService<any>(getOutdatedAgents, undefined);
+  const outdatedAgents = data?.length;
+  const contentType = outdatedAgents > 0 ? 'warning' : 'success';
+  const contentIcon = outdatedAgents > 0 ? 'alert' : 'check';
+  const [showOutdatedAgents, setShowOutdatedAgents] =
+    React.useState<boolean>(false);
+
+  const onShowOutdatedAgents = () => setShowOutdatedAgents(!showOutdatedAgents);
+  const onHideOutdatedAgents = () => setShowOutdatedAgents(false);
+
+  const handleClick = () => {
+    if (onClick) {
+      onClick(data);
+      setShowOutdatedAgents(false);
+    }
+  };
+
+  const renderMetric = () => {
+    return (
+      <div
+        className='wazuh-outdated-agents-panel'
+        onClick={onShowOutdatedAgents}
+      >
+        <EuiIcon
+          type={contentIcon}
+          color={contentType}
+          className='wazuh-outdated-icon'
+        />
+        <EuiStat
+          className='wazuh-outdated-metric'
+          title={
+            <EuiTextColor
+              data-testid='wazuh-endpoints-summary-outdated-agents-number'
+              color={contentType}
+            >
+              {outdatedAgents}
+            </EuiTextColor>
+          }
+          description={
+            <EuiTextColor color={contentType}>
+              <small>Agents</small>
+            </EuiTextColor>
+          }
+          titleColor='danger'
+          isLoading={loading}
+          titleSize='l'
+          textAlign='center'
+          reverse
+        />
+      </div>
+    );
+  };
+
+  return (
+    <EuiFlexItem>
+      <EuiCard title='' betaBadgeLabel='Outdated'>
+        <EuiPopover
+          button={renderMetric()}
+          isOpen={showOutdatedAgents}
+          closePopover={onHideOutdatedAgents}
+          anchorPosition='downLeft'
+          panelStyle={{ overflowY: 'auto' }}
+        >
+          <EuiButtonEmpty
+            iconType='filter'
+            onClick={handleClick}
+            isDisabled={!(data?.length > 0)}
+          >
+            Filter outdated agents
+          </EuiButtonEmpty>
+          <EuiPopoverFooter>
+            <EuiTextColor color='subdued'>
+              <EuiLink
+                href={webDocumentationLink(
+                  'upgrade-guide/wazuh-agent/index.html',
+                )}
+                target='_blank'
+                external
+                rel='noopener noreferrer'
+              >
+                How to update agents
+              </EuiLink>
+            </EuiTextColor>
+          </EuiPopoverFooter>
+        </EuiPopover>
+      </EuiCard>
+    </EuiFlexItem>
+  );
+};
+
+export default OutdatedAgentsCard;
