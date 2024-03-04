78e262eb8677eb3c6b1f71d6acd5a21704f5bcca
Ian Yenien Serrano <63758389+yenienserrano@users.noreply.github.com>
2024-03-04 13:07:55 +0100

    Replace discover Mitre Attack, Virustotal and Malware detection (#6459)
    * Replace discover mitre and virustotal
    
    * Replace discover malware detection
    
    * Remove unnecessary files
    
    * Add changelog
    
    * Update changelog

---
+++ plugins/main/public/components/overview/mitre/events/mitre-attack-columns.tsx
@@ -0,0 +1,22 @@
+import { tDataGridColumn } from '../../../common/data-grid';
+
+export const mitreAttackColumns: tDataGridColumn[] = [
+  {
+    id: 'agent.name',
+  },
+  {
+    id: 'rule.mitre.id',
+  },
+  {
+    id: 'rule.mitre.tactic',
+  },
+  {
+    id: 'rule.description',
+  },
+  {
+    id: 'rule.level',
+  },
+  {
+    id: 'rule.id',
+  },
+];
