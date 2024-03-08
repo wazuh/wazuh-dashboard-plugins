358c8241a969409c88f25cca8efdaf96ebe95bd6
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2024-03-08 08:35:45 +0100

    fix: manage errors on request when using responseType: blob

--- plugins/main/public/services/request-handler.js
+++ plugins/main/public/services/request-handler.js
@@ -2,16 +2,16 @@ import axios from 'axios';
 import { HTTP_STATUS_CODES } from '../../common/constants';
 
 let allow = true;
-export let unregisterInterceptor = () => { };
+export let unregisterInterceptor = () => {};
 const source = axios.CancelToken.source();
 
 const disableRequests = () => {
   allow = false;
   source.cancel('Requests cancelled');
   return;
-}
+};
 
-export const initializeInterceptor = (core) => {
+export const initializeInterceptor = core => {
   unregisterInterceptor = core.http.intercept({
     responseError: (httpErrorResponse, controller) => {
       if (
@@ -21,37 +21,47 @@ export const initializeInterceptor = (core) => {
       }
     },
     request: (current, controller) => {
-      if (
-        !allow
-      ) {
-        throw new Error("Disable request");
-      };
+      if (!allow) {
+        throw new Error('Disable request');
+      }
     },
   });
-}
+};
 
 export const request = async (options = {}) => {
   if (!allow) {
     return Promise.reject('Requests are disabled');
-  };
+  }
   if (!options.method || !options.url) {
-    return Promise.reject("Missing parameters");
-  };
+    return Promise.reject('Missing parameters');
+  }
   options = {
-    ...options, cancelToken: source?.token
+    ...options,
+    cancelToken: source?.token,
   };
 
   if (allow) {
     try {
       const requestData = await axios(options);
       return Promise.resolve(requestData);
-    }
-    catch (e) {
-      if (e.response?.data?.message === 'Unauthorized' || e.response?.data?.message === 'Authentication required') {
+    } catch (e) {
+      /* Transform the response when the data is comming as a blob to POJO.
+      Using the responseType: blob causes the response data is formatted to a blob, despite
+      the response can be a json. */
+      if (
+        e.response?.data?.constructor?.name === 'Blob' &&
+        e?.response?.headers?.['content-type']?.includes?.('application/json')
+      ) {
+        e.response.data = JSON.parse(await e.response.data.text());
+      }
+      if (
+        e.response?.data?.message === 'Unauthorized' ||
+        e.response?.data?.message === 'Authentication required'
+      ) {
         disableRequests();
         window.location.reload();
       }
       return Promise.reject(e);
     }
   }
-}
+};
