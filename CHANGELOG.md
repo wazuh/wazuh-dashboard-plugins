18686a65fe3921d44ab97987d76b196c5f66358d
Federico Rodriguez <federico.rodriguez@wazuh.com>
2024-03-22 13:24:05 +0100

    Change macOS fields displayed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,7 @@ All notable changes to the Wazuh app project will be documented in this file.
 - Added edit agent groups and upgrade agents actions to Endpoints Summary [#6250](https://github.com/wazuh/wazuh-dashboard-plugins/pull/6250) [#6476](https://github.com/wazuh/wazuh-dashboard-plugins/pull/6476) [#6274](https://github.com/wazuh/wazuh-dashboard-plugins/pull/6274) [#6501](https://github.com/wazuh/wazuh-dashboard-plugins/pull/6501)
 - Added propagation of updates from the table to dashboard visualizations in Endpoints summary [#6460](https://github.com/wazuh/wazuh-dashboard-plugins/pull/6460)
 - Handle index pattern selector on new discover [#6499](https://github.com/wazuh/wazuh-dashboard-plugins/pull/6499)
+- Added macOS log collector tab [#6545](https://github.com/wazuh/wazuh-dashboard-plugins/pull/6545)
 
 ### Changed
 
--- plugins/main/public/controllers/management/components/management/configuration/log-collection/log-collection-macosevents.js
+++ plugins/main/public/controllers/management/components/management/configuration/log-collection/log-collection-macosevents.js
@@ -15,6 +15,7 @@ import React, { Component, Fragment } from 'react';
 import WzNoConfig from '../util-components/no-config';
 import WzConfigurationSettingsHeader from '../util-components/configuration-settings-header';
 import WzConfigurationListSelector from '../util-components/configuration-settings-list-selector';
+import WzConfigurationSettingsGroup from '../util-components/configuration-settings-group';
 import { renderValueOrNoValue, isString } from '../utils/utils';
 import { settingsListBuilder } from '../utils/builders';
 import helpLinks from './help-links';
@@ -22,22 +23,6 @@ import {
   LOGCOLLECTOR_LOCALFILE_PROP,
   LOCALFILE_MACOSEVENT_PROP,
 } from './types';
-
-/**
- *  Return input label based on logformat value
- *
- * @param {*} value => field value
- * @param {*} item => settings item
- * @param {*} config => all log data
- * @returns string => value to show in Channel label
- */
-const channelLabel = (value, item, config) => {
-  return config.logformat === 'eventlog'
-    ? 'Log'
-    : config.logformat === 'eventchannel'
-    ? 'Channel'
-    : 'Channel';
-};
 
 /**
  *
@@ -57,34 +42,30 @@ const queryValue = data => {
  * @param {*} item
  * @returns string => target
  */
-const renderTargetField = item => (item ? item.join(', ') : 'agent');
+const renderTargetField = item =>
+  Array.isArray(item) ? item.join(', ') : 'agent';
+
 /**
  * Return panels title
  * @param {*} item => log data
  * @returns
  */
 const panelsLabel = item =>
-  !item.channel
-    ? `${item.logformat} - ${renderTargetField(item.target)}`
-    : `${item.channel} (${item.logformat})`;
+  `${item.logformat} - ${renderTargetField(item.target)}`;
 
 const mainSettings = [
   { field: 'logformat', label: 'Log format' },
+  { field: 'query', label: 'Query value', render: queryValue },
+  { field: 'query.level', label: 'Query level', render: renderValueOrNoValue },
+  { field: 'query.type', label: 'Query type', render: renderValueOrNoValue },
   {
-    field: 'channel',
-    label: 'Channel',
+    field: 'ignore_binaries',
+    label: 'Ignore binaries',
     render: renderValueOrNoValue,
-    renderLabel: channelLabel,
   },
-  { field: 'query', label: 'Query', render: queryValue },
   {
     field: 'only-future-events',
     label: 'Only future events',
-    render: renderValueOrNoValue,
-  },
-  {
-    field: 'reconnect_time',
-    label: 'Reconnect Time',
     render: renderValueOrNoValue,
   },
 ];
@@ -103,6 +84,7 @@ class WzConfigurationLogCollectionMacOSEvents extends Component {
           panelsLabel,
         )
       : [];
+
     return (
       <Fragment>
         {isString(currentConfig?.[LOGCOLLECTOR_LOCALFILE_PROP]) && (
@@ -118,7 +100,7 @@ class WzConfigurationLogCollectionMacOSEvents extends Component {
         ) : null}
         {currentConfig?.[LOGCOLLECTOR_LOCALFILE_PROP]?.[
           LOCALFILE_MACOSEVENT_PROP
-        ]?.length ? (
+        ]?.length > 1 ? (
           <WzConfigurationSettingsHeader
             title='macOS events logs'
             description='List of macOS logs that will be processed'
@@ -129,6 +111,14 @@ class WzConfigurationLogCollectionMacOSEvents extends Component {
               settings={mainSettings}
             />
           </WzConfigurationSettingsHeader>
+        ) : null}
+        {currentConfig?.[LOGCOLLECTOR_LOCALFILE_PROP]?.[
+          LOCALFILE_MACOSEVENT_PROP
+        ]?.length === 1 ? (
+          <WzConfigurationSettingsGroup
+            config={items[0].data}
+            items={mainSettings}
+          />
         ) : null}
       </Fragment>
     );
