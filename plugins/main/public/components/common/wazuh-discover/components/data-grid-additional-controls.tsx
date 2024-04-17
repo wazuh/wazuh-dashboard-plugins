79e3d877117d29818e13a54ed02b6010da687f27
Maximiliano <maximiliano.ibarra@wazuh.com>
2024-04-17 20:36:56 -0300

    Create component to reuse in discover

--- plugins/main/public/components/common/wazuh-discover/components/data-grid-additional-controls.tsx
+++ plugins/main/public/components/common/wazuh-discover/components/data-grid-additional-controls.tsx
@@ -1,58 +1,60 @@
 import React from 'react';
-import {
-  EuiButtonEmpty,
-} from '@elastic/eui';
+import { EuiButtonEmpty } from '@elastic/eui';
 import { HitsCounter } from '../../../../kibana-integrations/discover/application/components/hits_counter';
 import { formatNumWithCommas } from '../../../../kibana-integrations/discover/application/helpers';
 
 type tDiscoverDataGridAdditionalControlsProps = {
-    totalHits: number;
-    isExporting: boolean;
-    onClickExportResults: () => void;
-    maxEntriesPerQuery?: number;
-}
+  totalHits: number;
+  isExporting: boolean;
+  onClickExportResults: () => void;
+  maxEntriesPerQuery?: number;
+};
 
 export const MAX_ENTRIES_PER_QUERY = 10000;
 
 const DiscoverDataGridAdditionalControls = (props: tDiscoverDataGridAdditionalControlsProps) => {
-    const { totalHits, isExporting, maxEntriesPerQuery = MAX_ENTRIES_PER_QUERY, onClickExportResults } = props;
+  const {
+    totalHits,
+    isExporting,
+    maxEntriesPerQuery = MAX_ENTRIES_PER_QUERY,
+    onClickExportResults,
+  } = props;
 
-    const onHandleExportResults = () => {
-        onClickExportResults && onClickExportResults();
-    }
+  const onHandleExportResults = () => {
+    onClickExportResults && onClickExportResults();
+  };
 
-    return (
-        <>
-        <HitsCounter
-          hits={totalHits}
-          showResetButton={false}
-          tooltip={
-            totalHits &&
-            totalHits > maxEntriesPerQuery
-              ? {
-                  ariaLabel: 'Warning',
-                  content: `The query results has exceeded the limit of 10,000 hits. To provide a better experience the table only shows the first ${formatNumWithCommas(
-                    maxEntriesPerQuery,
-                  )} hits.`,
-                  iconType: 'alert',
-                  position: 'top',
-                }
-              : undefined
-          }
-        />
-        <EuiButtonEmpty
-          disabled={totalHits === 0 || isExporting}
-          size='xs'
-          iconType='exportAction'
-          color='primary'
-          isLoading={isExporting}
-          className='euiDataGrid__controlBtn'
-          onClick={onHandleExportResults}
-        >
-          Export Formated
-        </EuiButtonEmpty>
-      </>
-    )
-}
+  return (
+    <>
+      <HitsCounter
+        hits={totalHits}
+        showResetButton={false}
+        tooltip={
+          totalHits && totalHits > maxEntriesPerQuery
+            ? {
+                ariaLabel: 'Warning',
+                content: `The query results has exceeded the limit of 10,000 hits. To provide a better experience the table only shows the first ${formatNumWithCommas(
+                  maxEntriesPerQuery
+                )} hits.`,
+                iconType: 'alert',
+                position: 'top',
+              }
+            : undefined
+        }
+      />
+      <EuiButtonEmpty
+        disabled={totalHits === 0 || isExporting}
+        size="xs"
+        iconType="exportAction"
+        color="primary"
+        isLoading={isExporting}
+        className="euiDataGrid__controlBtn"
+        onClick={onHandleExportResults}
+      >
+        Export Formated
+      </EuiButtonEmpty>
+    </>
+  );
+};
 
 export default DiscoverDataGridAdditionalControls;
--- plugins/main/public/components/common/wazuh-discover/components/doc-view-inspect-button.tsx
+++ plugins/main/public/components/common/wazuh-discover/components/doc-view-inspect-button.tsx
@@ -2,27 +2,27 @@ import React from 'react';
 import { EuiToolTip, EuiButtonIcon } from '@elastic/eui';
 
 type tDocViewInspectButtonProps = {
-    rowIndex: number;
-    onClick: (index: number) => void;
-}
+  rowIndex: number;
+  onClick: (index: number) => void;
+};
 
 const DocViewInspectButton = (props: tDocViewInspectButtonProps) => {
-    const { rowIndex, onClick } = props;
-    const inspectHintMsg = 'Inspect document details';
+  const { rowIndex, onClick } = props;
+  const inspectHintMsg = 'Inspect document details';
 
-    const onHandleInspectDoc = () => {
-      onClick && onClick(rowIndex);
-    }
-
-    return (
-      <EuiToolTip content={inspectHintMsg}>
-        <EuiButtonIcon
-          onClick={() => onHandleInspectDoc()}
-          iconType='inspect'
-          aria-label={inspectHintMsg}
-        />
-      </EuiToolTip>
-    );
+  const onHandleInspectDoc = () => {
+    onClick && onClick(rowIndex);
   };
+
+  return (
+    <EuiToolTip content={inspectHintMsg}>
+      <EuiButtonIcon
+        onClick={() => onHandleInspectDoc()}
+        iconType="inspect"
+        aria-label={inspectHintMsg}
+      />
+    </EuiToolTip>
+  );
+};
 
 export default DocViewInspectButton;
