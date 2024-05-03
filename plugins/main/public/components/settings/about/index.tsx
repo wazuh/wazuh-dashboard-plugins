179f05b77845b6202155e12adf1e33238c38c8f0
Antonio <34042064+Desvelao@users.noreply.github.com>
2024-05-03 09:34:43 +0200

    Add ability to disable the edition of configuration through API endpoints and UI (#6607)
    * feat(configuration): add ability to disable the edition of configuration from API endpoints
    
    - Add ability to disable the edition of configuration from API endpoints
     - Add plugin setting to manage this ability
     - Add route controlle decorator
     - Protect the related API route controllers to updating the configuration
    - Changed the sign of routeDecoratorProtectedAdministrator
     - Adapted its usage on the API endpoints
    - Create compose utility to compose functions
    - Add test related to API controllers decorators
    - Add test about PUT /utils/configuration related to API endpoint
      protection
    
    * fix(typo): plugin setting
    
    * fix: remove repeated message on updating configuration from UI
    
    * changelog: add entry
    
    * remove: unused code on settings component
    
    * fix(settings): refactor some unused code
    
    * feat(configuration): configuration tab accessibility depends on plugin setting
    
    * fix(configuration: typo
    
    * fix: replace branding reference
    
    * feat(configuration): hide the button to save the enrollment.dns plugin setting from the Deploy new agent guide
    
    * feat(configuration): hide Settings button on Statistics application depending on if the configuration can be editable through UI

--- plugins/main/public/components/settings/about/index.tsx
+++ plugins/main/public/components/settings/about/index.tsx
@@ -2,24 +2,24 @@ import React from 'react';
 import { EuiPage, EuiPageBody, EuiSpacer } from '@elastic/eui';
 import { SettingsAboutAppInfo } from './appInfo';
 import { SettingsAboutGeneralInfo } from './generalInfo';
+import { PLUGIN_APP_NAME } from '../../../../common/constants';
 
 interface SettingsAboutProps {
   appInfo?: {
     'app-version': string;
     revision: string;
   };
-  pluginAppName: string;
 }
 
 export const SettingsAbout = (props: SettingsAboutProps) => {
-  const { appInfo, pluginAppName } = props;
+  const { appInfo } = props;
 
   return (
     <EuiPage paddingSize='m'>
       <EuiPageBody>
         <SettingsAboutAppInfo appInfo={appInfo} />
         <EuiSpacer size='l' />
-        <SettingsAboutGeneralInfo pluginAppName={pluginAppName} />
+        <SettingsAboutGeneralInfo pluginAppName={PLUGIN_APP_NAME} />
       </EuiPageBody>
     </EuiPage>
   );
