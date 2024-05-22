d43c73d345417a472e6b033dc55d6c175630504a
Luciano Gorza <lucianogorza@gmail.com>
2024-05-22 14:18:11 -0300

    Remove mitre attack columns displayAsText

--- plugins/main/public/components/overview/mitre/events/mitre-attack-columns.tsx
+++ plugins/main/public/components/overview/mitre/events/mitre-attack-columns.tsx
@@ -1,17 +1,11 @@
 import { tDataGridColumn } from '../../../common/data-grid';
 
 export const mitreAttackColumns: tDataGridColumn[] = [
-  {
-    id: 'timestamp',
-    displayAsText: 'Time',
-  },
-  { id: 'agent.name', displayAsText: 'Agent Name' },
-  {
-    id: 'rule.mitre.id',
-    displayAsText: 'Technique(s)',
-  },
-  { id: 'rule.mitre.tactic', displayAsText: 'Tactic(s)' },
-  { id: 'rule.description', displayAsText: 'Description' },
-  { id: 'rule.level', displayAsText: 'Level' },
-  { id: 'rule.id', displayAsText: 'Rule ID' },
+  { id: 'timestamp' },
+  { id: 'agent.name' },
+  { id: 'rule.mitre.id' },
+  { id: 'rule.mitre.tactic' },
+  { id: 'rule.description' },
+  { id: 'rule.level' },
+  { id: 'rule.id' },
 ];
