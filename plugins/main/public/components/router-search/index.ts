aef433918a31d34041a441c264e3abb69407552f
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2024-05-29 11:46:45 +0200

    fix(routing): enhance custom router components based on search parameters

--- plugins/main/public/components/router-search/index.ts
+++ plugins/main/public/components/router-search/index.ts
@@ -4,16 +4,19 @@ import NavigationService from '../../react-services/navigation-service';
 
 const childrenMatchSearchRoute = (routeSearch, search) => {
   const searchParams = new URLSearchParams(routeSearch);
-  return [...searchParams.entries()].every(
-    ([key, value]) => search[key] === value,
-  );
+  return [...searchParams.entries()].every(([key, value]) => {
+    if (value.startsWith(':')) {
+      return search[key];
+    }
+    return search[key] === value;
+  });
 };
 
 /* Create similar components of react-router-dom library to route the changes in
 the search params */
 export const Switch = ({ children }) => {
   const search = useRouterSearch();
-  const child = children.find(child =>
+  const child = (Array.isArray(children) ? children : [children]).find(child =>
     child.props.path
       ? childrenMatchSearchRoute(child.props.path, search)
       : child.props.to,
