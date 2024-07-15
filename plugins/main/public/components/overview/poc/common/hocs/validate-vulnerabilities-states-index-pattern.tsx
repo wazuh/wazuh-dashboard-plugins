bf09fee0b4e0b79873c4141047215ef589dea091
chantal.kelm <chantal.kelm@wazuh.com>
2024-07-15 13:16:14 -0300

    logic for creating dashboards

---
+++ plugins/main/public/components/overview/poc/common/hocs/validate-vulnerabilities-states-index-pattern.tsx
@@ -0,0 +1,199 @@
+import React from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { withGuardAsync } from '../../../../common/hocs';
+import { getSavedObjects } from '../../../../../kibana-services';
+import { SavedObject } from '../../../../../react-services';
+import { NOT_TIME_FIELD_NAME_INDEX_PATTERN } from '../../../../../../common/constants';
+import { EuiButton, EuiEmptyPrompt, EuiLink } from '@elastic/eui';
+import { webDocumentationLink } from '../../../../../../common/services/web_documentation';
+import { vulnerabilityDetection } from '../../../../../utils/applications';
+import { LoadingSpinnerDataSource } from '../../../../common/loading/loading-spinner-data-source';
+import NavigationService from '../../../../../react-services/navigation-service';
+
+const INDEX_PATTERN_CREATION_NO_INDEX = 'INDEX_PATTERN_CREATION_NO_INDEX';
+
+async function checkExistenceIndexPattern(indexPatternID: string) {
+  return await getSavedObjects().client.get('index-pattern', indexPatternID);
+}
+
+async function checkExistenceIndices(indexPatternId: string) {
+  try {
+    const fields = await SavedObject.getIndicesFields(indexPatternId);
+    return { exist: true, fields };
+  } catch (error) {
+    return { exist: false };
+  }
+}
+
+async function createIndexPattern(indexPattern, fields: any) {
+  try {
+    await SavedObject.createSavedObject(
+      'index-pattern',
+      indexPattern,
+      {
+        attributes: {
+          title: indexPattern,
+          timeFieldName: NOT_TIME_FIELD_NAME_INDEX_PATTERN,
+        },
+      },
+      fields,
+    );
+    await SavedObject.validateIndexPatternSavedObjectCanBeFound([indexPattern]);
+  } catch (error) {
+    return { error: error.message };
+  }
+}
+
+async function createDashboard() {
+  const dashboardId = '6e71e2a1-89ca-49c9-b9e6-1f2aa404903b';
+
+  const dashboardData = {
+    title: 'Agents and Vuls',
+    description: 'Revenue dashboard',
+    panelsJSON:
+      '[{"version":"2.13.0","gridData":{"x":0,"y":0,"w":24,"h":15,"i":"5db1d75d-f680-4869-a0e8-0f2b8b05b99c"},"panelIndex":"5db1d75d-f680-4869-a0e8-0f2b8b05b99c","embeddableConfig":{},"panelRefName":"panel_0"}]',
+    optionsJSON: '{"hidePanelTitles":false,"useMargins":true}',
+    version: 1,
+    timeRestore: true,
+    kibanaSavedObjectMeta: {
+      searchSourceJSON: '{"query":{"language":"kuery","query":""},"filter":[]}',
+    },
+    references: [
+      {
+        id: '37cc8650-b882-11e8-a6d9-e546fe2bba5f',
+        name: 'panel_0',
+        type: 'visualization',
+      },
+    ],
+  };
+
+  try {
+    // Create dashboard
+    const result = await SavedObject.createSavedObject(
+      'dashboard',
+      dashboardId,
+      dashboardData,
+    );
+    console.log('Dashboard created:', result);
+  } catch (error) {
+    console.error('Error creating dashboard:', error);
+  }
+}
+
+createDashboard();
+
+export async function validateVulnerabilitiesStateDataSources({
+  vulnerabilitiesStatesindexPatternID: indexPatternID,
+}) {
+  try {
+    // Check the existence of related index pattern
+    const existIndexPattern = await checkExistenceIndexPattern(indexPatternID);
+    let indexPattern = existIndexPattern;
+
+    // If the idnex pattern does not exist, then check the existence of index
+    if (existIndexPattern?.error?.statusCode === 404) {
+      // Check the existence of indices
+      const { exist, fields } = await checkExistenceIndices(indexPatternID);
+
+      if (!exist) {
+        return {
+          ok: true,
+          data: {
+            error: {
+              title:
+                'Vulnerability detection seems to be disabled or has a problem',
+              type: INDEX_PATTERN_CREATION_NO_INDEX,
+            },
+          },
+        };
+      }
+      // If the some index match the index pattern, then create the index pattern
+      const resultCreateIndexPattern = await createIndexPattern(
+        indexPatternID,
+        fields,
+      );
+      if (resultCreateIndexPattern?.error) {
+        return {
+          ok: true,
+          data: {
+            error: {
+              title: 'There was a problem creating the index pattern',
+              message: resultCreateIndexPattern?.error,
+            },
+          },
+        };
+      }
+      /* WORKAROUND: Redirect to the root of Vulnerabilities Detection application that should
+        redirects to the Dashboard tab. We want to redirect to this view, because we need the
+        component is visible (visualizations) to ensure the process that defines the filters for the
+        Events tab is run when the Dashboard component is unmounted. This workaround solves a
+        problem in the Events tabs related there are no implicit filters when accessing if the HOC
+        that protect the view is passed.
+      */
+      NavigationService.getInstance().navigateToApp(vulnerabilityDetection.id);
+    }
+    return {
+      ok: false,
+      data: { indexPattern },
+    };
+  } catch (error) {
+    return {
+      ok: true,
+      data: {
+        error: { title: 'There was a problem', message: error.message },
+      },
+    };
+  }
+}
+
+const errorPromptBody = {
+  INDEX_PATTERN_CREATION_NO_INDEX: (
+    <p>
+      Please check the cluster status. Also, you can check the{' '}
+      <EuiLink
+        href={webDocumentationLink(
+          'user-manual/capabilities/vulnerability-detection/index.html',
+        )}
+        target='_blank'
+        rel='noopener noreferrer'
+        external
+      >
+        vulnerability detection documentation.
+      </EuiLink>
+    </p>
+  ),
+};
+
+export const PromptCheckIndex = props => {
+  const { refresh } = props;
+  const { title, message } = props?.error;
+  const body = errorPromptBody?.[props?.error?.type] || <p>{message}</p>;
+
+  return (
+    <EuiEmptyPrompt
+      iconType='alert'
+      title={<h2>{title}</h2>}
+      body={body}
+      actions={
+        <EuiButton color='primary' fill onClick={refresh}>
+          Refresh
+        </EuiButton>
+      }
+    />
+  );
+};
+
+const mapStateToProps = state => ({
+  vulnerabilitiesStatesindexPatternID:
+    state.appConfig.data['vulnerabilities.pattern'],
+});
+
+export const withVulnerabilitiesStateDataSource = compose(
+  connect(mapStateToProps),
+  withGuardAsync(
+    validateVulnerabilitiesStateDataSources,
+    ({ error, check }) => <PromptCheckIndex error={error} refresh={check} />,
+    () => <LoadingSpinnerDataSource />,
+  ),
+);
