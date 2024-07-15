68e78f046b39ae1a7938c82d93c97829ba9c6cb4
chantal.kelm <chantal.kelm@wazuh.com>
2024-07-15 13:17:07 -0300

    show dashboard in vulnerabilities

--- plugins/main/public/components/common/modules/modules-defaults.tsx
+++ plugins/main/public/components/common/modules/modules-defaults.tsx
@@ -19,7 +19,9 @@ import { ButtonModuleGenerateReport } from '../modules/buttons';
 import { OfficePanel } from '../../overview/office/panel';
 import { GitHubPanel } from '../../overview/github/panel';
 import { InventoryVuls } from '../../overview/vulnerabilities';
-import { DashboardVuls } from '../../overview/poc-dashboards-by-reference/dashboards/dashboard';
+// import { DashboardVuls } from '../../overview/poc-dashboards-by-reference/dashboards/dashboard';
+import { DashboardVuls } from '../../overview/poc/dashboards/overview/dashboard';
+
 import { DashboardMITRE } from '../../overview/mitre/dashboard';
 import { withModuleNotForAgent } from '../hocs';
 import {
