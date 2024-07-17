73eaa4e94d7d0b0ac7ecf9babb7fbd67f04582c3
jbiset <julio.biset@wazuh.com>
2024-07-17 16:39:02 -0300

    Changed permalink render in Events tab in Virustotal

--- plugins/main/public/components/overview/virustotal/events/virustotal-columns.tsx
+++ plugins/main/public/components/overview/virustotal/events/virustotal-columns.tsx
@@ -1,4 +1,6 @@
 import { tDataGridColumn } from '../../../common/data-grid';
+import React from 'react';
+import { EuiLink } from '@elastic/eui';
 
 export const virustotalColumns: tDataGridColumn[] = [
   {
@@ -12,6 +14,11 @@ export const virustotalColumns: tDataGridColumn[] = [
   },
   {
     id: 'data.virustotal.permalink',
+    render: value => (
+      <EuiLink href={value} target='_blank' external>
+        {value}
+      </EuiLink>
+    ),
   },
   {
     id: 'data.virustotal.malicious',
