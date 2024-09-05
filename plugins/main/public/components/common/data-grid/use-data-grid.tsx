13059bcc87d7c1fc73720dc37870ab30d7805b41
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2024-09-05 12:58:17 +0200

    fix: use the field formatter to render the fields in the WzDataGrid

--- plugins/main/public/components/common/data-grid/use-data-grid.tsx
+++ plugins/main/public/components/common/data-grid/use-data-grid.tsx
@@ -13,6 +13,7 @@ import {
   parseColumns,
 } from './data-grid-service';
 import { IndexPattern } from '../../../../../../src/plugins/data/common';
+import dompurify from 'dompurify';
 
 const MAX_ENTRIES_PER_QUERY = 10000;
 const DEFAULT_PAGE_SIZE_OPTIONS = [20, 50, 100];
@@ -129,7 +130,18 @@ export const useDataGrid = (props: tDataGridProps): EuiDataGridProps => {
         );
       }
 
-      return fieldFormatted;
+      // Format the value using the field formatter
+      // https://github.com/opensearch-project/OpenSearch-Dashboards/blob/2.16.0/src/plugins/discover/public/application/components/data_grid/data_grid_table_cell_value.tsx#L80-L89
+      const formattedValue = indexPattern.formatField(rows[rowIndex], columnId);
+      if (typeof formattedValue === 'undefined') {
+        return <span>-</span>;
+      } else {
+        const sanitizedCellValue = dompurify.sanitize(formattedValue);
+        return (
+          // eslint-disable-next-line react/no-danger
+          <span dangerouslySetInnerHTML={{ __html: sanitizedCellValue }} />
+        );
+      }
     }
     return null;
   };
