bb57f6ba35344bddaee2c8b4a35b3be2c5ff7b47
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2024-09-10 11:31:54 +0200

    fix: unsupported module definition by agent OS
    - Fix vulnerabilities detection unsupported component by agent OS
    - Replace the varible used to define the unsupported component for
      Docker
    - Add unsupported module Office 365 to SunOS and "Others" OS
    - Fix bug on the available options on the agent configuration report

--- plugins/main/public/utils/components-os-support.js
+++ plugins/main/public/utils/components-os-support.js
@@ -10,11 +10,29 @@
  * Find more information about this on the LICENSE file.
  */
 import { WAZUH_AGENTS_OS_TYPE, WAZUH_MODULES_ID } from '../../common/constants';
+import { vulnerabilityDetection, docker, office365 } from './applications';
 
 export const UnsupportedComponents = {
   [WAZUH_AGENTS_OS_TYPE.LINUX]: [],
-  [WAZUH_AGENTS_OS_TYPE.WINDOWS]: [WAZUH_MODULES_ID.AUDITING, WAZUH_MODULES_ID.DOCKER, WAZUH_MODULES_ID.OPEN_SCAP],
-  [WAZUH_AGENTS_OS_TYPE.DARWIN]: [WAZUH_MODULES_ID.AUDITING, WAZUH_MODULES_ID.DOCKER, WAZUH_MODULES_ID.OPEN_SCAP],
-  [WAZUH_AGENTS_OS_TYPE.SUNOS]: [WAZUH_MODULES_ID.VULNERABILITIES],
-  [WAZUH_AGENTS_OS_TYPE.OTHERS]: [WAZUH_MODULES_ID.AUDITING, WAZUH_MODULES_ID.DOCKER, WAZUH_MODULES_ID.OPEN_SCAP, WAZUH_MODULES_ID.VULNERABILITIES]
+  [WAZUH_AGENTS_OS_TYPE.WINDOWS]: [
+    WAZUH_MODULES_ID.AUDITING,
+    docker.id,
+    WAZUH_MODULES_ID.OPEN_SCAP,
+  ],
+  [WAZUH_AGENTS_OS_TYPE.DARWIN]: [
+    WAZUH_MODULES_ID.AUDITING,
+    docker.id,
+    WAZUH_MODULES_ID.OPEN_SCAP,
+  ],
+  [WAZUH_AGENTS_OS_TYPE.SUNOS]: [vulnerabilityDetection.id, office365.id],
+  [WAZUH_AGENTS_OS_TYPE.OTHERS]: [
+    WAZUH_MODULES_ID.AUDITING,
+    docker.id,
+    WAZUH_MODULES_ID.OPEN_SCAP,
+    vulnerabilityDetection.id,
+    office365.id,
+  ],
 };
+
+// Office 365 is only compatible with Linux, macOS and Windows
+// https://documentation.wazuh.com/current/user-manual/reference/ossec-conf/office365-module.html#office365
