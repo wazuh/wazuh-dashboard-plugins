ce8069e072223c90bd1b6ec4e9c94c2d22b7a6f6
Guido Modarelli <guidomodarelli25@gmail.com>
2024-09-19 19:19:01 -0300

    Add tests for rule-generator and regulatory-compliance

---
+++ plugins/main/server/lib/generate-alerts/sample-data/regulatory-compliance.test.js
@@ -0,0 +1,7 @@
+const { FREQUENCY } = require('./regulatory-compliance');
+
+describe('regulatory-compliance', () => {
+  it('should_verify_all_values_in_frequency_are_integers', () => {
+    expect(FREQUENCY.every((value) => Number.isInteger(value))).toBe(true);
+  });
+});
