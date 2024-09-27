686150b4e1533c27c570ace3d9186bce6ee7b7f8
JuanGarriuz <juan.garcia@wazuh.com>
2024-09-27 16:06:42 +0200

    Fixed no-agent spawn

---
+++ plugins/main/public/components/common/hocs/withAgentSync.tsx
@@ -0,0 +1,26 @@
+/*
+ * Wazuh app - React HOCs handles rendering errors
+ * Copyright (C) 2015-2022 Wazuh, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Find more information about this on the LICENSE file.
+ */
+
+import React, { useState, useEffect } from 'react';
+import { PinnedAgentManager } from '../../wz-agent-selector/wz-agent-selector-service';
+import { withGuardAsync } from './withGuard';
+
+export const withAgentSync = WrappedComponent => props => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const pinnedAgentManager = new PinnedAgentManager();
+    pinnedAgentManager.syncPinnedAgentSources().finally(() => {
+      setLoading(false);
+    });
+  }, []);
+  return loading ? null : <WrappedComponent {...props}></WrappedComponent>;
+};
