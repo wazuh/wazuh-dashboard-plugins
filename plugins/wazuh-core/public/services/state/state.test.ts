bf5a9ab43747dd822da9c9178c38526a408d211c
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2024-09-30 16:38:35 +0200

    feat(state): add tests

---
+++ plugins/wazuh-core/public/services/state/state.test.ts
@@ -0,0 +1,61 @@
+import { CoreState } from './state';
+
+const noop = () => {};
+const logger = {
+  info: noop,
+  warn: noop,
+  error: noop,
+  debug: noop,
+};
+
+describe('State', () => {
+  it('Throw error accessing to non-existent state container', () => {
+    const state = new CoreState(logger);
+    expect(() => {
+      state.get('no_existent_state_container');
+    }).toThrow(
+      'State container [no_existent_state_container] does not exist. Did you forget to register it?',
+    );
+
+    expect(() => {
+      state.set('no_existent_state_container', {});
+    }).toThrow(
+      'State container [no_existent_state_container] does not exist. Did you forget to register it?',
+    );
+
+    expect(() => {
+      state.remove('no_existent_state_container');
+    }).toThrow(
+      'State container [no_existent_state_container] does not exist. Did you forget to register it?',
+    );
+
+    expect(() => {
+      state.subscribe('no_existent_state_container', () => {});
+    }).toThrow(
+      'State container [no_existent_state_container] does not exist. Did you forget to register it?',
+    );
+  });
+
+  it('Register a state container, get value, set new value and get new value', () => {
+    const state = new CoreState(logger);
+
+    const subscribe = jest.fn();
+    state.register('state_container', {
+      _value: true, // mock state. This is not exist in the StateContainer type
+      get() {
+        return this._value;
+      },
+      set() {
+        this._value = false;
+      },
+      remove: () => {},
+      subscribe: subscribe,
+    });
+
+    expect(state.get('state_container')).toBe(true);
+
+    state.set('state_container', false);
+
+    expect(state.get('state_container')).toBe(false);
+  });
+});
