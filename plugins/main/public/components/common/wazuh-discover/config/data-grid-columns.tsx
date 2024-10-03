d8e1dc7c11415c508e055704269ff3b2522b1a02
Guido Modarelli <guidomodarelli25@gmail.com>
2024-10-03 09:05:55 -0300

    Change initial widths for data grid columns

--- plugins/main/public/components/common/wazuh-discover/config/data-grid-columns.tsx
+++ plugins/main/public/components/common/wazuh-discover/config/data-grid-columns.tsx
@@ -6,17 +6,21 @@ export const threatHuntingColumns: tDataGridColumn[] = [
     id: 'timestamp',
     isSortable: true,
     defaultSortDirection: 'desc',
+    initialWidth: 203.8,
   },
   {
     id: 'agent.name',
+    initialWidth: 318.05,
   },
   {
     id: 'rule.description',
   },
   {
     id: 'rule.level',
+    initialWidth: 93.05,
   },
   {
     id: 'rule.id',
+    initialWidth: 93.05,
   },
 ];
