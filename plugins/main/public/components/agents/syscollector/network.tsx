6700d65b1b13b04f28d94f4bf30a051b2099e961
Guido Modarelli <guidomodarelli25@gmail.com>
2024-10-18 15:11:05 -0300

    feat: add Network component for displaying agent network details, including interfaces, ports, and settings in a structured layout

---
+++ plugins/main/public/components/agents/syscollector/network.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+import { NetworkInterfacesTable, NetworkPortsTable, NetworkSettingsTable } from './components';
+import { Agent } from '../../endpoints-summary/types';
+
+interface NetworkProps {
+  agent: Agent;
+  soPlatform?: string;
+}
+
+const Network = ({ agent, soPlatform }: NetworkProps) => {
+  return (
+    <EuiFlexGroup direction='column' gutterSize='s'>
+      <EuiFlexGroup gutterSize='s'>
+        <EuiFlexItem grow={2}>
+          <NetworkInterfacesTable agent={agent} />
+        </EuiFlexItem>
+        <EuiFlexItem grow={2}>
+          <NetworkPortsTable agent={agent} soPlatform={soPlatform} />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+      <EuiFlexItem style={{ marginInline: 0, marginTop: 8 }}>
+        <NetworkSettingsTable agent={agent} />
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+};
+
+export default Network;
