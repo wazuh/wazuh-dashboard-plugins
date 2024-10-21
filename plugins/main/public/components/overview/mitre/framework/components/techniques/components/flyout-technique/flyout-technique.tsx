2c92b06d2e8e6aa88be82067fe9548f4248a8591
chantal.kelm <chantal.kelm@wazuh.com>
2024-10-21 16:25:23 -0300

    add a validation to filterParams and filterParams.filters

--- plugins/main/public/components/overview/mitre/framework/components/techniques/components/flyout-technique/flyout-technique.tsx
+++ plugins/main/public/components/overview/mitre/framework/components/techniques/components/flyout-technique/flyout-technique.tsx
@@ -397,7 +397,7 @@ export const FlyoutTechnique = (props: tFlyoutTechniqueProps) => {
             DataSource={PatternDataSource}
             tableColumns={getDiscoverColumns()}
             filterManager={filterManager}
-            initialFetchFilters={filterParams.filters}
+            initialFetchFilters={filterParams?.filters || []}
             expandedRowComponent={expandedRow}
           />
         </EuiAccordion>
