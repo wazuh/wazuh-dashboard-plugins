5e639aba06db861214c776862369086506837f93
Guido Modarelli <guidomodarelli25@gmail.com>
2024-10-22 16:27:49 -0300

    test: improve onFilterCellActions tests with clearer descriptions for filters and added date filter cases in data grid component

--- plugins/main/public/components/common/data-grid/filter-cell-actions.test.ts
+++ plugins/main/public/components/common/data-grid/filter-cell-actions.test.ts
@@ -49,7 +49,7 @@ describe('onFilterCellActions', () => {
     ]);
   });
 
-  it('should add single filter with is not operator for key and number value (3)', () => {
+  it('should add single filter with is not operator for given key (rule.level) and number value (3)', () => {
     const key = 'rule.level';
     const value = 3;
     const operation = FILTER_OPERATOR.IS_NOT;
@@ -65,7 +65,7 @@ describe('onFilterCellActions', () => {
     ]);
   });
 
-  it('should add single filter with key (rule.id) and string value (19003)', () => {
+  it('should add single filter with given key (rule.id) and string value (19003)', () => {
     const key = 'rule.id';
     const value = '19003';
     const operation = FILTER_OPERATOR.IS;
@@ -81,9 +81,41 @@ describe('onFilterCellActions', () => {
     ]);
   });
 
-  it('should add single filter with key (rule.id) and string value (19003)', () => {
+  it('should add single filter with is not operator for given key (rule.id) and string value (19003)', () => {
     const key = 'rule.id';
     const value = '19003';
+    const operation = FILTER_OPERATOR.IS_NOT;
+
+    onFilterCellActions(INDEX_PATTERN_ID, [], setFilters)(
+      key,
+      operation,
+      value,
+    );
+
+    expect(setFilters).toHaveBeenCalledWith([
+      buildFilter(key, operation, value),
+    ]);
+  });
+
+  it('should add single filter with given key (timestamp) and date value (2024-10-19T18:44:40.487Z)', () => {
+    const key = 'timestamp';
+    const value = '2024-10-19T18:44:40.487Z';
+    const operation = FILTER_OPERATOR.IS;
+
+    onFilterCellActions(INDEX_PATTERN_ID, [], setFilters)(
+      key,
+      operation,
+      value,
+    );
+
+    expect(setFilters).toHaveBeenCalledWith([
+      buildFilter(key, operation, value),
+    ]);
+  });
+
+  it('should add single filter is not operator for given key (timestamp) and date value (2024-10-19T18:44:40.487Z)', () => {
+    const key = 'timestamp';
+    const value = '2024-10-19T18:44:40.487Z';
     const operation = FILTER_OPERATOR.IS_NOT;
 
     onFilterCellActions(INDEX_PATTERN_ID, [], setFilters)(
