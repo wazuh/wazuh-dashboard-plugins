6d3307f79a6998fffafb5c060b17df32d59be020
Guido Modarelli <guidomodarelli25@gmail.com>
2024-10-22 15:30:15 -0300

    fix: refactor onFilterCellActions to use isNullish for improved handling of FILTER_OPERATOR and value inputs in data grid service

--- plugins/main/public/components/common/data-grid/data-grid-service.ts
+++ plugins/main/public/components/common/data-grid/data-grid-service.ts
@@ -14,6 +14,7 @@ import {
   FILTER_OPERATOR,
   PatternDataSourceFilterManager,
 } from '../data-source/pattern/pattern-data-source-filter-manager';
+import { isNullish } from '../util';
 
 type ParseData<T> =
   | {
@@ -197,29 +198,47 @@ export const onFilterCellActions = (
   filters: Filter[],
   setFilters: (filters: Filter[]) => void,
 ) => {
-  return (field: string, operation: FILTER_OPERATOR, value?: any) => {
-    if (!value) return;
+  return (
+    field: string,
+    operation:
+      | FILTER_OPERATOR.EXISTS
+      | FILTER_OPERATOR.IS
+      | FILTER_OPERATOR.IS_NOT,
+    values?: string | string[],
+  ) => {
+    // https://github.com/opensearch-project/OpenSearch-Dashboards/blob/4e34a7a5141d089f6c341a535be5a7ba2737d965/src/plugins/data/public/query/filter_manager/lib/generate_filters.ts#L89
+    const negated = [FILTER_OPERATOR.IS_NOT].includes(operation);
+    let _operation: FILTER_OPERATOR = operation;
+    if (isNullish(values) && ![FILTER_OPERATOR.EXISTS].includes(operation)) {
+      if (negated) {
+        _operation = FILTER_OPERATOR.EXISTS;
+      } else {
+        _operation = FILTER_OPERATOR.DOES_NOT_EXISTS;
+      }
+    }
+
     const newFilters: Filter[] = [];
-    if (Array.isArray(value)) {
-      value.forEach(item => {
+    if (isNullish(values)) {
+      newFilters.push(
+        PatternDataSourceFilterManager.createFilter(
+          _operation,
+          field,
+          values,
+          indexPatternId,
+        ),
+      );
+    } else {
+      values = Array.isArray(values) ? values : [values];
+      values.forEach(item => {
         newFilters.push(
           PatternDataSourceFilterManager.createFilter(
-            operation,
+            _operation,
             field,
             item,
             indexPatternId,
           ),
         );
       });
-    } else {
-      newFilters.push(
-        PatternDataSourceFilterManager.createFilter(
-          operation,
-          field,
-          value,
-          indexPatternId,
-        ),
-      );
     }
     setFilters([...filters, ...newFilters]);
   };
