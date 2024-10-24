6e1a600bbd4d3f53f4bbcd552f7860119fa7ca78
Guido Modarelli <guidomodarelli25@gmail.com>
2024-10-24 12:19:13 -0300

    test(agent-stats): add tests to verify absence of generate report button and agent info ribbon in AgentStats component

--- plugins/main/public/components/agents/stats/agent-stats.test.tsx
+++ plugins/main/public/components/agents/stats/agent-stats.test.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import { AgentStats } from './agent-stats';
 import { queryDataTestAttr } from '../../../../test/public/query-attr';
 
-const AGENT = {
-  id: '10101',
-};
+jest.mock('../../../react-services', () => ({
+  WzRequest: {
+    apiReq: jest.fn().mockResolvedValue(undefined),
+  },
+}));
 
-describe('AgentStats', () => {});
+describe('AgentStats', () => {
+  it('should dont render generate report button', async () => {
+    await act(async () => {
+      const { container } = render(<AgentStats agent={{ id: '000' }} />);
+
+      const generateReportButton = container.querySelector(
+        queryDataTestAttr('generate-report-button'),
+      );
+
+      expect(generateReportButton).toBeFalsy();
+    });
+  });
+
+  it('should dont render agent info ribbon', async () => {
+    await act(async () => {
+      const { container } = render(<AgentStats agent={{ id: '000' }} />);
+
+      const agentInfoRibbon = container.querySelector(
+        queryDataTestAttr('agent-info'),
+      );
+      expect(agentInfoRibbon).toBeFalsy();
+    });
+  });
+});
