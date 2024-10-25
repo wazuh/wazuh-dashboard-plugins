c9bab9fe225509cad9a8c52c896f9719d2bff6fd
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2024-10-25 12:31:21 +0200

    feat(bump): add ability to edit the imposter specification file reference on bump script of main plugin

--- plugins/main/scripts/release/bump.js
+++ plugins/main/scripts/release/bump.js
@@ -53,6 +53,47 @@ const cli = require('../../../../scripts/lib/cli/cli')(
   ],
 );
 
+function updateImposterSpecificationReference(configuration, logger) {
+  try {
+    logger.debug('Editing imposter specification reference');
+    const fs = require('fs');
+    const path = require('path');
+    const specificationFile = path.join(
+      __dirname,
+      '../../../..',
+      'docker/imposter/wazuh-config.yml',
+    );
+
+    logger.debug(`Reading ${specificationFile} file`);
+    const content = fs.readFileSync(specificationFile, 'utf8');
+
+    const { version } = configuration;
+
+    if (!version) {
+      throw new Error('Version is not specified.');
+    }
+
+    // specFile: https://raw.githubusercontent.com/wazuh/wazuh/<BRANCH_VERSION>/api/api/spec/spec.yaml
+    const updatedContent = content.replace(
+      /specFile:\s+\S+/m,
+      `specFile: https://raw.githubusercontent.com/wazuh/wazuh/${version}/api/api/spec/spec.yaml`,
+    );
+
+    if (content !== updatedContent) {
+      logger.debug(
+        `Updating [${specificationFile}] imposter specification file with latest changes`,
+      );
+      fs.writeFileSync(specificationFile, updatedContent, 'utf8');
+      logger.info(`${specificationFile} file has been updated`);
+    } else {
+      logger.debug(`Nothing to change in ${specificationFile} file`);
+    }
+  } catch (error) {
+    logger.error(`Error editing the specification file: ${error.message}`);
+    process.exit(1);
+  }
+}
+
 function run(configuration, logger) {
   let localConfiguration = {
     ...configuration,
@@ -90,6 +131,8 @@ function run(configuration, logger) {
     );
     process.exit(1);
   }
+
+  updateImposterSpecificationReference(localConfiguration, logger);
 }
 
 const logger = require('../../../../scripts/release/lib/logger').create([
