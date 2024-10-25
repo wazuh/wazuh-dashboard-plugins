93d2eb5e4301b0aeabb348ed5fa2f02177357a3e
Guido Modarelli <guidomodarelli25@gmail.com>
2024-10-24 21:47:33 -0300

    feat(tests): add ribbon rendering tests in AgentStats to ensure accurate display of status and metrics attributes

--- plugins/main/public/components/agents/stats/agent-stats.test.tsx
+++ plugins/main/public/components/agents/stats/agent-stats.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, act } from '@testing-library/react';
 import { AgentStats } from './agent-stats';
 import { queryDataTestAttr } from '../../../../test/public/query-attr';
+import { CSS } from '../../../../test/utils/CSS';
 
 jest.mock('../../../react-services', () => ({
   WzRequest: {
@@ -18,6 +19,50 @@ describe('AgentStats', () => {
         queryDataTestAttr('agent-info'),
       );
       expect(agentInfoRibbon).toBeFalsy();
+    });
+  });
+
+  it('should render stats info ribbon', async () => {
+    await act(async () => {
+      const { container } = render(<AgentStats agent={{ id: '000' }} />);
+
+      expect(
+        container.querySelector(queryDataTestAttr('ribbon-item-status')),
+      ).toBeTruthy();
+
+      expect(
+        container.querySelector(
+          queryDataTestAttr('ribbon-item-buffer_enabled'),
+        ),
+      ).toBeTruthy();
+
+      expect(
+        container.querySelector(queryDataTestAttr('ribbon-item-msg_buffer')),
+      ).toBeTruthy();
+
+      expect(
+        container.querySelector(queryDataTestAttr('ribbon-item-msg_count')),
+      ).toBeTruthy();
+
+      expect(
+        container.querySelector(queryDataTestAttr('ribbon-item-msg_sent')),
+      ).toBeTruthy();
+
+      expect(
+        container.querySelector(queryDataTestAttr('ribbon-item-last_ack')),
+      ).toBeTruthy();
+
+      expect(
+        container.querySelector(
+          queryDataTestAttr('ribbon-item-last_keepalive'),
+        ),
+      ).toBeTruthy();
+
+      expect(
+        container.querySelectorAll(
+          queryDataTestAttr('ribbon-item-', CSS.Attribute.Substring),
+        ).length,
+      ).toHaveLength(7);
     });
   });
 });
