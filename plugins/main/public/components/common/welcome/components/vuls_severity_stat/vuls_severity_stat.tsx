f9c69ac0fc5319c7bc05a1db77b3186dd74b8564
Maximiliano Ibarra <6089438+Machi3mfl@users.noreply.github.com>
2024-10-28 11:58:32 -0300

    Remove unused imports
    Co-authored-by: Guido Modarelli <38738725+guidomodarelli@users.noreply.github.com>

--- plugins/main/public/components/common/welcome/components/vuls_severity_stat/vuls_severity_stat.tsx
+++ plugins/main/public/components/common/welcome/components/vuls_severity_stat/vuls_severity_stat.tsx
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { EuiStat, EuiStatProps } from '@elastic/eui';
-import { RedirectAppLinks } from '../../../../../../../../src/plugins/opensearch_dashboards_react/public';
-import { tUseDataSourceLoadedReturns } from '../../../data-source/hooks/use-data-source';
 
 type VulsSeverityStatProps = {
   value: string;
