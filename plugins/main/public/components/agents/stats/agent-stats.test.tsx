b5d65abd9d8e49bc641901c453d5e69ab28ca272
Guido Modarelli <guidomodarelli25@gmail.com>
2024-11-01 00:03:56 -0300

    test: improve AgentStats tests to ensure correct column structure, titles, and CSV filename updates on rendering changes

--- plugins/main/public/components/agents/stats/agent-stats.test.tsx
+++ plugins/main/public/components/agents/stats/agent-stats.test.tsx
@@ -4,8 +4,13 @@ import { AgentStats } from './agent-stats';
 import { queryDataTestAttr } from '../../../../test/public/query-attr';
 import { CSS } from '../../../../test/utils/CSS';
 import { WzRequest } from '../../../react-services';
+import { AgentStatTable } from './table';
+
+const agent000 = '000';
+const agent001 = '001';
 
 const apiReqMock = WzRequest.apiReq as jest.Mock;
+const AgentStatTableMock = AgentStatTable as jest.Mock;
 
 jest.mock('../../../react-services', () => ({
   WzRequest: {
@@ -107,42 +112,150 @@ describe('AgentStats', () => {
   });
 
   it('should call api with correct agent ids and endpoints when changing agent', async () => {
-    apiReqMock.mockReset();
-    const agentId = '000';
-    const agentId2 = '001';
+    apiReqMock.mockClear();
 
     let rerender: RenderResult['rerender'];
 
     await act(async () => {
-      ({ rerender } = render(<AgentStats agent={{ id: agentId }} />));
+      ({ rerender } = render(<AgentStats agent={{ id: agent000 }} />));
     });
 
     expect(apiReqMock).toHaveBeenCalledTimes(2);
     expect(apiReqMock.mock.calls[0]).toEqual([
       'GET',
-      `/agents/${agentId}/stats/logcollector`,
+      `/agents/${agent000}/stats/logcollector`,
       {},
     ]);
     expect(apiReqMock.mock.calls[1]).toEqual([
       'GET',
-      `/agents/${agentId}/stats/agent`,
+      `/agents/${agent000}/stats/agent`,
       {},
     ]);
+
+    apiReqMock.mockClear();
 
     await act(async () => {
-      rerender(<AgentStats agent={{ id: agentId2 }} />);
+      rerender(<AgentStats agent={{ id: agent001 }} />);
     });
 
-    expect(apiReqMock).toHaveBeenCalledTimes(4);
-    expect(apiReqMock.mock.calls[2]).toEqual([
+    expect(apiReqMock).toHaveBeenCalledTimes(2);
+    expect(apiReqMock.mock.calls[0]).toEqual([
       'GET',
-      `/agents/${agentId2}/stats/logcollector`,
+      `/agents/${agent001}/stats/logcollector`,
       {},
     ]);
-    expect(apiReqMock.mock.calls[3]).toEqual([
+    expect(apiReqMock.mock.calls[1]).toEqual([
       'GET',
-      `/agents/${agentId2}/stats/agent`,
+      `/agents/${agent001}/stats/agent`,
       {},
     ]);
+  });
+
+  it('should maintain column structure across multiple renders', async () => {
+    AgentStatTableMock.mockClear();
+
+    const mockColumns = [
+      {
+        field: 'location',
+        name: 'Location',
+        sortable: true,
+      },
+      {
+        field: 'events',
+        name: 'Events',
+        sortable: true,
+      },
+      {
+        field: 'bytes',
+        name: 'Bytes',
+        sortable: true,
+      },
+    ];
+
+    let rerender: RenderResult['rerender'];
+
+    await act(async () => {
+      ({ rerender } = render(<AgentStats agent={{ id: agent000 }} />));
+    });
+
+    expect(AgentStatTableMock.mock.calls[0][0].columns).toEqual(mockColumns);
+    expect(AgentStatTableMock.mock.calls[1][0].columns).toEqual(mockColumns);
+
+    AgentStatTableMock.mockClear();
+
+    await act(async () => {
+      rerender(<AgentStats agent={{ id: agent001 }} />);
+    });
+
+    expect(AgentStatTableMock.mock.calls[0][0].columns).toEqual(mockColumns);
+    expect(AgentStatTableMock.mock.calls[1][0].columns).toEqual(mockColumns);
+  });
+
+  it('should apply correct titles after render and rerender', async () => {
+    AgentStatTableMock.mockClear();
+
+    const mockDataStatLogcollectorTitle = 'Global';
+    const mockDataStatAgentTitle = 'Interval';
+
+    let rerender: RenderResult['rerender'];
+
+    await act(async () => {
+      ({ rerender } = render(<AgentStats agent={{ id: agent000 }} />));
+    });
+
+    expect(AgentStatTableMock.mock.calls[0][0].title).toEqual(
+      mockDataStatLogcollectorTitle,
+    );
+    expect(AgentStatTableMock.mock.calls[1][0].title).toEqual(
+      mockDataStatAgentTitle,
+    );
+
+    AgentStatTableMock.mockClear();
+
+    await act(async () => {
+      rerender(<AgentStats agent={{ id: agent001 }} />);
+    });
+
+    expect(AgentStatTableMock.mock.calls[0][0].title).toEqual(
+      mockDataStatLogcollectorTitle,
+    );
+    expect(AgentStatTableMock.mock.calls[1][0].title).toEqual(
+      mockDataStatAgentTitle,
+    );
+  });
+
+  it('should update export csv filename correctly on rerender', async () => {
+    AgentStatTableMock.mockClear();
+
+    const mockExportCSVFilename = (
+      agent000: string,
+      suffix: 'global' | 'interval',
+    ) => `agent-stats-${agent000}-logcollector-${suffix}`;
+
+    let rerender: RenderResult['rerender'];
+
+    await act(async () => {
+      ({ rerender } = render(<AgentStats agent={{ id: agent000 }} />));
+    });
+
+    expect(AgentStatTableMock.mock.calls[0][0].exportCSVFilename).toEqual(
+      mockExportCSVFilename(agent000, 'global'),
+    );
+    expect(AgentStatTableMock.mock.calls[1][0].exportCSVFilename).toEqual(
+      mockExportCSVFilename(agent000, 'interval'),
+    );
+
+    AgentStatTableMock.mockClear();
+
+    await act(async () => {
+      rerender(<AgentStats agent={{ id: agent001 }} />);
+    });
+
+    expect(AgentStatTableMock.mock.calls[0][0].exportCSVFilename).toEqual(
+      mockExportCSVFilename(agent001, 'global'),
+    );
+    expect(AgentStatTableMock.mock.calls[1][0].exportCSVFilename).toEqual(
+      mockExportCSVFilename(agent001, 'interval'),
+    );
   });
 });
