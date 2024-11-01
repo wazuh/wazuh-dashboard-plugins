ead93b27a55db64b4a724e2f95423a376a816085
Guido Modarelli <guidomodarelli25@gmail.com>
2024-11-01 16:24:07 -0300

    refactor: add unit test for WindowsUpdatesTable to verify correct hotfixes endpoint when changing agents in syscollector component

---
+++ plugins/main/public/components/agents/syscollector/components/windows-updates-table.test.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { WindowsUpdatesTable } from './windows-updates-table';
+import { TableWzAPI } from '../../../common/tables';
+
+const AGENT_000 = '000';
+const AGENT_001 = '001';
+
+let TableWzAPIMock = TableWzAPI as jest.Mock;
+
+jest.mock('../../../common/tables', () => ({
+  TableWzAPI: jest.fn(() => <></>),
+}));
+
+jest.mock('../../../../react-services', () => ({
+  WzRequest: {
+    apiReq: jest.fn().mockResolvedValue({
+      data: {
+        data: {
+          affected_items: [],
+        },
+      },
+    }),
+  },
+}));
+
+describe('WindowsUpdatesTable', () => {
+  it('should render table with correct hotfixes endpoint for agent either when changing agent or not', async () => {
+    const { rerender } = render(
+      <WindowsUpdatesTable agent={{ id: AGENT_000 }} />,
+    );
+
+    expect(TableWzAPIMock.mock.calls[0][0].endpoint).toContain(
+      `/syscollector/${AGENT_000}/hotfixes`,
+    );
+
+    TableWzAPIMock.mockClear();
+
+    rerender(<WindowsUpdatesTable agent={{ id: AGENT_001 }} />);
+
+    expect(TableWzAPIMock.mock.calls[0][0].endpoint).toContain(
+      `/syscollector/${AGENT_001}/hotfixes `,
+    );
+  });
+});
