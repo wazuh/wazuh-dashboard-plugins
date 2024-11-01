30e08ccacd9da273d4ef82d23f1a8c31b3a0242c
Guido Modarelli <guidomodarelli25@gmail.com>
2024-11-01 16:31:00 -0300

    refactor: enhance WindowsUpdatesTable test to validate API requests for different agent IDs in syscollector component

--- plugins/main/public/components/agents/syscollector/components/windows-updates-table.test.tsx
+++ plugins/main/public/components/agents/syscollector/components/windows-updates-table.test.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { WindowsUpdatesTable } from './windows-updates-table';
 import { TableWzAPI } from '../../../common/tables';
+import { WzRequest } from '../../../../react-services';
 
 const AGENT_000 = '000';
 const AGENT_001 = '001';
 
 let TableWzAPIMock = TableWzAPI as jest.Mock;
+let apiReqMock = WzRequest.apiReq as jest.Mock;
 
 jest.mock('../../../common/tables', () => ({
-  TableWzAPI: jest.fn(() => <></>),
+  TableWzAPI: jest.fn(({ searchBarWQL }) => {
+    searchBarWQL.suggestions.value(undefined, { field: 'hotfix' });
+    return <></>;
+  }),
 }));
 
 jest.mock('../../../../react-services', () => ({
@@ -39,6 +44,26 @@ describe('WindowsUpdatesTable', () => {
     rerender(<WindowsUpdatesTable agent={{ id: AGENT_001 }} />);
 
     expect(TableWzAPIMock.mock.calls[0][0].endpoint).toContain(
+      `/syscollector/${AGENT_001}/hotfixes`,
+    );
+  });
+
+  it('should fetch syscollector data for given agent id either when changing agent or not', async () => {
+    const { rerender } = render(
+      <WindowsUpdatesTable agent={{ id: AGENT_000 }} />,
+    );
+
+    expect(apiReqMock.mock.calls[0][0]).toEqual('GET');
+    expect(apiReqMock.mock.calls[0][1]).toEqual(
+      `/syscollector/${AGENT_000}/hotfixes`,
+    );
+
+    apiReqMock.mockClear();
+
+    rerender(<WindowsUpdatesTable agent={{ id: AGENT_001 }} />);
+
+    expect(apiReqMock.mock.calls[0][0]).toEqual('GET');
+    expect(apiReqMock.mock.calls[0][1]).toEqual(
       `/syscollector/${AGENT_001}/hotfixes`,
     );
   });
