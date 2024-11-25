e2ecc3d49ebaedb11d2cc8f7129b4f2857d603a1
Maximiliano Ibarra <maximiliano.ibarra@wazuh.com>
2024-11-25 15:59:57 -0300

    Added wazuh configuration in wazuh_core plugin

--- plugins/wazuh-core/server/index.ts
+++ plugins/wazuh-core/server/index.ts
@@ -1,5 +1,6 @@
-import { PluginInitializerContext } from '../../../src/core/server';
+import { PluginConfigDescriptor, PluginInitializerContext } from '../../../src/core/server';
 import { WazuhCorePlugin } from './plugin';
+import { schema, TypeOf } from '@osd/config-schema';
 
 // This exports static code and TypeScript types,
 // as well as, OpenSearch Dashboards Platform `plugin()` initializer.
@@ -7,6 +8,33 @@ import { WazuhCorePlugin } from './plugin';
 export function plugin(initializerContext: PluginInitializerContext) {
   return new WazuhCorePlugin(initializerContext);
 }
+// Define the schema for a single host entry
+const hostSchema = schema.object({
+  url: schema.string(),
+  port: schema.number(),
+  username: schema.string(),
+  password: schema.string(),
+  run_as: schema.boolean()
+});
+
+// Define the schema for the hosts config with dynamic keys
+const hostsConfigSchema = schema.recordOf(schema.string(), hostSchema);
+
+
+export const configSchema = schema.object({
+  hosts: hostsConfigSchema
+});
+
+export type CorePluginConfigType = TypeOf<typeof configSchema>;
+
+
+export const config: PluginConfigDescriptor<CorePluginConfigType> = {
+  exposeToBrowser: {
+    hosts: true,
+  },
+  schema: configSchema,
+}
+
 
 export type { WazuhCorePluginSetup, WazuhCorePluginStart } from './types';
 export type { IConfigurationEnhanced } from './services/enhance-configuration';
