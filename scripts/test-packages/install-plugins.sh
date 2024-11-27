c665466fc6ca439f46aa569a2f02fbc5379f3ff5
Federico Rodriguez <federico.rodriguez@wazuh.com>
2024-11-27 18:22:12 +0100

    Remove set -e test

--- scripts/test-packages/install-plugins.sh
+++ scripts/test-packages/install-plugins.sh
@@ -3,6 +3,6 @@ set -e
 plugins=$(ls /tmp)
 for plugin in $plugins; do
   echo $plugin
-  /usr/share/opensearch-dashboards/bin/opensearch-dashboards-plugin install file:///tmpf/$plugin/$(ls /tmp/$plugin)
+  /usr/share/opensearch-dashboards/bin/opensearch-dashboards-plugin install file:///tmp/$plugin/$(ls /tmp/$plugin)
 done
 echo "All plugins installed successfully"
