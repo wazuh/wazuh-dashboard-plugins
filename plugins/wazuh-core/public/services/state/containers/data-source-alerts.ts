d5dd4bb4558b503bb498a80862088be6af968c87
Antonio <34042064+Desvelao@users.noreply.github.com>
2024-11-27 11:53:36 +0100

    Apply suggestions from code review
    Co-authored-by: Guido Modarelli <38738725+guidomodarelli@users.noreply.github.com>

--- plugins/wazuh-core/public/services/state/containers/data-source-alerts.ts
+++ plugins/wazuh-core/public/services/state/containers/data-source-alerts.ts
@@ -5,7 +5,7 @@ import { BehaviorSubject } from 'rxjs';
 export class DataSourceAlertsStateContainer implements StateContainer {
   private store: any;
   private storeKey: string = 'currentPattern';
-  updater$: BehaviorSubject;
+  updater$: BehaviorSubject<string>;
   constructor(private logger: ILogger, { store }) {
     this.store = store;
     this.updater$ = new BehaviorSubject(this.get());
