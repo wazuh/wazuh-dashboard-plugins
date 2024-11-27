aedff74c5f32929f4c51886974dca817aba61765
Ian Yenien Serrano <63758389+yenienserrano@users.noreply.github.com>
2024-11-27 20:12:09 +0100

    Test jest(list-editor): Add test to add and remove

--- plugins/main/public/controllers/management/components/management/cdblists/views/list-editor.test.tsx
+++ plugins/main/public/controllers/management/components/management/cdblists/views/list-editor.test.tsx
@@ -29,69 +29,31 @@ describe('WzListEditor', () => {
       value: '":testValue"',
     },
     {
-      key: 'key',
+      key: 'test1',
       value: '":key"',
     },
     {
-      key: '":key1"',
+      key: '":test1"',
       value: 'value1',
     },
     {
-      key: '"key2"',
+      key: '"test2"',
       value: '"value2"',
     },
     {
-      key: 'key3',
+      key: 'test3',
       value: '',
     },
     {
       key: '"a0:a0:a0:a0:a0:a0"',
       value: '',
     },
-    {
-      key: '"b1:b1:b1:b1:b1:b1"',
-      value: '"test:6"',
-    },
   ];
 
-  const cdblistErrors = [
-    {
-      key: '":key',
-      value: 'value',
-    },
-    {
-      key: 'key',
-      value: '":value',
-    },
-    {
-      key: 'key"key',
-      value: 'value',
-    },
-    {
-      key: 'key',
-      value: 'value"value',
-    },
-    {
-      key: 'key',
-      value: 'value"',
-    },
-    {
-      key: 'key',
-      value: '"value',
-    },
-    {
-      key: '"key"key"',
-      value: 'value',
-    },
-    {
-      key: 'key',
-      value: '"value"value"',
-    },
-    {
-      key: 'key:"key',
-      value: 'value',
-    },
-  ];
+  const messagesError = {
+    quotesError: 'Must start and end with quotes or have no quotes at all',
+    colonError: 'Must start and end with quotes when using colon',
+  };
 
   beforeEach(() => {
     const cdblistMap = cdblist.map(item => {
@@ -112,29 +74,109 @@ describe('WzListEditor', () => {
   it('should render the component', () => {
     cdblist.forEach(item => {
       expect(screen.getByText(item.key)).toBeInTheDocument();
-      if (!item.value === '') {
+      if (!(item.value === '')) {
         expect(screen.getByText(item.value)).toBeInTheDocument();
       }
       expect(screen.queryByText(`${item.key}:${item.value}`)).toBeFalsy();
     });
   });
 
-  it('shoudl render the message when try to add invalid key or value', () => {
-    const button = screen.getByText('Add new entry');
+  it('should delete the item correctly', async () => {
+    const deleteButton = screen.queryAllByTestId('deleteButton');
+    fireEvent.click(deleteButton[0].closest('button'));
 
-    fireEvent.click(button);
-
-    const keyInput = screen.getByPlaceholderText('Key');
-    const valueInput = screen.getByPlaceholderText('Value');
-
-    cdblistErrors.forEach(item => {
-      fireEvent.change(keyInput, { target: { value: item.key } });
-      fireEvent.change(valueInput, { target: { value: item.value } });
-      expect(
-        screen.getByText(
-          'Must start and end with quotes or have no quotes at all',
-        ),
-      ).toBeInTheDocument();
-    });
+    expect(screen.queryByText(cdblist[0].key)).toBeFalsy();
   });
+
+  it.each`
+    key                      | value
+    ${'key'}                 | ${'value'}
+    ${'"key"'}               | ${'value'}
+    ${'"key"'}               | ${'"value"'}
+    ${'"key:key"'}           | ${'value'}
+    ${'"key:key"'}           | ${'"value:value"'}
+    ${'"key"'}               | ${'"value:value"'}
+    ${'key'}                 | ${'"value:value"'}
+    ${'key'}                 | ${''}
+    ${'"key"'}               | ${''}
+    ${'"key:key"'}           | ${''}
+    ${'"b1:b1:b1:b1:b1:b1"'} | ${'test6'}
+  `(
+    'should add keys($key) and values($value) correctly',
+    ({ key, value }: { key: string; value: string }) => {
+      const button = screen.getByText('Add new entry');
+      fireEvent.click(button);
+
+      const keyInput = screen.getByPlaceholderText('Key');
+      const valueInput = screen.getByPlaceholderText('Value');
+
+      fireEvent.change(keyInput, { target: { value: key } });
+      fireEvent.change(valueInput, { target: { value: value } });
+
+      const addButton = screen.getByText('Add');
+
+      expect(addButton.closest('button')).not.toBeDisabled();
+
+      fireEvent.click(addButton);
+
+      expect(screen.getByText(key)).toBeInTheDocument();
+      if (!(value === '')) {
+        expect(screen.getByText(value)).toBeInTheDocument();
+      }
+    },
+  );
+
+  it.each`
+    key            | value              | quotesError                  | colonError
+    ${'":key'}     | ${'value'}         | ${messagesError.quotesError} | ${messagesError.colonError}
+    ${'key'}       | ${'":value'}       | ${messagesError.quotesError} | ${messagesError.colonError}
+    ${'key"key'}   | ${'value'}         | ${messagesError.quotesError} | ${''}
+    ${'key'}       | ${'value"value'}   | ${messagesError.quotesError} | ${''}
+    ${'key'}       | ${'value"'}        | ${messagesError.quotesError} | ${''}
+    ${'key'}       | ${'"value'}        | ${messagesError.quotesError} | ${''}
+    ${'"key"key"'} | ${'value'}         | ${messagesError.quotesError} | ${''}
+    ${'key'}       | ${'"value"value"'} | ${messagesError.quotesError} | ${''}
+    ${'key:key'}   | ${'value'}         | ${''}                        | ${messagesError.colonError}
+    ${'key:"key"'} | ${'value'}         | ${messagesError.quotesError} | ${messagesError.colonError}
+    ${'key'}       | ${':value'}        | ${''}                        | ${messagesError.colonError}
+    ${'"key:key"'} | ${'"value":'}      | ${messagesError.quotesError} | ${messagesError.colonError}
+  `(
+    'should render the message when try to add invalid key($key) or value($value)',
+    ({
+      key,
+      value,
+      quotesError,
+      colonError,
+    }: {
+      key: string;
+      value: string;
+      quotesError: string;
+      colonError: string;
+    }) => {
+      const button = screen.getByText('Add new entry');
+      fireEvent.click(button);
+
+      const keyInput = screen.getByPlaceholderText('Key');
+      const valueInput = screen.getByPlaceholderText('Value');
+
+      fireEvent.change(keyInput, { target: { value: key } });
+      fireEvent.change(valueInput, { target: { value: value } });
+
+      const addButton = screen.getByText('Add');
+
+      expect(addButton.closest('button')).toBeDisabled();
+
+      if (quotesError) {
+        expect(screen.getByText(quotesError)).toBeInTheDocument();
+      } else {
+        expect(screen.queryByText(messagesError.quotesError)).toBeFalsy();
+      }
+
+      if (colonError) {
+        expect(screen.getByText(colonError)).toBeInTheDocument();
+      } else {
+        expect(screen.queryByText(messagesError.colonError)).toBeFalsy();
+      }
+    },
+  );
 });
