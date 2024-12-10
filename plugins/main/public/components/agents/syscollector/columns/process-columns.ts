fb0b939f97a54bad378f059c6384e1dc26049f0c
chantal.kelm <chantal.kelm@wazuh.com>
2024-12-10 11:41:03 -0300

    add comments to the code

--- plugins/main/public/components/agents/syscollector/columns/process-columns.ts
+++ plugins/main/public/components/agents/syscollector/columns/process-columns.ts
@@ -1,13 +1,17 @@
 import { KeyEquivalence } from '../../../../../common/csv-key-equivalence';
 
+// Function to determine the column width according to the field and the operating system.
 const getColumnWidth = (field: string, isLinux: boolean = false): string => {
+  // If it is Linux and the field is 'argvs' or 'cmd', the width is fixed at '15%'.
   if (isLinux && (field === 'argvs' || field === 'cmd')) {
     return '15%';
   }
+  // Definition of specific widths for certain fields when isLinux is false
   const widths: Record<string, string> = {
     cmd: '30%',
     argvs: '15%',
   };
+  // Returns the defined width or a default value of '7%' if there is no match
   return widths[field] || '7%';
 };
 
@@ -18,7 +22,7 @@ const mapColumns = (
   ...rest,
   field,
   name: rest.name || KeyEquivalence[field] || field,
-  width: getColumnWidth(field, isLinux),
+  width: getColumnWidth(field, isLinux), // Calculates the column width based on the field and the operating system
 });
 
 const windowsColumns = [
