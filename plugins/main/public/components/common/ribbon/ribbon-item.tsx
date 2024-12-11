d93efd4d790ea46d8636870fb556c496b0247a06
Guido Modarelli <guidomodarelli25@gmail.com>
2024-12-11 11:16:52 -0300

    feat(platform): add getPlatformIcon and getOsName functions for improved agent OS representation in the application

--- plugins/main/public/components/common/ribbon/ribbon-item.tsx
+++ plugins/main/public/components/common/ribbon/ribbon-item.tsx
@@ -1,13 +1,11 @@
 import { EuiFlexItem, EuiLoadingSpinner } from '@elastic/eui';
 import React from 'react';
-import { WAZUH_AGENTS_OS_TYPE } from '../../../../common/constants';
 import { AgentStatus } from '../../agents/agent-status';
 import { Agent } from '../../endpoints-summary/types';
 import { WzStat } from '../../wz-stat';
 import { GroupTruncate } from '../util/agent-group-truncate';
 import WzTextWithTooltipIfTruncated from '../wz-text-with-tooltip-if-truncated';
 import './ribbon-item.scss';
-import { getAgentOSType } from '../../../react-services';
 import { TAgentStatus } from '../../../../common/services/wz_agent_status';
 
 const FONT_SIZE = 12;
@@ -24,7 +22,6 @@ export type IRibbonItem<LABEL extends string = string, VALUE = any> = {
   value: VALUE;
   style?: React.CSSProperties;
   isLoading?: boolean;
-  icon?: React.ReactNode;
   condensed?: boolean;
   render?: () => React.ReactNode;
 };
@@ -33,12 +30,6 @@ const isGroups = (
   item: IRibbonItem,
 ): item is IRibbonItem<RibbonItemLabel.GROUPS, string[]> => {
   return item.key === RibbonItemLabel.GROUPS;
-};
-
-const isOperatingSystem = (
-  item: IRibbonItem,
-): item is IRibbonItem<RibbonItemLabel.OPERATING_SYSTEM, Agent> => {
-  return item.key === RibbonItemLabel.OPERATING_SYSTEM;
 };
 
 const isStatus = (
@@ -56,55 +47,9 @@ const WzRibbonItem = (props: RibbonItemProps) => {
 
   const contentStyle = { ...item.style, fontSize: FONT_SIZE };
 
-  const getPlatformIcon = (agent?: Agent) => {
-    let icon = '';
-    let osType = getAgentOSType(agent);
-    if (osType === WAZUH_AGENTS_OS_TYPE.DARWIN) {
-      icon = 'apple';
-    } else if (
-      [WAZUH_AGENTS_OS_TYPE.WINDOWS, WAZUH_AGENTS_OS_TYPE.LINUX].includes(
-        osType,
-      )
-    ) {
-      icon = osType;
-    }
-
-    if (icon) {
-      return (
-        <i
-          className={`fa fa-${icon} AgentsTable__soBadge AgentsTable__soBadge--${osType}`}
-          aria-hidden='true'
-        ></i>
-      );
-    }
-    return <></>;
-  };
-
-  const getOsName = (agent?: Agent) => {
-    const { name, version } = agent?.os || {};
-
-    if (!name && !version) return '-';
-
-    if (!version) return name;
-
-    if (!name) return version;
-
-    return `${name} ${version}`;
-  };
-
   const renderOptionalField = function <T>(field?: T): T | string {
     return field !== undefined || field ? field : '-';
   };
-
-  if (isOperatingSystem(item)) {
-    item.render = () => {
-      return (
-        <>
-          {getPlatformIcon(item.value)} {getOsName(item.value)}
-        </>
-      );
-    };
-  }
 
   const renderValue = () => {
     return item.isLoading ? (
