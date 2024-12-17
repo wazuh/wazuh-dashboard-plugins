aa4c03fa17c70599b621048c95289f3fe5bff985
Guido Modarelli <guidomodarelli25@gmail.com>
2024-12-17 11:41:09 -0300

    fix(get-updates): refactor test setup to improve mocking of saved objects and requests for more accurate updates handling

--- plugins/wazuh-check-updates/server/services/updates/get-updates.test.ts
+++ plugins/wazuh-check-updates/server/services/updates/get-updates.test.ts
@@ -7,16 +7,36 @@ import {
 import { getSavedObject } from '../saved-object/get-saved-object';
 import { setSavedObject } from '../saved-object/set-saved-object';
 import { getUpdates } from './get-updates';
-import { SAVED_OBJECT_UPDATES } from '../../../common/constants';
 
-const mockedGetSavedObject = getSavedObject as jest.Mock;
+const mockGetSavedObject = getSavedObject as jest.Mock;
 jest.mock('../saved-object/get-saved-object');
 
-const mockedSetSavedObject = setSavedObject as jest.Mock;
-mockedSetSavedObject.mockImplementation(() => ({}));
+const mockSetSavedObject = setSavedObject as jest.Mock;
+mockSetSavedObject.mockImplementation(() => ({}));
 jest.mock('../saved-object/set-saved-object');
 
-const mockedGetWazuhCore = getWazuhCore as jest.Mock;
+const mockGetWazuhCore = getWazuhCore as jest.Mock;
+const mockRequest = jest.fn();
+const mockManageHosts = {
+  get: jest.fn(() => [{ id: 'api id' }]),
+};
+const mockGetHostsEntries = jest.fn(() => []);
+mockGetWazuhCore.mockImplementationOnce(() => {
+  return {
+    api: {
+      client: {
+        asInternalUser: {
+          request: mockRequest,
+        },
+      },
+    },
+    manageHosts: mockManageHosts,
+    serverAPIHostEntries: {
+      getHostsEntries: mockGetHostsEntries,
+    },
+  };
+});
+
 const mockedGetWazuhCheckUpdatesServices =
   getWazuhCheckUpdatesServices as jest.Mock;
 mockedGetWazuhCheckUpdatesServices.mockImplementation(() => ({
@@ -35,62 +55,38 @@ describe('getUpdates function', () => {
   });
 
   test('should return available updates from saved object', async () => {
-    mockedGetSavedObject.mockImplementation(() => ({
-      last_check_date: '2023-09-30T14:00:00.000Z',
+    const last_available_patch = {
+      description:
+        '## Manager\r\n\r\n### Fixed\r\n\r\n- Fixed a crash when overwrite rules are triggered...',
+      published_date: '2022-05-18T10:12:43Z',
+      semver: {
+        major: 4,
+        minor: 3,
+        patch: 8,
+      },
+      tag: 'v4.3.8',
+      title: 'Wazuh v4.3.8',
+    };
+    const last_check_date = '2023-09-30T14:00:00.000Z';
+    const savedObject = {
+      last_check_date,
       apis_available_updates: [
         {
           api_id: 'api id',
           current_version: 'v4.3.1',
           status: API_UPDATES_STATUS.UP_TO_DATE,
-          last_available_patch: {
-            description:
-              '## Manager\r\n\r\n### Fixed\r\n\r\n- Fixed a crash when overwrite rules are triggered...',
-            published_date: '2022-05-18T10:12:43Z',
-            semver: {
-              major: 4,
-              minor: 3,
-              patch: 8,
-            },
-            tag: 'v4.3.8',
-            title: 'Wazuh v4.3.8',
-          },
+          last_available_patch,
         },
       ],
-    }));
-
-    mockedGetWazuhCore.mockImplementation(() => ({
-      serverAPIHostEntries: {
-        getHostsEntries: jest.fn(() => []),
-      },
-    }));
+    };
+    mockGetSavedObject.mockImplementation(() => savedObject);
 
     const updates = await getUpdates();
 
     expect(getSavedObject).toHaveBeenCalledTimes(1);
     expect(getSavedObject).toHaveBeenCalledWith(SAVED_OBJECT_UPDATES);
 
-    expect(updates).toEqual({
-      last_check_date: '2023-09-30T14:00:00.000Z',
-      apis_available_updates: [
-        {
-          api_id: 'api id',
-          current_version: 'v4.3.1',
-          status: API_UPDATES_STATUS.UP_TO_DATE,
-          last_available_patch: {
-            description:
-              '## Manager\r\n\r\n### Fixed\r\n\r\n- Fixed a crash when overwrite rules are triggered...',
-            published_date: '2022-05-18T10:12:43Z',
-            semver: {
-              major: 4,
-              minor: 3,
-              patch: 8,
-            },
-            tag: 'v4.3.8',
-            title: 'Wazuh v4.3.8',
-          },
-        },
-      ],
-    });
+    expect(updates).toEqual(savedObject);
   });
 
   test('should return available updates from api', async () => {
@@ -106,41 +102,27 @@ describe('getUpdates function', () => {
       tag: 'v4.3.8',
       title: 'Wazuh v4.3.8',
     };
-    mockedGetWazuhCore.mockImplementationOnce(() => {
-      return {
-        api: {
-          client: {
-            asInternalUser: {
-              request: jest
-                .fn()
-                .mockImplementationOnce(() => ({
-                  data: {
-                    data: {
-                      api_version: '4.3.1',
-                    },
-                  },
-                }))
-                .mockImplementationOnce(() => ({
-                  data: {
-                    data: {
-                      uuid: '7f828fd6-ef68-4656-b363-247b5861b84c',
-                      current_version: 'v4.3.1',
-                      update_check: undefined,
-                      last_available_major: undefined,
-                      last_available_minor: undefined,
-                      last_available_patch,
-                      last_check_date: undefined,
-                    },
-                  },
-                })),
-            },
+    mockRequest
+      .mockImplementationOnce(() => ({
+        data: {
+          data: {
+            api_version: '4.3.1',
           },
         },
-        manageHosts: {
-          get: jest.fn(() => [{ id: 'api id' }]),
+      }))
+      .mockImplementationOnce(() => ({
+        data: {
+          data: {
+            uuid: '7f828fd6-ef68-4656-b363-247b5861b84c',
+            current_version: 'v4.3.1',
+            update_check: undefined,
+            last_available_major: undefined,
+            last_available_minor: undefined,
+            last_available_patch,
+            last_check_date: undefined,
+          },
         },
-      };
-    });
+      }));
 
     const updates = await getUpdates(true);
 
