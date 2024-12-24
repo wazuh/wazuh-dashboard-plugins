6db1a113794f3ce151bbceddfb223549829bb886
Maximiliano Ibarra <maximiliano.ibarra@wazuh.com>
2024-12-24 09:24:30 -0300

    Fix linter errors

--- plugins/wazuh-core/common/services/configuration/configuration-store.ts
+++ plugins/wazuh-core/common/services/configuration/configuration-store.ts
@@ -3,8 +3,9 @@ import { IConfigurationStore } from './configuration';
 import { IConfigurationProvider } from './configuration-provider';
 
 export class ConfigurationStore implements IConfigurationStore {
-  private providers: Map<string, IConfigurationProvider>;
-  constructor(private logger: Logger) {
+  private readonly providers: Map<string, IConfigurationProvider>;
+
+  constructor(private readonly logger: Logger) {
     this.providers = new Map();
   }
 
@@ -19,80 +20,101 @@ export class ConfigurationStore implements IConfigurationStore {
 
   getProvider(name: string): IConfigurationProvider {
     const provider = this.providers.get(name);
+
     if (!provider) {
       throw new Error(`Provider ${name} not found`);
     }
+
     return provider;
   }
 
   async getProviderConfiguration(key: string): Promise<Record<string, any>> {
     try {
       const provider = this.providers.get(key);
+
       if (!provider) {
         throw new Error(`Provider ${key} not found`);
       }
+
       const configuration = await provider.getAll();
+
       return configuration;
     } catch (error) {
-      let err = error as Error;
+      const errorCasted = error as Error;
       const enhancedError = new Error(
-        `Error getting configuration: ${err?.message}`,
+        `Error getting configuration: ${errorCasted?.message}`,
       );
+
       this.logger.error(enhancedError.message);
       throw enhancedError;
     }
   }
 
-  async setup(dependencies: any = {}) {
+  async setup(_dependencies: any = {}) {
     this.logger.debug('Setup');
   }
+
   async start() {
     try {
       this.logger.debug('Start');
     } catch (error) {
-      let err = error as Error;
-      this.logger.error(`Error starting: ${err?.message}`);
+      const errorCasted = error as Error;
+
+      this.logger.error(`Error starting: ${errorCasted?.message}`);
     }
   }
+
   async stop() {
     this.logger.debug('Stop');
   }
-  async get(configName: string): Promise<any | { [key: string]: any }> {
+
+  async get(configName: string): Promise<any | Record<string, any>> {
     // get all the providers and check if the setting is in any of them
     const configuration = await this.getAll();
+
     // check if the configName exist in the object
     if (Object.keys(configuration).includes(configName)) {
       return configuration[configName];
     }
+
     throw new Error(`Configuration ${configName} not found`);
   }
 
-  async getAll(): Promise<{ [key: string]: any }> {
-    const providers = Array.from(this.providers.values());
-    let configuration = {};
-    for (const provider of providers) {
-      try {
-        const settings = await provider.getAll();
-        configuration = {
-          ...configuration,
-          ...settings,
-        };
-      } catch (error) {
-        let err = error as Error;
-        this.logger.error(
-          `Error getting configuration from ${provider.constructor.name}: ${err?.message}`,
-        );
+  async getAll(): Promise<Record<string, any>> {
+    const providers = [...this.providers.values()];
+    const configurations = await Promise.all(
+      providers.map(async provider => {
+        try {
+          return await provider.getAll();
+        } catch (error) {
+          const errorCasted = error as Error;
+
+          this.logger.error(
+            `Error getting configuration from ${provider.constructor.name}: ${errorCasted?.message}`,
+          );
+
+          return {};
+        }
+      }),
+    );
+    const result: Record<string, any> = {};
+
+    for (const config of configurations) {
+      for (const key of Object.keys(config)) {
+        if (result[key] === undefined) {
+          result[key] = config[key];
+        }
       }
     }
 
-    return configuration;
+    return result;
   }
 
-  async set(settings: { [key: string]: any }): Promise<any> {
+  async set(_settings: Record<string, any>): Promise<any> {
     throw new Error('Method not implemented yet.');
   }
 
-  async clear(...settings: string[]): Promise<any> {
+  async clear(_settings: string[]): Promise<any> {
     throw new Error('Method not implemented yet.');
   }
 }
