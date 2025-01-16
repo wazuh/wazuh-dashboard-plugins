269ed79a7ed9ff094cf8ec18b030a56c4542bd1b
Guido Modarelli <guidomodarelli25@gmail.com>
2025-01-16 17:47:58 -0300

    Refactor AnalysisApp component to use EUI layout components

--- plugins/wazuh-analysis/public/components/analysis-app.tsx
+++ plugins/wazuh-analysis/public/components/analysis-app.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { EuiPage, EuiPageBody, EuiPageContentBody } from '@elastic/eui';
 import { AppMountParameters } from '../../../../src/core/public';
 
 export interface AnalysisAppDependencies {}
@@ -9,4 +10,10 @@ interface AnalysisAppProps {
   dependencies: AnalysisAppDependencies;
 }
 
-export const AnalysisApp = (_props: AnalysisAppProps) => <>AnalysisApp</>;
+export const AnalysisApp = (_props: AnalysisAppProps) => (
+  <EuiPage>
+    <EuiPageBody>
+      <EuiPageContentBody>AnalysisApp</EuiPageContentBody>
+    </EuiPageBody>
+  </EuiPage>
+);
