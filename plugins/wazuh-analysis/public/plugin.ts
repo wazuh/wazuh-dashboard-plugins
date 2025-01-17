7ba426ba4a66c43b407e4805c8a9752edd69a05a
Guido Modarelli <guidomodarelli25@gmail.com>
2025-01-17 12:22:09 -0300

    Add translation messages and application categories for Wazuh Analysis plugin

--- plugins/wazuh-analysis/public/plugin.ts
+++ plugins/wazuh-analysis/public/plugin.ts
@@ -1,5 +1,5 @@
 import { i18n } from '@osd/i18n';
-import { AppMountParameters } from 'opensearch-dashboards/public';
+import { AppCategory, AppMountParameters } from 'opensearch-dashboards/public';
 import { App, CoreSetup, CoreStart, Plugin } from '../../../src/core/public';
 import { NavigationPublicPluginStart } from '../../../src/plugins/navigation/public';
 import { AnalysisSetup, AnalysisStart } from './types';
@@ -14,9 +14,28 @@ export class AnalysisPlugin
   implements
     Plugin<AnalysisSetup, AnalysisStart, object, AnalysisStartDependencies>
 {
-  private readonly title = i18n.translate('analysis.title', {
-    defaultMessage: 'Analysis',
-  });
+  private readonly translationMessages = {
+    ANALYSIS_PLUGIN_TITLE: i18n.translate('analysis.title', {
+      defaultMessage: 'Analysis',
+    }),
+    ENDPOINT_SECURITY: i18n.translate('analysis.endpoint_security', {
+      defaultMessage: 'Endpoint Security',
+    }),
+    THREAT_INTELLIGENCE: i18n.translate('analysis.threat_intelligence', {
+      defaultMessage: 'Threat Intelligence',
+    }),
+    SECURITY_OPERATIONS: i18n.translate('analysis.security_operations', {
+      defaultMessage: 'Security Operations',
+    }),
+    CLOUD_SECURITY: i18n.translate('analysis.cloud_security', {
+      defaultMessage: 'Cloud Security',
+    }),
+  };
+  private readonly CATEGORY: AppCategory = {
+    id: 'analysis',
+    label: this.translationMessages.ANALYSIS_PLUGIN_TITLE,
+    order: 5000,
+  };
 
   public setup(
     core: CoreSetup,
@@ -25,9 +44,41 @@ export class AnalysisPlugin
     console.debug('AnalysisPlugin started');
 
     const ApplicationsMap: Record<string, Omit<App, 'id'>> = {
-      analysis: {
-        title: this.title,
+      endpoint_security: {
+        title: this.translationMessages.ENDPOINT_SECURITY,
+        category: this.CATEGORY,
         async mount(params: AppMountParameters) {
+          // TODO: Implement the endpoint security application
+          const { renderApp } = await import('./application');
+
+          return renderApp(params, {});
+        },
+      },
+      threat_intelligence: {
+        title: this.translationMessages.THREAT_INTELLIGENCE,
+        category: this.CATEGORY,
+        async mount(params: AppMountParameters) {
+          // TODO: Implement the threat intelligence application
+          const { renderApp } = await import('./application');
+
+          return renderApp(params, {});
+        },
+      },
+      security_operations: {
+        title: this.translationMessages.SECURITY_OPERATIONS,
+        category: this.CATEGORY,
+        async mount(params: AppMountParameters) {
+          // TODO: Implement the security operations application
+          const { renderApp } = await import('./application');
+
+          return renderApp(params, {});
+        },
+      },
+      cloud_security: {
+        title: this.translationMessages.CLOUD_SECURITY,
+        category: this.CATEGORY,
+        async mount(params: AppMountParameters) {
+          // TODO: Implement the cloud security application
           const { renderApp } = await import('./application');
 
           return renderApp(params, {});
