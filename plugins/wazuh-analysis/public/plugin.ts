72cc11b773f22a5cd496148d1a8448298996d873
Guido Modarelli <guidomodarelli25@gmail.com>
2025-01-17 17:23:52 -0300

    Add navigation links for security categories in AnalysisPlugin

--- plugins/wazuh-analysis/public/plugin.ts
+++ plugins/wazuh-analysis/public/plugin.ts
@@ -1,6 +1,12 @@
 import { i18n } from '@osd/i18n';
 import { AppCategory, AppMountParameters } from 'opensearch-dashboards/public';
-import { App, CoreSetup, CoreStart, Plugin } from '../../../src/core/public';
+import {
+  App,
+  CoreSetup,
+  CoreStart,
+  Plugin,
+  DEFAULT_NAV_GROUPS,
+} from '../../../src/core/public';
 import { NavigationPublicPluginStart } from '../../../src/plugins/navigation/public';
 import { OmitStrict } from '../../wazuh-core/common/types';
 import { AnalysisSetup, AnalysisStart } from './types';
@@ -24,18 +30,30 @@ export class AnalysisPlugin
     ANALYSIS_PLUGIN_TITLE: i18n.translate('analysis.title', {
       defaultMessage: 'Analysis',
     }),
-    ENDPOINT_SECURITY: i18n.translate('analysis.endpoint_security', {
+    ENDPOINT_SECURITY: i18n.translate(
+      `analysis.category.${this.ENDPOINT_SECURITY_ID}`,
+      {
       defaultMessage: 'Endpoint Security',
-    }),
-    THREAT_INTELLIGENCE: i18n.translate('analysis.threat_intelligence', {
+      },
+    ),
+    THREAT_INTELLIGENCE: i18n.translate(
+      `analysis.category.${this.THREAT_INTELLIGENCE_ID}`,
+      {
       defaultMessage: 'Threat Intelligence',
-    }),
-    SECURITY_OPERATIONS: i18n.translate('analysis.security_operations', {
+      },
+    ),
+    SECURITY_OPERATIONS: i18n.translate(
+      `analysis.category.${this.SECURITY_OPERATIONS_ID}`,
+      {
       defaultMessage: 'Security Operations',
-    }),
-    CLOUD_SECURITY: i18n.translate('analysis.cloud_security', {
+      },
+    ),
+    CLOUD_SECURITY: i18n.translate(
+      `analysis.category.${this.CLOUD_SECURITY_ID}`,
+      {
       defaultMessage: 'Cloud Security',
-    }),
+      },
+    ),
   };
   private readonly CATEGORY: AppCategory = {
     id: this.PLUGIN_ID,
@@ -99,6 +117,33 @@ export class AnalysisPlugin
     for (const app of APPLICATIONS) {
       core.application.register(app);
     }
+
+    core.chrome.navGroup.addNavLinksToGroup(DEFAULT_NAV_GROUPS.all, [
+      {
+        id: this.ENDPOINT_SECURITY_ID,
+        title: this.translationMessages.ENDPOINT_SECURITY,
+        order: 0,
+        category: this.CATEGORY,
+      },
+      {
+        id: this.THREAT_INTELLIGENCE_ID,
+        title: this.translationMessages.THREAT_INTELLIGENCE,
+        order: 1,
+        category: this.CATEGORY,
+      },
+      {
+        id: this.SECURITY_OPERATIONS_ID,
+        title: this.translationMessages.SECURITY_OPERATIONS,
+        order: 2,
+        category: this.CATEGORY,
+      },
+      {
+        id: this.CLOUD_SECURITY_ID,
+        title: this.translationMessages.CLOUD_SECURITY,
+        order: 3,
+        category: this.CATEGORY,
+      },
+    ]);
 
     return {};
   }
