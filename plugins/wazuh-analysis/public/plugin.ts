4253b06b307dceae11e77e4418c7fb4a9e85c1aa
Guido Modarelli <guidomodarelli25@gmail.com>
2025-01-20 16:22:30 -0300

    Add translation messages for new categories in AnalysisPlugin

--- plugins/wazuh-analysis/public/plugin.ts
+++ plugins/wazuh-analysis/public/plugin.ts
@@ -26,34 +26,52 @@ export class AnalysisPlugin
   private readonly THREAT_INTELLIGENCE_ID = 'threat_intelligence';
   private readonly SECURITY_OPERATIONS_ID = 'security_operations';
   private readonly CLOUD_SECURITY_ID = 'cloud_security';
+  private readonly CONFIGURATION_ASSESSMENT_ID = 'configuration_assessment';
+  private readonly MALWARE_DETECTION_ID = 'malware_detection';
+  private readonly FIM_ID = 'fim';
   private readonly translationMessages = {
     ANALYSIS_PLUGIN_TITLE: i18n.translate('analysis.title', {
       defaultMessage: 'Analysis',
     }),
-    ENDPOINT_SECURITY: i18n.translate(
+    ENDPOINT_SECURITY_TITLE: i18n.translate(
       `${this.PLUGIN_ID}.category.${this.ENDPOINT_SECURITY_ID}`,
       {
         defaultMessage: 'Endpoint Security',
       },
     ),
-    THREAT_INTELLIGENCE: i18n.translate(
+    THREAT_INTELLIGENCE_TITLE: i18n.translate(
       `${this.PLUGIN_ID}.category.${this.THREAT_INTELLIGENCE_ID}`,
       {
         defaultMessage: 'Threat Intelligence',
       },
     ),
-    SECURITY_OPERATIONS: i18n.translate(
+    SECURITY_OPERATIONS_TITLE: i18n.translate(
       `${this.PLUGIN_ID}.category.${this.SECURITY_OPERATIONS_ID}`,
       {
         defaultMessage: 'Security Operations',
       },
     ),
-    CLOUD_SECURITY: i18n.translate(
+    CLOUD_SECURITY_TITLE: i18n.translate(
       `${this.PLUGIN_ID}.category.${this.CLOUD_SECURITY_ID}`,
       {
         defaultMessage: 'Cloud Security',
       },
     ),
+    CONFIGURATION_ASSESSMENT_TITLE: i18n.translate(
+      `${this.PLUGIN_ID}.category.${this.CONFIGURATION_ASSESSMENT_ID}`,
+      {
+        defaultMessage: 'Configuration Assessment',
+      },
+    ),
+    MALWARE_DETECTION_TITLE: i18n.translate(
+      `${this.PLUGIN_ID}.category.${this.MALWARE_DETECTION_ID}`,
+      {
+        defaultMessage: 'Malware Detection',
+      },
+    ),
+    FIM_TITLE: i18n.translate(`${this.PLUGIN_ID}.category.${this.FIM_ID}`, {
+      defaultMessage: 'File Integrity Monitoring',
+    }),
   };
   private readonly CATEGORY: AppCategory = {
     id: this.PLUGIN_ID,
@@ -69,7 +87,7 @@ export class AnalysisPlugin
 
     const ApplicationsMap: Record<string, OmitStrict<App, 'id'>> = {
       endpoint_security: {
-        title: this.translationMessages.ENDPOINT_SECURITY,
+        title: this.translationMessages.ENDPOINT_SECURITY_TITLE,
         category: this.CATEGORY,
         async mount(params: AppMountParameters) {
           // TODO: Implement the endpoint security application
@@ -79,7 +97,7 @@ export class AnalysisPlugin
         },
       },
       threat_intelligence: {
-        title: this.translationMessages.THREAT_INTELLIGENCE,
+        title: this.translationMessages.THREAT_INTELLIGENCE_TITLE,
         category: this.CATEGORY,
         async mount(params: AppMountParameters) {
           // TODO: Implement the threat intelligence application
@@ -89,7 +107,7 @@ export class AnalysisPlugin
         },
       },
       security_operations: {
-        title: this.translationMessages.SECURITY_OPERATIONS,
+        title: this.translationMessages.SECURITY_OPERATIONS_TITLE,
         category: this.CATEGORY,
         async mount(params: AppMountParameters) {
           // TODO: Implement the security operations application
@@ -99,10 +117,37 @@ export class AnalysisPlugin
         },
       },
       cloud_security: {
-        title: this.translationMessages.CLOUD_SECURITY,
+        title: this.translationMessages.CLOUD_SECURITY_TITLE,
         category: this.CATEGORY,
         async mount(params: AppMountParameters) {
           // TODO: Implement the cloud security application
+          const { renderApp } = await import('./application');
+
+          return renderApp(params, {});
+        },
+      },
+      configuration_assessment: {
+        title: this.translationMessages.CONFIGURATION_ASSESSMENT_TITLE,
+        async mount(params: AppMountParameters) {
+          // TODO: Implement the configuration assessment application
+          const { renderApp } = await import('./application');
+
+          return renderApp(params, {});
+        },
+      },
+      malware_detection: {
+        title: this.translationMessages.MALWARE_DETECTION_TITLE,
+        async mount(params: AppMountParameters) {
+          // TODO: Implement the malware detection application
+          const { renderApp } = await import('./application');
+
+          return renderApp(params, {});
+        },
+      },
+      fim: {
+        title: this.translationMessages.FIM_TITLE,
+        async mount(params: AppMountParameters) {
+          // TODO: Implement the fim application
           const { renderApp } = await import('./application');
 
           return renderApp(params, {});
@@ -121,25 +166,25 @@ export class AnalysisPlugin
     core.chrome.navGroup.addNavLinksToGroup(DEFAULT_NAV_GROUPS.all, [
       {
         id: this.ENDPOINT_SECURITY_ID,
-        title: this.translationMessages.ENDPOINT_SECURITY,
+        title: this.translationMessages.ENDPOINT_SECURITY_TITLE,
         order: 0,
         category: this.CATEGORY,
       },
       {
         id: this.THREAT_INTELLIGENCE_ID,
-        title: this.translationMessages.THREAT_INTELLIGENCE,
+        title: this.translationMessages.THREAT_INTELLIGENCE_TITLE,
         order: 1,
         category: this.CATEGORY,
       },
       {
         id: this.SECURITY_OPERATIONS_ID,
-        title: this.translationMessages.SECURITY_OPERATIONS,
+        title: this.translationMessages.SECURITY_OPERATIONS_TITLE,
         order: 2,
         category: this.CATEGORY,
       },
       {
         id: this.CLOUD_SECURITY_ID,
-        title: this.translationMessages.CLOUD_SECURITY,
+        title: this.translationMessages.CLOUD_SECURITY_TITLE,
         order: 3,
         category: this.CATEGORY,
       },
