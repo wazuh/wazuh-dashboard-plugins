febcf7da619e5ec547fbde3a3ef4466efb72e04e
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-03 08:34:49 -0300

    Move navigateToFirstAppInNavGroup function to utils and update imports

--- plugins/wazuh-analysis/public/plugin.ts
+++ plugins/wazuh-analysis/public/plugin.ts
@@ -3,7 +3,6 @@ import {
   AppMount,
   AppMountParameters,
   AppUpdater,
-  NavGroupItemInMap,
 } from 'opensearch-dashboards/public';
 import { first } from 'rxjs/operators';
 import { Subject } from 'rxjs';
@@ -41,7 +40,7 @@ import {
   CloudSecurityApp,
 } from './groups/cloud-security/cloud-security';
 import { NAV_GROUPS } from './groups/nav-groups';
-import { buildSubAppId } from './utils';
+import { buildSubAppId, navigateToFirstAppInNavGroup } from './utils';
 
 interface AnalysisSetupDependencies {}
 
@@ -171,31 +170,6 @@ function setNavLinkHidden(): Partial<App> {
 async function getCurrentNavGroup(core: CoreStart) {
   return core.chrome.navGroup.getCurrentNavGroup$().pipe(first()).toPromise();
 }
-
-/**
- * The function `navigateToFirstAppInNavGroup` navigates to the first app in a
- * specified navigation group if it exists.
- * @param {CoreStart} core - The `core` parameter is an object that provides access
- * to core services in Kibana, such as application navigation, HTTP requests, and
- * more. It is typically provided by the Kibana platform to plugins and can be used
- * to interact with various functionalities within the Kibana application.
- * @param {NavGroupItemInMap | undefined} navGroup - The `navGroup` parameter is
- * expected to be an object that represents a navigation group item in a map. It
- * should have a property `navLinks` which is an array of navigation links. Each
- * navigation link in the `navLinks` array should have an `id` property that
- * represents the ID
- */
-const navigateToFirstAppInNavGroup = async (
-  core: CoreStart,
-  navGroup: NavGroupItemInMap | undefined,
-) => {
-  // Get the first nav item, if it exists navigate to the app
-  const firstNavItem = navGroup?.navLinks[0];
-
-  if (firstNavItem?.id) {
-    core.application.navigateToApp(firstNavItem.id);
-  }
-};
 
 export class AnalysisPlugin
   implements
