93e9b62ad88aced02b2dc2f3c5e0467bf27ec31c
JuanGarriuz <juan.garcia@wazuh.com>
2025-02-03 09:34:10 +0100

    Move test

---
+++ plugins/main/public/utils/known-fields.test.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { threatHuntingColumns } from '../components/common/wazuh-discover/config/data-grid-columns';
+import { vulnerabilitiesColumns } from '../components/overview/vulnerabilities/events/vulnerabilities-columns';
+import { dockerColumns } from '../components/overview/docker/events/docker-columns';
+import { googleCloudColumns } from '../components/overview/google-cloud/events/google-cloud-columns';
+import { amazonWebServicesColumns } from '../components/overview/amazon-web-services/events/amazon-web-services-columns';
+import { office365Columns } from '../components/overview/office/events/office-365-columns';
+import { fileIntegrityMonitoringColumns } from '../components/overview/fim/events/file-integrity-monitoring-columns';
+import { configurationAssessmentColumns } from '../components/agents/sca/events/configuration-assessment-columns';
+import { pciColumns } from '../components/overview/pci/events/pci-columns';
+import { hipaaColumns } from '../components/overview/hipaa/events/hipaa-columns';
+import { nistColumns } from '../components/overview/nist/events/nist-columns';
+import { gdprColumns } from '../components/overview/gdpr/events/gdpr-columns';
+import { tscColumns } from '../components/overview/tsc/events/tsc-columns';
+import { githubColumns } from '../components/overview/github/events/github-columns';
+import { mitreAttackColumns } from '../components/overview/mitre/events/mitre-attack-columns';
+import { malwareDetectionColumns } from '../components/overview/malware-detection/events/malware-detection-columns';
+import { commonColumns } from '../components/overview/common/data-grid-columns';
+import { KnownFields } from './known-fields';
+import {
+  agentTechniquesColumns,
+  techniquesColumns,
+} from '../components/overview/mitre/framework/components/techniques/components/flyout-technique/flyout-technique-columns';
+import { wzDiscoverRenderColumns } from '../components/common/wazuh-discover/render-columns';
+
+const eventsColumns = [
+  ...threatHuntingColumns,
+  ...vulnerabilitiesColumns,
+  ...dockerColumns,
+  ...googleCloudColumns,
+  ...amazonWebServicesColumns,
+  ...office365Columns,
+  ...fileIntegrityMonitoringColumns,
+  ...configurationAssessmentColumns,
+  ...pciColumns,
+  ...hipaaColumns,
+  ...nistColumns,
+  ...gdprColumns,
+  ...tscColumns,
+  ...githubColumns,
+  ...mitreAttackColumns,
+  ...malwareDetectionColumns,
+];
+
+function compareColumnsValue(knownColumns, columnsToCompare) {
+  const unmatchedColumns = columnsToCompare.filter(
+    column => !knownColumns.some(knowColumn => knowColumn.name === column.id),
+  );
+  return unmatchedColumns.length === 0
+    ? true
+    : `This columns doesn't match: ${unmatchedColumns
+        .map(column => column.id)
+        .join(', ')}`;
+}
+
+test('All events columns in KnowFields', () => {
+  expect(compareColumnsValue(KnownFields, eventsColumns)).toBe(true);
+});
+
+test('All technique columns in KnowFields', () => {
+  expect(
+    compareColumnsValue(KnownFields, [
+      ...agentTechniquesColumns,
+      ...techniquesColumns,
+    ]),
+  ).toBe(true);
+});
+
+test('All discover columns in KnowFields', () => {
+  expect(compareColumnsValue(KnownFields, wzDiscoverRenderColumns)).toBe(true);
+});
+
+test('All commons columns in KnowFields', () => {
+  expect(
+    compareColumnsValue(
+      KnownFields,
+      Object.keys(commonColumns).map(key => ({ id: key })),
+    ),
+  ).toBe(true);
+});
