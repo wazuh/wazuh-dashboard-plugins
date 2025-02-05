026ed918b3b6b86ca5511260952f497c4fbd8f98
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-05 16:30:02 -0300

    Add Layout component for structured page layout with sidebar navigation

---
+++ plugins/wazuh-analysis/public/groups/layout.tsx
@@ -0,0 +1,29 @@
+import {
+  EuiPage,
+  EuiPageBody,
+  EuiPageContentBody,
+  EuiPageSideBar,
+  EuiSideNav,
+  EuiSideNavItemType,
+} from '@elastic/eui';
+import React from 'react';
+
+interface LayoutProps {
+  'aria-label': string;
+  items: EuiSideNavItemType<any>[];
+  children: React.ReactChild[];
+}
+
+export const Layout = (props: LayoutProps) => (
+  <EuiPage>
+    <EuiPageSideBar>
+      <EuiSideNav
+        aria-label={props['aria-label']}
+        items={props.items}
+      ></EuiSideNav>
+    </EuiPageSideBar>
+    <EuiPageBody>
+      <EuiPageContentBody>{props.children}</EuiPageContentBody>
+    </EuiPageBody>
+  </EuiPage>
+);
