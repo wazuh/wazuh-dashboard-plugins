55ace2d15c3897292a60959a96b0f31c77dc7bac
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-05 10:46:59 -0300

    Remove unused getCurrentNavGroup utility function from nav-group.ts

--- plugins/wazuh-analysis/public/utils/nav-group.ts
+++
@@ -1,6 +0,0 @@
-import { first } from 'rxjs/operators';
-import { CoreStart } from '../../../../src/core/public';
-
-export async function getCurrentNavGroup(core: CoreStart) {
-  return core.chrome.navGroup.getCurrentNavGroup$().pipe(first()).toPromise();
-}
