ccabc8a79833ee2dabf2d394efad06225f44c52a
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-12 20:35:21 +0000

    fix: correct translation key for analysis plugin title

--- plugins/wazuh-analysis/common/constants.ts
+++ plugins/wazuh-analysis/common/constants.ts
@@ -2,7 +2,7 @@ import { i18n } from '@osd/i18n';
 
 export const PLUGIN_ID = 'analysis';
 export const ANALYSIS_PLUGIN_TITLE = i18n.translate(
-  'wazuhAnalysisanalysis.title',
+  'wazuhAnalysis.title',
   {
     defaultMessage: 'Analysis',
   },
