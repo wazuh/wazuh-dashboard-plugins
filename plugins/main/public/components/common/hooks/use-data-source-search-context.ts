8784cf78bdacec98bfbacf7fd7c8849de72b0220
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2025-03-05 13:54:22 +0100

    feat: add a basic grid table component to display indexer data

---
+++ plugins/main/public/components/common/hooks/use-data-source-search-context.ts
@@ -0,0 +1,177 @@
+import React, { useMemo, useState } from 'react';
+import {
+  TimeRange,
+  Query,
+  Filter,
+  IndexPattern,
+  FilterManager,
+} from '../../../../../../src/plugins/data/public';
+import { getUiSettings } from '../../../kibana-services';
+import {
+  IDataSourceFactoryConstructor,
+  PatternDataSource,
+  tDataSourceRepository,
+  tParsedIndexPattern,
+  useDataSource,
+} from '../data-source';
+import useSearchBar, {
+  tUseSearchBarProps,
+} from '../../common/search-bar/use-search-bar';
+import { useTimeFilter } from './use-time-filter';
+import { transformDateRange } from '../search-bar';
+
+export type CreateNewSearchContext =
+  | boolean
+  | {
+      filters?: Filter[];
+      fetchFilters?: Filter[];
+    };
+
+interface UseDataSourceSearchParams {
+  createNewSearchContext?: CreateNewSearchContext;
+  DataSource: IDataSourceFactoryConstructor<PatternDataSource>;
+  DataSourceRepositoryCreator: tDataSourceRepository<tParsedIndexPattern>;
+}
+
+interface UseSearchBarParams extends UseDataSourceSearchParams {
+  useAbsoluteDateRange: boolean;
+}
+
+function useDataSourceNewSearchContext(context: CreateNewSearchContext) {
+  const filterManager = useMemo(() => new FilterManager(getUiSettings()), []);
+
+  return {
+    filterManager,
+    filters: context?.filters || [],
+    fetchFilters: context?.fetchFilters || [],
+  };
+}
+
+function useDateRangeContext({ createNewSearchContext, dataSource }) {
+  const { timeFilter } = useTimeFilter();
+  const [dateRange, setDateRange] = useState<TimeRange>({
+    from: timeFilter.from,
+    to: timeFilter.to,
+  });
+  const [absoluteDateRange, setAbsoluteDateRange] = useState<TimeRange>(
+    transformDateRange({ from: dateRange.from, to: dateRange.to }),
+  );
+
+  const setAbsoluteDateRangeTransfrom = ({ from, to }) =>
+    setAbsoluteDateRange(transformDateRange({ from, to }));
+
+  return dataSource?.indexPattern?.timeField
+    ? {
+        dateRange,
+        setDateRange,
+        absoluteDateRange,
+        setAbsoluteDateRange: setAbsoluteDateRangeTransfrom,
+      }
+    : {};
+}
+
+export function useDataSourceSearchContext({
+  createNewSearchContext,
+  DataSource,
+  DataSourceRepositoryCreator,
+}: UseDataSourceSearchParams) {
+  const dataSourceDeps = createNewSearchContext
+    ? useDataSourceNewSearchContext(createNewSearchContext)
+    : {};
+
+  const {
+    dataSource,
+    filters,
+    fetchFilters,
+    fixedFilters,
+    isLoading,
+    fetchData,
+    setFilters,
+  } = useDataSource<tParsedIndexPattern, PatternDataSource>({
+    DataSource: DataSource,
+    repository: new DataSourceRepositoryCreator(),
+    ...dataSourceDeps,
+  });
+
+  return {
+    dataSource,
+    filters,
+    fetchFilters,
+    fixedFilters,
+    isLoading,
+    fetchData,
+    setFilters,
+  };
+}
+
+export function useDataSeourceWithSearchBar({
+  createNewSearchContext,
+  DataSource,
+  DataSourceRepositoryCreator,
+  useAbsoluteDateRange = false,
+}: UseSearchBarParams) {
+  // Data source
+  const {
+    dataSource,
+    filters,
+    fixedFilters,
+    fetchFilters,
+    isLoading,
+    fetchData,
+    setFilters,
+  } = useDataSourceSearchContext({
+    createNewSearchContext,
+    DataSource,
+    DataSourceRepositoryCreator,
+  });
+
+  // Query
+  const [newQuery, setNewQuery] = createNewSearchContext
+    ? useState<Query>({ query: '', language: 'kuery' })
+    : [undefined, undefined];
+
+  // Timefilter
+  const { dateRange, setDateRange, absoluteDateRange, setAbsoluteDateRange } =
+    useDateRangeContext({
+      createNewSearchContext,
+      dataSource,
+    });
+
+  // Search bar
+  const { searchBarProps, fingerprint, autoRefreshFingerprint } = useSearchBar({
+    indexPattern: dataSource?.indexPattern as IndexPattern,
+    filters,
+    setFilters,
+    setQuery: setNewQuery,
+    query: newQuery,
+    setTimeFilter: setDateRange,
+  } as tUseSearchBarProps);
+
+  const fetchDataWrapQueryDateRange = params =>
+    fetchData({
+      query: searchBarProps.query,
+      dateRange: dataSource?.indexPattern?.timeField
+        ? useAbsoluteDateRange
+          ? absoluteDateRange
+          : dateRange
+        : undefined,
+      ...params,
+    });
+
+  return {
+    dataSource,
+    filters,
+    fixedFilters,
+    fetchFilters,
+    isLoading,
+    fetchData: fetchDataWrapQueryDateRange,
+    setFilters,
+    searchBarProps,
+    fingerprint,
+    autoRefreshFingerprint,
+    query: searchBarProps.query,
+    dateRange,
+    absoluteDateRange,
+    setAbsoluteDateRange,
+  };
+}
---
+++ plugins/main/public/components/common/wazuh-discover/table.tsx
@@ -0,0 +1,313 @@
+import React, { useEffect, useMemo, useState } from 'react';
+import { IntlProvider } from 'react-intl';
+import {
+  EuiDataGrid,
+  EuiPageTemplate,
+  EuiToolTip,
+  EuiButtonIcon,
+  EuiDataGridCellValueElementProps,
+  EuiFlexGroup,
+  EuiFlyout,
+  EuiFlyoutBody,
+  EuiFlyoutHeader,
+  EuiTitle,
+  EuiButtonEmpty,
+  EuiPanel,
+} from '@elastic/eui';
+import { SearchResponse } from '../../../../../../src/core/server';
+import { HitsCounter } from '../../../kibana-integrations/discover/application/components/hits_counter';
+import { formatNumWithCommas } from '../../../kibana-integrations/discover/application/helpers';
+formatNumWithCommas;
+import { getWazuhCorePlugin } from '../../../kibana-services';
+import {
+  ErrorHandler,
+  ErrorFactory,
+  HttpError,
+} from '../../../react-services/error-management';
+
+import './table.scss';
+
+import {
+  exportSearchToCSV,
+  getAllCustomRenders,
+  MAX_ENTRIES_PER_QUERY,
+  tDataGridColumn,
+  useDataGrid,
+} from '../data-grid';
+
+import { LoadingSearchbarProgress } from '../loading-searchbar-progress/loading-searchbar-progress';
+// common components/hooks
+import { IndexPattern } from '../../../../../../../src/plugins/data/public';
+import { wzDiscoverRenderColumns } from './render-columns';
+import { DocumentViewTableAndJson } from './components/document-view-table-and-json';
+import { WzSearchBar } from '../search-bar';
+import { DataGridVisibleColumnsSelector } from './components/visible-columns-selector';
+import {
+  CreateNewSearchContext,
+  useDataSeourceWithSearchBar,
+} from '../hooks/use-data-source-search-context';
+import {
+  IDataSourceFactoryConstructor,
+  PatternDataSource,
+  tDataSourceRepository,
+  tParsedIndexPattern,
+} from '../data-source';
+
+export interface WzTableDiscoverProps {
+  DataSource: IDataSourceFactoryConstructor<PatternDataSource>;
+  DataSourceRepositoryCreator: tDataSourceRepository<tParsedIndexPattern>;
+  tableDefaultColumns: tDataGridColumn[];
+  createNewSearchContext?: CreateNewSearchContext;
+  useAbsoluteDateRange?: boolean;
+  title?: React.ReactNode;
+}
+
+/**
+ * React component that renders a search bar and a grid table using a data
+ * source, data source repository creator and table default columns
+ * @param param0
+ * @returns
+ */
+export const WzTableDiscover = ({
+  DataSource,
+  DataSourceRepositoryCreator,
+  tableDefaultColumns,
+  createNewSearchContext,
+  useAbsoluteDateRange = false,
+  title,
+}: WzTableDiscoverProps) => {
+  const {
+    dataSource,
+    filters,
+    fetchFilters,
+    fixedFilters,
+    isLoading: isDataSourceLoading,
+    fetchData,
+    setFilters,
+    searchBarProps,
+    fingerprint,
+  } = useDataSeourceWithSearchBar({
+    DataSource,
+    DataSourceRepositoryCreator,
+    createNewSearchContext,
+    useAbsoluteDateRange,
+  });
+  const { query } = searchBarProps;
+
+  const [results, setResults] = useState<SearchResponse>({} as SearchResponse);
+  const [inspectedHit, setInspectedHit] = useState<any>(undefined);
+  const [isExporting, setIsExporting] = useState<boolean>(false);
+
+  const sideNavDocked = getWazuhCorePlugin().hooks.useDockedSideNav();
+
+  const onClickInspectDoc = useMemo(
+    () => (index: number) => {
+      const rowClicked = results.hits.hits[index];
+      setInspectedHit(rowClicked);
+    },
+    [results],
+  );
+
+  const DocViewInspectButton = ({
+    rowIndex,
+  }: EuiDataGridCellValueElementProps) => {
+    const inspectHintMsg = 'Inspect details';
+    return (
+      <EuiToolTip content={inspectHintMsg}>
+        <EuiButtonIcon
+          onClick={() => onClickInspectDoc(rowIndex)}
+          iconType='inspect'
+          aria-label={inspectHintMsg}
+        />
+      </EuiToolTip>
+    );
+  };
+
+  const dataGridProps = useDataGrid({
+    ariaLabelledBy: 'Table',
+    defaultColumns: tableDefaultColumns,
+    renderColumns: wzDiscoverRenderColumns,
+    results,
+    indexPattern: dataSource?.indexPattern as IndexPattern,
+    DocViewInspectButton,
+    filters,
+    setFilters,
+  });
+
+  const { pagination, sorting, columnVisibility } = dataGridProps;
+
+  const onClickExportResults = async () => {
+    const params = {
+      indexPattern: dataSource?.indexPattern as IndexPattern,
+      filters: fetchFilters,
+      query,
+      fields: columnVisibility.visibleColumns,
+      pagination: {
+        pageIndex: 0,
+        pageSize: results.hits.total,
+      },
+      sorting,
+    };
+    try {
+      setIsExporting(true);
+      await exportSearchToCSV(params);
+    } catch (error) {
+      const searchError = ErrorFactory.create(HttpError, {
+        error,
+        message: 'Error downloading csv report',
+      });
+      ErrorHandler.handleError(searchError);
+    } finally {
+      setIsExporting(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isDataSourceLoading) {
+      return;
+    }
+    fetchData({ pagination, sorting })
+      .then(results => {
+        setResults(results);
+      })
+      .catch(error => {
+        const searchError = ErrorFactory.create(HttpError, {
+          error,
+          message: 'Error fetching data',
+        });
+        ErrorHandler.handleError(searchError);
+      });
+  }, [
+    JSON.stringify(fetchFilters),
+    JSON.stringify(query),
+    JSON.stringify(pagination),
+    JSON.stringify(sorting),
+    fingerprint,
+    isDataSourceLoading,
+  ]);
+
+  const closeFlyoutHandler = () => setInspectedHit(undefined);
+
+  return (
+    <IntlProvider locale='en'>
+      <>
+        <EuiPageTemplate
+          className='wz-table-discover-container'
+          restrictWidth='100%'
+          fullHeight={true}
+          grow
+          paddingSize='none'
+          pageContentProps={{ color: 'transparent' }}
+        >
+          <>
+            {isDataSourceLoading ? (
+              <LoadingSearchbarProgress />
+            ) : (
+              <>
+                {title && (
+                  <EuiTitle data-test-subj='wz-discovertitle' size='s'>
+                    <h1>{title}</h1>
+                  </EuiTitle>
+                )}
+                <WzSearchBar
+                  appName='wzTable'
+                  {...searchBarProps}
+                  filters={filters}
+                  fixedFilters={fixedFilters}
+                  showDatePicker={false}
+                  showQueryInput={true}
+                  showQueryBar={true}
+                  showSaveQuery={true}
+                />
+              </>
+            )}
+            {!isDataSourceLoading ? (
+              <EuiPanel
+                paddingSize='s'
+                hasShadow={false}
+                hasBorder={false}
+                color='transparent'
+              >
+                <div>
+                  <EuiDataGrid
+                    {...dataGridProps}
+                    className={sideNavDocked ? 'dataGridDockedNav' : ''}
+                    toolbarVisibility={{
+                      showColumnSelector: { allowHide: false },
+                      additionalControls: (
+                        <>
+                          <HitsCounter
+                            hits={results?.hits?.total}
+                            showResetButton={false}
+                            tooltip={
+                              results?.hits?.total &&
+                              results?.hits?.total > MAX_ENTRIES_PER_QUERY
+                                ? {
+                                    ariaLabel: 'Info',
+                                    content: `The query results has exceeded the limit of ${formatNumWithCommas(
+                                      MAX_ENTRIES_PER_QUERY,
+                                    )} hits. To provide a better experience the table only shows the first ${formatNumWithCommas(
+                                      MAX_ENTRIES_PER_QUERY,
+                                    )} hits.`,
+                                    iconType: 'iInCircle',
+                                    position: 'top',
+                                  }
+                                : undefined
+                            }
+                          />
+                          <EuiButtonEmpty
+                            disabled={
+                              results?.hits?.total === 0 ||
+                              !columnVisibility?.visibleColumns?.length
+                            }
+                            size='xs'
+                            iconType='exportAction'
+                            color='text'
+                            isLoading={isExporting}
+                            className='euiDataGrid__controlBtn'
+                            onClick={onClickExportResults}
+                          >
+                            Export Formatted
+                          </EuiButtonEmpty>
+
+                          <DataGridVisibleColumnsSelector
+                            availableColumns={dataGridProps.columnsAvailable}
+                            columnVisibility={dataGridProps.columnVisibility}
+                          />
+                        </>
+                      ),
+                    }}
+                  />
+                </div>
+              </EuiPanel>
+            ) : null}
+            {inspectedHit && (
+              <EuiFlyout onClose={closeFlyoutHandler} size='m'>
+                <EuiFlyoutHeader>
+                  <EuiTitle>
+                    <h2>Details</h2>
+                  </EuiTitle>
+                </EuiFlyoutHeader>
+                <EuiFlyoutBody>
+                  <EuiFlexGroup direction='column'>
+                    <DocumentViewTableAndJson
+                      document={inspectedHit}
+                      indexPattern={dataSource?.indexPattern}
+                      renderFields={getAllCustomRenders(
+                        tableDefaultColumns,
+                        wzDiscoverRenderColumns,
+                      )}
+                      filters={filters}
+                      setFilters={setFilters}
+                      onFilter={closeFlyoutHandler}
+                    />
+                  </EuiFlexGroup>
+                </EuiFlyoutBody>
+              </EuiFlyout>
+            )}
+          </>
+        </EuiPageTemplate>
+      </>
+    </IntlProvider>
+  );
+};
