0e15951bdea2922fc0b39b703cd6de18ad16069d
Guido Modarelli <guidomodarelli25@gmail.com>
2025-03-07 14:08:33 +0000

    modularize text styling functions
    Extracts text styling functions into a separate script to improve code organization and reusability. Comments out case statement temporarily.

--- docker/osd-dev/dev.sh
+++ docker/osd-dev/dev.sh
@@ -2,139 +2,7 @@
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
-# Color name to code mapping
-declare -A COLORS=(
-  [black]=30
-  [red]=31
-  [green]=32
-  [yellow]=33
-  [blue]=34
-  [magenta]=35
-  [cyan]=36
-  [white]=37
-  [gray]=90
-)
-
-# Modifiers
-BOLD=1
-ITALIC=3
-UNDERLINE=4
-REVERSE=7
-STRIKETHROUGH=9
-
-styleText() {
-  local MODIFIERS=""
-  while [[ $# -gt 0 ]]; do
-    if [[ "$1" != -* ]]; then
-      break
-    fi
-    case "$1" in
-    -b | --bold)
-      MODIFIERS="${MODIFIERS};$BOLD"
-      shift
-      ;;
-    -i | --italic)
-      MODIFIERS="${MODIFIERS};$ITALIC"
-      shift
-      ;;
-    -u | --underline)
-      MODIFIERS="${MODIFIERS};$UNDERLINE"
-      shift
-      ;;
-    -s | --strikethrough)
-      MODIFIERS="${MODIFIERS};$STRIKETHROUGH"
-      shift
-      ;;
-    -r | --reverse)
-      MODIFIERS="${MODIFIERS};$REVERSE"
-      shift
-      ;;
-    -c | --color)
-      if [[ -n "$2" && -n "${COLORS[$2]}" ]]; then
-        MODIFIERS="${MODIFIERS};${COLORS[$2]}"
-        shift 2
-      else
-        echo "Invalid color name: $2"
-        echo "Available colors: ${!COLORS[@]}"
-        return 1
-      fi
-      ;;
-    --)
-      shift
-      break
-      ;;
-    *)
-      echo
-      echo
-      echo "Unknown option: $0 '$(printCyan -i -- "$1")'"
-      local help=""
-      help+="Usage: styleText [OPTIONS] TEXT\n"
-      help+="Options:\n"
-      printf "$help"
-      {
-        echo "  -b, --bold|Bold text"
-        echo "  -i, --italic|Italic text"
-        echo "  -u, --underline|Underline text"
-        echo "  -s, --strikethrough|Strikethrough text"
-        echo "  -r, --reverse|Reverse colors"
-        echo "  -c, --color|Text color name (${!COLORS[@]})"
-      } | column -t -s '|'
-      exit 1
-      ;;
-    esac
-  done
-  local ANSI_ESCAPE=$(printf "\033[${MODIFIERS}m")
-  local ANSI_END=$(printf "\033[m")
-  printf "$ANSI_ESCAPE%s$ANSI_END" "$@"
-}
-
-printWhite() {
-  styleText -c white "$@"
-}
-
-printCyan() {
-  styleText -c cyan "$@"
-}
-
-printMagenta() {
-  styleText -c magenta "$@"
-}
-
-printBlue() {
-  styleText -c blue "$@"
-}
-
-printYellow() {
-  styleText -c yellow "$@"
-}
-
-printGreen() {
-  styleText -c green "$@"
-}
-
-printRed() {
-  styleText -c red "$@"
-}
-
-printGray() {
-  styleText -c gray "$@"
-}
-
-printInfo() {
-  printf "[ $(printBlue "INFO") ] $@\n"
-}
-
-printWarn() {
-  printf "[ $(printYellow "WARN") ] $@\n"
-}
-
-printError() {
-  printf "[ $(printRed "ERROR") ] $@\n"
-}
-
-printCommand() {
-  printf "$(printGreen -b "$") $@\n"
-}
+source $ROOT_DIR/docker/scripts/style_text.sh
 
 usage() {
   echo
@@ -271,44 +139,44 @@ if [[ "$OSD_MAJOR" == "2.x" ]]; then
   export SEC_CONFIG_PATH=/usr/share/opensearch/config/opensearch-security
 fi
 
-case "$2" in
-up)
-  /bin/bash ../scripts/create_docker_networks.sh
-  docker compose --profile $profile -f dev.yml up -Vd
+# case "$2" in
+# up)
+#   /bin/bash ../scripts/create_docker_networks.sh
+#   docker compose --profile $profile -f dev.yml up -Vd
 
-  # Display a command to deploy an agent when using the real server
-  if [[ "$3" =~ "server" ]]; then
-    echo
-    echo "**************WARNING**************"
-    echo "The agent version must be a published one. This uses only released versions."
-    echo "If you need to change de version, edit the command as you see fit."
-    echo "***********************************"
-    echo "1. (Optional) Enroll an agent (Ubuntu 20.04):"
-    echo "docker run --name ${COMPOSE_PROJECT_NAME}-agent-\$(date +%s) --network os-dev-${OS_VERSION} --label com.docker.compose.project=${COMPOSE_PROJECT_NAME} --env WAZUH_AGENT_VERSION=${WAZUH_STACK} -d ubuntu:20.04 bash -c '"
-    echo "  apt update -y"
-    echo "  apt install -y curl lsb-release"
-    echo "  curl -so \wazuh-agent-\${WAZUH_AGENT_VERSION}.deb \\"
-    echo "    https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_\${WAZUH_AGENT_VERSION}-1_amd64.deb \\"
-    echo "    && WAZUH_MANAGER='wazuh.manager' WAZUH_AGENT_GROUP='default' dpkg -i ./wazuh-agent-\${WAZUH_AGENT_VERSION}.deb"
-    echo
-    echo "  /etc/init.d/wazuh-agent start"
-    echo "  tail -f /var/ossec/logs/ossec.log"
-    echo "'"
-    echo
-  fi
-  ;;
-down)
-  docker compose --profile $profile -f dev.yml down -v --remove-orphans
-  ;;
-start)
-  docker compose --profile $profile -f dev.yml -p ${COMPOSE_PROJECT_NAME} start
-  ;;
-stop)
-  docker compose --profile $profile -f dev.yml -p ${COMPOSE_PROJECT_NAME} stop
-  ;;
-*)
-  echo "[ERROR] Action must be up | down | stop | start: "
-  echo
-  usage
-  ;;
-esac
+#   # Display a command to deploy an agent when using the real server
+#   if [[ "$3" =~ "server" ]]; then
+#     echo
+#     echo "**************WARNING**************"
+#     echo "The agent version must be a published one. This uses only released versions."
+#     echo "If you need to change de version, edit the command as you see fit."
+#     echo "***********************************"
+#     echo "1. (Optional) Enroll an agent (Ubuntu 20.04):"
+#     echo "docker run --name ${COMPOSE_PROJECT_NAME}-agent-\$(date +%s) --network os-dev-${OS_VERSION} --label com.docker.compose.project=${COMPOSE_PROJECT_NAME} --env WAZUH_AGENT_VERSION=${WAZUH_STACK} -d ubuntu:20.04 bash -c '"
+#     echo "  apt update -y"
+#     echo "  apt install -y curl lsb-release"
+#     echo "  curl -so \wazuh-agent-\${WAZUH_AGENT_VERSION}.deb \\"
+#     echo "    https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_\${WAZUH_AGENT_VERSION}-1_amd64.deb \\"
+#     echo "    && WAZUH_MANAGER='wazuh.manager' WAZUH_AGENT_GROUP='default' dpkg -i ./wazuh-agent-\${WAZUH_AGENT_VERSION}.deb"
+#     echo
+#     echo "  /etc/init.d/wazuh-agent start"
+#     echo "  tail -f /var/ossec/logs/ossec.log"
+#     echo "'"
+#     echo
+#   fi
+#   ;;
+# down)
+#   docker compose --profile $profile -f dev.yml down -v --remove-orphans
+#   ;;
+# start)
+#   docker compose --profile $profile -f dev.yml -p ${COMPOSE_PROJECT_NAME} start
+#   ;;
+# stop)
+#   docker compose --profile $profile -f dev.yml -p ${COMPOSE_PROJECT_NAME} stop
+#   ;;
+# *)
+#   echo "[ERROR] Action must be up | down | stop | start: "
+#   echo
+#   usage
+#   ;;
+# esac
---
+++ docker/scripts/style_text.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+# Color name to code mapping
+declare -A COLORS=(
+  [black]=30
+  [red]=31
+  [green]=32
+  [yellow]=33
+  [blue]=34
+  [magenta]=35
+  [cyan]=36
+  [white]=37
+  [gray]=90
+)
+
+# Modifiers
+BOLD=1
+ITALIC=3
+UNDERLINE=4
+REVERSE=7
+STRIKETHROUGH=9
+
+styleText() {
+  local MODIFIERS=""
+  while [[ $# -gt 0 ]]; do
+    if [[ "$1" != -* ]]; then
+      break
+    fi
+    case "$1" in
+    -b | --bold)
+      MODIFIERS="${MODIFIERS};$BOLD"
+      shift
+      ;;
+    -i | --italic)
+      MODIFIERS="${MODIFIERS};$ITALIC"
+      shift
+      ;;
+    -u | --underline)
+      MODIFIERS="${MODIFIERS};$UNDERLINE"
+      shift
+      ;;
+    -s | --strikethrough)
+      MODIFIERS="${MODIFIERS};$STRIKETHROUGH"
+      shift
+      ;;
+    -r | --reverse)
+      MODIFIERS="${MODIFIERS};$REVERSE"
+      shift
+      ;;
+    -c | --color)
+      if [[ -n "$2" && -n "${COLORS[$2]}" ]]; then
+        MODIFIERS="${MODIFIERS};${COLORS[$2]}"
+        shift 2
+      else
+        echo "Invalid color name: $2"
+        echo "Available colors: ${!COLORS[@]}"
+        return 1
+      fi
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo
+      echo
+      echo "Unknown option: $0 '$(printCyan -i -- "$1")'"
+      local help=""
+      help+="Usage: styleText [OPTIONS] TEXT\n"
+      help+="Options:\n"
+      printf "$help"
+      {
+        echo "  -b, --bold|Bold text"
+        echo "  -i, --italic|Italic text"
+        echo "  -u, --underline|Underline text"
+        echo "  -s, --strikethrough|Strikethrough text"
+        echo "  -r, --reverse|Reverse colors"
+        echo "  -c, --color|Text color name (${!COLORS[@]})"
+      } | column -t -s '|'
+      exit 1
+      ;;
+    esac
+  done
+  local ANSI_ESCAPE=$(printf "\033[${MODIFIERS}m")
+  local ANSI_END=$(printf "\033[m")
+  printf "$ANSI_ESCAPE%s$ANSI_END" "$@"
+}
+
+printWhite() {
+  styleText -c white "$@"
+}
+
+printCyan() {
+  styleText -c cyan "$@"
+}
+
+printMagenta() {
+  styleText -c magenta "$@"
+}
+
+printBlue() {
+  styleText -c blue "$@"
+}
+
+printYellow() {
+  styleText -c yellow "$@"
+}
+
+printGreen() {
+  styleText -c green "$@"
+}
+
+printRed() {
+  styleText -c red "$@"
+}
+
+printGray() {
+  styleText -c gray "$@"
+}
+
+printInfo() {
+  printf "[ $(printBlue "INFO") ] $@\n"
+}
+
+printWarn() {
+  printf "[ $(printYellow "WARN") ] $@\n"
+}
+
+printError() {
+  printf "[ $(printRed "ERROR") ] $@\n"
+}
+
+printCommand() {
+  printf "$(printGreen -b "$") $@\n"
+}
