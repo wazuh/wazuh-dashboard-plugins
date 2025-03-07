0179c39bd34afd9bb16dad03855e3fb98efe8f06
Guido Modarelli <guidomodarelli25@gmail.com>
2025-03-07 12:59:09 +0000

    add text styling functions and enhance usage instructions
    Introduces functions for text styling with color and modifiers
    Enhances error messages with styled output and usage instructions
    Updates script usage and option descriptions for clarity

--- docker/osd-dev/dev.sh
+++ docker/osd-dev/dev.sh
@@ -1,16 +1,152 @@
 #!/bin/bash
 
+# Color name to code mapping
+declare -A COLORS=(
+  [black]=30
+  [red]=31
+  [green]=32
+  [yellow]=33
+  [blue]=34
+  [magenta]=35
+  [cyan]=36
+  [white]=37
+  [gray]=90
+)
+
+# Modifiers
+BOLD=1
+ITALIC=3
+UNDERLINE=4
+REVERSE=7
+STRIKETHROUGH=9
+
+styleText() {
+  local MODIFIERS=""
+  while [[ $# -gt 0 ]]; do
+    if [[ "$1" != -* ]]; then
+      break
+    fi
+    case "$1" in
+    -b | --bold)
+      MODIFIERS="${MODIFIERS};$BOLD"
+      shift
+      ;;
+    -i | --italic)
+      MODIFIERS="${MODIFIERS};$ITALIC"
+      shift
+      ;;
+    -u | --underline)
+      MODIFIERS="${MODIFIERS};$UNDERLINE"
+      shift
+      ;;
+    -s | --strikethrough)
+      MODIFIERS="${MODIFIERS};$STRIKETHROUGH"
+      shift
+      ;;
+    -r | --reverse)
+      MODIFIERS="${MODIFIERS};$REVERSE"
+      shift
+      ;;
+    -c | --color)
+      if [[ -n "$2" && -n "${COLORS[$2]}" ]]; then
+        MODIFIERS="${MODIFIERS};${COLORS[$2]}"
+        shift 2
+      else
+        echo "Invalid color name: $2"
+        echo "Available colors: ${!COLORS[@]}"
+        return 1
+      fi
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo
+      echo
+      echo "Unknown option: $0 '$(printCyan -i -- "$1")'"
+      local help=""
+      help+="Usage: styleText [OPTIONS] TEXT\n"
+      help+="Options:\n"
+      printf "$help"
+      {
+        echo "  -b, --bold|Bold text"
+        echo "  -i, --italic|Italic text"
+        echo "  -u, --underline|Underline text"
+        echo "  -s, --strikethrough|Strikethrough text"
+        echo "  -r, --reverse|Reverse colors"
+        echo "  -c, --color|Text color name (${!COLORS[@]})"
+      } | column -t -s '|'
+      exit 1
+      ;;
+    esac
+  done
+  local ANSI_ESCAPE=$(printf "\033[${MODIFIERS}m")
+  local ANSI_END=$(printf "\033[m")
+  printf "$ANSI_ESCAPE%s$ANSI_END" "$@"
+}
+
+printWhite() {
+  styleText -c white "$@"
+}
+
+printCyan() {
+  styleText -c cyan "$@"
+}
+
+printMagenta() {
+  styleText -c magenta "$@"
+}
+
+printBlue() {
+  styleText -c blue "$@"
+}
+
+printYellow() {
+  styleText -c yellow "$@"
+}
+
+printGreen() {
+  styleText -c green "$@"
+}
+
+printRed() {
+  styleText -c red "$@"
+}
+
+printGray() {
+  styleText -c gray "$@"
+}
+
+printInfo() {
+  printf "[ $(printBlue "INFO") ] $@\n"
+}
+
+printWarn() {
+  printf "[ $(printYellow "WARN") ] $@\n"
+}
+
+printError() {
+  printf "[ $(printRed "ERROR") ] $@\n"
+}
+
+printCommand() {
+  printf "$(printGreen -b "$") $@\n"
+}
+
 usage() {
   echo
-  echo "./dev.sh [-os os_version] [-osd osd_version] /wazuh_app_src action [saml/server] [server_version]"
+  echo "$0 [-os os_version] [-osd osd_version] [--wz-home] [--saml|--server server_version] (-a | --action)"
   echo
-  echo "where"
-  echo "  -o os_version Specify the OS version (optional)"
-  echo "  -d osd_version Specify the OSD version (optional)"
-  echo "  wazuh_app_src is the path to the wazuh application source code"
-  echo "  action is one of up | down | stop"
-  echo "  saml to deploy a saml enabled environment (optional)"
-  echo "  server to deploy a real server enabled environment (optional)"
+  echo "Where:"
+  {
+    echo "  $(printGreen -- "-o <os_version>") @ ($(printGray optional)) Specify the OS version"
+    echo "  $(printGreen -- "-d <osd_version>") @ ($(printGray optional)) Specify the OSD version"
+    echo "  $(printGreen -- "--wz-home <wazuh_app_source>") @ ($(printGray optional), default: '$(printCyan -- "../../plugins")') The path where the wazuh application source code is located"
+    echo "  $(printGreen -- "--saml") @ ($(printGray optional)) To deploy a saml enabled environment"
+    echo "  $(printGreen -- "--server <server_version>") @ ($(printGray optional)) To deploy a real server enabled environment"
+    echo "  $(printGreen -- "-a"), $(printGreen -- "--action") @ Is one of ( up | down | stop | start )"
+  } | column -t -s '@'
   exit -1
 }
 
@@ -19,12 +155,17 @@ exit_with_message() {
   exit -1
 }
 
-if ! command -v jq &> /dev/null; then
-  echo "[ERROR] jq is not installed. Please install jq to continue."
-  echo "sudo apt-get install jq in Debian/Ubuntu OS"
-  echo "sudo yum install jq in RedHat/CentOS OS"
-  echo "sudo pacman -Sy --noconfirm jq in Arch OS"
-  echo "brew install jq in MAC OS"
+if ! command -v jq &>/dev/null; then
+  echo
+  printError "'$(printCyan -b -i jq)' is not installed. Please install '$(printCyan -b -i jq)' to continue."
+  echo
+  {
+    printCommand "sudo apt-get install jq @ $(printGray "# in Debian/Ubuntu OS")"
+    printCommand "sudo yum install jq @ $(printGray "# in RedHat/CentOS OS")"
+    printCommand "sudo pacman -Sy --noconfirm jq @ $(printGray "# in Arch OS")"
+    printCommand "brew install jq @ $(printGray "# in MAC OS")"
+  } | column -t -s '@'
+  echo
   exit 1
 fi
 
@@ -129,42 +270,42 @@ fi
 
 case "$2" in
 up)
-   /bin/bash ../scripts/create_docker_networks.sh
-   docker compose --profile $profile -f dev.yml up -Vd
+  /bin/bash ../scripts/create_docker_networks.sh
+  docker compose --profile $profile -f dev.yml up -Vd
 
-   # Display a command to deploy an agent when using the real server
-   if [[ "$3" =~ "server" ]]; then
-     echo
-     echo "**************WARNING**************"
-     echo "The agent version must be a published one. This uses only released versions."
-     echo "If you need to change de version, edit the command as you see fit."
-     echo "***********************************"
-     echo "1. (Optional) Enroll an agent (Ubuntu 20.04):"
-     echo "docker run --name ${COMPOSE_PROJECT_NAME}-agent-\$(date +%s) --network os-dev-${OS_VERSION} --label com.docker.compose.project=${COMPOSE_PROJECT_NAME} --env WAZUH_AGENT_VERSION=${WAZUH_STACK} -d ubuntu:20.04 bash -c '"
-     echo "  apt update -y"
-     echo "  apt install -y curl lsb-release"
-     echo "  curl -so \wazuh-agent-\${WAZUH_AGENT_VERSION}.deb \\"
-     echo "    https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_\${WAZUH_AGENT_VERSION}-1_amd64.deb \\"
-     echo "    && WAZUH_MANAGER='wazuh.manager' WAZUH_AGENT_GROUP='default' dpkg -i ./wazuh-agent-\${WAZUH_AGENT_VERSION}.deb"
-     echo
-     echo "  /etc/init.d/wazuh-agent start"
-     echo "  tail -f /var/ossec/logs/ossec.log"
-     echo "'"
-     echo
-   fi
-   ;;
- down)
-   docker compose --profile $profile -f dev.yml down -v --remove-orphans
-   ;;
- start)
-   docker compose --profile $profile -f dev.yml -p ${COMPOSE_PROJECT_NAME} start
-   ;;
- stop)
-   docker compose --profile $profile -f dev.yml -p ${COMPOSE_PROJECT_NAME} stop
-   ;;
- *)
-   echo "[ERROR] Action must be up | down | stop | start: "
-   echo
-   usage
-   ;;
+  # Display a command to deploy an agent when using the real server
+  if [[ "$3" =~ "server" ]]; then
+    echo
+    echo "**************WARNING**************"
+    echo "The agent version must be a published one. This uses only released versions."
+    echo "If you need to change de version, edit the command as you see fit."
+    echo "***********************************"
+    echo "1. (Optional) Enroll an agent (Ubuntu 20.04):"
+    echo "docker run --name ${COMPOSE_PROJECT_NAME}-agent-\$(date +%s) --network os-dev-${OS_VERSION} --label com.docker.compose.project=${COMPOSE_PROJECT_NAME} --env WAZUH_AGENT_VERSION=${WAZUH_STACK} -d ubuntu:20.04 bash -c '"
+    echo "  apt update -y"
+    echo "  apt install -y curl lsb-release"
+    echo "  curl -so \wazuh-agent-\${WAZUH_AGENT_VERSION}.deb \\"
+    echo "    https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_\${WAZUH_AGENT_VERSION}-1_amd64.deb \\"
+    echo "    && WAZUH_MANAGER='wazuh.manager' WAZUH_AGENT_GROUP='default' dpkg -i ./wazuh-agent-\${WAZUH_AGENT_VERSION}.deb"
+    echo
+    echo "  /etc/init.d/wazuh-agent start"
+    echo "  tail -f /var/ossec/logs/ossec.log"
+    echo "'"
+    echo
+  fi
+  ;;
+down)
+  docker compose --profile $profile -f dev.yml down -v --remove-orphans
+  ;;
+start)
+  docker compose --profile $profile -f dev.yml -p ${COMPOSE_PROJECT_NAME} start
+  ;;
+stop)
+  docker compose --profile $profile -f dev.yml -p ${COMPOSE_PROJECT_NAME} stop
+  ;;
+*)
+  echo "[ERROR] Action must be up | down | stop | start: "
+  echo
+  usage
+  ;;
 esac
