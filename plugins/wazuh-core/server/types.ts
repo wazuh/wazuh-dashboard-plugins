e30b446ca95232a56a0a5fb342db6f094966be6e
Antonio <34042064+Desvelao@users.noreply.github.com>
2025-03-13 12:24:33 +0100

    Remove server hosts configuration (#7347)
    change: remove wazuh_core.hosts settings
    
    - Remove wazuh_core.hosts settings
    - Removed initiation of ManageHosts and ServerAPIClient in the server
      side
    - Removed server API client exposed through the wazuh_core plugin in the
      plugin lifecycle methods
    - Disconnected/mocked some services to allow the plugins does not fail
    
    Co-authored-by: Federico Rodriguez <federico.rodriguez@wazuh.com>

--- plugins/wazuh-core/server/types.ts
+++ plugins/wazuh-core/server/types.ts
@@ -1,27 +1,9 @@
-import {
-  ServerAPIInternalUserClient,
-  ServerAPIScopedUserClient,
-  WazuhCoreServices,
-} from './services';
+import { WazuhCoreServices } from './services';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface WazuhCorePluginSetup extends WazuhCoreServices {
-  api: {
-    client: {
-      asInternalUser: ServerAPIInternalUserClient;
-      asScoped: (context: any, request: any) => ServerAPIScopedUserClient;
-    };
-  };
-}
+export interface WazuhCorePluginSetup extends WazuhCoreServices {}
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface WazuhCorePluginStart extends WazuhCoreServices {
-  api: {
-    client: {
-      asInternalUser: ServerAPIInternalUserClient;
-      asScoped: (context: any, request: any) => ServerAPIScopedUserClient;
-    };
-  };
-}
+export interface WazuhCorePluginStart extends WazuhCoreServices {}
 
 export interface PluginSetup {
   securityDashboards?: object; // TODO: Add OpenSearch Dashboards Security interface
