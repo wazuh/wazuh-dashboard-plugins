627ba2aa28fde3bb82f393334f4d74ffdc06b55d
Guido Modarelli <guidomodarelli25@gmail.com>
2025-03-31 17:40:22 +0000

    extract pagination constants to a separate file
    Refactors pagination-related constants by moving them from
    the data-grid hook to a dedicated constants file for better
    reusability and maintainability across components.

--- plugins/main/public/components/common/data-grid/use-data-grid.tsx
+++ plugins/main/public/components/common/data-grid/use-data-grid.tsx
@@ -27,16 +27,6 @@ export interface PaginationOptions
 
 type SortingColumns = EuiDataGridSorting['columns'];
 
-const MAX_ENTRIES_PER_QUERY = 10000;
-const DEFAULT_PAGE_INDEX = 0;
-const DEFAULT_PAGE_SIZE = 15;
-const DEFAULT_PAGE_SIZE_OPTIONS = [DEFAULT_PAGE_SIZE, 25, 50, 100];
-const DEFAULT_PAGINATION_OPTIONS: PaginationOptions = {
-  pageIndex: DEFAULT_PAGE_INDEX,
-  pageSize: DEFAULT_PAGE_SIZE,
-  pageSizeOptions: DEFAULT_PAGE_SIZE_OPTIONS,
-};
-
 export interface RenderColumn {
   render: (value: any, rowItem: object) => string | React.ReactNode;
 }
