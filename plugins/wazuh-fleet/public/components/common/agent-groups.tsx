fe183089baf6486cb4f780b4eccd5eb6bee2749d
Federico Rodriguez <federico.rodriguez@wazuh.com>
2025-04-03 21:37:07 +0200

    Enhance groups column render

--- plugins/wazuh-fleet/public/components/common/agent-groups.tsx
+++ plugins/wazuh-fleet/public/components/common/agent-groups.tsx
@@ -1,16 +1,88 @@
-import React from 'react';
-import { EuiFlexGroup, EuiFlexItem, EuiBadge } from '@elastic/eui';
+import React, { useState } from 'react';
+import {
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiPopover,
+  EuiFlexGrid,
+  EuiLink,
+  EuiBadge,
+} from '@elastic/eui';
 
 export interface AgentGroupsProps {
   groups: string[];
+  strLength?: number;
 }
 
-export const AgentGroups = ({ groups }: AgentGroupsProps) => (
-  <EuiFlexGroup responsive={false} wrap gutterSize='xs'>
-    {groups?.map((group, index) => (
-      <EuiFlexItem key={`${group}-${index}`} grow={false}>
-        <EuiBadge color='hollow'>{group}</EuiBadge>
-      </EuiFlexItem>
-    ))}
-  </EuiFlexGroup>
+const renderBadge = (group: string, index: number) => (
+  <EuiBadge
+    color={'hollow'}
+    key={`agent-group-${index}`}
+    onClickAriaLabel={`agent-group-${index}`}
+  >
+    {group}
+  </EuiBadge>
 );
+
+export const AgentGroups = ({ groups, strLength = 15 }: AgentGroupsProps) => {
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const auxGroups: React.ReactNode[] = [];
+  const tooltipGroups: React.ReactNode[] = [];
+  let auxLength = 0;
+  let auxIndex = 0;
+
+  if (groups.length >= 2 && groups.toString().length >= strLength) {
+    groups.map((group, index) => {
+      auxLength = auxLength + group.length;
+
+      if (auxLength >= strLength) {
+        tooltipGroups.push(
+          <EuiFlexItem grow={1} key={`agent-group-${index}`}>
+            {renderBadge(group, index)}
+          </EuiFlexItem>,
+        );
+        ++auxIndex;
+      } else {
+        auxGroups.push(renderBadge(group, index));
+      }
+    });
+  } else {
+    groups.map((group, index) => {
+      auxGroups.push(renderBadge(group, index));
+    });
+  }
+
+  return (
+    <span style={{ display: 'inline' }}>
+      <>{auxGroups}</>
+      {auxIndex > 0 && (
+        <EuiPopover
+          button={
+            <EuiLink
+              style={{ textDecoration: 'none', fontWeight: 'normal' }}
+              className={'no-focus'}
+              onMouseDown={(event: React.MouseEvent) => {
+                event.stopPropagation();
+              }}
+              onClick={(event: React.MouseEvent) => {
+                event.stopPropagation();
+                setPopoverOpen(!popoverOpen);
+              }}
+            >
+              &nbsp;{`+${auxIndex} more`}
+            </EuiLink>
+          }
+          isOpen={popoverOpen}
+          closePopover={() => setPopoverOpen(!popoverOpen)}
+        >
+          <EuiFlexGroup style={{ maxWidth: '500px' }} gutterSize='none'>
+            <EuiFlexItem grow={false}>
+              <EuiFlexGrid columns={4} gutterSize={'s'}>
+                {tooltipGroups}
+              </EuiFlexGrid>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiPopover>
+      )}
+    </span>
+  );
+};
