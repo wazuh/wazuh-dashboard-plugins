07f710acd31eb18e202bec38970ac4ae4505aa1d
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2025-04-14 14:23:43 +0200

    fix: refactor the data source repository to use the setting key value instead, fix the related to vulnerabilities

---
+++ plugins/main/public/components/common/data-source/pattern/pattern-data-source-repository-use-setting-value.ts
@@ -0,0 +1,49 @@
+import { PatternDataSourceRepository } from './pattern-data-source-repository';
+import store from '../../../../redux/store';
+import { get } from 'lodash';
+
+export const createPatternDataSourceRepositoryUseSettingValue = (
+  settingKey: string,
+) => {
+  return class PatternDataSourceRepositoryUseSettingValue extends PatternDataSourceRepository {
+    constructor() {
+      super();
+    }
+
+    async get(id: string) {
+      const dataSource = await super.get(id);
+      if (this.validate(dataSource)) {
+        return dataSource;
+      } else {
+        throw new Error('Index pattern not found');
+      }
+    }
+
+    async getAll() {
+      const indexs = await super.getAll();
+      return indexs.filter(this.validate);
+    }
+
+    validate(dataSource): boolean {
+      const fieldsToCheck = ['id', 'attributes.title']; // search in these attributes
+      return fieldsToCheck.some(
+        key =>
+          store.getState()?.appConfig?.data?.[settingKey] &&
+          get(dataSource, key) ===
+            store.getState()?.appConfig?.data?.[settingKey],
+      ); /* WORKAROUND: Get the index pattern configurared in the plugin setting. This imports from
+    the Redux store, but this should use some service as indejected dependency instead
+    Caveats:
+      - configuration should be loaded and set the Redux store
+    */
+    }
+
+    getDefault() {
+      return Promise.resolve(null);
+    }
+
+    setDefault(dataSource: tParsedIndexPattern) {
+      return;
+    }
+  };
+};
