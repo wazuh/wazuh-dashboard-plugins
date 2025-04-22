21a24a4da07ff8c85e4cce082e0c70daca4ca863
Guido Modarelli <guidomodarelli25@gmail.com>
2025-04-22 21:35:28 +0000

    Adds packet loss rate panel to network interfaces inventory
    Defines a metric visualization to calculate and display the global packet loss rate based on ingress/egress drops and packets.
    
    Integrates this visualization as a dashboard panel within the network interfaces inventory view.

---
+++ plugins/main/public/components/overview/it-hygiene/networks/inventories/interfaces/dashboard.ts
@@ -0,0 +1,70 @@
+import { generateVisualization } from '../../../common/create-new-visualization';
+import { STYLE } from '../../../common/saved-vis/constants';
+import {
+  createIndexPatternReferences,
+  createSearchSource,
+} from '../../../common/saved-vis/create-saved-vis-data';
+import { SavedVis } from '../../../common/types';
+
+const getVisStateNetworkInterfacesGlobalPacketLossRate = (
+  indexPatternId: string,
+): SavedVis => {
+  return {
+    id: 'it-hygiene-network-interfaces-global-packet-loss-rate',
+    title: 'Global packet loss rate',
+    type: 'metric',
+    params: {
+      addTooltip: true,
+      addLegend: false,
+      type: 'metric',
+      metric: {
+        percentageMode: true,
+        useRanges: false,
+        colorSchema: 'Green to Red',
+        metricColorMode: 'None',
+        colorsRange: [
+          {
+            from: 0,
+            to: 10000,
+          },
+        ],
+        labels: {
+          show: true,
+        },
+        invertColors: false,
+        style: STYLE,
+      },
+    },
+    data: {
+      searchSource: createSearchSource(indexPatternId),
+      references: createIndexPatternReferences(indexPatternId),
+      aggs: [
+        {
+          id: '1',
+          enabled: true,
+          type: 'sum',
+          params: {
+            field: 'host.network.ingress.drops',
+            json: "{\r\n  \"script\": {\r\n    \"source\": \"double d=(doc['host.network.ingress.drops'].size()!=0?doc['host.network.ingress.drops'].value:0)+(doc['host.network.egress.drops'].size()!=0?doc['host.network.egress.drops'].value:0); double p=(doc['host.network.ingress.packets'].size()!=0?doc['host.network.ingress.packets'].value:0)+(doc['host.network.egress.packets'].size()!=0?doc['host.network.egress.packets'].value:0); return p==0?0:(d/p)*100;\",\r\n    \"lang\": \"painless\"\r\n  }\r\n}\r\n",
+            customLabel: 'Global packet loss rate',
+          },
+          schema: 'metric',
+        },
+      ],
+    },
+  };
+};
+
+export const getOverviewNetworksInterfacesTab = (indexPatternId: string) => {
+  return {
+    ...generateVisualization({
+      key: '0',
+      width: 12,
+      height: 6,
+      positionX: 0,
+      positionY: 0,
+      savedVis:
+        getVisStateNetworkInterfacesGlobalPacketLossRate(indexPatternId),
+    }),
+  };
+};
--- plugins/main/public/components/overview/it-hygiene/networks/inventories/interfaces/inventory.tsx
+++ plugins/main/public/components/overview/it-hygiene/networks/inventories/interfaces/inventory.tsx
@@ -6,18 +6,25 @@ import {
 import tableColumns from './table-columns';
 import managedFilters from './managed-filters';
 import { withSystemInventoryInterfacesDataSource } from '../../../common/hocs/validate-system-inventory-index-pattern';
-import { ITHygieneInventoryDashboardTable } from '../../../common/components/inventory';
+import {
+  ITHygieneInventoryDashboardTable,
+  ITHygieneInventoryDashboardTableProps,
+} from '../../../common/components/inventory';
+import { getOverviewNetworksInterfacesTab } from './dashboard';
 
 export const ITHygieneNetworksInventoryInterfaces =
-  withSystemInventoryInterfacesDataSource(props => {
-    return (
-      <ITHygieneInventoryDashboardTable
-        DataSource={SystemInventoryStatesDataSource}
-        DataSourceRepositoryCreator={
-          SystemInventoryInterfacesStatesDataSourceRepository
-        }
-        tableDefaultColumns={tableColumns}
-        managedFilters={managedFilters}
-      />
-    );
-  });
+  withSystemInventoryInterfacesDataSource(
+    (props: ITHygieneInventoryDashboardTableProps) => {
+      return (
+        <ITHygieneInventoryDashboardTable
+          DataSource={SystemInventoryStatesDataSource}
+          DataSourceRepositoryCreator={
+            SystemInventoryInterfacesStatesDataSourceRepository
+          }
+          tableDefaultColumns={tableColumns}
+          managedFilters={managedFilters}
+          getDashboardPanels={getOverviewNetworksInterfacesTab}
+        />
+      );
+    },
+  );
