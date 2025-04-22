df8fda2c210bbebcfd6924028b74a45309d1f706
Guido Modarelli <guidomodarelli25@gmail.com>
2025-04-22 16:35:25 +0000

    Types props for network inventory component
    Adds explicit type annotation for the component props to improve type safety and code clarity.

--- plugins/main/public/components/overview/it-hygiene/networks/inventories/networks/inventory.tsx
+++ plugins/main/public/components/overview/it-hygiene/networks/inventories/networks/inventory.tsx
@@ -6,20 +6,25 @@ import {
 import tableColumns from './table-columns';
 import managedFilters from './managed-filters';
 import { withSystemInventoryNetworksDataSource } from '../../../common/hocs/validate-system-inventory-index-pattern';
-import { ITHygieneInventoryDashboardTable } from '../../../common/components/inventory';
+import {
+  ITHygieneInventoryDashboardTable,
+  ITHygieneInventoryDashboardTableProps,
+} from '../../../common/components/inventory';
 import { getOverviewNetworksTab } from './dashboard';
 
 export const ITHygieneNetworksInventoryNetworks =
-  withSystemInventoryNetworksDataSource(props => {
-    return (
-      <ITHygieneInventoryDashboardTable
-        DataSource={SystemInventoryStatesDataSource}
-        DataSourceRepositoryCreator={
-          SystemInventoryNetworksStatesDataSourceRepository
-        }
-        tableDefaultColumns={tableColumns}
-        managedFilters={managedFilters}
-        getDashboardPanels={getOverviewNetworksTab}
-      />
-    );
-  });
+  withSystemInventoryNetworksDataSource(
+    (props: ITHygieneInventoryDashboardTableProps) => {
+      return (
+        <ITHygieneInventoryDashboardTable
+          DataSource={SystemInventoryStatesDataSource}
+          DataSourceRepositoryCreator={
+            SystemInventoryNetworksStatesDataSourceRepository
+          }
+          tableDefaultColumns={tableColumns}
+          managedFilters={managedFilters}
+          getDashboardPanels={getOverviewNetworksTab}
+        />
+      );
+    },
+  );
