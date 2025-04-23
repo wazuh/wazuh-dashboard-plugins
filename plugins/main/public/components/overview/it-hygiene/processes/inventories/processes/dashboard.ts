0b767f3136d7cf55d939728e8420494a57f086c5
Guido Modarelli <guidomodarelli25@gmail.com>
2025-04-23 18:17:03 +0000

    Adds dynamic dashboard panels to processes inventory
    Passes `getDashboardPanels` prop to enable fetching specific dashboard configurations for the processes tab.

---
+++ plugins/main/public/components/overview/it-hygiene/processes/inventories/processes/dashboard.ts
@@ -0,0 +1,87 @@
+import { buildDashboardKPIPanels } from '../../../common/create-dashboard-panels-kpis';
+import { STYLE } from '../../../common/saved-vis/constants';
+import {
+  createIndexPatternReferences,
+  createSearchSource,
+} from '../../../common/saved-vis/create-saved-vis-data';
+import { SavedVis } from '../../../common/types';
+
+type ProcessState =
+  | 'Stopped'
+  | 'Zombie'
+  | 'Interruptable Sleep'
+  | 'Uninterruptible Sleep';
+
+const getVisStateProcessesState = (
+  indexPatternId: string,
+  processState: ProcessState,
+): SavedVis => {
+  return {
+    id: 'it-hygiene-processes-state',
+    title: 'Processes state',
+    type: 'metric',
+    params: {
+      addLegend: false,
+      addTooltip: true,
+      metric: {
+        colorSchema: 'Green to Red',
+        colorsRange: [
+          {
+            from: 0,
+            to: 10000,
+          },
+        ],
+        invertColors: false,
+        labels: {
+          show: true,
+        },
+        metricColorMode: 'None',
+        percentageMode: false,
+        style: STYLE,
+        useRanges: false,
+      },
+      type: 'metric',
+    },
+    data: {
+      searchSource: createSearchSource(indexPatternId),
+      references: createIndexPatternReferences(indexPatternId),
+      aggs: [
+        {
+          id: '1',
+          enabled: true,
+          type: 'count',
+          params: {
+            customLabel: processState,
+          },
+          schema: 'metric',
+        },
+        {
+          id: '2',
+          enabled: true,
+          type: 'filters',
+          params: {
+            filters: [
+              {
+                input: {
+                  query: `process.state: ${processState}`,
+                  language: 'kuery',
+                },
+                label: 'Process State',
+              },
+            ],
+          },
+          schema: 'group',
+        },
+      ],
+    },
+  };
+};
+
+export const getOverviewProcessesProcessesTab = (indexPatternId: string) => {
+  return buildDashboardKPIPanels([
+    getVisStateProcessesState(indexPatternId, 'Stopped'),
+    getVisStateProcessesState(indexPatternId, 'Zombie'),
+    getVisStateProcessesState(indexPatternId, 'Interruptable Sleep'),
+    getVisStateProcessesState(indexPatternId, 'Uninterruptible Sleep'),
+  ]);
+};
--- plugins/main/public/components/overview/it-hygiene/processes/inventories/processes/inventory.tsx
+++ plugins/main/public/components/overview/it-hygiene/processes/inventories/processes/inventory.tsx
@@ -7,6 +7,7 @@ import tableColumns from './table-columns';
 import managedFilters from './managed-filters';
 import { withSystemInventoryProcessesDataSource } from '../../../common/hocs/validate-system-inventory-index-pattern';
 import { ITHygieneInventoryDashboardTable } from '../../../common/components/inventory';
+import { getOverviewProcessesProcessesTab } from './dashboard';
 
 export const ITHygieneProccessesInventoryProcesses =
   withSystemInventoryProcessesDataSource(props => {
@@ -18,6 +19,7 @@ export const ITHygieneProccessesInventoryProcesses =
         }
         tableDefaultColumns={tableColumns}
         managedFilters={managedFilters}
+        getDashboardPanels={getOverviewProcessesProcessesTab}
       />
     );
   });
