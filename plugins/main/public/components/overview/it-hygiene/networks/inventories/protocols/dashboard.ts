515c0728cf7a11ad10db50b735e03f4b14b1247d
Guido Modarelli <guidomodarelli25@gmail.com>
2025-04-23 17:48:32 +0000

    Adds wireless network interfaces metric
    Introduces a new Key Performance Indicator (KPI) to display the count of wireless network interfaces.
    Integrates the new metric into the networks protocols dashboard tab.

--- plugins/main/public/components/overview/it-hygiene/networks/inventories/protocols/dashboard.ts
+++ plugins/main/public/components/overview/it-hygiene/networks/inventories/protocols/dashboard.ts
@@ -1,12 +1,83 @@
 import { buildDashboardKPIPanels } from '../../../common/create-dashboard-panels-kpis';
+import { STYLE } from '../../../common/saved-vis/constants';
+import {
+  createIndexPatternReferences,
+  createSearchSource,
+} from '../../../common/saved-vis/create-saved-vis-data';
+import { SavedVis } from '../../../common/types';
 import {
   getVisStateDHCPEnabledInterfacesMetric,
   getVisStateNetworkAveragePriorityMetric,
 } from '../common/dashboard';
 
+const getVisStateWirelessNetworkInterfacesMetric = (
+  indexPatternId: string,
+): SavedVis => {
+  return {
+    id: 'it-hygiene-network-interfaces-type-wireless',
+    title: 'Interfaces type Wireless',
+    type: 'metric',
+    params: {
+      addTooltip: true,
+      addLegend: false,
+      type: 'metric',
+      metric: {
+        percentageMode: false,
+        useRanges: false,
+        colorSchema: 'Green to Red',
+        metricColorMode: 'None',
+        colorsRange: [
+          {
+            from: 0,
+            to: 10000,
+          },
+        ],
+        labels: {
+          show: true,
+        },
+        invertColors: false,
+        style: STYLE,
+      },
+    },
+    data: {
+      searchSource: createSearchSource(indexPatternId),
+      references: createIndexPatternReferences(indexPatternId),
+      aggs: [
+        {
+          id: '1',
+          enabled: true,
+          type: 'count',
+          params: {
+            customLabel: 'Wireless',
+          },
+          schema: 'metric',
+        },
+        {
+          id: '2',
+          enabled: true,
+          type: 'filters',
+          params: {
+            filters: [
+              {
+                input: {
+                  query: 'network.type: wireless',
+                  language: 'kuery',
+                },
+                label: 'Network type',
+              },
+            ],
+          },
+          schema: 'group',
+        },
+      ],
+    },
+  };
+};
+
 export const getOverviewNetworksProtocolsTab = (indexPatternId: string) => {
   return buildDashboardKPIPanels([
     getVisStateNetworkAveragePriorityMetric(indexPatternId),
     getVisStateDHCPEnabledInterfacesMetric(indexPatternId),
+    getVisStateWirelessNetworkInterfacesMetric(indexPatternId),
   ]);
 };
