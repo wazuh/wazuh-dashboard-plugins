45c445555a9f99da52359e94d9eb3d8c38ca94fd
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2025-04-30 10:09:26 +0200

    fix: replace logic ensuring the component is mounted in agent welcome view

--- plugins/main/public/components/endpoints-summary/agent/index.tsx
+++ plugins/main/public/components/endpoints-summary/agent/index.tsx
@@ -29,6 +29,7 @@ import { endpointSummary } from '../../../utils/applications';
 import { withAgentSync } from '../../common/hocs/withAgentSync';
 import { AgentTabs } from './agent-tabs';
 import { SECTIONS } from '../../../sections';
+import { useEffectAvoidOnNotMount } from '../../common/hooks';
 
 const mapStateToProps = state => ({
   agent: state.appStateReducers?.currentAgentData,
@@ -91,7 +92,6 @@ export const AgentView = compose(
   const pinnedAgentManager = new PinnedAgentManager();
 
   const [isLoadingAgent, setIsLoadingAgent] = useState(false);
-  const isMounted = useRef(false);
 
   const syncAgent = async () => {
     setIsLoadingAgent(true);
@@ -99,19 +99,13 @@ export const AgentView = compose(
     setIsLoadingAgent(false);
   };
 
-  useEffect(() => {
+  useEffectAvoidOnNotMount(() => {
     /* The component has the withAgentSync hook that synchronizes the agent data and this
     effect would trigger on mount causing the views are mounted 2 times, that causes problems with
     the views that are wrapped with index pattern creation due to conflict document errors (same index pattern could be tried to create despite it exist). Avoiding the effect is triggered on mount mitigates the problem or canceling the async operations (index patten validation/creation)
     */
-    if (isMounted.current) {
-      syncAgent();
-    }
+    syncAgent();
   }, [tab]);
-
-  useEffect(() => {
-    isMounted.current = true;
-  }, []);
 
   const switchTab = (tab: string) => {
     navigationService.navigate(`/agents?tab=${tab}&agent=${agentData?.id}`);
