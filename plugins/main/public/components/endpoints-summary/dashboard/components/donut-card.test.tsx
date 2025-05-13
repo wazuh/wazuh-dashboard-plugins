c1a830bfb7b1186eb82adf5df1a222a0e069be02
Nicolas Agustin Guevara Pihen <nicolas.guevara.pihen@gmail.com>
2025-05-13 10:15:28 +0000

    Adapt donut card test to new changes

--- plugins/main/public/components/endpoints-summary/dashboard/components/donut-card.test.tsx
+++ plugins/main/public/components/endpoints-summary/dashboard/components/donut-card.test.tsx
@@ -11,13 +11,8 @@ class ResizeObserver {
 }
 global.ResizeObserver = ResizeObserver;
 
-jest.mock('../../../common/hooks/use-service', () => ({
-  __esModule: true,
-  useService: jest.fn(),
-}));
-
 describe('DonutCard', () => {
-  const mockLoading = false;
+  const mockIsLoading = false;
   const mockData = [
     {
       status: 'active',
@@ -44,40 +39,48 @@ describe('DonutCard', () => {
       color: '#646A77',
     },
   ];
-  const mockGetInfo = jest.fn().mockResolvedValue(mockData);
-  const useServiceMock = jest.fn(() => ({data: mockData, isLoading: mockLoading}));
-  const mockGetInfoNoData = jest.fn().mockResolvedValue([]);
-  const useServiceMockNoData = jest.fn(() => ({data: [], isLoading: mockLoading}));
-  
+  const mockNoData = [];
+
   it('renders with data', async () => {
-    require('../../../common/hooks/use-service').useService = useServiceMock;
-    
     await act(async () => {
       const { getByText } = render(
-        <DonutCard title='Component title example' description='Component description example' betaBadgeLabel='Component betaBadgeLabel example' getInfo={mockGetInfo} />,
+        <DonutCard
+          title='Component title example'
+          description='Component description example'
+          betaBadgeLabel='Component betaBadgeLabel example'
+          data={mockData}
+          isLoading={mockIsLoading}
+        />,
       );
 
       expect(getByText('Component title example')).toBeInTheDocument();
       expect(getByText('Component description example')).toBeInTheDocument();
       expect(getByText('Component betaBadgeLabel example')).toBeInTheDocument();
       mockData.forEach(element => {
-        expect(getByText(`${element.label} (${element.value})`)).toBeInTheDocument();  
+        expect(
+          getByText(`${element.label} (${element.value})`),
+        ).toBeInTheDocument();
       });
     });
   });
 
   it('handles click on data', async () => {
-    require('../../../common/hooks/use-service').useService = useServiceMock;
-    
     const handleClick = jest.fn();
     const firstMockData = mockData[0];
-   
+
     await act(async () => {
       const { getByText } = render(
-        <DonutCard title="Component title example" getInfo={mockGetInfo} onClickLabel={handleClick} />
+        <DonutCard
+          title='Component title example'
+          data={mockData}
+          isLoading={mockIsLoading}
+          onClickLabel={handleClick}
+        />,
       );
-      
-      fireEvent.click(getByText(`${firstMockData.label} (${firstMockData.value})`));
+
+      fireEvent.click(
+        getByText(`${firstMockData.label} (${firstMockData.value})`),
+      );
 
       expect(handleClick).toHaveBeenCalledTimes(1);
 
@@ -86,15 +89,22 @@ describe('DonutCard', () => {
   });
 
   it('show noDataTitle and noDataMessage when no data', async () => {
-    require('../../../common/hooks/use-service').useService = useServiceMockNoData;
-    
     await act(async () => {
       const { getByText } = render(
-        <DonutCard getInfo={mockGetInfoNoData} noDataTitle='Component no data title example message' noDataMessage='Component no data example message' />
+        <DonutCard
+          data={mockNoData}
+          isLoading={mockIsLoading}
+          noDataTitle='Component no data title example message'
+          noDataMessage='Component no data example message'
+        />,
       );
 
-      expect(getByText('Component no data title example message')).toBeInTheDocument();
-      expect(getByText('Component no data example message')).toBeInTheDocument();
+      expect(
+        getByText('Component no data title example message'),
+      ).toBeInTheDocument();
+      expect(
+        getByText('Component no data example message'),
+      ).toBeInTheDocument();
     });
   });
 });
