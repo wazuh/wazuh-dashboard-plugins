e1a367ca0453b43f27986a72afa453dba4130664
Ian Yenien Serrano <63758389+yenienserrano@users.noreply.github.com>
2025-05-15 21:55:53 +0200

    feat(dev.yml): add server-local profile and update wazuh manager and agent configurations
    This commit introduces a new `server-local` profile to the development environment, allowing for local server configurations. It also updates the Wazuh manager and agent settings to support this new profile, including renaming `wazuh.manager.dev` to `wazuh.manager.local` and adding a new `wazuh.agent.deb.local` service. Additionally, the `dev.sh` script is updated to handle the new `server-local` profile.

--- docker/osd-dev/dev.sh
+++ docker/osd-dev/dev.sh
@@ -11,6 +11,7 @@ usage() {
   echo "  action is one of up | down | stop"
   echo "  saml to deploy a saml enabled environment (optional)"
   echo "  server to deploy a real server enabled environment (optional)"
+  echo "  server-local to deploy a real server enabled environment (optional)"
   exit -1
 }
 
@@ -19,7 +20,7 @@ exit_with_message() {
   exit -1
 }
 
-if ! command -v jq &> /dev/null; then
+if ! command -v jq &>/dev/null; then
   echo "[ERROR] jq is not installed. Please install jq to continue."
   echo "sudo apt-get install jq in Debian/Ubuntu OS"
   echo "sudo yum install jq in RedHat/CentOS OS"
@@ -32,7 +33,7 @@ PACKAGE_PATH="../../plugins/wazuh-core/package.json"
 os_version=""
 osd_version=""
 
-while getopts ":o:d:" opt; do
+while getopts ":o:d:a:" opt; do
   case ${opt} in
   o)
     os_version=$OPTARG
@@ -122,6 +123,11 @@ if [[ "$3" =~ "server" ]]; then
   export WAZUH_STACK="${4}"
 fi
 
+if [[ "$3" =~ "server-local" ]]; then
+  profile="server-local"
+  export WAZUH_STACK="${4}"
+fi
+
 export SEC_CONFIG_PATH=/usr/share/opensearch/plugins/opensearch-security/securityconfig
 if [[ "$OSD_MAJOR" == "2.x" ]]; then
   export SEC_CONFIG_PATH=/usr/share/opensearch/config/opensearch-security
@@ -129,39 +135,39 @@ fi
 
 case "$2" in
 up)
-   /bin/bash ../scripts/create_docker_networks.sh
-   docker compose --profile $profile -f dev.yml up -Vd
+  /bin/bash ../scripts/create_docker_networks.sh
+  docker compose --profile $profile -f dev.yml up -Vd
 
-   # Display a command to deploy an agent when using the real server
-   if [[ "$3" =~ "server" ]]; then
-     echo
-     echo "**************WARNING**************"
-     echo "The agent version must be a published one. This uses only released versions."
-     echo "If you need to change de version, edit the command as you see fit."
-     echo "***********************************"
-     echo "1. (Optional) Enroll an agent (Ubuntu 20.04):"
-     echo "docker run --name ${COMPOSE_PROJECT_NAME}-agent-\$(date +%s) --network os-dev-${OS_VERSION} --label com.docker.compose.project=${COMPOSE_PROJECT_NAME} --env WAZUH_AGENT_VERSION=${WAZUH_STACK} -d ubuntu:20.04 bash -c '"
-     echo "  apt update -y"
-     echo "  apt install -y curl lsb-release"
-     echo "  curl -so \wazuh-agent-\${WAZUH_AGENT_VERSION}.deb \\"
-     echo "    https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_\${WAZUH_AGENT_VERSION}-1_amd64.deb \\"
-     echo "    && WAZUH_MANAGER='wazuh.manager' WAZUH_AGENT_GROUP='default' dpkg -i ./wazuh-agent-\${WAZUH_AGENT_VERSION}.deb"
-     echo
-     echo "  /etc/init.d/wazuh-agent start"
-     echo "  tail -f /var/ossec/logs/ossec.log"
-     echo "'"
-     echo
-   fi
-   ;;
- down)
-   docker compose --profile $profile -f dev.yml down -v --remove-orphans
-   ;;
- stop)
-   docker compose --profile $profile -f dev.yml -p ${COMPOSE_PROJECT_NAME} stop
-   ;;
- *)
-   echo "[ERROR] Action must be up | down | stop: "
-   echo
-   usage
-   ;;
+  # Display a command to deploy an agent when using the real server
+  if [[ "$3" =~ "^server$" ]]; then
+    echo
+    echo "**************WARNING**************"
+    echo "The agent version must be a published one. This uses only released versions."
+    echo "If you need to change de version, edit the command as you see fit."
+    echo "***********************************"
+    echo "1. (Optional) Enroll an agent (Ubuntu 20.04):"
+    echo "docker run --name ${COMPOSE_PROJECT_NAME}-agent-\$(date +%s) --network os-dev-${OS_VERSION} --label com.docker.compose.project=${COMPOSE_PROJECT_NAME} --env WAZUH_AGENT_VERSION=${WAZUH_STACK} -d ubuntu:20.04 bash -c '"
+    echo "  apt update -y"
+    echo "  apt install -y curl lsb-release"
+    echo "  curl -so \wazuh-agent-\${WAZUH_AGENT_VERSION}.deb \\"
+    echo "    https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_\${WAZUH_AGENT_VERSION}-1_amd64.deb \\"
+    echo "    && WAZUH_MANAGER='wazuh.manager' WAZUH_AGENT_GROUP='default' dpkg -i ./wazuh-agent-\${WAZUH_AGENT_VERSION}.deb"
+    echo
+    echo "  /etc/init.d/wazuh-agent start"
+    echo "  tail -f /var/ossec/logs/ossec.log"
+    echo "'"
+    echo
+  fi
+  ;;
+down)
+  docker compose --profile $profile -f dev.yml down -v --remove-orphans
+  ;;
+stop)
+  docker compose --profile $profile -f dev.yml -p ${COMPOSE_PROJECT_NAME} stop
+  ;;
+*)
+  echo "[ERROR] Action must be up | down | stop: "
+  echo
+  usage
+  ;;
 esac
