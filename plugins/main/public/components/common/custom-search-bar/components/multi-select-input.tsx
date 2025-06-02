817c42599ddfdb0606221ea4b2e2586b47be01e6
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2025-06-02 12:27:42 +0200

    fix: year of copyright

--- plugins/main/public/components/common/custom-search-bar/components/multi-select-input.tsx
+++ plugins/main/public/components/common/custom-search-bar/components/multi-select-input.tsx
@@ -1,6 +1,6 @@
 /*
  * Wazuh app - React component Multi-select
- * Copyright (C) 2015-2022 Wazuh, Inc.
+ * Copyright (C) 2015-2025 Wazuh, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
