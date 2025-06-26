31cb4c704e1f108212784708326ef430a67bf73d
Maximiliano Ibarra <maximiliano.ibarra@wazuh.com>
2025-06-26 02:06:45 -0300

    Applied linter

--- plugins/main/public/components/overview/it-hygiene/users/inventories/groups/inventory.tsx
+++ plugins/main/public/components/overview/it-hygiene/users/inventories/groups/inventory.tsx
@@ -5,7 +5,9 @@ import {
 } from '../../../../../common/data-source';
 import tableColumns from './table-columns';
 import managedFilters from './managed-filters';
-import { withSystemInventoryGroupsDataSource } from '../../../common/hocs/validate-system-inventory-index-pattern';
+import {
+  withSystemInventoryGroupsDataSource
+} from '../../../common/hocs/validate-system-inventory-index-pattern';
 import {
   ITHygieneInventoryDashboardTable,
   ITHygieneInventoryDashboardTableProps,
