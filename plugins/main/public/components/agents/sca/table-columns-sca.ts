5594a0b22cf454048615c0530839cefc92c7df5f
gonzaarancibia <gonzaloarancibia48@gmail.com>
2025-07-16 10:01:33 -0300

    feat(sca-inventory): Add new inventory sca to migrate endpoints to index and old visualization
    Signed-off-by: gonzaarancibia <gonzaloarancibia48@gmail.com>

---
+++ plugins/main/public/components/agents/sca/table-columns-sca.ts
@@ -0,0 +1,7 @@
+export default [
+  { id: 'policy.name' },
+  { id: 'policy.description' },
+  { id: 'policy.file' },
+  { id: 'check.result' },
+  { id: 'agent.name' },
+]
---
+++ plugins/main/public/components/common/data-source/pattern/sca/sca-data-source-repository.ts
@@ -0,0 +1,7 @@
+import {
+  WAZUH_SCA_PATTERN,
+} from '../../../../../../common/constants';
+import { createPatternDataSourceRepositoryUseValue } from '../pattern-data-source-repository-use-setting-value';
+
+export const SCAStatesDataSourceRepository =
+  createPatternDataSourceRepositoryUseValue(WAZUH_SCA_PATTERN);
