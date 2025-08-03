837a8770b42af4970da37ddd6a681b048927ff11
Guido Modarelli <guidomodarelli25@gmail.com>
2025-08-03 21:30:30 +0000

    Refactors repository interface
    Breaks down the monolithic repository interface into smaller, more focused interfaces.
    This improves code organization and allows for more granular dependency injection.

--- plugins/main/public/components/overview/dashboard-assistant/common/domain/repository.ts
+++ plugins/main/public/components/overview/dashboard-assistant/common/domain/repository.ts
@@ -1,7 +1,22 @@
-export interface Repository<T> {
-  create(data: T): Promise<string>;
-  findById(id: string): Promise<T | null>;
-  getAll(): Promise<T[]>;
-  update(id: string, data: T): Promise<void>;
+export interface CreateRepository<Entity, CreateEntityDTO> {
+  create(data: CreateEntityDTO): Promise<Entity>;
+}
+
+export interface UpdateRepository<
+  Entity,
+  UpdateEntityDTO = Partial<Exclude<Entity, 'id'>>,
+> {
+  update(id: string, data: UpdateEntityDTO): Promise<Entity>;
+}
+
+export interface FindRepository<Entity> {
+  findById(id: string): Promise<Entity | null>;
+}
+
+export interface GetAllRepository<Entity> {
+  getAll(): Promise<Entity[]>;
+}
+
+export interface DeleteRepository {
   delete(id: string): Promise<void>;
 }
