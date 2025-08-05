951ebfdfcae4ac737b83dc0b785e7822d221495d
Felipe Gonzalez <felipegonzalezmv@hotmail.com>
2025-08-05 14:59:26 -0500

    feat: remove CIS-CAT testing infrastructure and Docker configurations
    - Remove CIS-CAT selectors from Cypress page objects across 4 environments
    - Remove CIS-CAT mappings from test utils
    - Remove CIS-CAT wodle configurations from 9 Docker environments
    - Remove CIS-CAT security policies and actions from Imposter
    - Remove CIS-CAT API endpoints from Imposter configuration
    - Update main README.md to remove CIS-CAT description

--- plugins/main/test/cypress/cypress/integration/pageobjects/wzd/settings/modules.page.js
+++ plugins/main/test/cypress/cypress/integration/pageobjects/wzd/settings/modules.page.js
@@ -6,8 +6,6 @@ export const MODULES_PAGE = {
   gitHubCardToggleButton:
     ':nth-child(1) > .euiPanel > :nth-child(3) > :nth-child(1) > :nth-child(1) > :nth-child(6) > .euiFlexItem--flexGrowZero > .euiSwitch > button',
 
-  cisCatToggleButton:
-    ':nth-child(2) > .euiPanel > :nth-child(3) > :nth-child(1) > :nth-child(1) > :nth-child(5) > .euiFlexItem--flexGrowZero > .euiSwitch > button',
   virusTotalToggleButton:
     ':nth-child(3) > .euiPanel > :nth-child(3) > :nth-child(1) > :nth-child(1) > :nth-child(3) > .euiFlexItem--flexGrowZero > .euiSwitch > button',
   osqueryToggleButton:
