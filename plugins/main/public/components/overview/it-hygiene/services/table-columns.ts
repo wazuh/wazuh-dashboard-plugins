08dd96a4638406af269489bdce3bad22d21c62f9
Nicolas Guevara <nicolas.guevara.pihen@gmail.com>
2025-08-07 14:17:02 -0300

    Change default columns

--- plugins/main/public/components/overview/it-hygiene/services/table-columns.ts
+++ plugins/main/public/components/overview/it-hygiene/services/table-columns.ts
@@ -2,7 +2,8 @@ export default [
   { id: 'agent.name' },
   { id: 'service.name' },
   { id: 'service.state' },
-  { id: 'service.sub_state' },
   { id: 'service.enabled' },
+  { id: 'service.start_type' },
   { id: 'service.exit_code' },
+  { id: 'process.executable' },
 ];
