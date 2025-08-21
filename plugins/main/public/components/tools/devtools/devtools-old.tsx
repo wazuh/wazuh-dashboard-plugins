1f2eb26a8b2d4edfd13c833164afd4cb1f76a592
Guido Modarelli <guidomodarelli25@gmail.com>
2025-08-21 15:25:27 +0000

    replace separator span with accessible SVG icon
    Improves visual accessibility by replacing the separator span
    with a scalable SVG icon. Enhances usability through semantic
    markup and better screen reader support.

--- plugins/main/public/components/tools/devtools/devtools-old.tsx
+++ plugins/main/public/components/tools/devtools/devtools-old.tsx
@@ -1046,7 +1046,21 @@ export const ToolDevTools = withGlobalBreadcrumb([
           <textarea style={{ display: 'flex' }} id='api_input'></textarea>
         </div>
         <div className='wz-dev-column-separator layout-column'>
-          <span>︙</span>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='16'
+            height='16'
+            viewBox='0 0 16 16'
+            className='euiIcon euiIcon--medium euiIcon-isLoaded'
+            focusable='false'
+            role='img'
+            aria-hidden='true'
+          >
+            <path
+              fill-rule='evenodd'
+              d='M6 2.5c0-.276.232-.5.5-.5.276 0 .5.229.5.5v11c0 .276-.232.5-.5.5a.503.503 0 0 1-.5-.5v-11Zm3 0c0-.276.232-.5.5-.5.276 0 .5.229.5.5v11c0 .276-.232.5-.5.5a.503.503 0 0 1-.5-.5v-11Z'
+            ></path>
+          </svg>
         </div>
         <div
           id='wz-dev-right-column'
