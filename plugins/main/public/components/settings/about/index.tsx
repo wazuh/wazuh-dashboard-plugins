41df8ed2288bfe426fe2d9bb47ab48960bbf0187
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2025-09-04 13:26:09 +0200

    remove: unused useAboutData hook

--- plugins/main/public/components/settings/about/index.tsx
+++ plugins/main/public/components/settings/about/index.tsx
@@ -32,40 +32,6 @@ async function fetchAboutData() {
   }
 }
 
-// export const useAboutData = () => {
-//   const [appInfo, setAppInfo] = useState<string>();
-//   const [loading, setLoading] = useState<boolean>(true);
-//   const [error, setError] = useState<boolean>(false);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const data = await GenericRequest.request('GET', '/api/setup');
-//         const response = data.data.data;
-//         setAppInfo(response['app-version'] as string);
-//       } catch (error) {
-//         setError(true);
-//         const options = {
-//           context: `${Settings.name}.getAppInfo`,
-//           level: UI_LOGGER_LEVELS.ERROR,
-//           severity: UI_ERROR_SEVERITIES.BUSINESS,
-//           error: {
-//             error: error,
-//             message: error.message || error,
-//             title: error.name || error,
-//           },
-//         };
-//         getErrorOrchestrator().handleError(options);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-//     fetchData();
-//   }, []);
-
-//   return { appInfo, loading, error };
-// };
-
 export const SettingsAbout: React.FC = () => {
   const { data, running, error } = useAsyncActionRunOnStart(fetchAboutData);
 
