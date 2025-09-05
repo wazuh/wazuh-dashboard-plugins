02c39b9381e9b5cd69f21f442cd522d1622573b5
Guido Modarelli <38738725+guidomodarelli@users.noreply.github.com>
2025-09-05 18:50:11 -0300

    Update plugins/main/public/components/tools/devtools/application/components/dev-tools-action-buttons.tsx
    Co-authored-by: Antonio <34042064+Desvelao@users.noreply.github.com>

--- plugins/main/public/components/tools/devtools/application/components/dev-tools-action-buttons.tsx
+++ plugins/main/public/components/tools/devtools/application/components/dev-tools-action-buttons.tsx
@@ -22,7 +22,7 @@ const DevToolsActionButtons = ({
         id={DEV_TOOLS_BUTTONS.PLAY_BUTTON_ID}
         color='success'
       />
-      <a href='' target='__blank' id={DEV_TOOLS_BUTTONS.DOCS_BUTTON_ID}>
+      <a href='' target='__blank' id={DEV_TOOLS_BUTTONS.DOCS_BUTTON_ID} rel='noopener noreferrer'>
         <EuiIcon type='documentation' />
       </a>
     </EuiFlexGroup>
