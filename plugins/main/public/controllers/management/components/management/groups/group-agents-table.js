20916215c4a0c2aeabf682ecf1e8326c0654c5cf
gonzaarancibia <gonzaloarancibia48@gmail.com>
2025-09-12 13:41:13 -0300

    fix(group-agents-table): add missing import
    Signed-off-by: gonzaarancibia <gonzaloarancibia48@gmail.com>

--- plugins/main/public/controllers/management/components/management/groups/group-agents-table.js
+++ plugins/main/public/controllers/management/components/management/groups/group-agents-table.js
@@ -37,7 +37,7 @@ import { get as getLodash } from 'lodash';
 import { UI_ERROR_SEVERITIES } from '../../../../../react-services/error-orchestrator/types';
 import { getErrorOrchestrator } from '../../../../../react-services/common-services';
 import { AgentStatus } from '../../../../../components/agents/agent-status';
-import { WzRequest } from '../../../../../react-services';
+import { getAgentOSType, WzRequest } from '../../../../../react-services';
 import { endpointSummary } from '../../../../../utils/applications';
 import NavigationService from '../../../../../react-services/navigation-service';
 import WzIconSVG from '../../../../../components/common/icons/wz-icon-svg';
