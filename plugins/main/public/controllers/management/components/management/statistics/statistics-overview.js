7d8d4db31af69fed8536dc02fbdc5768a95f3301
Antonio <34042064+Desvelao@users.noreply.github.com>
2025-09-17 18:59:39 +0200

    Remove Settings button in Statistics (#7698)
    * remove(statistics): Settings button that redirects to unexistant category in configuration
    
    * chore(changelog): add entry
    
    ---------
    
    Co-authored-by: Ian Yenien Serrano <63758389+yenienserrano@users.noreply.github.com>

--- plugins/main/public/controllers/management/components/management/statistics/statistics-overview.js
+++ plugins/main/public/controllers/management/components/management/statistics/statistics-overview.js
@@ -14,7 +14,6 @@ import React, { Component } from 'react';
 import {
   EuiFlexItem,
   EuiFlexGroup,
-  EuiButtonEmpty,
   EuiPanel,
   EuiTitle,
   EuiPage,
@@ -31,18 +30,10 @@ import {
   withUserAuthorizationPrompt,
 } from '../../../../../components/common/hocs';
 import { UI_ERROR_SEVERITIES } from '../../../../../react-services/error-orchestrator/types';
-import {
-  UI_LOGGER_LEVELS,
-  WAZUH_STATISTICS_PATTERN,
-} from '../../../../../../common/constants';
+import { UI_LOGGER_LEVELS } from '../../../../../../common/constants';
 import { getErrorOrchestrator } from '../../../../../react-services/common-services';
-import { getCore } from '../../../../../kibana-services';
-import { appSettings, statistics } from '../../../../../utils/applications';
-import { RedirectAppLinks } from '../../../../../../../../src/plugins/opensearch_dashboards_react/public';
+import { statistics } from '../../../../../utils/applications';
 import { DashboardTabsPanels } from '../../../../../components/overview/server-management-statistics/dashboards/dashboardTabsPanels';
-import { connect } from 'react-redux';
-import NavigationService from '../../../../../react-services/navigation-service';
-
 export class WzStatisticsOverview extends Component {
   _isMounted = false;
   constructor(props) {
@@ -171,24 +162,6 @@ export class WzStatisticsOverview extends Component {
                 </EuiFlexItem>
               </EuiFlexGroup>
             </EuiFlexItem>
-            {this.props.configurationUIEditable && (
-              <EuiFlexItem grow={false}>
-                <RedirectAppLinks application={getCore().application}>
-                  <EuiButtonEmpty
-                    href={NavigationService.getInstance().getUrlForApp(
-                      appSettings.id,
-                      {
-                        path: '#/settings?tab=configuration&category=Task:Statistics',
-                      },
-                    )}
-                    iconType='gear'
-                    iconSide='left'
-                  >
-                    Settings
-                  </EuiButtonEmpty>
-                </RedirectAppLinks>
-              </EuiFlexItem>
-            )}
           </EuiFlexGroup>
           <EuiFlexGroup>
             <EuiFlexItem>
@@ -217,17 +190,10 @@ export class WzStatisticsOverview extends Component {
   }
 }
 
-const mapStateToProps = state => ({
-  configurationUIEditable:
-    state.appConfig.data?.['configuration.ui_api_editable'],
-  statisticsIndexPatternID: WAZUH_STATISTICS_PATTERN, // TODO: this could be removed
-});
-
 export default compose(
   withGlobalBreadcrumb([{ text: statistics.breadcrumbLabel }]),
   withUserAuthorizationPrompt([
     { action: 'cluster:status', resource: '*:*:*' },
     { action: 'cluster:read', resource: 'node:id:*' },
   ]),
-  connect(mapStateToProps),
 )(WzStatisticsOverview);
