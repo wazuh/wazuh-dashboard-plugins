dceff49c3bdecbeed1e2ffecb61f7e767fba2abc
Guido Modarelli <guidomodarelli25@gmail.com>
2025-10-01 17:54:18 +0000

    add support for base dashboard development mode
    Introduce a new development mode, «dashboard-src», to enable running OpenSearch Dashboards from a local source repository. Add the «-base» flag to the dev script, allowing users to mount a local «wazuh-dashboard» checkout and dynamically configure the development environment based on its settings.
    
    Update Docker configurations to support this mode, including adjustments for «dashboard-src» profiles, custom entry points, and dynamic volume setups. Enhance the dev script with additional path resolution logic, environment variable management, and compatibility checks for the base dashboard repository. Include tests to validate the new functionality.
    
    Improve README documentation with details on the «-base» flag and usage examples for base dashboard development. Extend Docker Compose configurations to handle the new mode and ensure the appropriate workflow is maintained within the container.

---
+++ docker/osd-dev/dashboard-src/entrypoint.sh
@@ -0,0 +1,19 @@
+# Update and install dependencies
+
+ROOT_APP_DIR="/home/node/kbn"
+APP_PLUGINS_DIR="${ROOT_APP_DIR}/plugins"
+plugins=$(ls $APP_PLUGINS_DIR)
+for plugin in $plugins; do
+  echo "Checking if $plugin has node_modules"
+  if [ ! -d "$APP_PLUGINS_DIR/$plugin/node_modules" ]; then
+    cd $APP_PLUGINS_DIR/$plugin
+    echo "Installing dependencies for $plugin"
+    yarn
+  fi
+done
+
+cd $ROOT_APP_DIR
+yarn start --no-base-path
+
+# Block to avoid the container exit
+tail -f /dev/null
