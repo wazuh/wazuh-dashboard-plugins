8c7aabdaefef377f18796173836db9876c44c562
Federico Rodriguez <federico.rodriguez@wazuh.com>
2025-10-01 18:49:08 +0200

    Add top 5 users visualization

--- plugins/main/public/components/overview/it-hygiene/users/inventories/users/dashboard.ts
+++ plugins/main/public/components/overview/it-hygiene/users/inventories/users/dashboard.ts
@@ -60,17 +60,24 @@ export const getOverviewUsersUsersTab = (indexPatternId: string) => {
   return buildDashboardKPIPanels([
     getVisStateHorizontalBarByField(
       indexPatternId,
+      'user.name',
+      'Top 5 users',
+      'it-hygiene-users',
+      { fieldCustomLabel: 'Users' },
+    ),
+    getVisStateHorizontalBarByField(
+      indexPatternId,
       'user.groups',
       'Top 5 user groups',
       'it-hygiene-users',
-      { fieldCustomLabel: 'User group' },
+      { fieldCustomLabel: 'User groups' },
     ),
     getVisStateHorizontalBarByField(
       indexPatternId,
       'user.shell',
       'Top 5 user shells',
       'it-hygiene-users',
-      { fieldCustomLabel: 'User shell' },
+      { fieldCustomLabel: 'User shells' },
     ),
   ]);
 };
