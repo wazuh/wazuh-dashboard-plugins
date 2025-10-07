1fb4355862cb0d502c92c769fb554c2ec5b2c07d
gonzaarancibia <gonzaloarancibia48@gmail.com>
2025-10-07 10:41:08 -0300

    refactor(endpoints): delete getOutdatedAgentsService services
    Signed-off-by: gonzaarancibia <gonzaloarancibia48@gmail.com>

--- plugins/main/public/components/endpoints-summary/services/index.tsx
+++ plugins/main/public/components/endpoints-summary/services/index.tsx
@@ -6,6 +6,5 @@ export { addAgentsToGroupService } from './add-agents-to-group';
 export { getGroupsService } from './get-groups';
 export { upgradeAgentService } from './upgrade-agent';
 export { upgradeAgentsService } from './upgrade-agents';
-export { getOutdatedAgentsService } from './get-outdated-agents';
 export { getTasks } from './get-tasks';
 export { getAgentsInfo } from './get-agents-info';
