07e2d9295c5ed0016ed3fea899a65f070da86321
Guido Modarelli <guidomodarelli25@gmail.com>
2025-11-03 12:06:04 +0000

    move functions «getVisStateClusterAlertsSummary» and «getVisStateAlertsByNodeSummary» to a new file
    This change organizes the code by moving the visualization state functions into their own file, improving modularity and maintainability. The functions are now imported in the relevant dashboard configuration file, which helps in keeping the codebase clean and easier to navigate.

---
+++ plugins/main/common/dashboards/management/cluster/monitoring/vis-states.ts
@@ -0,0 +1,68 @@
+export const getVisStateClusterAlertsSummary = (
+  indexPattern: { id: string; title: string },
+  clusterName?: string,
+) => {
+  let expression = `.es(index=${indexPattern.title},q="cluster.name: ${clusterName}").label("${clusterName} cluster")`;
+  expression = expression.replace(/'/g, '"');
+  return {
+    id: 'Wazuh-App-Cluster-monitoring-Overview-Manager',
+    title: 'Cluster alerts summary',
+    type: 'timelion',
+    params: { expression, interval: 'auto' },
+    data: {
+      searchSource: {
+        query: {
+          language: 'kuery',
+          query: '',
+        },
+        filter: [],
+        index: indexPattern.id,
+      },
+      references: [
+        {
+          name: 'kibanaSavedObjectMeta.searchSourceJSON.index',
+          type: 'index-pattern',
+          id: indexPattern.id,
+        },
+      ],
+      aggs: [],
+    },
+  };
+};
+
+export const getVisStateAlertsByNodeSummary = (
+  indexPattern: { id: string; title: string },
+  nodeList: any[],
+  clusterName?: string,
+) => {
+  let expression = '';
+  for (const node of nodeList) {
+    expression += `.es(index=${indexPattern.title},q="cluster.name: ${clusterName} AND cluster.node: ${node.name}").label("${node.name}"),`;
+  }
+  expression = expression.substring(0, expression.length - 1);
+  expression = expression.replace(/'/g, '"');
+  return {
+    id: 'Wazuh-App-Cluster-monitoring-Overview',
+    title: 'Alerts by node summary',
+    type: 'timelion',
+    params: { expression, interval: 'auto' },
+    data: {
+      searchSource: {
+        query: {
+          language: 'kuery',
+          query: '',
+        },
+        filter: [],
+        index: indexPattern.id,
+      },
+      references: [
+        {
+          name: 'kibanaSavedObjectMeta.searchSourceJSON.index',
+          type: 'index-pattern',
+          id: indexPattern.id,
+        },
+      ],
+      aggs: [],
+    },
+  };
+};
