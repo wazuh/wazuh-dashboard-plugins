622055d89f1249d9a1dba549197d4f239921d09d
Guido Modarelli <guidomodarelli25@gmail.com>
2025-11-03 12:05:00 +0000

    move function «getVisStateTop5Nodes» to its own file
    This change separates the function «getVisStateTop5Nodes» from the main dashboard configuration file, improving code organization and maintainability. The function is now imported from its new location, allowing for better modularity and potential reuse in other components.

--- plugins/main/common/dashboards/management/cluster/configuration/dashboard.ts
+++ plugins/main/common/dashboards/management/cluster/configuration/dashboard.ts
@@ -2,62 +2,7 @@ import {
   DashboardByRendererConfig,
   DashboardLayoutConfig,
 } from '../../../dashboard-builder';
-import type { DashboardByValueSavedVis } from '../../../types';
-
-const getVisStateTop5Nodes = (
-  indexPatternId: string,
-): DashboardByValueSavedVis => {
-  return {
-    id: 'Wazuh-App-Cluster-monitoring-Overview-Node-Pie',
-    title: 'Top 5 nodes',
-    type: 'pie',
-    params: {
-      type: 'pie',
-      addTooltip: true,
-      addLegend: true,
-      legendPosition: 'right',
-      isDonut: true,
-      labels: { show: false, values: true, last_level: true, truncate: 100 },
-    },
-    uiState: { spy: { mode: { name: 'table' } } },
-    data: {
-      searchSource: {
-        query: {
-          language: 'kuery',
-          query: '',
-        },
-        filter: [],
-        index: indexPatternId,
-      },
-      references: [
-        {
-          name: 'kibanaSavedObjectMeta.searchSourceJSON.index',
-          type: 'index-pattern',
-          id: indexPatternId,
-        },
-      ],
-      aggs: [
-        { id: '1', enabled: true, type: 'count', schema: 'metric', params: {} },
-        {
-          id: '2',
-          enabled: true,
-          type: 'terms',
-          schema: 'segment',
-          params: {
-            field: 'cluster.node',
-            otherBucket: false,
-            otherBucketLabel: 'Other',
-            missingBucket: false,
-            missingBucketLabel: 'Missing',
-            size: 5,
-            order: 'desc',
-            orderBy: '1',
-          },
-        },
-      ],
-    },
-  };
-};
+import { getVisStateTop5Nodes } from "./vis-states";
 
 export class ClusterConfigurationDashboardLayoutConfig extends DashboardLayoutConfig {
   constructor(indexPatternId: string) {
