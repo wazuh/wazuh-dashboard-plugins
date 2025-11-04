cf714e2bd659b8c7212fde5adfc6206b3d8582a1
Guido Modarelli <guidomodarelli25@gmail.com>
2025-11-04 22:24:07 +0000

    Refactor code structure for improved readability and maintainability

---
+++ plugins/main/common/dashboards/vis-definitions/overview/server-management/statistics/dashboard.ts
@@ -0,0 +1,123 @@
+import {
+  DashboardByRendererConfig,
+  DashboardLayoutDefinition,
+} from '../../../../lib/dashboard-builder';
+import {
+  getVisStateEventsDecodedSummary,
+  getVisStateEventsDroppedByNode,
+  getVisStateEventsProcessedByNode,
+  getVisStateHostInfo,
+  getVisStateQueueUsage,
+  getVisStateRootcheck,
+  getVisStateSCA,
+  getVisStateSyscheck,
+  getVisStateSyscollector,
+} from './vis-states';
+
+export class ServerManagementStatisticsDashboardLayoutDefinition extends DashboardLayoutDefinition {
+  constructor(indexPatternId: string) {
+    super();
+    this.setGridVisualizationPairs(
+      {
+        gridData: {
+          w: 48,
+          h: 13,
+          x: 0,
+          y: 13,
+        },
+        savedVis: getVisStateQueueUsage(indexPatternId),
+      },
+      {
+        gridData: {
+          w: 48,
+          h: 13,
+          x: 0,
+          y: 26,
+        },
+        savedVis: getVisStateEventsDecodedSummary(indexPatternId),
+      },
+      {
+        gridData: {
+          w: 24,
+          h: 13,
+          x: 0,
+          y: 39,
+        },
+        savedVis: getVisStateSyscheck(indexPatternId),
+      },
+      {
+        gridData: {
+          w: 24,
+          h: 13,
+          x: 24,
+          y: 39,
+        },
+        savedVis: getVisStateSyscollector(indexPatternId),
+      },
+      {
+        gridData: {
+          w: 24,
+          h: 13,
+          x: 0,
+          y: 52,
+        },
+        savedVis: getVisStateRootcheck(indexPatternId),
+      },
+      {
+        gridData: {
+          w: 24,
+          h: 13,
+          x: 24,
+          y: 52,
+        },
+        savedVis: getVisStateSCA(indexPatternId),
+      },
+      {
+        gridData: {
+          w: 48,
+          h: 13,
+          x: 0,
+          y: 65,
+        },
+        savedVis: getVisStateHostInfo(indexPatternId),
+      },
+      {
+        gridData: {
+          w: 24,
+          h: 13,
+          x: 0,
+          y: 0,
+        },
+        savedVis: getVisStateEventsProcessedByNode(indexPatternId),
+      },
+      {
+        gridData: {
+          w: 24,
+          h: 13,
+          x: 24,
+          y: 0,
+        },
+        savedVis: getVisStateEventsDroppedByNode(indexPatternId),
+      },
+    );
+  }
+}
+
+export class ServerManagementStatisticsDashboardByRendererConfig extends DashboardByRendererConfig {
+  constructor(indexPatternId: string) {
+    super(
+      indexPatternId,
+      new ServerManagementStatisticsDashboardLayoutDefinition(indexPatternId),
+    );
+  }
+
+  protected override getId(): string {
+    return 'server-management-statistics-dashboard';
+  }
+  protected override getTitle(): string {
+    return 'Server Management Statistics Dashboard';
+  }
+  protected override getDescription(): string {
+    return 'Dashboard for Server Management Statistics';
+  }
+}
