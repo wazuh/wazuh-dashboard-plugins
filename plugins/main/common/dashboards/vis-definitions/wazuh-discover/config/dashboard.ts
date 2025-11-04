caf6ad66f8a4aae47df403d2f31f6b66db1c419a
Guido Modarelli <guidomodarelli25@gmail.com>
2025-11-04 19:56:18 +0000

    add Wazuh Discover dashboard components
    Introduce new dashboard layout and visualization configurations for the Wazuh Discover feature. This includes the creation of the classes «WazuhDiscoverDashboardLayoutDefinition» and «WazuhDiscoverDashboardByRendererConfig» to define the layout and rendering logic. Additionally, the function «getVisStateHitsHistogram» is implemented to generate the histogram visualization state, enhancing the dashboard's functionality. The existing histogram chart configuration is refactored to utilize the new classes, promoting better code organization and reusability.

---
+++ plugins/main/common/dashboards/vis-definitions/wazuh-discover/config/dashboard.ts
@@ -0,0 +1,40 @@
+import {
+  DashboardByRendererConfig,
+  DashboardLayoutDefinition,
+} from '../../../lib/dashboard-builder';
+import { getVisStateHitsHistogram } from './vis-states';
+
+export class WazuhDiscoverDashboardLayoutDefinition extends DashboardLayoutDefinition {
+  constructor(indexPatternId: string) {
+    super();
+    this.setGridVisualizationPairs({
+      gridData: { w: 100, h: 10, x: 0, y: 0 },
+      savedVis: getVisStateHitsHistogram(indexPatternId),
+    });
+  }
+}
+
+export class WazuhDiscoverDashboardByRendererConfig extends DashboardByRendererConfig {
+  constructor(indexPatternId: string) {
+    super(
+      indexPatternId,
+      new WazuhDiscoverDashboardLayoutDefinition(indexPatternId),
+    );
+  }
+
+  protected override getId(): string {
+    return 'wz-discover-events-histogram';
+  }
+  protected override getTitle(): string {
+    return 'Wazuh Discover Events Histogram';
+  }
+  protected override getDescription(): string {
+    return 'Histogram of Wazuh events by date';
+  }
+  protected override get useMargins(): boolean {
+    return false;
+  }
+  protected override get hidePanelTitles(): boolean {
+    return true;
+  }
+}
