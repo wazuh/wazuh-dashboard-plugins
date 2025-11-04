be5700390f3741384f2fe27ae2965210f7bb50d0
Guido Modarelli <guidomodarelli25@gmail.com>
2025-11-04 22:02:11 +0000

    refactor dashboard KPI components
    This commit refactors the dashboard KPI components by removing unused visualization functions and consolidating the KPI panel logic. The previous implementation had multiple functions for visual states that were not being utilized, leading to unnecessary complexity. The new structure leverages the «OfficeKPIsDashboardByRendererConfig» class to streamline the creation of KPI panels, improving maintainability and readability of the code.

---
+++ plugins/main/common/dashboards/vis-definitions/overview/office/kpis/vis-states.ts
@@ -0,0 +1,327 @@
+import type { SavedVis } from '../../../../types';
+
+export const getVisStateMaxRuleLevel = (indexPatternId: string): SavedVis => {
+  return {
+    id: 'Wazuh-App-Overview-Office-Metric-Max-Rule-Level',
+    title: 'Max Rule Level',
+    type: 'metric',
+    params: {
+      addTooltip: true,
+      addLegend: false,
+      type: 'metric',
+      metric: {
+        percentageMode: false,
+        useRanges: false,
+        colorSchema: 'Greens',
+        metricColorMode: 'Labels',
+        colorsRange: [
+          {
+            from: 0,
+            to: 0,
+          },
+          {
+            from: 0,
+            to: 0,
+          },
+        ],
+        labels: {
+          show: true,
+        },
+        invertColors: false,
+        style: {
+          bgFill: '#000',
+          bgColor: false,
+          labelColor: false,
+          subText: '',
+          fontSize: 40,
+        },
+      },
+    },
+    data: {
+      searchSource: {
+        query: {
+          language: 'kuery',
+          query: '',
+        },
+        filter: [],
+        index: indexPatternId,
+      },
+      references: [
+        {
+          name: 'kibanaSavedObjectMeta.searchSourceJSON.index',
+          type: 'index-pattern',
+          id: indexPatternId,
+        },
+      ],
+      aggs: [
+        {
+          id: '1',
+          enabled: true,
+          type: 'max',
+          params: {
+            field: 'rule.level',
+            customLabel: '- Max Rule Level -',
+          },
+          schema: 'metric',
+        },
+      ],
+    },
+  };
+};
+
+export const getVisStateSuspiciousDownloads = (
+  indexPatternId: string,
+): SavedVis => {
+  return {
+    id: 'Wazuh-App-Overview-Office-Metric-Suspicious-Downloads',
+    title: 'Suspicious Downloads',
+    type: 'metric',
+    params: {
+      addTooltip: true,
+      addLegend: false,
+      type: 'metric',
+      metric: {
+        percentageMode: false,
+        useRanges: false,
+        colorSchema: 'Reds',
+        metricColorMode: 'Labels',
+        colorsRange: [
+          {
+            from: 0,
+            to: 0,
+          },
+          {
+            from: 0,
+            to: 0,
+          },
+        ],
+        labels: {
+          show: true,
+        },
+        invertColors: false,
+        style: {
+          bgFill: '#000',
+          bgColor: false,
+          labelColor: false,
+          subText: '',
+          fontSize: 40,
+        },
+      },
+    },
+    data: {
+      searchSource: {
+        query: {
+          language: 'kuery',
+          query: '',
+        },
+        filter: [],
+        index: indexPatternId,
+      },
+      references: [
+        {
+          name: 'kibanaSavedObjectMeta.searchSourceJSON.index',
+          type: 'index-pattern',
+          id: indexPatternId,
+        },
+      ],
+      aggs: [
+        {
+          id: '1',
+          enabled: true,
+          type: 'count',
+          params: { customLabel: ' ' },
+          schema: 'metric',
+        },
+        {
+          id: '2',
+          enabled: true,
+          type: 'filters',
+          params: {
+            filters: [
+              {
+                input: {
+                  query: 'rule.id: "91724"',
+                  language: 'kuery',
+                },
+                label: '- Suspicious Downloads',
+              },
+            ],
+          },
+          schema: 'group',
+        },
+      ],
+    },
+  };
+};
+
+export const getVisStateFullAccess = (indexPatternId: string): SavedVis => {
+  return {
+    id: 'Wazuh-App-Overview-Office-Metric-FullAccess-Permissions',
+    title: 'Full Access Permissions',
+    type: 'metric',
+    params: {
+      addTooltip: true,
+      addLegend: false,
+      type: 'metric',
+      metric: {
+        percentageMode: false,
+        useRanges: false,
+        colorSchema: 'Greens',
+        metricColorMode: 'Labels',
+        colorsRange: [
+          {
+            from: 0,
+            to: 0,
+          },
+          {
+            from: 0,
+            to: 0,
+          },
+        ],
+        labels: {
+          show: true,
+        },
+        invertColors: false,
+        style: {
+          bgFill: '#000',
+          bgColor: false,
+          labelColor: false,
+          subText: '',
+          fontSize: 40,
+        },
+      },
+    },
+    data: {
+      searchSource: {
+        query: {
+          language: 'kuery',
+          query: '',
+        },
+        filter: [],
+        index: indexPatternId,
+      },
+      references: [
+        {
+          name: 'kibanaSavedObjectMeta.searchSourceJSON.index',
+          type: 'index-pattern',
+          id: indexPatternId,
+        },
+      ],
+      aggs: [
+        {
+          id: '1',
+          enabled: true,
+          type: 'count',
+          params: { customLabel: ' ' },
+          schema: 'metric',
+        },
+        {
+          id: '2',
+          enabled: true,
+          type: 'filters',
+          params: {
+            filters: [
+              {
+                input: {
+                  query: 'rule.id: "91725"',
+                  language: 'kuery',
+                },
+                label: '- Full Access Permissions',
+              },
+            ],
+          },
+          schema: 'group',
+        },
+      ],
+    },
+  };
+};
+
+export const getVisStatePhishingMalware = (
+  indexPatternId: string,
+): SavedVis => {
+  return {
+    id: 'Wazuh-App-Overview-Office-Phishing-Malware',
+    title: 'Authentication success',
+    type: 'metric',
+    params: {
+      addTooltip: true,
+      addLegend: false,
+      type: 'metric',
+      metric: {
+        percentageMode: false,
+        useRanges: false,
+        colorSchema: 'Reds',
+        metricColorMode: 'Labels',
+        colorsRange: [
+          {
+            from: 0,
+            to: 0,
+          },
+          {
+            from: 0,
+            to: 0,
+          },
+        ],
+        labels: {
+          show: true,
+        },
+        invertColors: false,
+        style: {
+          bgFill: '#000',
+          bgColor: false,
+          labelColor: false,
+          subText: '',
+          fontSize: 40,
+        },
+      },
+    },
+    uiState: {
+      vis: { defaultColors: { '0 - 100': 'rgb(0,104,55)' } },
+    },
+    data: {
+      searchSource: {
+        query: {
+          language: 'kuery',
+          query: '',
+        },
+        filter: [],
+        index: indexPatternId,
+      },
+      references: [
+        {
+          name: 'kibanaSavedObjectMeta.searchSourceJSON.index',
+          type: 'index-pattern',
+          id: indexPatternId,
+        },
+      ],
+      aggs: [
+        {
+          id: '1',
+          enabled: true,
+          type: 'count',
+          schema: 'metric',
+          params: { customLabel: ' ' },
+        },
+        {
+          id: '2',
+          enabled: true,
+          type: 'filters',
+          params: {
+            filters: [
+              {
+                input: {
+                  query:
+                    'rule.id: "91556" OR rule.id: "91575" OR rule.id: "91700"',
+                  language: 'kuery',
+                },
+                label: '- Phishing and Malware',
+              },
+            ],
+          },
+          schema: 'group',
+        },
+      ],
+    },
+  };
+};
