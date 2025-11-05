cb343877655649f045c1fbd4d961e14f03631903
Guido Modarelli <guidomodarelli25@gmail.com>
2025-11-05 15:12:39 +0000

    refactor dashboard filters and visualization logic
    This commit refactors the dashboard filters by replacing the existing implementation with a new configuration class «VulnerabilitiesFiltersDashboardByRendererConfig». The new structure enhances the organization of the dashboard panels and improves the rendering logic for visualizations. Additionally, the function «getVisStateFilter» has been defined to streamline the creation of visualization states, ensuring better maintainability and scalability of the dashboard components.

---
+++ plugins/main/common/dashboards/vis-definitions/overview/vulnerabilities/vis-states.ts
@@ -0,0 +1,78 @@
+import type { SavedVis } from '../../../types';
+
+export const getVisStateFilter = (
+  id: string,
+  indexPatternId: string,
+  title: string,
+  label: string,
+  fieldName: string,
+): SavedVis => {
+  return {
+    id,
+    title,
+    type: 'table',
+    params: {
+      perPage: 5,
+      percentageCol: '',
+      row: true,
+      showMetricsAtAllLevels: false,
+      showPartialRows: false,
+      showTotal: false,
+      totalFunc: 'sum',
+    },
+    uiState: {
+      vis: {
+        columnsWidth: [
+          {
+            colIndex: 1,
+            width: 75,
+          },
+        ],
+      },
+    },
+    data: {
+      searchSource: {
+        query: {
+          language: 'kuery',
+          query: '',
+        },
+        index: indexPatternId,
+      },
+      references: [
+        {
+          name: 'kibanaSavedObjectMeta.searchSourceJSON.index',
+          type: 'index-pattern',
+          id: indexPatternId,
+        },
+      ],
+      aggs: [
+        {
+          id: '1',
+          enabled: true,
+          type: 'count',
+          params: {
+            customLabel: 'Count',
+          },
+          schema: 'metric',
+        },
+        {
+          id: '2',
+          enabled: true,
+          type: 'terms',
+          params: {
+            field: fieldName,
+            orderBy: '1',
+            order: 'desc',
+            size: 5,
+            otherBucket: false,
+            otherBucketLabel: 'Other',
+            missingBucket: false,
+            missingBucketLabel: 'Missing',
+            customLabel: label,
+          },
+          schema: 'bucket',
+        },
+      ],
+    },
+  };
+};
