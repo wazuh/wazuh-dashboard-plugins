3f559447dfed94401ac61b07d1841d5544677058
Guido Modarelli <guidomodarelli25@gmail.com>
2025-11-05 18:59:05 +0000

    refactor «getOverviewUsersGroupsTab» to use «ITHygieneUsersInventoriesGroupsDashboardPanelsService»
    This change updates the implementation of the function «getOverviewUsersGroupsTab» to utilize the new service «ITHygieneUsersInventoriesGroupsDashboardPanelsService» for retrieving dashboard panels. The previous logic, which built dashboard KPI panels directly, has been removed to streamline the code and improve maintainability. This refactor aligns with the overall architecture of the dashboard services.

--- plugins/main/public/components/overview/it-hygiene/users/inventories/groups/dashboard.ts
+++ plugins/main/public/components/overview/it-hygiene/users/inventories/groups/dashboard.ts
@@ -1,24 +1,7 @@
-import {
-  buildDashboardKPIPanels,
-  getVisStateHorizontalBarByField,
-  getVisStateMetricUniqueCountByField,
-} from '../../../../../../../common/dashboards/lib';
+import { ITHygieneUsersInventoriesGroupsDashboardPanelsService } from '../../../../../../../common/dashboards/vis-definitions/overview/it-hygiene/users/inventories/groups/dashboard';
 
 export const getOverviewUsersGroupsTab = (indexPatternId: string) => {
-  return buildDashboardKPIPanels([
-    getVisStateHorizontalBarByField(
-      indexPatternId,
-      'group.name',
-      'Top 5 groups',
-      'it-hygiene-groups',
-      { fieldCustomLabel: 'Groups' },
-    ),
-    getVisStateMetricUniqueCountByField(
-      indexPatternId,
-      'group.name',
-      'Unique groups',
-      'it-hygiene-groups-unique-count',
-      'Unique groups',
-    ),
-  ]);
+  return ITHygieneUsersInventoriesGroupsDashboardPanelsService.getDashboardPanels(
+    indexPatternId,
+  );
 };
