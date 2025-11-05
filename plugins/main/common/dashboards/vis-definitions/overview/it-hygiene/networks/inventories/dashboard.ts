2c9f5cefae52dc56e37cb85596a22680b5f0d8c9
Guido Modarelli <guidomodarelli25@gmail.com>
2025-11-05 13:39:43 +0000

    refactor dashboard layout and visualization logic
    This commit refactors the dashboard layout and visualization logic for the IT Hygiene Networks Inventories dashboard. The previous implementation has been replaced with a new structure that utilizes the «ITHygieneNetworksInventoriesDashboardByRendererConfig» and «ITHygieneNetworksInventoriesDashboardLayoutDefinition» classes. This change enhances the organization of the code and improves the maintainability of the dashboard components. Additionally, it integrates the visualization state for global packet loss and interface states, providing a more comprehensive overview of network inventories.

---
+++ plugins/main/common/dashboards/vis-definitions/overview/it-hygiene/networks/inventories/dashboard.ts
@@ -0,0 +1,75 @@
+import {
+  buildDashboardKPIPanels,
+  getVisStateHorizontalBarSplitSeries,
+} from '../../../../../lib';
+import {
+  DashboardByRendererConfig,
+  DashboardLayoutDefinition,
+} from '../../../../../lib/dashboard-config-service';
+import { getVisStateGlobalPacketLossMetric } from './vis-states';
+
+class ITHygieneNetworksInventoriesDashboardLayoutDefinition extends DashboardLayoutDefinition {
+  constructor(indexPatternId: string) {
+    super();
+
+    const panels = Object.values(
+      buildDashboardKPIPanels([
+        getVisStateGlobalPacketLossMetric(indexPatternId),
+        getVisStateHorizontalBarSplitSeries(
+          indexPatternId,
+          'interface.state',
+          'Interface states',
+          'it-hygiene-interfaces',
+          {
+            fieldSize: 4,
+            otherBucket: 'Others',
+            metricCustomLabel: 'Interfaces state count',
+            valueAxesTitleText: ' ',
+            seriesLabel: 'Interfaces state',
+            seriesMode: 'stacked',
+            fieldCustomLabel: 'Interfaces state',
+          },
+        ),
+        getVisStateHorizontalBarSplitSeries(
+          indexPatternId,
+          'interface.type',
+          'Interface types',
+          'it-hygiene-interfaces',
+          {
+            fieldSize: 4,
+            otherBucket: 'Others',
+            metricCustomLabel: 'Interfaces type count',
+            valueAxesTitleText: ' ',
+            seriesLabel: 'Type',
+            seriesMode: 'stacked',
+            fieldCustomLabel: 'Type',
+          },
+        ),
+      ]),
+    ).map(panel => ({
+      gridData: panel.gridData,
+      savedVis: panel.explicitInput.savedVis,
+    }));
+
+    this.setGridVisualizationPairs(...panels);
+  }
+}
+
+export class ITHygieneNetworksInventoriesDashboardByRendererConfig extends DashboardByRendererConfig {
+  constructor(indexPatternId: string) {
+    super(
+      indexPatternId,
+      new ITHygieneNetworksInventoriesDashboardLayoutDefinition(indexPatternId),
+    );
+  }
+
+  protected override getId(): string {
+    return 'it-hygiene-networks-inventories-dashboard-tab';
+  }
+  protected override getTitle(): string {
+    return 'IT Hygiene - Networks Inventories';
+  }
+  protected override getDescription(): string {
+    return 'Dashboard providing an overview of network inventories for IT hygiene.';
+  }
+}
---
+++ plugins/main/common/dashboards/vis-definitions/overview/it-hygiene/networks/inventories/vis-states.ts
@@ -0,0 +1,79 @@
+import {
+  buildIndexPatternReferenceList,
+  buildSearchSource,
+  STYLE,
+} from '../../../../../lib';
+import type { SavedVis } from '../../../../../types';
+
+/**
+ *  You can apply the same logic here using ERRORS instead of DROPS. In both cases, a lower percentage indicates everything is working fine. If the percentage rises too much, it means there's a problem that needs attention.
+ */
+export const getVisStateGlobalPacketLossMetric = (
+  indexPatternId: string,
+): SavedVis => {
+  return {
+    id: 'it-hygiene-network-interfaces-global-packet-loss-rate',
+    title: 'Average packet loss rate',
+    type: 'metric',
+    params: {
+      addTooltip: true,
+      addLegend: false,
+      type: 'metric',
+      metric: {
+        percentageMode: true,
+        useRanges: false,
+        colorSchema: 'Green to Red',
+        metricColorMode: 'None',
+        colorsRange: [
+          {
+            from: 0,
+            to: 10000,
+          },
+        ],
+        labels: {
+          show: true,
+        },
+        invertColors: false,
+        style: STYLE,
+      },
+    },
+    data: {
+      searchSource: buildSearchSource(indexPatternId),
+      references: buildIndexPatternReferenceList(indexPatternId),
+      aggs: [
+        {
+          id: '1',
+          enabled: true,
+          type: 'avg',
+          params: {
+            field: 'host.network.ingress.drops',
+            json: JSON.stringify({
+              script: {
+                source: `
+                  float in_drops=(doc['host.network.ingress.drops'].size() != 0 ? doc['host.network.ingress.drops'].value : 0);
+                  float in_errors=(doc['host.network.ingress.errors'].size() != 0 ? doc['host.network.ingress.errors'].value : 0);
+                  float in_packets=(doc['host.network.ingress.packets'].size() != 0 ? doc['host.network.ingress.packets'].value : 0);
+                  float out_drops=(doc['host.network.egress.drops'].size() != 0 ? doc['host.network.egress.drops'].value : 0);
+                  float out_errors=(doc['host.network.egress.errors'].size() != 0 ? doc['host.network.egress.errors'].value : 0);
+                  float out_packets=(doc['host.network.egress.packets'].size() != 0 ? doc['host.network.egress.packets'].value : 0);
+                  float d=(in_drops + out_drops);
+                  float p=(in_drops + in_errors + in_packets + out_drops + out_errors + out_packets);
+                  return p == 0 ? 0 : Math.round((d/p)*100*100);
+                `.trim(),
+                lang: 'painless',
+              },
+            }) /*
+                The total packets is the sum of: packets, drops and erros.
+                The result is multiplied by:
+                - 100 to convert the value (d/p) to percent (%)
+                WORKAROUND: multiply 100 to equilibrate the division by 100 done when the `isPercentMode` is true
+              */,
+
+            customLabel: 'Average packet loss rate',
+          },
+          schema: 'metric',
+        },
+      ],
+    },
+  };
+};
