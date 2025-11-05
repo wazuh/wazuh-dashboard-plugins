9e9ccf68d9e721742dfc014e63456e25ec4f6ee1
Guido Modarelli <guidomodarelli25@gmail.com>
2025-11-05 18:57:03 +0000

    refactor «getOverviewSystemSystemTab» to use «ITHygieneSystemInventoriesSystemDashboardPanelsService»
    This change updates the implementation of the function «getOverviewSystemSystemTab» to utilize the new service «ITHygieneSystemInventoriesSystemDashboardPanelsService» for retrieving dashboard panels. The previous logic, which involved building KPI panels directly, has been removed to streamline the code and improve maintainability. This refactor enhances the separation of concerns by delegating the panel retrieval logic to the dedicated service.

---
+++ plugins/main/common/dashboards/vis-definitions/overview/it-hygiene/system/inventories/system/dashboard.ts
@@ -0,0 +1,91 @@
+import {
+  buildDashboardKPIPanels,
+  getVisStateHorizontalBarByField,
+  getVisStateHorizontalBarSplitSeries,
+} from '../../../../../../lib';
+import {
+  DashboardByRendererConfig,
+  DashboardLayoutDefinition,
+} from '../../../../../../lib/dashboard-config-service';
+import type { DashboardByRendererPanels } from '../../../../../../types';
+
+export class ITHygieneSystemInventoriesSystemDashboardLayoutDefinition extends DashboardLayoutDefinition {
+  constructor(indexPatternId: string) {
+    super();
+
+    const panels = Object.values(
+      buildDashboardKPIPanels([
+        getVisStateHorizontalBarByField(
+          indexPatternId,
+          'host.os.platform',
+          'Top 5 platforms',
+          'it-hygiene-system',
+          { fieldCustomLabel: 'Platforms' },
+        ),
+        getVisStateHorizontalBarByField(
+          indexPatternId,
+          'host.os.name',
+          'Top 5 operating systems',
+          'it-hygiene-system',
+          { fieldCustomLabel: 'OS' },
+        ),
+        getVisStateHorizontalBarSplitSeries(
+          indexPatternId,
+          'host.architecture',
+          'Architecture',
+          'it-hygiene-system',
+          {
+            fieldSize: 4,
+            otherBucket: 'Others',
+            metricCustomLabel: 'Host architecture count',
+            valueAxesTitleText: ' ',
+            fieldCustomLabel: 'Host architecture',
+            seriesLabel: 'Host architecture',
+          },
+        ),
+      ]),
+    ).map(panel => ({
+      gridData: panel.gridData,
+      savedVis: panel.explicitInput.savedVis,
+    }));
+
+    this.setGridVisualizationPairs(...panels);
+  }
+}
+
+export class ITHygieneSystemInventoriesSystemDashboardByRendererConfig extends DashboardByRendererConfig {
+  constructor(indexPatternId: string) {
+    super(
+      indexPatternId,
+      new ITHygieneSystemInventoriesSystemDashboardLayoutDefinition(
+        indexPatternId,
+      ),
+    );
+  }
+
+  protected override getId(): string {
+    return 'it-hygiene-system-inventories-system-dashboard-tab';
+  }
+  protected override getTitle(): string {
+    return 'IT Hygiene - System Inventories - System';
+  }
+  protected override getDescription(): string {
+    return 'Dashboard of IT Hygiene System Inventories - System';
+  }
+}
+
+export class ITHygieneSystemInventoriesSystemDashboardPanelsService {
+  private static getOverviewDashboardPanels = (
+    indexPatternId: string,
+  ): DashboardByRendererPanels => {
+    return new ITHygieneSystemInventoriesSystemDashboardByRendererConfig(
+      indexPatternId,
+    ).getDashboardPanels();
+  };
+
+  public static getDashboardPanels(
+    indexPatternId: string,
+  ): DashboardByRendererPanels {
+    return this.getOverviewDashboardPanels(indexPatternId);
+  }
+}
--- plugins/main/public/components/overview/it-hygiene/system/inventories/system/dashboard.ts
+++ plugins/main/public/components/overview/it-hygiene/system/inventories/system/dashboard.ts
@@ -1,38 +1,7 @@
-import {
-  buildDashboardKPIPanels,
-  getVisStateHorizontalBarByField,
-  getVisStateHorizontalBarSplitSeries,
-} from '../../../../../../../common/dashboards/lib';
+import { ITHygieneSystemInventoriesSystemDashboardPanelsService } from '../../../../../../../common/dashboards/vis-definitions/overview/it-hygiene/system/inventories/system/dashboard';
 
 export const getOverviewSystemSystemTab = (indexPatternId: string) => {
-  return buildDashboardKPIPanels([
-    getVisStateHorizontalBarByField(
-      indexPatternId,
-      'host.os.platform',
-      'Top 5 platforms',
-      'it-hygiene-system',
-      { fieldCustomLabel: 'Platforms' },
-    ),
-    getVisStateHorizontalBarByField(
-      indexPatternId,
-      'host.os.name',
-      'Top 5 operating systems',
-      'it-hygiene-system',
-      { fieldCustomLabel: 'OS' },
-    ),
-    getVisStateHorizontalBarSplitSeries(
-      indexPatternId,
-      'host.architecture',
-      'Architecture',
-      'it-hygiene-system',
-      {
-        fieldSize: 4,
-        otherBucket: 'Others',
-        metricCustomLabel: 'Host architecture count',
-        valueAxesTitleText: ' ',
-        fieldCustomLabel: 'Host architecture',
-        seriesLabel: 'Host architecture',
-      },
-    ),
-  ]);
+  return ITHygieneSystemInventoriesSystemDashboardPanelsService.getDashboardPanels(
+    indexPatternId,
+  );
 };
