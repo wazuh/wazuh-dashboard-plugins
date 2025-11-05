b6fe5e6fbb48b8d13baf0337b41e903e6b424bea
Guido Modarelli <guidomodarelli25@gmail.com>
2025-11-05 14:30:14 +0000

    refactor dashboard components for IT Hygiene Packages Inventories
    This commit refactors the dashboard components for IT Hygiene Packages Inventories by introducing the new classes «ITHygienePackagesInventoriesPackagesDashboardLayoutDefinition» and «ITHygienePackagesInventoriesPackagesDashboardByRendererConfig». The previous implementation has been replaced to streamline the dashboard panel creation process and improve maintainability. The new structure enhances the organization of the dashboard layout and configuration, ensuring better integration with the existing dashboard framework.

---
+++ plugins/main/common/dashboards/vis-definitions/overview/it-hygiene/packages/inventories/packages/dashboard.ts
@@ -0,0 +1,75 @@
+import {
+  buildDashboardKPIPanels,
+  getVisStateHorizontalBarSplitSeries,
+  getVisStateMetricUniqueCountByField,
+} from '../../../../../../lib';
+import {
+  DashboardByRendererConfig,
+  DashboardLayoutDefinition,
+} from '../../../../../../lib/dashboard-config-service';
+import { getVisStateFilter } from './vis-states';
+
+export class ITHygienePackagesInventoriesPackagesDashboardLayoutDefinition extends DashboardLayoutDefinition {
+  constructor(indexPatternId: string) {
+    super();
+
+    const panels = Object.values(
+      buildDashboardKPIPanels([
+        getVisStateFilter(
+          'Vendors',
+          indexPatternId,
+          '',
+          'Top 5 vendors',
+          'package.vendor',
+        ),
+        getVisStateMetricUniqueCountByField(
+          indexPatternId,
+          'package.name',
+          '',
+          'it-hygiene-packages',
+          'Unique packages',
+        ),
+        getVisStateHorizontalBarSplitSeries(
+          indexPatternId,
+          'package.type',
+          'Package types',
+          'it-hygiene-packages',
+          {
+            fieldSize: 4,
+            otherBucket: 'Others',
+            metricCustomLabel: 'Package type count',
+            valueAxesTitleText: ' ',
+            seriesLabel: 'Package type count',
+            fieldCustomLabel: 'Package type',
+          },
+        ),
+      ]),
+    ).map(panel => ({
+      gridData: panel.gridData,
+      savedVis: panel.explicitInput.savedVis,
+    }));
+
+    this.setGridVisualizationPairs(...panels);
+  }
+}
+
+export class ITHygienePackagesInventoriesPackagesDashboardByRendererConfig extends DashboardByRendererConfig {
+  constructor(indexPatternId: string) {
+    super(
+      indexPatternId,
+      new ITHygienePackagesInventoriesPackagesDashboardLayoutDefinition(
+        indexPatternId,
+      ),
+    );
+  }
+
+  protected override getId(): string {
+    return 'it-hygiene-packages-inventories-packages-dashboard-tab';
+  }
+  protected override getTitle(): string {
+    return 'IT Hygiene - Packages Inventories - Packages';
+  }
+  protected override getDescription(): string {
+    return 'Dashboard of IT Hygiene Packages Inventories - Packages';
+  }
+}
