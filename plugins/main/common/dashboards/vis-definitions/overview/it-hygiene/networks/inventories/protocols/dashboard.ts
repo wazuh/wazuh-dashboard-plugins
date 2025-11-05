8e2bcaff8d8cf1b64982a93c9b49cf38015eada1
Guido Modarelli <guidomodarelli25@gmail.com>
2025-11-05 14:12:37 +0000

    refactor dashboard components for IT Hygiene Networks Inventories
    This commit refactors the dashboard components related to IT Hygiene Networks Inventories by introducing the new class «ITHygieneNetworksInventoriesProtocolsDashboardByRendererConfig» and its layout definition. The previous implementation has been streamlined to utilize the new configuration class, enhancing maintainability and readability. The changes also include the addition of the «getVisStateNetworkMetricsMinMax» function to manage network metrics visualization, ensuring a more modular approach to dashboard panel creation.

---
+++ plugins/main/common/dashboards/vis-definitions/overview/it-hygiene/networks/inventories/protocols/dashboard.ts
@@ -0,0 +1,77 @@
+import {
+  buildDashboardKPIPanels,
+  getVisStateHorizontalBarSplitSeries,
+} from '../../../../../../lib';
+import {
+  DashboardByRendererConfig,
+  DashboardLayoutDefinition,
+} from '../../../../../../lib/dashboard-config-service';
+import { getVisStateNetworkMetricsMinMax } from './vis-states';
+
+export class ITHygieneNetworksInventoriesProtocolsDashboardLayoutDefinition extends DashboardLayoutDefinition {
+  constructor(indexPatternId: string) {
+    super();
+
+    const panels = Object.values(
+      buildDashboardKPIPanels([
+        getVisStateHorizontalBarSplitSeries(
+          indexPatternId,
+          'network.type',
+          'Network types',
+          'it-hygiene-protocols',
+          {
+            fieldSize: 4,
+            otherBucket: 'Others',
+            metricCustomLabel: 'Network type count',
+            valueAxesTitleText: ' ',
+            seriesLabel: 'Type',
+            seriesMode: 'stacked',
+            fieldCustomLabel: 'Type',
+          },
+        ),
+        getVisStateNetworkMetricsMinMax(indexPatternId),
+        getVisStateHorizontalBarSplitSeries(
+          indexPatternId,
+          'network.dhcp',
+          'DHCP enabled',
+          'it-hygiene-protocols',
+          {
+            fieldSize: 4,
+            otherBucket: 'Others',
+            metricCustomLabel: 'Network DHCP count',
+            valueAxesTitleText: ' ',
+            seriesLabel: 'DHCP enabled',
+            seriesMode: 'stacked',
+            fieldCustomLabel: 'DHCP enabled',
+          },
+        ),
+      ]),
+    ).map(panel => ({
+      gridData: panel.gridData,
+      savedVis: panel.explicitInput.savedVis,
+    }));
+
+    this.setGridVisualizationPairs(...panels);
+  }
+}
+
+export class ITHygieneNetworksInventoriesProtocolsDashboardByRendererConfig extends DashboardByRendererConfig {
+  constructor(indexPatternId: string) {
+    super(
+      indexPatternId,
+      new ITHygieneNetworksInventoriesProtocolsDashboardLayoutDefinition(
+        indexPatternId,
+      ),
+    );
+  }
+
+  protected override getId(): string {
+    return 'it-hygiene-networks-inventories-protocols-dashboard-tab';
+  }
+  protected override getTitle(): string {
+    return 'IT Hygiene - Networks Inventories - Protocols';
+  }
+  protected override getDescription(): string {
+    return 'Dashboard of network protocols in IT hygiene context';
+  }
+}
