f2f0339cd2a99e03dfa08bf94d4d34ccf599e231
Guido Modarelli <guidomodarelli25@gmail.com>
2025-11-05 15:30:17 +0000

    add vulnerabilities overview dashboard layout and visualization logic
    This commit introduces the layout and visualization logic for the vulnerabilities overview dashboard. It implements the classes «VulnerabilitiesOverviewDashboardLayoutDefinition» and «VulnerabilitiesOverviewDashboardByRendererConfig», which define the structure and behavior of the dashboard panels. The new layout includes visualizations for the most detected vulnerabilities, vulnerable OS families, and vulnerabilities by year of publication, enhancing the dashboard's functionality and user experience.

---
+++ plugins/main/common/dashboards/vis-definitions/overview/vulnerabilities/dashboard.ts
@@ -0,0 +1,85 @@
+import {
+  DashboardByRendererConfig,
+  DashboardLayoutDefinition,
+} from '../../../lib/dashboard-config-service';
+import {
+  getVisStateAccumulationMostDetectedVulnerabilities,
+  getVisStateTopVulnerabilitiesScore,
+  getVisStateTopVulnerableOSFamilies,
+} from './vis-states';
+
+export class VulnerabilitiesOverviewDashboardLayoutDefinition extends DashboardLayoutDefinition {
+  constructor(indexPatternId: string) {
+    super();
+
+    const panels = Object.values({
+      '6': {
+        gridData: {
+          w: 16,
+          h: 12,
+          x: 0,
+          y: 0,
+          i: '6',
+        },
+        type: 'visualization',
+        explicitInput: {
+          id: '6',
+          savedVis: getVisStateTopVulnerabilitiesScore(indexPatternId),
+        },
+      },
+      '7': {
+        gridData: {
+          w: 16,
+          h: 12,
+          x: 16,
+          y: 0,
+          i: '7',
+        },
+        type: 'visualization',
+        explicitInput: {
+          id: '7',
+          savedVis: getVisStateTopVulnerableOSFamilies(indexPatternId),
+        },
+      },
+      '8': {
+        gridData: {
+          w: 16,
+          h: 12,
+          x: 32,
+          y: 0,
+          i: '8',
+        },
+        type: 'visualization',
+        explicitInput: {
+          id: '8',
+          savedVis:
+            getVisStateAccumulationMostDetectedVulnerabilities(indexPatternId),
+        },
+      },
+    }).map(panel => ({
+      gridData: panel.gridData,
+      savedVis: panel.explicitInput.savedVis,
+    }));
+
+    this.setGridVisualizationPairs(...panels);
+  }
+}
+
+export class VulnerabilitiesOverviewDashboardByRendererConfig extends DashboardByRendererConfig {
+  constructor(indexPatternId: string) {
+    super(
+      indexPatternId,
+      new VulnerabilitiesOverviewDashboardLayoutDefinition(indexPatternId),
+    );
+  }
+
+  protected override getId(): string {
+    return 'vulnerabilities-overview-dashboard-tab';
+  }
+  protected override getTitle(): string {
+    return 'Vulnerabilities Overview';
+  }
+  protected override getDescription(): string {
+    return 'Dashboard of Vulnerabilities Overview';
+  }
+}
