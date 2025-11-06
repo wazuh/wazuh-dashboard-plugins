3b7e4de9ac8055a66b5dcebf8d4a6f0c4e5dcdd0
Guido Modarelli <guidomodarelli25@gmail.com>
2025-11-06 18:47:30 +0000

    refactor function «getVisStateEventsCountEvolution» for improved clarity and structure
    Updated the implementation of the «getVisStateEventsCountEvolution» function to enhance readability and maintainability. The function now returns a more structured object, with clearer parameter definitions and improved organization of the visualization state properties. This refactor aims to streamline future modifications and ensure consistency across the dashboard visualizations.

--- plugins/main/common/dashboards/vis-definitions/management/agent/welcome/vis-states.ts
+++ plugins/main/common/dashboards/vis-definitions/management/agent/welcome/vis-states.ts
@@ -6,100 +6,102 @@ import type { SavedVis } from '../../../../types';
 
 export const getVisStateEventsCountEvolution = (
   indexPatternId: string,
-): SavedVis => ({
-  id: 'App-Agents-Welcome-Events-Evolution',
-  title: 'Events count evolution',
-  type: 'line',
-  params: {
+): SavedVis => {
+  return {
+    id: 'agents-welcome-events-evolution',
+    title: 'Events count evolution',
     type: 'line',
-    grid: { categoryLines: false },
-    categoryAxes: [
-      {
-        id: 'CategoryAxis-1',
-        type: 'category',
-        position: 'bottom',
-        show: true,
-        style: {},
-        scale: { type: 'linear' },
-        labels: { show: true, filter: true, truncate: 100 },
-        title: {},
-      },
-    ],
-    valueAxes: [
-      {
-        id: 'ValueAxis-1',
-        name: 'LeftAxis-1',
-        type: 'value',
-        position: 'left',
-        show: true,
-        style: {},
-        scale: { type: 'linear', mode: 'normal' },
-        labels: { show: true, rotate: 0, filter: false, truncate: 100 },
-        title: { text: 'Count' },
-      },
-    ],
-    seriesParams: [
-      {
-        show: true,
-        type: 'line',
-        mode: 'normal',
-        data: { label: 'Count', id: '1' },
-        valueAxis: 'ValueAxis-1',
-        drawLinesBetweenPoints: true,
-        lineWidth: 2,
-        interpolate: 'linear',
-        showCircles: true,
-      },
-    ],
-    addTooltip: true,
-    addLegend: false,
-    legendPosition: 'right',
-    times: [],
-    addTimeMarker: false,
-    labels: {},
-    thresholdLine: {
-      show: false,
-      value: 10,
-      width: 1,
-      style: 'full',
-      color: '#E7664C',
-    },
-    dimensions: {
-      x: null,
-      y: [
+    params: {
+      type: 'line',
+      grid: { categoryLines: false },
+      categoryAxes: [
         {
-          accessor: 0,
-          format: { id: 'number' },
-          params: {},
-          label: 'Count',
-          aggType: 'count',
+          id: 'CategoryAxis-1',
+          type: 'category',
+          position: 'bottom',
+          show: true,
+          style: {},
+          scale: { type: 'linear' },
+          labels: { show: true, filter: true, truncate: 100 },
+          title: {},
+        },
+      ],
+      valueAxes: [
+        {
+          id: 'ValueAxis-1',
+          name: 'LeftAxis-1',
+          type: 'value',
+          position: 'left',
+          show: true,
+          style: {},
+          scale: { type: 'linear', mode: 'normal' },
+          labels: { show: true, rotate: 0, filter: false, truncate: 100 },
+          title: { text: 'Count' },
+        },
+      ],
+      seriesParams: [
+        {
+          show: true,
+          type: 'line',
+          mode: 'normal',
+          data: { label: 'Count', id: '1' },
+          valueAxis: 'ValueAxis-1',
+          drawLinesBetweenPoints: true,
+          lineWidth: 2,
+          interpolate: 'linear',
+          showCircles: true,
+        },
+      ],
+      addTooltip: true,
+      addLegend: false,
+      legendPosition: 'right',
+      times: [],
+      addTimeMarker: false,
+      labels: {},
+      thresholdLine: {
+        show: false,
+        value: 10,
+        width: 1,
+        style: 'full',
+        color: '#E7664C',
+      },
+      dimensions: {
+        x: null,
+        y: [
+          {
+            accessor: 0,
+            format: { id: 'number' },
+            params: {},
+            label: 'Count',
+            aggType: 'count',
+          },
+        ],
+      },
+    },
+    uiState: {
+      vis: { params: { sort: { columnIndex: 2, direction: 'desc' } } },
+    },
+    data: {
+      searchSource: buildSearchSource(indexPatternId),
+      references: buildIndexPatternReferenceList(indexPatternId),
+      aggs: [
+        { id: '1', enabled: true, type: 'count', schema: 'metric', params: {} },
+        {
+          id: '2',
+          enabled: true,
+          type: 'date_histogram',
+          schema: 'segment',
+          params: {
+            field: 'timestamp',
+            useNormalizedEsInterval: true,
+            scaleMetricValues: false,
+            interval: 'auto',
+            drop_partials: false,
+            min_doc_count: 1,
+            extended_bounds: {},
+          },
         },
       ],
     },
-  },
-  uiState: {
-    vis: { params: { sort: { columnIndex: 2, direction: 'desc' } } },
-  },
-  data: {
-    searchSource: buildSearchSource(indexPatternId),
-    references: buildIndexPatternReferenceList(indexPatternId),
-    aggs: [
-      { id: '1', enabled: true, type: 'count', schema: 'metric', params: {} },
-      {
-        id: '2',
-        enabled: true,
-        type: 'date_histogram',
-        schema: 'segment',
-        params: {
-          field: 'timestamp',
-          useNormalizedEsInterval: true,
-          scaleMetricValues: false,
-          interval: 'auto',
-          drop_partials: false,
-          min_doc_count: 1,
-          extended_bounds: {},
-        },
-      },
-    ],
-  },
-});
+  };
+};
