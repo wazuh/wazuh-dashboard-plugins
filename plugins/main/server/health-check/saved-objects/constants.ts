68cef9169c0db3f515b0fe602bff7e087a926eb1
Guido Modarelli <guidomodarelli25@gmail.com>
2025-11-13 12:36:18 +0000

    remove unused constant «INDEX_PATTERN_REPLACE_ME»
    This change removes the unused constant «INDEX_PATTERN_REPLACE_ME» from the constants file. This helps to clean up the codebase by eliminating unnecessary variables, improving maintainability and readability.

--- plugins/main/server/health-check/saved-objects/constants.ts
+++ plugins/main/server/health-check/saved-objects/constants.ts
@@ -1,6 +1,5 @@
 import path from 'path';
 
-export const INDEX_PATTERN_REPLACE_ME = '__index_pattern_replace_me__';
 export const DEFAULT_DEFINITIONS_FOLDER = path.resolve(
   __dirname,
   '../../../common/dashboards/dashboard-definitions',
