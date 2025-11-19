1543cd7764c1f073ad2ec6de62310530b9f52d14
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2025-11-19 13:46:58 +0100

    remove: example request related to logtest in Dev Tools

--- plugins/main/public/components/tools/devtools/initial-buffer.ts
+++ plugins/main/public/components/tools/devtools/initial-buffer.ts
@@ -15,17 +15,4 @@ GET /manager/info
 # Add agent: Add a new agent
 POST /agents
 ${JSON.stringify({ name: 'NewAgent' }, null, 2)}
-
-# Run logtest: Run logtest tool to check if a specified log raises any alert among other information
-PUT /logtest
-${JSON.stringify(
-  {
-    log_format: 'syslog',
-    location: 'logtest',
-    event:
-      'Jul 06 22:00:22 linux-agent sshd[29205]: Invalid user blimey from 1.3.1.3 port 48928',
-  },
-  null,
-  2,
-)}
 `;
