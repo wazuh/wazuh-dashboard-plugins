22a9ebb740ba8a70065d1b4fa4d74291d7729aea
Felipe Gonzalez <felipegonzalezmv@hotmail.com>
2025-12-02 14:41:32 -0500

    Refactor file integrity monitoring, configuration assessment, and vulnerabilities columns to use common column definitions
    This commit updates the column definitions in the file integrity monitoring, configuration assessment, and vulnerabilities components to utilize common column references. Deprecated column configurations have been commented out for future reference, enhancing code clarity and maintainability.

--- plugins/main/public/components/overview/fim/events/file-integrity-monitoring-columns.tsx
+++ plugins/main/public/components/overview/fim/events/file-integrity-monitoring-columns.tsx
@@ -4,15 +4,12 @@ import { commonColumns } from '../../common/data-grid-columns';
 export const fileIntegrityMonitoringColumns: tDataGridColumn[] = [
   commonColumns.timestamp,
   commonColumns['agent.name'],
-  {
-    id: 'syscheck.path',
-    initialWidth: 392,
-  },
-  {
-    id: 'syscheck.event',
-    initialWidth: 140,
-  },
-  commonColumns['rule.description'],
-  commonColumns['rule.level'],
-  commonColumns['rule.id'],
+  //   {
+  //     id: 'syscheck.path',
+  //     initialWidth: 392,
+  //   },
+  //   {
+  //     id: 'syscheck.event',
+  //     initialWidth: 140,
+  //   },
 ];
