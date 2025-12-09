b3e7445e836ad18494050d88891e279eb00994c8
Felipe Gonzalez <felipegonzalezmv@hotmail.com>
2025-12-09 10:33:14 -0500

    Remove commented-out vulnerability columns from vulnerabilities-columns.tsx for cleaner code and improved readability.

--- plugins/main/public/components/overview/vulnerabilities/events/vulnerabilities-columns.tsx
+++ plugins/main/public/components/overview/vulnerabilities/events/vulnerabilities-columns.tsx
@@ -4,19 +4,4 @@ import { commonColumns } from '../../common/data-grid-columns';
 export const vulnerabilitiesColumns: tDataGridColumn[] = [
   commonColumns.timestamp,
   commonColumns['agent.name'],
-  // {
-  //   id: 'data.vulnerability.cve',
-  //   initialWidth: 186,
-  // },
-  // {
-  //   id: 'data.vulnerability.severity',
-  //   initialWidth: 217,
-  // },
-  // {
-  //   id: 'data.vulnerability.package.name',
-  //   initialWidth: 257,
-  // },
-  // {
-  //   id: 'data.vulnerability.package.version',
-  // },
 ];
