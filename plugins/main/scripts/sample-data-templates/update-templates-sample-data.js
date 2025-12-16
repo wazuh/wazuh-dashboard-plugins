15dc5cca0ea9f8f742fbaa96e6f45cab9fe3b3ce
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2025-12-16 18:33:26 +0100

    fix: add workaround to ingore the update of wazuh-events templates for sample data

--- plugins/main/scripts/sample-data-templates/update-templates-sample-data.js
+++ plugins/main/scripts/sample-data-templates/update-templates-sample-data.js
@@ -82,6 +82,8 @@ const config = {
     'states-inventory-browser-extensions':
       'templates/states/inventory-browser-extensions.json',
   },
+  // WORKAROUND: This dataset is ignored due to issues with its template file (no existing in source repository). The wazuh-events dataset should be composed with the other wazuh-events related datasets in the future.
+  ignore_datasets: ['wazuh-events'],
 };
 
 // Function to get the list of datasets
@@ -89,6 +91,10 @@ function getDatasets() {
   try {
     const items = fs.readdirSync(config.localDatasetDir);
     config.datasets = items.filter(item => {
+      // Ignore specific datasets
+      if (config.ignore_datasets.includes(item)) {
+        return false;
+      }
       const itemPath = path.join(config.localDatasetDir, item);
       return fs.statSync(itemPath).isDirectory();
     });
