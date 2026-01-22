c3c6031418e0aeec418be496f65b1608bde3d95e
Antonio <34042064+Desvelao@users.noreply.github.com>
2026-01-22 14:03:51 +0100

    Remove the UI permission validation for the upgrade and remove agent actions (#8060)
    * fix: remove the UI permissions validation for the upgrade and remove agent actions
    
    * chore(changelog): add entry

--- plugins/main/public/components/endpoints-summary/table/agents-table.tsx
+++ plugins/main/public/components/endpoints-summary/table/agents-table.tsx
@@ -82,13 +82,6 @@ export const AgentsTable = withErrorBoundary((props: AgentsTableProps) => {
   const [denyEditGroups] = useUserPermissionsRequirements([
     { action: 'group:modify_assignments', resource: 'group:id:*' },
   ]);
-  const [denyUpgrade] = useUserPermissionsRequirements([
-    { action: 'agent:upgrade', resource: 'agent:id:*' },
-  ]);
-  // FIXME: This defines if the user has the capability to not remove agent, negating it we got the capability to this action. This is used to enable the action despite this should use the specific agent ID and group. Same happens with the upgrade and probably with the edition of groups too.
-  const [denyRemove] = useUserPermissionsRequirements([
-    { action: 'agent:delete', resource: 'agent:id:*' },
-  ]);
   const [denyGetTasks] = useUserPermissionsRequirements([
     { action: 'task:status', resource: '*:*:*' },
   ]);
@@ -259,8 +252,6 @@ export const AgentsTable = withErrorBoundary((props: AgentsTableProps) => {
                   allAgentsCount={agentList.totalItems}
                   filters={filters?.q}
                   allowEditGroups={!denyEditGroups}
-                  allowRemove={!denyRemove}
-                  allowUpgrade={!denyUpgrade}
                   allowGetTasks={!denyGetTasks}
                   reloadAgents={() => reloadAgents()}
                   setIsUpgradeTasksModalVisible={setIsUpgradeTasksModalVisible}
@@ -271,13 +262,12 @@ export const AgentsTable = withErrorBoundary((props: AgentsTableProps) => {
             endpoint={'/agents'}
             tableColumns={agentsTableColumns(
               !denyEditGroups,
-              !denyUpgrade,
               setAgent,
               setIsEditGroupsVisible,
               setIsUpgradeModalVisible,
               setFilters,
               apiVersion,
-              { setIsRemoveModalVisible, allowRemove: !denyRemove },
+              { setIsRemoveModalVisible },
             )}
             tableInitialSortingField='id'
             tablePageSizeOptions={[10, 25, 50, 100]}
