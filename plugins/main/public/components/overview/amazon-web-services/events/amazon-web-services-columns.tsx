e8411f5f20d9e9c764c6016b842d89acfe04e99a
Ian Yenien Serrano <63758389+yenienserrano@users.noreply.github.com>
2026-02-10 12:44:40 -0300

    Remove agent column

--- plugins/main/public/components/overview/amazon-web-services/events/amazon-web-services-columns.tsx
+++ plugins/main/public/components/overview/amazon-web-services/events/amazon-web-services-columns.tsx
@@ -3,15 +3,11 @@ import { commonColumns } from '../../common/data-grid-columns';
 
 export const amazonWebServicesColumns: tDataGridColumn[] = [
   commonColumns.timestamp,
-  commonColumns['wazuh.agent.id'],
-  commonColumns['wazuh.agent.name'],
   {
     id: 'aws.cloudtrail_event_source',
-    initialWidth: 280,
   },
   {
     id: 'event.action',
-    initialWidth: 280,
   },
   {
     id: 'cloud.region',
