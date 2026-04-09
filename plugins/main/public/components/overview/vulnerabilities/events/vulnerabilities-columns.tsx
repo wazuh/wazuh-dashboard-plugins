3c531295c432c92a33294ab53aaa92b6e1080ebc
Federico Rodriguez <federico.rodriguez@wazuh.com>
2026-04-09 11:42:34 +0200

    Add columns to VD findings

--- plugins/main/public/components/overview/vulnerabilities/events/vulnerabilities-columns.tsx
+++ plugins/main/public/components/overview/vulnerabilities/events/vulnerabilities-columns.tsx
@@ -4,4 +4,9 @@ import { commonColumns } from '../../common/data-grid-columns';
 export const vulnerabilitiesColumns: tDataGridColumn[] = [
   commonColumns.timestamp,
   commonColumns['wazuh.agent.name'],
+  { id: 'package.name' },
+  { id: 'package.version' },
+  { id: 'vulnerability.severity' },
+  { id: 'vulnerability.id' },
+
 ];
