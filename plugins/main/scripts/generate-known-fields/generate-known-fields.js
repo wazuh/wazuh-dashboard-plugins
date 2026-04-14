db38b48f3e43c341c9fa5bdc02b5d85f7aeb0c01
Adam Navarro Megías <adam.navarro@wazuh.com>
2026-04-14 13:09:35 +0200

    Removed wazuh-events-v5-cloud-services-aws
    Signed-off-by: Adam Navarro Megías <adam.navarro@wazuh.com>

--- plugins/main/scripts/generate-known-fields/generate-known-fields.js
+++ plugins/main/scripts/generate-known-fields/generate-known-fields.js
@@ -82,14 +82,6 @@ const TEMPLATE_SOURCES = {
     ],
     outputFile: 'events-cloud-services.json',
   },
-  'events-cloud-services-aws': {
-    urls: [
-      wazuhUrl(
-        'plugins/setup/src/main/resources/templates/streams/events.json',
-      ),
-    ],
-    outputFile: 'events-cloud-services-aws.json',
-  },
   'events-cloud-services-azure': {
     urls: [
       wazuhUrl(
