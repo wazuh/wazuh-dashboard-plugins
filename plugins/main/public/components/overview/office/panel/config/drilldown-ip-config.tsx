a77afdaa61ee2e17539c69d12ff05d87c91ec191
Rodrigo Lopez <rodrigo.lopez@wazuh.com>
2026-04-29 17:11:02 +0000

    fix: use dashboard renderer in o365 drilldown

--- plugins/main/public/components/overview/office/panel/config/drilldown-ip-config.tsx
+++ plugins/main/public/components/overview/office/panel/config/drilldown-ip-config.tsx
@@ -1,4 +1,3 @@
-'use strict';
 /*
  * Wazuh app - Office 365 Drilldown IP field Config.
  *
@@ -13,126 +12,24 @@
  */
 
 import React from 'react';
-import { ViewMode } from '../../../../../../../../src/plugins/embeddable/public';
-import { getPlugins } from '../../../../../kibana-services';
-import { DashboardPanelState } from '../../../../../../../../src/plugins/dashboard/public/application';
-import { EmbeddableInput } from '../../../../../../../../src/plugins/embeddable/public';
-import {
-  getVisStateOfficeMetricStats,
-  getVisStateOfficeTopsEventsPie,
-  getVisStateOfficeUserOperationLevel,
-  getVisStateOfficeAlertsEvolutionByUser,
-} from './visualizations';
-
-const DashboardByRenderer =
-  getPlugins().dashboard.DashboardContainerByValueRenderer;
-
-const getDashboardPanels = (
-  indexPatternId: string,
-): {
-  [panelId: string]: DashboardPanelState<
-    EmbeddableInput & { [k: string]: unknown }
-  >;
-} => {
-  return {
-    d0: {
-      gridData: {
-        w: 15,
-        h: 14,
-        x: 0,
-        y: 0,
-        i: 'd0',
-      },
-      type: 'visualization',
-      explicitInput: {
-        id: 'd0',
-        savedVis: getVisStateOfficeMetricStats(indexPatternId),
-      },
-    },
-    d1: {
-      gridData: {
-        w: 15,
-        h: 14,
-        x: 15,
-        y: 0,
-        i: 'd1',
-      },
-      type: 'visualization',
-      explicitInput: {
-        id: 'd1',
-        savedVis: getVisStateOfficeTopsEventsPie(indexPatternId),
-      },
-    },
-    d2: {
-      gridData: {
-        w: 18,
-        h: 14,
-        x: 30,
-        y: 0,
-        i: 'd2',
-      },
-      type: 'visualization',
-      explicitInput: {
-        id: 'd2',
-        savedVis: getVisStateOfficeUserOperationLevel(indexPatternId),
-      },
-    },
-    d3: {
-      gridData: {
-        w: 48,
-        h: 11,
-        x: 0,
-        y: 14,
-        i: 'd3',
-      },
-      type: 'visualization',
-      explicitInput: {
-        id: 'd3',
-        savedVis: getVisStateOfficeAlertsEvolutionByUser(indexPatternId),
-      },
-    },
-  };
-};
+import DashboardRenderer from '../../../../common/dashboards/dashboard-renderer/dashboard-renderer';
 
 export const drilldownIPConfig = props => {
-  const { fetchFilters, searchBarProps, indexPattern } = props;
-
+  const { fetchFilters, searchBarProps } = props;
   return {
     rows: [
       {
         columns: [
           {
             width: 100,
-            component: props => {
-              return (
-                <div style={{ width: '100%' }}>
-                  <DashboardByRenderer
-                    input={{
-                      viewMode: ViewMode.VIEW,
-                      panels: getDashboardPanels(indexPattern.id),
-                      isFullScreenMode: false,
-                      filters: fetchFilters ?? [],
-                      useMargins: true,
-                      id: 'office-drilldown-ip-config-panel-tab',
-                      timeRange: {
-                        from: searchBarProps.dateRangeFrom,
-                        to: searchBarProps.dateRangeTo,
-                      },
-                      title: 'Office drilldown ip config dashboard',
-                      description:
-                        'Dashboard of the Office drilldown ip config',
-                      query: searchBarProps.query,
-                      refreshConfig: {
-                        pause: false,
-                        value: 15,
-                      },
-                      hidePanelTitles: false,
-                    }}
-                    onInputUpdated={() => {}}
-                  />
+            component: () => (
+              <div style={{ width: '100%' }}>
+                <DashboardRenderer
+                  dashboardId='office-drilldown-ip-config-panel-tab'
+                  hasPinnedAgent={false}
+                  config={{ dataSource: { fetchFilters, searchBarProps } }} />
                 </div>
-              );
-            },
+            ),
           },
         ],
       },
