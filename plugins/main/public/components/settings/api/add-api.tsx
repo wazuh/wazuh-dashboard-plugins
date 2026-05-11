eea0e60da424807ebc131fc1be11209b97c2f73d
Ian Yenien Serrano <63758389+yenienserrano@users.noreply.github.com>
2026-05-11 17:25:24 -0300

    Fix prettier

--- plugins/main/public/components/settings/api/add-api.tsx
+++ plugins/main/public/components/settings/api/add-api.tsx
@@ -12,9 +12,9 @@ import { getWazuhCorePlugin } from '../../../kibana-services';
 
 export const AddApi = withErrorBoundary(() => {
   const data = React.useMemo(() => {
-
     const settings = Object.entries(
-      getWazuhCorePlugin().configuration.getSettingDefinition('hosts').options.objectOf
+      getWazuhCorePlugin().configuration.getSettingDefinition('hosts').options
+        .objectOf,
     ).map(([key, { description }]) => [
       key,
       description.charAt(0).toLowerCase() + description.slice(1), // lower case the first letter
@@ -25,9 +25,9 @@ export const AddApi = withErrorBoundary(() => {
       example: `wazuh_core.hosts:
     - <id>:
      ${settings
-          .filter(([key]) => key !== 'id')
-          .map(([key]) => `${key}: <${key}>`)
-          .join('\n     ')}`,
+       .filter(([key]) => key !== 'id')
+       .map(([key]) => `${key}: <${key}>`)
+       .join('\n     ')}`,
     };
   }, []);
 
@@ -36,9 +36,8 @@ export const AddApi = withErrorBoundary(() => {
       <EuiFlexGroup>
         <EuiFlexItem>
           <EuiText>
-            Modify{' '}
-            <EuiCode>opensearch_dashboards.yml</EuiCode>{' '}
-            to set the connection information.
+            Modify <EuiCode>opensearch_dashboards.yml</EuiCode> to set the
+            connection information.
           </EuiText>
         </EuiFlexItem>
       </EuiFlexGroup>
