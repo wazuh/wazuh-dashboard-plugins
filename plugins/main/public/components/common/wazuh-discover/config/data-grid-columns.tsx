542f1a30e4d03e5a33f4d2168e2c1791f943b78c
Fernando Castilla <164048568+FernandoCastilla@users.noreply.github.com>
2026-05-13 18:08:27 +0200

    Improved threat hunting dashboard and findings tab (#8479)
    * Main fixes
    
    * Update CHANGELOG
    
    * Update CHANGELOG

--- plugins/main/public/components/common/wazuh-discover/config/data-grid-columns.tsx
+++ plugins/main/public/components/common/wazuh-discover/config/data-grid-columns.tsx
@@ -4,8 +4,8 @@ import { commonColumns } from '../../../overview/common/data-grid-columns';
 export const threatHuntingColumns: tDataGridColumn[] = [
   commonColumns.timestamp,
   commonColumns['wazuh.agent.name'],
+  { id: 'wazuh.integration.category', initialWidth: 130 },
   { id: 'wazuh.integration.name' },
-  { id: 'wazuh.integration.category' },
   { id: 'wazuh.rule.title' },
-  { id: 'wazuh.rule.level' },
+  { id: 'wazuh.rule.level', initialWidth: 150 },
 ];
