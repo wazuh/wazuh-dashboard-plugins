076efe58a112e1489c1df3445c485dd0c1f37522
Adam Navarro Megías <adam.navarro@wazuh.com>
2026-05-15 15:09:47 +0200

    small fix
    Signed-off-by: Adam Navarro Megías <adam.navarro@wazuh.com>

--- plugins/main/public/components/overview/regulatory-compliance/tsc/index.tsx
+++ plugins/main/public/components/overview/regulatory-compliance/tsc/index.tsx
@@ -16,7 +16,6 @@ export const RegulatoryComplianceTSC = () => {
     dataSource: TSCDataSource,
     tableColumns: createRegulatoryComplianceColumns(
       'wazuh.rule.compliance.tsc',
-      260,
     ),
   });
 
