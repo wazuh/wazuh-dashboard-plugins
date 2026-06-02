2fa2ef481873cede206c279f333557599c6ccfbd
Rodrigo Lopez <rodrigo.lopez@wazuh.com>
2026-06-02 19:38:28 +0000

    feat: update mitre attack columns

--- plugins/main/public/components/overview/mitre/events/mitre-attack-columns.tsx
+++ plugins/main/public/components/overview/mitre/events/mitre-attack-columns.tsx
@@ -3,14 +3,9 @@ import { commonColumns } from '../../common/data-grid-columns';
 
 export const mitreAttackColumns: tDataGridColumn[] = [
   commonColumns.timestamp,
-  // commonColumns['wazuh.agent.name'],
-  {
-    id: 'wazuh.rule.mitre.tactic.name',
-  },
-  {
-    id: 'wazuh.rule.title',
-  },
-  {
-    id: 'wazuh.rule.level',
-  },
+  commonColumns['wazuh.agent.name'],
+  { id: 'wazuh.rule.title' },
+  { id: 'wazuh.rule.level', initialWidth: 150 },
+  { id: 'wazuh.rule.mitre.tactic.name' },
+  { id: 'wazuh.rule.mitre.technique.name' },
 ];
