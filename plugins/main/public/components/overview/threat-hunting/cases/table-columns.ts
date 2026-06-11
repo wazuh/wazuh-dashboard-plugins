0e9d3e3f6b3084de928da3b541ff70dddf92b95f
Fernando Castilla Ropero <fernando.castilla@wazuh.com>
2026-06-11 13:09:53 +0200

    fix width

--- plugins/main/public/components/overview/threat-hunting/cases/table-columns.ts
+++ plugins/main/public/components/overview/threat-hunting/cases/table-columns.ts
@@ -3,8 +3,8 @@ import { commonColumns } from '../../common/data-grid-columns';
 export default [
   commonColumns.timestamp,
   commonColumns['wazuh.agent.name'],
-  { id: 'wazuh.case.user.name', initialWidth: 170 },
-  { id: 'wazuh.case.status', initialWidth: 170 },
+  { id: 'wazuh.case.user.name', initialWidth: 190 },
+  { id: 'wazuh.case.status', initialWidth: 180 },
   { id: 'wazuh.case.tags', initialWidth: 200 },
   { id: 'wazuh.case.comment' },
 ];
