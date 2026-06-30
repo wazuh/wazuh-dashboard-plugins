dde2aa1e485a5b9d761c47119791f870248eed3f
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2026-06-30 13:32:32 +0200

    change: adapt GET /cluster/{node_id}/status API manager response
    - Adapt checkDaemons function in frontend side
    - Adapt checkDaemons function in backend side
    - Redesing daemons view in Status app:
      - Add ready and running status information to each daemon
      - Add overiew ready information
      - Wrap daemons information into a panel

--- plugins/main/public/controllers/management/components/management/status/status-daemons.js
+++ plugins/main/public/controllers/management/components/management/status/status-daemons.js
@@ -11,58 +11,118 @@
  * Find more information about this on the LICENSE file.
  */
 import React, { Component } from 'react';
-import { EuiFlexGroup, EuiIcon, EuiText, EuiPage } from '@elastic/eui';
+import {
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiIconTip,
+  EuiPanel,
+  EuiText,
+  EuiTitle,
+} from '@elastic/eui';
 
 import { connect } from 'react-redux';
 
+const statusColors = {
+  ok: '#00a69b',
+  warn: '#ffd45c',
+  error: '#ff645c',
+};
+
 export class WzStatusDaemons extends Component {
-  _isMounted = false;
   constructor(props) {
     super(props);
     this.state = {};
-  }
-
-  componentDidMount() {
-    this._isMounted = true;
-  }
-
-  componentDidUpdate() {}
-
-  componentWillUnmount() {
-    this._isMounted = false;
   }
 
   render() {
     const { listDaemons } = this.props.state;
 
     const textStyle = {
-      marginLeft: '4px'
+      marginLeft: '4px',
     };
 
+    const generalStatus =
+      listDaemons.find(({ key }) => key === 'ready')?.value === true;
+
     return (
-      <EuiPage style={{ background: 'transparent' }}>
-        <EuiFlexGroup wrap responsive={true}>
-          <div className="daemons-card">
-            {listDaemons.map(daemon => (
-              <div className="daemon-label" key={daemon.key}>
+      <EuiPanel>
+        <EuiFlexGroup alignItems='center'>
+          <EuiFlexItem>
+            <EuiFlexGroup>
+              <EuiFlexItem grow={false}>
+                <EuiTitle size='m'>
+                  <h2>Daemons</h2>
+                </EuiTitle>
+              </EuiFlexItem>
+              <EuiFlexItem
+                grow={false}
+                className='wz-status-daemons-overview-ready'
+              >
                 <EuiText>
-                  {(daemon.value === 'running' && (
-                    <EuiIcon type="dot" color="#00a69b" size="m" />
-                  )) || <EuiIcon type="dot" color="#ff645c" size="m" />}
-                  <span style={textStyle}>{daemon.key}</span>
+                  <EuiIconTip
+                    type='dot'
+                    color={generalStatus ? statusColors.ok : statusColors.warn}
+                    size='l'
+                    aria-label='Daemon status general'
+                    content={`Ready: ${generalStatus ? 'yes' : 'no'}`}
+                  />
+                  <span style={textStyle}>
+                    {generalStatus ? 'ready' : 'not ready'}
+                  </span>
                 </EuiText>
-              </div>
-            ))}
-          </div>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiFlexItem>
         </EuiFlexGroup>
-      </EuiPage>
+        <EuiFlexGroup wrap responsive={true}>
+          <EuiFlexItem>
+            <EuiFlexGroup wrap responsive={true}>
+              <div className='daemons-card'>
+                {listDaemons
+                  .filter(({ key }) => key !== 'ready')
+                  .map(daemon => {
+                    const ready = daemon.value?.['ready'] === true;
+                    const running = daemon.value?.['running'] === true;
+
+                    let statusColor = '';
+
+                    if (running && ready) {
+                      statusColor = statusColors.ok;
+                    } else if (!running) {
+                      statusColor = statusColors.error;
+                    } else {
+                      statusColor = statusColors.warn;
+                    }
+
+                    return (
+                      <div className='daemon-label' key={daemon.key}>
+                        <EuiText>
+                          <EuiIconTip
+                            type='dot'
+                            color={statusColor}
+                            size='m'
+                            aria-label='Daemon status info'
+                            content={`Ready:  ${
+                              ready ? 'yes' : 'no'
+                            }. Running: ${running ? 'yes' : 'no'}`}
+                          />
+                          <span style={textStyle}>{daemon.key}</span>
+                        </EuiText>
+                      </div>
+                    );
+                  })}
+              </div>
+            </EuiFlexGroup>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </EuiPanel>
     );
   }
 }
 
 const mapStateToProps = state => {
   return {
-    state: state.statusReducers
+    state: state.statusReducers,
   };
 };
 
