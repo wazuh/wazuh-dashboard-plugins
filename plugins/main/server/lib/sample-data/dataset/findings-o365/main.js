019de25bd2c20f8928c64ba0802c82536a0eedcf
Fernando Castilla Ropero <fernando.castilla@wazuh.com>
2026-07-09 14:08:22 +0200

    Add default index

--- plugins/main/server/lib/sample-data/dataset/findings-o365/main.js
+++ plugins/main/server/lib/sample-data/dataset/findings-o365/main.js
@@ -1,5 +1,7 @@
 const { generateFinding } = require('../../finding-generator');
 
+const DATASET_INDEX = 'wazuh-findings-v5';
+
 function generateDocument(params = {}) {
   return generateFinding(params, __dirname);
 }
--- plugins/main/server/lib/sample-data/index.js
+++ plugins/main/server/lib/sample-data/index.js
@@ -1,9 +1,10 @@
-const fs = require('fs');
-const path = require('path');
-
 function generateSampleDataWithDataset(dataset, options) {
   const datasetDefinition = require(`./dataset/${dataset}/main`);
   return datasetDefinition.generateDocument(options);
 }
-
+function getDatasetIndex(dataset) {
+  const datasetDefinition = require(`./dataset/${dataset}/main`);
+  return datasetDefinition.DATASET_INDEX || null;
+}
 module.exports.generateSampleDataWithDataset = generateSampleDataWithDataset;
+module.exports.getDatasetIndex = getDatasetIndex;
