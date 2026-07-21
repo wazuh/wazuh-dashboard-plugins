5190f9a02afecd32e7ed8533dc739b47117421c3
Rodrigo Lopez <rodrigo.lopez@wazuh.com>
2026-07-21 20:22:57 +0000

    chore: style issue

--- plugins/main/public/components/common/welcome/home-overview/interfaces/types.ts
+++ plugins/main/public/components/common/welcome/home-overview/interfaces/types.ts
@@ -1,5 +1,9 @@
 export type SeverityBand =
-  'critical' | 'high' | 'medium' | 'low' | 'informational';
+  | 'critical'
+  | 'high'
+  | 'medium'
+  | 'low'
+  | 'informational';
 
 export type SeverityCounts = Partial<Record<SeverityBand, number>>;
 
