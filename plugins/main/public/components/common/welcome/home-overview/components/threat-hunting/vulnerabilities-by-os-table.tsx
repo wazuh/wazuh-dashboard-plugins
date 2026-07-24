f305fe4480f909a43d766b4e607786a915f6059d
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2026-07-24 11:35:13 +0200

    fix(home): rename table header

--- plugins/main/public/components/common/welcome/home-overview/components/threat-hunting/vulnerabilities-by-os-table.tsx
+++ plugins/main/public/components/common/welcome/home-overview/components/threat-hunting/vulnerabilities-by-os-table.tsx
@@ -1,7 +1,7 @@
 import { createTopNTable } from '../common';
 
 export const VulnerabilitiesByOsTable = createTopNTable({
-  keyColumnName: 'Vulnerabilities by OS',
+  keyColumnName: 'Top 5 OS platforms',
   noItemsMessage: 'No vulnerabilities found',
   'data-test-subj': 'vulnerabilities-by-os-table',
 });
