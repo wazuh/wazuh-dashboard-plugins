7bf84a7ec026118a5ef7f1dacffe14e8aa846b83
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2026-09-08 18:00:11 +0200

    fix: correct copyright year in refresh-agent-config-button.js
    Signed-off-by: Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>

--- plugins/main/public/controllers/management/components/management/configuration/util-components/refresh-agent-config-button.js
+++ plugins/main/public/controllers/management/components/management/configuration/util-components/refresh-agent-config-button.js
@@ -1,6 +1,6 @@
 /*
  * Wazuh app - React component for render button to refresh an agent's reported configuration.
- * Copyright (C) 2015-2022 Wazuh, Inc.
+ * Copyright (C) 2015-2026 Wazuh, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
