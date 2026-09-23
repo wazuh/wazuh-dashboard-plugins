8c458e15c9423e1ae7d86eaab37e550009d4c15d
Rodrigo Lopez <rodrigo.lopez@wazuh.com>
2026-09-23 21:09:40 +0000

    style: format the files the i18n migration touched
    The CI Prettier check covers every changed file, including four that
    were not Prettier-clean on 5.0.0.
    
    Signed-off-by: Rodrigo Lopez <rodrigo.lopez@wazuh.com>

--- plugins/main/public/components/eui-suggest/suggest_input.js
+++ plugins/main/public/components/eui-suggest/suggest_input.js
@@ -7,7 +7,7 @@ import {
   EuiFieldText,
   EuiToolTip,
   EuiIcon,
-  EuiPopover
+  EuiPopover,
 } from '@elastic/eui';
 import { EuiInputPopover } from '@elastic/eui';
 import { i18n } from '@osd/i18n';
@@ -18,31 +18,31 @@ const statusMap = {
     color: 'accent',
     tooltip: i18n.translate('wazuh.core.suggestInput.unsavedTooltip', {
       defaultMessage: 'Changes have not been saved.',
-    })
+    }),
   },
   saved: {
     icon: 'checkInCircleFilled',
     color: 'secondary',
     tooltip: i18n.translate('wazuh.core.suggestInput.savedTooltip', {
       defaultMessage: 'Saved.',
-    })
+    }),
   },
   unchanged: {
     icon: '',
-    color: 'secondary'
-  }
+    color: 'secondary',
+  },
 };
 
 export class EuiSuggestInput extends Component {
   state = {
     value: '',
-    isPopoverOpen: false
+    isPopoverOpen: false,
   };
 
   onFieldChange = e => {
     this.setState({
       value: e.target.value,
-      isPopoverOpen: e.target.value !== '' ? true : false
+      isPopoverOpen: e.target.value !== '' ? true : false,
     });
     this.props.sendValue(e.target.value);
   };
@@ -76,11 +76,11 @@ export class EuiSuggestInput extends Component {
 
     const statusElement = (status === 'saved' || status === 'unsaved') && (
       <EuiToolTip
-        position="left"
+        position='left'
         content={tooltipContent || statusMap[status].tooltip}
       >
         <EuiIcon
-          className="euiSuggestInput__statusIcon"
+          className='euiSuggestInput__statusIcon'
           color={color}
           type={icon}
         />
@@ -108,10 +108,10 @@ export class EuiSuggestInput extends Component {
     return (
       <div className={classes}>
         <EuiInputPopover
-          id="popover"
+          id='popover'
           input={customInput}
           isOpen={isPopoverOpen}
-          panelPaddingSize="none"
+          panelPaddingSize='none'
           fullWidth
           closePopover={onClosePopover}
           disableFocusTrap={disableFocusTrap}
@@ -140,9 +140,9 @@ EuiSuggestInput.propTypes = {
   suggestions: PropTypes.array,
   isOpen: PropTypes.bool,
   onClosePopover: PropTypes.func,
-  onPopoverFocus: PropTypes.func
+  onPopoverFocus: PropTypes.func,
 };
 
 EuiSuggestInput.defaultProps = {
-  status: 'unchanged'
+  status: 'unchanged',
 };
--- plugins/main/public/components/wz-date-picker/wz-date-picker.tsx
+++ plugins/main/public/components/wz-date-picker/wz-date-picker.tsx
@@ -11,104 +11,110 @@
  */
 
 import React, { Fragment, Component } from 'react';
-import {
-  EuiSuperDatePicker,
-  OnTimeChangeProps,
-} from '@elastic/eui';
+import { EuiSuperDatePicker, OnTimeChangeProps } from '@elastic/eui';
 //@ts-ignore
 import { CondensedPicker } from './components';
 import { getDataPlugin } from '../../kibana-services';
 import { i18n } from '@osd/i18n';
 
-interface IDiscoverTime { from: string, to: string };
+interface IDiscoverTime {
+  from: string;
+  to: string;
+}
 
 export class WzDatePicker extends Component {
   commonDurationRanges = [
     {
-      "start": "now/d",
-      "end": "now/d",
-      "label": i18n.translate('wazuh.core.datePicker.commonlyUsed.today', {
+      start: 'now/d',
+      end: 'now/d',
+      label: i18n.translate('wazuh.core.datePicker.commonlyUsed.today', {
         defaultMessage: 'Today',
       }),
     },
     {
-      "start": "now/w",
-      "end": "now/w",
-      "label": i18n.translate('wazuh.core.datePicker.commonlyUsed.thisWeek', {
+      start: 'now/w',
+      end: 'now/w',
+      label: i18n.translate('wazuh.core.datePicker.commonlyUsed.thisWeek', {
         defaultMessage: 'This week',
       }),
     },
     {
-      "start": "now-15m",
-      "end": "now",
-      "label": i18n.translate('wazuh.core.datePicker.commonlyUsed.last15Minutes', {
-        defaultMessage: 'Last 15 minutes',
-      }),
+      start: 'now-15m',
+      end: 'now',
+      label: i18n.translate(
+        'wazuh.core.datePicker.commonlyUsed.last15Minutes',
+        {
+          defaultMessage: 'Last 15 minutes',
+        },
+      ),
     },
     {
-      "start": "now-30m",
-      "end": "now",
-      "label": i18n.translate('wazuh.core.datePicker.commonlyUsed.last30Minutes', {
-        defaultMessage: 'Last 30 minutes',
-      }),
+      start: 'now-30m',
+      end: 'now',
+      label: i18n.translate(
+        'wazuh.core.datePicker.commonlyUsed.last30Minutes',
+        {
+          defaultMessage: 'Last 30 minutes',
+        },
+      ),
     },
     {
-      "start": "now-1h",
-      "end": "now",
-      "label": i18n.translate('wazuh.core.datePicker.commonlyUsed.last1Hour', {
+      start: 'now-1h',
+      end: 'now',
+      label: i18n.translate('wazuh.core.datePicker.commonlyUsed.last1Hour', {
         defaultMessage: 'Last 1 hour',
       }),
     },
     {
-      "start": "now-24h",
-      "end": "now",
-      "label": i18n.translate('wazuh.core.datePicker.commonlyUsed.last24Hours', {
+      start: 'now-24h',
+      end: 'now',
+      label: i18n.translate('wazuh.core.datePicker.commonlyUsed.last24Hours', {
         defaultMessage: 'Last 24 hours',
       }),
     },
     {
-      "start": "now-7d",
-      "end": "now",
-      "label": i18n.translate('wazuh.core.datePicker.commonlyUsed.last7Days', {
+      start: 'now-7d',
+      end: 'now',
+      label: i18n.translate('wazuh.core.datePicker.commonlyUsed.last7Days', {
         defaultMessage: 'Last 7 days',
       }),
     },
     {
-      "start": "now-30d",
-      "end": "now",
-      "label": i18n.translate('wazuh.core.datePicker.commonlyUsed.last30Days', {
+      start: 'now-30d',
+      end: 'now',
+      label: i18n.translate('wazuh.core.datePicker.commonlyUsed.last30Days', {
         defaultMessage: 'Last 30 days',
       }),
     },
     {
-      "start": "now-90d",
-      "end": "now",
-      "label": i18n.translate('wazuh.core.datePicker.commonlyUsed.last90Days', {
+      start: 'now-90d',
+      end: 'now',
+      label: i18n.translate('wazuh.core.datePicker.commonlyUsed.last90Days', {
         defaultMessage: 'Last 90 days',
       }),
     },
     {
-      "start": "now-1y",
-      "end": "now",
-      "label": i18n.translate('wazuh.core.datePicker.commonlyUsed.last1Year', {
+      start: 'now-1y',
+      end: 'now',
+      label: i18n.translate('wazuh.core.datePicker.commonlyUsed.last1Year', {
         defaultMessage: 'Last 1 year',
       }),
     },
   ];
 
   timefilter: {
-    getTime(): IDiscoverTime
-    setTime(time: IDiscoverTime): void
-    _history: { history: { items: { from: string, to: string }[] } }
+    getTime(): IDiscoverTime;
+    setTime(time: IDiscoverTime): void;
+    _history: { history: { items: { from: string; to: string }[] } };
   };
 
   state: {
-    datePicker: OnTimeChangeProps,
+    datePicker: OnTimeChangeProps;
   };
 
   props!: {
-    onTimeChange(props: OnTimeChangeProps): void
-    condensed: boolean
+    onTimeChange(props: OnTimeChangeProps): void;
+    condensed: boolean;
   };
 
   constructor(props) {
@@ -122,32 +128,35 @@ export class WzDatePicker extends Component {
         isQuickSelection: true,
         isInvalid: false,
       },
-    }
+    };
   }
 
-  componentDidMount() {
-  }
+  componentDidMount() {}
 
   onTimeChange = (datePicker: OnTimeChangeProps) => {
     const { start: from, end: to } = datePicker;
     this.setState({ datePicker });
     this.timefilter.setTime({ from, to });
     this.props.onTimeChange(datePicker);
-  }
+  };
 
   render() {
     const { datePicker } = this.state;
     const recentlyUsedRanges = this.timefilter._history.history.items.map(
-      item => ({ start: item.from, end: item.to })
+      item => ({ start: item.from, end: item.to }),
     );
-    return !this.props.condensed
-      ? <EuiSuperDatePicker
+    return !this.props.condensed ? (
+      <EuiSuperDatePicker
         commonlyUsedRanges={this.commonDurationRanges}
         recentlyUsedRanges={recentlyUsedRanges}
         onTimeChange={this.onTimeChange}
-        {...datePicker} />
-      : <CondensedPicker
+        {...datePicker}
+      />
+    ) : (
+      <CondensedPicker
         onTimeChange={this.onTimeChange}
-        ranges={this.commonDurationRanges} />
+        ranges={this.commonDurationRanges}
+      />
+    );
   }
 }
